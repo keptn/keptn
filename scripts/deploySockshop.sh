6e8ff9b7c70c2189888bad65d7a33f71c7a82e4f
Florian Bacher <bacherfl@gmail.com>
2019-01-23 14:02:06 +0100

    fixed paths

--- scripts/deploySockshop.sh
+++ scripts/deploySockshop.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 cd ../repositories/sockshop-infrastructure
-kubectl apply -f carts-db.yml
-kubectl apply -f catalogue-db.yml
-kubectl apply -f orders-db.yml
-kubectl apply -f rabbitmq.yaml
-kubectl apply -f user-db.yaml
+kubectl apply -f manifests/carts-db.yaml
+kubectl apply -f manifests/catalogue-db.yaml
+kubectl apply -f manifests/orders-db.yaml
+kubectl apply -f manifests/rabbitmq.yaml
+kubectl apply -f manifests/user-db.yaml
+
+cd ..
 
 # Apply services
 declare -a repositories=("carts" "catalogue" "front-end" "orders" "payment" "queue-master" "shipping" "user")
 
 for repo in "${repositories[@]}"
 do
-    cd ../repositories/$repo/manifest
+    cd $repo/manifest
     # Deploy service to dev
     kubectl apply -f ./$repo.yml
 
@@ -25,4 +27,7 @@ do
     cat $repo.yml | sed 's#namespace: .*#namespace: production#' >> production_tmp.yml
     kubectl apply -f ./production_tmp.yml
     rm production_tmp.yml
+
+    cd ../..
 done
+cd ../scripts
