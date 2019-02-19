bad986ae75dc84ed5f3f566dc1948fb1a24e3a3a
Florian Bacher <bacherfl@gmail.com>
2019-02-19 10:53:19 +0100

    added cleanup of knative components

--- install/scripts/cleanupCluster.sh
+++ install/scripts/cleanupCluster.sh
@@ -22,6 +22,17 @@ kubectl delete namespace dynatrace
 kubectl delete services,deployments,pods --all -n tower
 kubectl delete namespace tower
 
+# Clean up keptn namespace
+kubectl delete services,deployments,pods --all -n keptn
+kubectl delete namespace keptn
+
+# Clean up knative components
+kubectl delete --filename https://github.com/knative/serving/releases/download/v0.3.0/serving.yaml
+kubectl delete --filename https://github.com/knative/build/releases/download/v0.3.0/release.yaml
+kubectl delete --filename https://github.com/knative/eventing/releases/download/v0.3.0/release.yaml
+kubectl delete --filename https://github.com/knative/eventing-sources/releases/download/v0.3.0/release.yaml
+kubectl delete --filename https://github.com/knative/serving/releases/download/v0.3.0/monitoring.yaml
+
 # Clean up istio namespace
 kubectl delete services,deployments,pods --all -n istio-system
 kubectl delete namespace istio-system
@@ -35,4 +46,6 @@ kubectl delete -f ../repositories/k8s-deploy-production/istio/virtual_service.ym
 kubectl delete clusterrolebindings.rbac.authorization.k8s.io dynatrace-cluster-admin-binding
 kubectl delete clusterrolebindings.rbac.authorization.k8s.io jenkins-rbac
 kubectl delete -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/master/deploy/kubernetes.yaml
+
+
 
