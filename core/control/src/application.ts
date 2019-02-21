5f242d258abd4a6dd0653bb85fe47f3b0ebd753d
Florian Bacher <bacherfl@gmail.com>
2019-02-21 11:13:48 +0100

    fixed linting errors

--- core/control/src/application.ts
+++ core/control/src/application.ts
@@ -1,42 +1,44 @@
-import express = require("express");
-import bodyParser = require("body-parser");
-import onboardRouter = require("./routes/onboardRouter");
-import appRouter = require("./routes/appRouter");
-import requestLogger = require("./middleware/requestLogger");
+import express = require('express');
+import bodyParser = require('body-parser');
+import configRouter = require('./routes/configRouter');
+import onboardRouter = require('./routes/onboardRouter');
+import appRouter = require('./routes/appRouter');
+import requestLogger = require('./middleware/requestLogger');
 import authenticator = require('./middleware/authenticator');
 
 export class WebApi {
-    
-    /**
-     * @param app - express application
-     * @param port - port to listen on
-     */
-    constructor(private app: express.Express, private port: number) {
-        this.configureMiddleware(app);
-        this.configureRoutes(app);
-    }
 
-    /**
-     * @param app - express application
-     */
-    private configureMiddleware(app: express.Express) {
-        app.use(bodyParser.json());
-        app.use(requestLogger);
-        app.use(authenticator);
-    }
+  /**
+   * @param app - express application
+   * @param port - port to listen on
+   */
+  constructor(private app: express.Express, private port: number) {
+    this.configureMiddleware(app);
+    this.configureRoutes(app);
+  }
 
-    /**
-     * @param app - express application
-     */
-    private configureRoutes(app: express.Express) {
-        app.use("/onboard", onboardRouter );
-        app.use("/app", appRouter );
+  /**
+   * @param app - express application
+   */
+  private configureMiddleware(app: express.Express) {
+    app.use(bodyParser.json());
+    app.use(requestLogger);
+    app.use(authenticator);
+  }
 
-        // mount more routers here
-        // e.g. app.use("/organisation", organisationRouter);
-    }
+  /**
+   * @param app - express application
+   */
+  private configureRoutes(app: express.Express) {
+    app.use('/config', configRouter);
+    app.use('/onboard', onboardRouter);
+    app.use('/app', appRouter);
 
-    public run() {
-        this.app.listen(this.port);  
-    }
+    // mount more routers here
+    // e.g. app.use("/organisation", organisationRouter);
+  }
+
+  public run() {
+    this.app.listen(this.port);
+  }
 }
