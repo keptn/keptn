0fe8a5f4ae959f6519104baf01d42ead8a439146
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-02-22 14:07:06 +0100

    Bug fix in deploy script - use -f option.

--- core/auth/deploy.sh
+++ core/auth/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 REGISTRY_URI=$1
 
-rm config/gen/authenticator.yaml
+rm -f config/gen/authenticator.yaml
 
 cat config/authenticator.yaml | \
   sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/authenticator.yaml 
