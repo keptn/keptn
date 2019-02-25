a6c362271f242ed773b6223c5518217fe86a0f8c
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-02-25 11:56:33 +0100

    Update description of root command and print logo

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -13,14 +13,10 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "keptn",
+	Short: "This is a CLI for using keptn",
+	Long: `This is a CLI for using keptn. The CLI allows to authenticate against keptn, to configure your Github organization,
+	to create projects, and to onboard services.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
