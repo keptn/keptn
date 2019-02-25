8463181c6ea2495f980e44d89181e7d4ae2d8f4e
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-02-25 11:56:33 +0100

    Update description of root command and print logo

--- cli/main.go
+++ cli/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/keptn/keptn/cli/cmd"
@@ -11,6 +13,13 @@ func init() {
 	utils.Init(os.Stdout, os.Stdout, os.Stderr)
 }
 
+const logoFileName = "logo.txt"
+
 func main() {
+	dat, err := ioutil.ReadFile(logoFileName)
+	if err == nil {
+		fmt.Println(string(dat))
+	}
+
 	cmd.Execute()
 }
