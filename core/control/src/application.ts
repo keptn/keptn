e6c83e0a3d25f4ddb140a36a1eb2ac900f603136
Florian Bacher <bacherfl@gmail.com>
2019-02-25 13:53:04 +0100

    added swagger doc library

--- core/control/src/application.ts
+++ core/control/src/application.ts
@@ -1,11 +1,19 @@
 import express = require('express');
 import bodyParser = require('body-parser');
-import configRouter = require('./routes/ConfigRouter');
-import projectRouter = require('./routes/ProjectRouter');
+import ConfigRouter from './routes/ConfigRouter';
+import ProjectRouter = require('./routes/ProjectRouter');
 import RequestLogger = require('./middleware/RequestLogger');
 import Authenticator = require('./middleware/Authenticator');
+import * as path from 'path';
+
+import * as swagger from 'swagger-express-ts';
+import { SwaggerDefinitionConstant } from 'swagger-express-ts';
+import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
+const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath();
 
 export class WebApi {
+
+  private swaggerSpec: any;
 
   /**
    * @param app - express application
@@ -20,6 +28,12 @@ export class WebApi {
    * @param app - express application
    */
   private configureMiddleware(app: express.Express) {
+    app.use('/api-docs/swagger', express.static(path.join(__dirname, '/src/swagger')));
+    app.use('/api-docs/swagger/assets',
+            express.static(
+              swaggerUiAssetPath,
+            ),
+      );
     app.use(bodyParser.json());
     app.use(RequestLogger);
     if (process.env.NODE_ENV === 'production') {
@@ -31,8 +45,22 @@ export class WebApi {
    * @param app - express application
    */
   private configureRoutes(app: express.Express) {
-    app.use('/config', configRouter);
-    app.use('/project', projectRouter);
+    app.use(swagger.express(
+      {
+        definition: {
+          info: {
+            title: 'Keptn Control API',
+            version: '0.2',
+          },
+          externalDocs: {
+            url: '',
+          },
+          // Models can be defined here
+        },
+      },
+    ));
+    app.use('/config', ConfigRouter);
+    app.use('/project', ProjectRouter);
 
     // mount more routers here
     // e.g. app.use("/organisation", organisationRouter);
