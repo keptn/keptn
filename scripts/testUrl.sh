db7fd3b6b77d17c4166cd13deccd67587b953c4c
alasdair.patton <alasdair.patton@dynatrace.com>
2019-02-27 13:22:31 -0600

    correcting k8s-jenkins-deployment.yml by copying file from v0.1.1

---
+++ scripts/testUrl.sh
@@ -0,0 +1,21 @@
+
+echo "--------------------------"
+echo "Setup Infrastructure "
+echo "--------------------------"
+
+# Script if you don't want to apply all yaml files manually
+
+export JENKINS_USER=$(cat creds.json | jq -r '.jenkinsUser')
+export JENKINS_PASSWORD=$(cat creds.json | jq -r '.jenkinsPassword')
+export GITHUB_PERSONAL_ACCESS_TOKEN=$(cat creds.json | jq -r '.githubPersonalAccessToken')
+export GITHUB_USER_NAME=$(cat creds.json | jq -r '.githubUserName')
+export GITHUB_USER_EMAIL=$(cat creds.json | jq -r '.githubUserEmail')
+export DT_TENANT_ID=$(cat creds.json | jq -r '.dynatraceTenant')
+export DT_API_TOKEN=$(cat creds.json | jq -r '.dynatraceApiToken')
+export DT_PAAS_TOKEN=$(cat creds.json | jq -r '.dynatracePaaSToken')
+export GITHUB_ORGANIZATION=$(cat creds.json | jq -r '.githubOrg')
+export DT_TENANT_URL=$(echo $DT_TENANT_ID | sed 's,/.*,,')
+
+echo "--------------------------"
+echo $DT_TENANT_URL
+echo "--------------------------"
