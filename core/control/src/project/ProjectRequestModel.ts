001f264932f0dbec7c2c5cafdaf25384c8561c30
Florian Bacher <bacherfl@gmail.com>
2019-02-27 11:12:48 +0100

    updated swagger doc

--- core/control/src/project/ProjectRequestModel.ts
+++ core/control/src/project/ProjectRequestModel.ts
@@ -8,23 +8,21 @@ export class ProjectRequestModel {
   @ApiModelProperty({
     description: 'Object containing the project information',
     example: [{
-      data: {
-        project: 'sockshop',
-        stages: [
-          {
-            name: 'dev',
-            deployment_strategy: 'direct',
-          },
-          {
-            name: 'staging',
-            deployment_strategy: 'blue_green_service',
-          },
-          {
-            name: 'production',
-            deployment_strategy: 'blue_green_service',
-          },
-        ],
-      },
+      project: 'sockshop',
+      stages: [
+        {
+          name: 'dev',
+          deployment_strategy: 'direct',
+        },
+        {
+          name: 'staging',
+          deployment_strategy: 'blue_green_service',
+        },
+        {
+          name: 'production',
+          deployment_strategy: 'blue_green_service',
+        },
+      ],
     }],
     type: SwaggerDefinitionConstant.Model.Type.OBJECT,
     required: true,
--- core/control/src/service/ServiceRequestModel.ts
+++ core/control/src/service/ServiceRequestModel.ts
@@ -8,10 +8,8 @@ export class ServiceRequestModel {
   @ApiModelProperty({
     description: 'Object containing service information',
     example: [{
-      data : {
-        project: 'sockshop',
-        file : 'deployment and service definition in YAML format',
-      },
+      project: 'sockshop',
+      file: 'deployment and service definition in YAML format',
     }],
     type: SwaggerDefinitionConstant.Model.Type.OBJECT,
     required: true,
