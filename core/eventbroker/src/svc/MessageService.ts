282eed2f3836cb800080893aeea14289feda4715
Florian Bacher <bacherfl@gmail.com>
2019-03-04 11:21:10 +0100

    use ChannelReconciler to get channel URI

--- core/eventbroker/src/svc/MessageService.ts
+++ core/eventbroker/src/svc/MessageService.ts
@@ -1,54 +1,31 @@
 import { injectable } from 'inversify';
 import axios from 'axios';
 import { KeptnRequestModel } from '../keptn/KeptnRequestModel';
+import { ChannelReconciler } from '../lib/channel/ChannelReconciler';
 
 @injectable()
 export class MessageService {
 
-  static KEPTN_EVENTS = [
-    {
-      eventType: 'sh.keptn.events.new-artefact',
-      channelUri: process.env.NEW_ARTEFACT_CHANNEL,
-    },
-    {
-      eventType: 'sh.keptn.events.start-deployment',
-      channelUri: process.env.START_DEPLOYMENT_CHANNEL,
-    },
-    {
-      eventType: 'sh.keptn.events.deployment-finished',
-      channelUri: process.env.DEPLOYMENT_FINISHED_CHANNEL,
-    },
-    {
-      eventType: 'sh.keptn.events.start-tests',
-      channelUri: process.env.START_TESTS_CHANNEL,
-    },
-    {
-      eventType: 'sh.keptn.events.tests-finished',
-      channelUri: process.env.TESTS_FINISHED_CHANNEL,
-    },
-    {
-      eventType: 'sh.keptn.events.start-evaluation',
-      channelUri: process.env.START_EVALUATION_CHANNEL,
-    },
-    {
-      eventType: 'sh.keptn.events.evaluation-finished',
-      channelUri: process.env.EVALUATION_FINISHED_CHANNEL,
-    },
-  ];
+  private channelReconciler: ChannelReconciler;
 
-  constructor() {}
+  constructor() {
+    this.channelReconciler = new ChannelReconciler();
+  }
 
   public async sendMessage(message: KeptnRequestModel): Promise<boolean> {
     let channelUri;
     const eventType = message.type;
     if (eventType !== undefined) {
-      const eventSpec = MessageService.KEPTN_EVENTS.find(item => item.eventType === eventType);
-      if (eventSpec !== undefined) {
-        channelUri = eventSpec.channelUri;
-      } else {
-        console.log(`No channel found for eventType ${eventType}`);
+      const split = message.type.split('.');
+      if (split.length < 4) {
         return false;
       }
+      const channelName = split[3];
+      channelUri = this.channelReconciler.resolveChannel(channelName);
+    }
+    if (channelUri === '') {
+      console.log(`Could not find channel URI for event of type ${message.type}`);
+      return false;
     }
     console.log(`Sending message to ${channelUri}`);
 
