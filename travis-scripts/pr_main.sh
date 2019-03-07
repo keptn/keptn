492eed89f961858fba09ab35c3a40713ecb938ec
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-03-07 09:17:56 +0100

    Correct bash syntax error

--- travis-scripts/pr_main.sh
+++ travis-scripts/pr_main.sh
@@ -5,8 +5,8 @@ source ./travis-scripts/setup_functions.sh
 # Causes the shell to exit immediately if a simple command exits with a nonzero exit value
 set -e
 
-setup_gcloud()
-install_yq()
-setup_knative()
-execute_core_component_tests()
-execute_cli_tests()
+setup_gcloud
+install_yq
+setup_knative
+execute_core_component_tests
+execute_cli_tests
