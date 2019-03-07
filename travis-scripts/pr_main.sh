f79cbf102b055f6b9004ef9dcfd4d51513fcda07
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-03-07 09:12:51 +0100

    Source PR functionality out into scripts

---
+++ travis-scripts/pr_main.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source ./travis-scripts/setup_functions.sh
+
+# Causes the shell to exit immediately if a simple command exits with a nonzero exit value
+set -e
+
+setup_gcloud()
+install_yq()
+setup_knative()
+execute_core_component_tests()
+execute_cli_tests()
