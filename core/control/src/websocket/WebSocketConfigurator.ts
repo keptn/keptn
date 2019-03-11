b4e0cb98d6a409242dfc2b7708ce674a6d95022f
Florian Bacher <bacherfl@gmail.com>
2019-03-11 18:17:02 +0100

    websocket endpoint

--- core/control/src/websocket/WebSocketConfigurator.ts
+++ core/control/src/websocket/WebSocketConfigurator.ts
@@ -1,5 +1,6 @@
 import * as Jwt from 'jsonwebtoken';
 import * as express from 'express';
+const WebSocket = require('ws');
 import websocketHandler = require('../websocket/websocketHandler');
 import { WebSocketService } from '../svc/WebSocketService';
 
@@ -21,11 +22,28 @@ export class WebSocketConfigurator {
   }
 
   configure() {
+    const server = this.server;
+    const wss = new WebSocket.Server({ server });
+    wss.on('connection', function connection(ws) {
+      ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+      });
+      ws.send('something');
+    });
+    /*
+    this.server.on('upgrade', (request, socket, head) => {
+      wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request);
+      });
+    });
+    */
+    /*
     require('express-ws')(this.app, this.server, {
       wsOptions: {
         verifyClient: WebSocketService.getInstance().verifyToken,
       },
     });
     this.app.ws('/comm', websocketHandler);
+    */
   }
 }
