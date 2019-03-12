c509cd409661d75955527dce5dabec5991440bf9
Florian Bacher <bacherfl@gmail.com>
2019-03-12 14:03:47 +0100

    istio 1.0.6

--- install/scripts/setupIstio.sh
+++ install/scripts/setupIstio.sh
@@ -3,23 +3,30 @@
 DT_TENANT_ID=$1
 DT_PAAS_TOKEN=$2
 
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio-crds.yaml && \
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio.yaml
+#kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio-crds.yaml && \
+#kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio.yaml
+
+
+# Determine the IP scope of the cluster (https://github.com/knative/docs/blob/master/serving/outbound-network-access.md)
+# Gcloud:
+# CLUSTER_IPV4_CIDR=$(gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE} | yq r - clusterIpv4Cidr)
+# SERVICES_IPV4_CIDR=$(gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE} | yq r - servicesIpv4Cidr)
+
+kubectl apply -f ../manifests/istio/istio-crds-knative.yaml
+kubectl apply -f ../manifests/istio/istio-knative.yaml
 
 echo "Wait 4 minutes for changes to apply... "
 sleep 240
 echo "Wait 4 additional minutes for changes to apply... "
 sleep 240
 
-kubectl label namespace production istio-injection=enabled
+kubectl label namespace keptn istio-injection=enabled
 
 ./createServiceEntry.sh $DT_TENANT_ID $DT_PAAS_TOKEN
 
 echo "Wait 10s for changes to apply... "
 sleep 10
 
-kubectl delete pods --all -n production
-kubectl delete pods --all -n staging
-kubectl delete pods --all -n dev
+kubectl delete pods --all -n keptn
 
-kubectl delete meshpolicies.authentication.istio.io default # fix for the MySQL connection error caused by Istio
+# kubectl delete meshpolicies.authentication.istio.io default # fix for the MySQL connection error caused by Istio
