58f9e2f4fb24d5c3e583bbf559ab164d13f9b723
Florian Bacher <bacherfl@gmail.com>
2019-03-12 12:38:23 +0100

    delete knative 0.4 resources in cleanup

--- install/scripts/cleanupCluster.sh
+++ install/scripts/cleanupCluster.sh
@@ -27,11 +27,13 @@ kubectl delete services,deployments,pods --all -n keptn
 kubectl delete namespace keptn
 
 # Clean up knative components
-kubectl delete --filename https://github.com/knative/serving/releases/download/v0.3.0/serving.yaml
-kubectl delete --filename https://github.com/knative/build/releases/download/v0.3.0/release.yaml
-kubectl delete --filename https://github.com/knative/eventing/releases/download/v0.3.0/release.yaml
-kubectl delete --filename https://github.com/knative/eventing-sources/releases/download/v0.3.0/release.yaml
-kubectl delete --filename https://github.com/knative/serving/releases/download/v0.3.0/monitoring.yaml
+kubectl delete --filename https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml
+kubectl delete --filename https://github.com/knative/build/releases/download/v0.4.0/build.yaml
+kubectl delete --filename https://github.com/knative/eventing/releases/download/v0.4.0/in-memory-channel.yaml
+kubectl delete --filename https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml
+kubectl delete --filename https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml
+kubectl delete --filename https://github.com/knative/serving/releases/download/v0.4.0/monitoring.yaml
+kubectl delete --filename https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml
 
 # Clean up istio namespace
 kubectl delete services,deployments,pods --all -n istio-system
