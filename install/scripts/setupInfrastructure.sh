6b85a90c9c3b6d2783b94396f4caddf898d73e2c
Florian Bacher <bacherfl@gmail.com>
2019-03-14 17:35:19 +0100

    extract DT installation from main script

--- install/scripts/setupInfrastructure.sh
+++ install/scripts/setupInfrastructure.sh
@@ -15,11 +15,7 @@ export JENKINS_PASSWORD=$(cat creds.json | jq -r '.jenkinsPassword')
 export GITHUB_PERSONAL_ACCESS_TOKEN=$(cat creds.json | jq -r '.githubPersonalAccessToken')
 export GITHUB_USER_NAME=$(cat creds.json | jq -r '.githubUserName')
 export GITHUB_USER_EMAIL=$(cat creds.json | jq -r '.githubUserEmail')
-export DT_TENANT_ID=$(cat creds.json | jq -r '.dynatraceTenant')
-export DT_API_TOKEN=$(cat creds.json | jq -r '.dynatraceApiToken')
-export DT_PAAS_TOKEN=$(cat creds.json | jq -r '.dynatracePaaSToken')
 export GITHUB_ORGANIZATION=$(cat creds.json | jq -r '.githubOrg')
-export DT_TENANT_URL="$DT_TENANT_ID.live.dynatrace.com"
 export CLUSTER_NAME=$(cat creds.json | jq -r '.clusterName')
 export CLUSTER_ZONE=$(cat creds.json | jq -r '.clusterZone')
 export CLUSTER_REGION=$(cat creds.json | jq -r '.clusterRegion')
@@ -50,42 +46,12 @@ sleep 100
 # Store the docker registry route in a variable
 export REGISTRY_URL=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
 
-
-# Deploy Dynatrace operator
-kubectl create namespace dynatrace
-export LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
-echo "Installing Dynatrace Operator $LATEST_RELEASE"
-kubectl create -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
-sleep 60
-kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken=$DT_API_TOKEN" --from-literal="paasToken=$DT_PAAS_TOKEN"
-
-rm -f ../manifests/gen/cr.yml
-
-curl -o ../manifests/dynatrace/cr.yml https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/cr.yaml
-cat ../manifests/dynatrace/cr.yml | sed 's/ENVIRONMENTID/'"$DT_TENANT_ID"'/' >> ../manifests/gen/cr.yml
-
-kubectl create -f ../manifests/gen/cr.yml
-
-# Apply auto tagging rules in Dynatrace
-echo "--------------------------"
-echo "Apply auto tagging rules in Dynatrace "
-echo "--------------------------"
-
-./applyAutoTaggingRules.sh $DT_TENANT_ID $DT_API_TOKEN
-
-echo "--------------------------"
-echo "End applying auto tagging rules in Dynatrace "
-echo "--------------------------"
-
-echo "Wait 150s for changes to apply..."
-sleep 150
-
 # Install Istio service mesh
 echo "--------------------------"
 echo "Setup Istio "
 echo "--------------------------"
 
-./setupIstio.sh $DT_TENANT_ID $DT_PAAS_TOKEN $CLUSTER_NAME $CLUSTER_ZONE
+./setupIstio.sh $CLUSTER_NAME $CLUSTER_ZONE
 
 echo "--------------------------"
 echo "End setup Istio "
