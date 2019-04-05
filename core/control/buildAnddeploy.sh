b97a7b3165292b3e1428bd1f1150632bdfa3454b
Florian Bacher <bacherfl@gmail.com>
2019-04-05 09:28:23 +0200

    pull control from docker hub

--- core/control/buildAnddeploy.sh
+++ core/control/buildAnddeploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
 CHANNEL_URI=$(kubectl describe channel keptn-channel -n keptn | grep "Hostname:" | sed 's~[ \t]*Hostname:[ \t]*~~')
 
 rm -f config/gen/control.yaml
 
 cat config/control.yaml | \
-  sed 's~CHANNEL_URI_PLACEHOLDER~'"$CHANNEL_URI"'~' >> config/gen/control.yaml 
+  sed 's~CHANNEL_URI_PLACEHOLDER~'"$CHANNEL_URI"'~' | \
+  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/control.yaml 
   
 kubectl delete -f config/gen/control.yaml --ignore-not-found
 kubectl apply -f config/gen/control.yaml
