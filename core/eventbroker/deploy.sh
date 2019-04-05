c604a66b08b712f3e86f7908ad7878e8c7dd91a5
Florian Bacher <bacherfl@gmail.com>
2019-04-05 09:35:53 +0200

    pull event broker from docker hub

--- core/eventbroker/deploy.sh
+++ core/eventbroker/deploy.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
-
-rm -f config/gen/event-broker.yaml
-
-cat config/event-broker.yaml | \
-  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/event-broker.yaml 
-
-kubectl delete -f config/gen/event-broker.yaml --ignore-not-found
-kubectl apply -f config/gen/event-broker.yaml
+kubectl delete -f config/event-broker.yaml --ignore-not-found
+kubectl apply -f config/event-broker.yaml
