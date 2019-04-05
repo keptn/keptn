9c224bc4dff234fcc67736fd366ca2c75b6f454f
Florian Bacher <bacherfl@gmail.com>
2019-04-05 09:43:17 +0200

    buildAndDeploy for control

--- core/control/buildAnddeploy.sh
+++ core/control/buildAnddeploy.sh
@@ -2,11 +2,11 @@
 REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
 CHANNEL_URI=$(kubectl describe channel keptn-channel -n keptn | grep "Hostname:" | sed 's~[ \t]*Hostname:[ \t]*~~')
 
-rm -f config/gen/control.yaml
+rm -f config/gen/control-build.yaml
 
-cat config/control.yaml | \
+cat config/control-build.yaml | \
   sed 's~CHANNEL_URI_PLACEHOLDER~'"$CHANNEL_URI"'~' | \
-  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/control.yaml 
+  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/control-build.yaml 
   
-kubectl delete -f config/gen/control.yaml --ignore-not-found
-kubectl apply -f config/gen/control.yaml
+kubectl delete -f config/gen/control-build.yaml --ignore-not-found
+kubectl apply -f config/gen/control-build.yaml
