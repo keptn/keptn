2dd20b5226d3ec8d3777185a7fd7ddcbeac7a7fd
Florian Bacher <bacherfl@gmail.com>
2019-04-05 09:20:22 +0200

    pull auth from docker hub

--- core/auth/deploy.sh
+++ core/auth/deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
-
-rm -f config/gen/authenticator.yaml
-
-cat config/authenticator.yaml | \
-  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/authenticator.yaml 
-  
-kubectl delete -f config/gen/authenticator.yaml --ignore-not-found
-kubectl apply -f config/gen/authenticator.yaml
+kubectl delete -f config/authenticator.yaml --ignore-not-found
+kubectl apply -f config/authenticator.yaml
