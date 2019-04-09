b1e6075238ad0a6a6e6adae15c571156999c20f5
Florian Bacher <bacherfl@gmail.com>
2019-04-09 17:28:14 +0200

    using correct creds file

--- install/scripts/deployServices.sh
+++ install/scripts/deployServices.sh
@@ -6,9 +6,9 @@ export JENKINS_PASSWORD=$(cat creds.json | jq -r '.jenkinsPassword')
 export GITHUB_PERSONAL_ACCESS_TOKEN=$(cat creds.json | jq -r '.githubPersonalAccessToken')
 export GITHUB_USER_NAME=$(cat creds.json | jq -r '.githubUserName')
 export GITHUB_USER_EMAIL=$(cat creds.json | jq -r '.githubUserEmail')
-export DT_TENANT_ID=$(cat creds.json | jq -r '.dynatraceTenant')
-export DT_API_TOKEN=$(cat creds.json | jq -r '.dynatraceApiToken')
-export DT_PAAS_TOKEN=$(cat creds.json | jq -r '.dynatracePaaSToken')
+export DT_TENANT_ID=$(cat creds_dt.json | jq -r '.dynatraceTenant')
+export DT_API_TOKEN=$(cat creds_dt.json | jq -r '.dynatraceApiToken')
+export DT_PAAS_TOKEN=$(cat creds_dt.json | jq -r '.dynatracePaaSToken')
 export GITHUB_ORGANIZATION=$(cat creds.json | jq -r '.githubOrg')
 export DT_TENANT_URL="$DT_TENANT_ID.live.dynatrace.com"
 
