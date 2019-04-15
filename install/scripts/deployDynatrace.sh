6bfc387cc26ed6b9e15b2068c2689a72e15261e5
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-04-15 12:56:43 +0200

    Clean-up of deployDynatrace script

--- install/scripts/deployDynatrace.sh
+++ install/scripts/deployDynatrace.sh
@@ -3,13 +3,14 @@
 export DT_TENANT_ID=$(cat creds_dt.json | jq -r '.dynatraceTenant')
 export DT_API_TOKEN=$(cat creds_dt.json | jq -r '.dynatraceApiToken')
 export DT_PAAS_TOKEN=$(cat creds_dt.json | jq -r '.dynatracePaaSToken')
-export DT_TENANT_URL="$DT_TENANT_ID.live.dynatrace.com"
 
 # Deploy Dynatrace operator
 kubectl create namespace dynatrace
 kubectl label namespace dynatrace istio-injection=disabled
+
 export LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
 echo "Installing Dynatrace Operator $LATEST_RELEASE"
+
 kubectl apply -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
 sleep 60
 kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken=$DT_API_TOKEN" --from-literal="paasToken=$DT_PAAS_TOKEN"
@@ -34,5 +35,5 @@ echo "--------------------------"
 
 ./createServiceEntry.sh $DT_TENANT_ID $DT_PAAS_TOKEN
 
-# Create Secrets to be used by keptn services
+# Create secrets to be used by keptn services
 kubectl -n keptn create secret generic dynatrace --from-literal="DT_API_TOKEN=$DT_API_TOKEN" --from-literal="DT_TENANT_ID=$DT_TENANT_ID"
