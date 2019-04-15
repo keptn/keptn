ce07f2babfe2d18f18167a3b44aeba498711ccad
Florian Bacher <bacherfl@gmail.com>
2019-04-15 17:04:31 +0200

    removed obsolete istio configuration

--- install/scripts/setupIstio.sh
+++ install/scripts/setupIstio.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-CLUSTER_NAME=$1
-CLUSTER_ZONE=$2
-
-#kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio-crds.yaml && \
-#kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio.yaml
-
-
-# Determine the IP scope of the cluster (https://github.com/knative/docs/blob/master/serving/outbound-network-access.md)
-# Gcloud:
-CLUSTER_IPV4_CIDR=$(gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE} | yq r - clusterIpv4Cidr)
-SERVICES_IPV4_CIDR=$(gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE} | yq r - servicesIpv4Cidr)
-
-rm -f ../manifests/gen/istio-knative.yaml
-
-cat ../manifests/istio/istio-knative.yaml | \
-  sed 's~INCLUDE_OUTBOUND_IP_RANGES_PLACEHOLDER~'"$CLUSTER_IPV4_CIDR,$SERVICES_IPV4_CIDR"'~' >> ../manifests/gen/istio-knative.yaml
-
 kubectl apply -f ../manifests/istio/istio-crds-knative.yaml
 sleep 60
-kubectl apply -f ../manifests/gen/istio-knative.yaml
+kubectl apply -f ../manifests/istio-knative.yaml
 
 echo "Wait 4 minutes for changes to apply... "
 sleep 240
