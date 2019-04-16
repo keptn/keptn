03f42b0e487758f04e8370e237159a92d09d9ac1
Florian Bacher <bacherfl@gmail.com>
2019-04-16 14:25:12 +0200

    downgrade to istio 1.0.7

--- install/scripts/setupIstio.sh
+++ install/scripts/setupIstio.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+CLUSTER_NAME=$1
+CLUSTER_ZONE=$2
+
+# Determine the IP scope of the cluster (https://github.com/knative/docs/blob/master/serving/outbound-network-access.md)
+# Gcloud:
+CLUSTER_IPV4_CIDR=$(gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE} | yq r - clusterIpv4Cidr)
+SERVICES_IPV4_CIDR=$(gcloud container clusters describe ${CLUSTER_NAME} --zone=${CLUSTER_ZONE} | yq r - servicesIpv4Cidr)
+
+rm -f ../manifests/gen/istio-knative.yaml
+
+cat ../manifests/istio/istio-knative.yaml | \
+  sed 's~INCLUDE_OUTBOUND_IP_RANGES_PLACEHOLDER~'"$CLUSTER_IPV4_CIDR,$SERVICES_IPV4_CIDR"'~' >> ../manifests/gen/istio-knative.yaml
+
 kubectl apply -f ../manifests/istio/istio-crds-knative.yaml
-sleep 60
-kubectl apply -f ../manifests/istio-knative.yaml
+kubectl apply -f ../manifests/gen/istio-knative.yaml
 
 echo "Wait 4 minutes for changes to apply... "
 sleep 240
@@ -11,5 +23,3 @@ echo "Wait 10s for changes to apply... "
 sleep 10
 
 kubectl delete pods --all -n keptn
-
-# kubectl delete meshpolicies.authentication.istio.io default # fix for the MySQL connection error caused by Istio
