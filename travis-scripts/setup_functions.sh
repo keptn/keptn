377f3ac84a4da77b439d64b364bba0bd783b66d6
Florian Bacher <bacherfl@gmail.com>
2019-04-16 10:14:49 +0200

    fixed test script

--- travis-scripts/setup_functions.sh
+++ travis-scripts/setup_functions.sh
@@ -95,8 +95,6 @@ function execute_cli_tests {
 
     AUTH_ENDPOINT="$(kubectl get ksvc authenticator -n keptn -o=yaml | yq r - status.domain)"
     while [ "$AUTH_ENDPOINT" = "null" ]; do sleep 30; AUTH_ENDPOINT="$(kubectl get ksvc authenticator -n keptn -o=yaml | yq r - status.domain)"; echo "waiting for authenticator service"; done
-    printf "https://" > ~/.keptnmock
-    kubectl get ksvc authenticator -n keptn -o=yaml  | yq r - status.domain >> ~/.keptnmock
 
     set +x
     SEC="$(kubectl get secret keptn-api-token  -n keptn -o=yaml | yq r - data.keptn-api-token | base64 --decode)"
