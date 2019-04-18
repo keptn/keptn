901a54dd30aef8b46685037162c16f72ece6e7a9
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-04-18 15:22:52 +0200

    Image that represents the installer

---
+++ install/scripts/testConnection.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GKE_PROJECT=$1
+CLUSTER_NAME=$2
+CLUSTER_ZONE=$3
+
+gcloud --quiet config set project $GKE_PROJECT
+gcloud --quiet config set container/cluster $CLUSTER_NAME
+gcloud --quiet config set compute/zone $CLUSTER_ZONE
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $CLUSTER_ZONE --project $GKE_PROJECT
+
+if [[ $? != '0' ]]
+then
+  echo -e "${RED}Could not connect to cluster. Please ensure you have set the correct values for your Cluster Name, GKE Project, and Cluster Zone during the credentials setup.${NC}"
+  exit 1
+fi
+
+if [[ $? = '0' ]]
+then
+  echo -e "${GREEN}Connection to cluster successful.${NC}"
+fi
