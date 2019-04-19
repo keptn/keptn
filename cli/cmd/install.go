61fd972d944f8e005586aafee096546cd43c5492
Florian Bacher <bacherfl@gmail.com>
2019-04-19 12:15:29 +0200

    store creds for installer in secure storage

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -16,15 +16,28 @@ package cmd
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
 
+	"github.com/keptn/keptn/cli/utils/credentialmanager"
 	"github.com/spf13/cobra"
 )
+
+type installCredentials struct {
+	ClusterName               string `json:"clusterName"`
+	ClusterZone               string `json:"clusterZone"`
+	JenkinsUser               string `json:"jenkinsUser"`
+	JenkinsPassword           string `json:"jenkinsPassword"`
+	GithubPersonalAccessToken string `json:"githubPersonalAccessToken"`
+	GithubUserEmail           string `json:"githubUserEmail"`
+	GithubOrg                 string `json:"githubOrg"`
+}
 
 // installCmd represents the version command
 var installCmd = &cobra.Command{
@@ -36,13 +49,16 @@ Example:
 	keptn install`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Installing keptn...")
+		// var creds *installCredentials
+		var err error
+		_, err = getInstallCredentials()
 		execCmd := exec.Command("kubectl", "logs", "control-lxxt6-deployment-569499f6cb-gbzgp", "-n", "keptn", "-c", "user-container", "-f")
 
 		var stdout, stderr []byte
 		var errStdout, errStderr error
 		stdoutIn, _ := execCmd.StdoutPipe()
 		stderrIn, _ := execCmd.StderrPipe()
-		err := execCmd.Start()
+		err = execCmd.Start()
 		if err != nil {
 			log.Fatalf("cmd.Start() failed with '%s'\n", err)
 		}
@@ -77,36 +93,71 @@ func init() {
 	rootCmd.AddCommand(installCmd)
 }
 
+func getInstallCredentials() (*installCredentials, error) {
+	var err error
+	var credsStr string
+	var creds *installCredentials
+	credsStr, err = credentialmanager.GetInstallCreds()
+	if err != nil {
+		credsStr = ""
+	}
+	err = json.Unmarshal([]byte(credsStr), &creds)
+	if err != nil {
+		creds = new(installCredentials)
+	}
+
+	fmt.Print("Please enter the following information (press enter to keep the old value):\n")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("GKE cluster name [%s]: ", creds.ClusterName)
+	clusterName, _ := reader.ReadString('\n')
+	if clusterName != "\n" {
+		creds.ClusterName = strings.TrimSuffix(clusterName, "\n")
+	}
+
+	fmt.Printf("GKE cluster zone [%s]: ", creds.ClusterZone)
+	clusterZone, _ := reader.ReadString('\n')
+	if clusterName != "\n" {
+		creds.ClusterZone = strings.TrimSuffix(clusterZone, "\n")
+	}
+
+	// default creds for jenkins
+	creds.JenkinsUser = "admin"
+	creds.JenkinsPassword = "AiTx4u8VyUV8tCKk"
+
+	fmt.Printf("GitHub User Email [%s]: ", creds.GithubUserEmail)
+	githubUserEmail, _ := reader.ReadString('\n')
+	if githubUserEmail != "\n" {
+		creds.GithubUserEmail = strings.TrimSuffix(githubUserEmail, "\n")
+	}
+
+	fmt.Printf("GitHub Organization [%s]: ", creds.GithubOrg)
+	githubOrg, _ := reader.ReadString('\n')
+	if githubOrg != "\n" {
+		creds.GithubOrg = strings.TrimSuffix(githubOrg, "\n")
+	}
+
+	fmt.Printf("GitHub Personal Access Token [%s]: ", creds.GithubPersonalAccessToken)
+	githubPersonalAccessToken, _ := reader.ReadString('\n')
+	if githubPersonalAccessToken != "\n" {
+		creds.GithubPersonalAccessToken = strings.TrimSuffix(githubPersonalAccessToken, "\n")
+	}
+
+	newCreds, _ := json.Marshal(creds)
+	newCredsStr := strings.Replace(string(newCreds), "\n", "", -1)
+	fmt.Printf("new creds file content: %s", newCredsStr)
+	credentialmanager.SetInstallCreds(newCredsStr)
+	return nil, err
+}
+
 func copyAndCapture(w io.Writer, r io.Reader) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		text := strings.Replace(scanner.Text(), "found", "", -1)
+		fmt.Println(text)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
-	/*
-		var out []byte
-		buf := make([]byte, 1024, 1024)
-		for {
-			n, err := r.Read(buf[:])
-			if n > 0 {
-				d := buf[:n]
-				out = append(out, d...)
-				str := string(d)
-				strArr := strings.Split(str, "\n")
-
-				_, err := w.Write(d)
-				if err != nil {
-					return out, err
-				}
-			}
-			if err != nil {
-				// Read returns io.EOF at the end of file, which is not an error for us
-				if err == io.EOF {
-					err = nil
-				}
-				return out, err
-			}
-	*/
 }
--- cli/utils/credentialmanager/handler.go
+++ cli/utils/credentialmanager/handler.go
@@ -18,8 +18,11 @@ const testAPIToken = "super-secret"
 
 const credsLab = "keptn"
 const serverURL = "https://keptn.sh"
+const installCredsKey = "https://keptn-install.sh"
 
 type bot interface {
+	SetInstallCreds(creds string) error
+	GetInstallCreds() (string, error)
 	SetCreds(endPoint string, apiToken string) error
 	GetCreds() (string, string, error)
 }
@@ -31,6 +34,9 @@ var MockCreds bool
 var apiTokenFileURI string
 var mockAPItokenFileURI string
 
+var credsFileURI string
+var mockCredsFileURI string
+
 func init() {
 	usr, err := user.Current()
 	if err != nil {
@@ -39,7 +45,36 @@ func init() {
 	apiTokenFileURI = usr.HomeDir + string(os.PathSeparator) + ".keptn"
 	mockAPItokenFileURI = usr.HomeDir + string(os.PathSeparator) + ".keptnmock"
 
+	credsFileURI = usr.HomeDir + string(os.PathSeparator) + ".keptn-creds"
+	mockCredsFileURI = usr.HomeDir + string(os.PathSeparator) + ".keptn-credsmock"
+
 	credentials.SetCredsLabel(credsLab)
+}
+
+func setInstallCreds(h credentials.Helper, creds string) error {
+	if MockCreds {
+		// Do nothing
+		return nil
+	}
+
+	c := &credentials.Credentials{
+		ServerURL: installCredsKey,
+		Username:  "creds",
+		Secret:    creds,
+	}
+	return h.Add(c)
+}
+
+func getInstallCreds(h credentials.Helper) (string, error) {
+	if MockCreds {
+		// Do nothing
+		return "", nil
+	}
+	_, creds, err := h.Get(installCredsKey)
+	if err != nil {
+		return "", err
+	}
+	return creds, err
 }
 
 func setCreds(h credentials.Helper, endPoint url.URL, apiToken string) error {
@@ -89,4 +124,21 @@ func readCredsFromFile() (url.URL, string, error) {
 	}
 	url, err := url.Parse(creds[0])
 	return *url, creds[1], err
+}
+
+// readInstallCredsFromFile reads the credentials from a file named "creds.json".
+// This function is used for testing
+func readInstallCredsFromFile() (string, error) {
+	var data []byte
+	var err error
+	if MockCreds {
+		data, err = ioutil.ReadFile(mockCredsFileURI)
+	} else {
+		data, err = ioutil.ReadFile(credsFileURI)
+	}
+	if err != nil {
+		return "", err
+	}
+	dataStr := strings.TrimSpace(strings.Replace(string(data), "\r\n", "\n", -1))
+	return dataStr, err
 }
