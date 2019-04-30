9ccddb0eea66374a3ef31b2bb2193724a5b26984
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-04-30 13:28:26 +0200

    Refactor websocket communication

--- cli/utils/websockethelper/websocketHelper.go
+++ cli/utils/websockethelper/websocketHelper.go
@@ -2,6 +2,7 @@ package websockethelper
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/cloudevents/sdk-go/pkg/cloudevents"
 	"github.com/gorilla/websocket"
 	"github.com/keptn/keptn/cli/utils/credentialmanager"
 )
@@ -32,8 +34,57 @@ type LogData struct {
 	LogLevel  string `json:"loglevel"`
 }
 
-// OpenWS opens a websocket
-func OpenWS(token string, channelID string) (*websocket.Conn, *http.Response, error) {
+// IncompleteCE is used for unmarshalling the CE data
+type IncompleteCE struct {
+	ConnData ConnectionData `json:"data"`
+}
+
+// ConnectionData stores ChannelInfo and Success data
+type ConnectionData struct {
+	ChannelInfo ChannelInfo `json:"channelInfo"`
+	Success     bool        `json:"success"`
+}
+
+// ChannelInfo stores a token and a channelID used for opening the websocket
+type ChannelInfo struct {
+	Token     string `json:"token"`
+	ChannelID string `json:"channelId"`
+}
+
+// PrintWSContent opens a websocket using the passed connection data and
+// prints status data
+func PrintWSContent(responseCE *cloudevents.Event, verbose bool) error {
+
+	ceData := &IncompleteCE{}
+	err := responseCE.DataAs(ceData)
+	if err != nil {
+		return err
+	}
+	connData := ceData.ConnData
+
+	err = validateConnectionData(connData)
+	if err != nil {
+		return err
+	}
+
+	ws, _, err := openWS(connData)
+	if err != nil {
+		fmt.Println("Opening websocket failed")
+		return err
+	}
+
+	return readAndPrintCE(ws, verbose)
+}
+
+func validateConnectionData(connData ConnectionData) error {
+	if !connData.Success || connData.ChannelInfo.Token == "" && connData.ChannelInfo.ChannelID == "" {
+		return errors.New("Could not open websocket because Token or Channel ID might be missing or request received unsuccessful status")
+	}
+	return nil
+}
+
+// openWS opens a websocket
+func openWS(connData ConnectionData) (*websocket.Conn, *http.Response, error) {
 
 	endPoint, _, err := credentialmanager.GetCreds()
 	if err != nil {
@@ -44,13 +95,13 @@ func OpenWS(token string, channelID string) (*websocket.Conn, *http.Response, er
 	wsEndPoint.Scheme = "ws"
 
 	header := http.Header{}
-	header.Add("Token", token)
-	header.Add("x-keptn-ws-channel-id", channelID)
+	header.Add("Token", connData.ChannelInfo.Token)
+	header.Add("x-keptn-ws-channel-id", connData.ChannelInfo.ChannelID)
 	return websocket.DefaultDialer.Dial(wsEndPoint.String(), header)
 }
 
-// readCE reads a cloud event
-func readAndPrintCE(ws *websocket.Conn, verboseLogging bool) (interface{}, error) {
+// readAndPrintCE reads a cloud event from the websocket
+func readAndPrintCE(ws *websocket.Conn, verboseLogging bool) error {
 	ws.SetReadDeadline(time.Now().Add(time.Minute))
 	for {
 		messageType, message, err := ws.ReadMessage()
@@ -63,17 +114,16 @@ func readAndPrintCE(ws *websocket.Conn, verboseLogging bool) (interface{}, error
 				log.Fatal(err)
 			}
 			if printCE(messageCE, verboseLogging) {
-				return nil, ws.Close()
+				return ws.Close()
 			}
-
 		}
 		if err != nil {
 			log.Println("read: ", err)
-			return nil, err
+			return err
 		}
 
 	}
-	return nil, nil
+	return nil
 }
 
 func printCE(ce MyCloudEvent, verboseLogging bool) bool {
@@ -98,13 +148,4 @@ func printLogLevel(log LogData, verboseLogging bool) {
 	} else if log.LogLevel != "DEBUG" {
 		fmt.Println(log.Message)
 	}
-}
-
-// PrintWSContent prints received cloud events
-func PrintWSContent(ws *websocket.Conn, verboseLogging bool) error {
-	ceData, err := readAndPrintCE(ws, verboseLogging)
-	if err != nil || ceData == nil {
-		return err
-	}
-	return nil
 }
