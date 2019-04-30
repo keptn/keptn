68a7e2f181c7c87f604b024ced927917786d0022
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-04-30 17:39:55 +0200

    #250 Changed knative from version 0.5.0 to 0.4.0

--- install/scripts/setupKnative-for_knative_05.sh
+++ install/scripts/setupKnative-for_knative_05.sh
@@ -12,33 +12,35 @@ kubectl create namespace keptn #2> /dev/null
 verify_install_step $? "Creating container registry failed."
 
 # Install knative serving, building, eventing
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/v0.5.0/serving.yaml
 verify_kubectl $? "Applying knative serving components failed."
-wait_for_crds "clusteringresses,configurations,images,podautoscalers,revisions,routes,services"
+wait_for_crds "certificates,clusteringresses,configurations,images,podautoscalers,revisions,routes,services,serverlessservices"
 wait_for_all_pods_in_namespace "knative-serving"
 
-kubectl apply -f https://github.com/knative/build/releases/download/v0.4.0/build.yaml
+kubectl apply -f https://github.com/knative/build/releases/download/v0.5.0/build.yaml
 verify_kubectl $? "Applying knative building components failed."
 wait_for_crds "builds,buildtemplates,clusterbuildtemplates,images"
 wait_for_all_pods_in_namespace "knative-build"
 
-kubectl apply -f https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml
+kubectl apply -f https://github.com/knative/eventing/releases/download/v0.5.0/release.yaml
 ## KNOWN ISSUE: Race condition regarding custom resource defintion ClusterChannelProvisioner when applying release.yaml - https://github.com/knative/eventing/issues/680
-print_info "KNOWN ISSUE: Applying the knative v.0.4.0 release.yaml runs into a race condition for the custom resource definition ClusterChannelProvisioner. This does not have an impact on keptn." 
+print_info "KNOWN ISSUE: Applying the knative v.0.5.0 release.yaml runs into a race condition for the custom resource definition ClusterChannelProvisioner. This does not have an impact on keptn." 
 #verify_kubectl $? "Applying knative eventing components failed."
-wait_for_crds "channels,clusterchannelprovisioners,subscriptions"
+wait_for_crds "brokers,channels,clusterchannelprovisioners,subscriptions,triggers"
 wait_for_all_pods_in_namespace "knative-eventing"
 
-kubectl apply -f https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml
+kubectl apply -f https://github.com/knative/eventing-sources/releases/download/v0.5.0/eventing-sources.yaml
 verify_kubectl $? "Applying knative sources failed."
 wait_for_crds "awssqssources,containersources,cronjobsources,githubsources,kuberneteseventsources"
 wait_for_all_pods_in_namespace "knative-sources"
 
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.4.0/monitoring.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/v0.5.0/monitoring.yaml
 verify_kubectl $? "Applying knative monitoring components failed."
-wait_for_all_pods_in_namespace "knative-monitoring"
+## (!) Two pods in knative-monitoring do not run.
+print_info "KNOWN ISSUE: Two pods in knative-monitoring do not run."
+#wait_for_all_pods_in_namespace "knative-monitoring"
 
-kubectl apply -f https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml
+kubectl apply -f https://raw.githubusercontent.com/knative/serving/v0.5.0/third_party/config/build/clusterrole.yaml
 verify_kubectl $? "Creating cluster role for knative failed."
 
 # Configure knative serving default domain
