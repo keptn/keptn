b3c3d4befc58554e62d16f2d5e8f1e93ee073eff
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-04-30 12:48:29 +0200

    #250 Hardening install scripts

--- install/scripts/setupContainerRegistry.sh
+++ install/scripts/setupContainerRegistry.sh
@@ -3,13 +3,13 @@
 source ./utils.sh
 
 kubectl apply -f ../manifests/container-registry/k8s-docker-registry-configmap.yml
-verify_kubectl $? "Configmap for container registry could not be created, stop installation."
+verify_kubectl $? "Creating config map for container registry failed."
 
 kubectl apply -f ../manifests/container-registry/k8s-docker-registry-pvc.yml
-verify_kubectl $? "Persistent volume claim for container registry could not be created, stop installation."
+verify_kubectl $? "Creating persistent volume claim for container registry failed."
 
 kubectl apply -f ../manifests/container-registry/k8s-docker-registry-deployment.yml
-verify_kubectl $? "Deployment for container registry could not be created, stop installation."
+verify_kubectl $? "Creating deployment for container registry failed."
 
 kubectl apply -f ../manifests/container-registry/k8s-docker-registry-service.yml
-verify_kubectl $? "Service for container registry could not be created, stop installation."
+verify_kubectl $? "Creating service for container registry failed."
