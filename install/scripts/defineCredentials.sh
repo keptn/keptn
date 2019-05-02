7d992c5bc4a7658d51304e5cf8a05a112079825d
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-05-02 08:15:20 +0200

    Move replaceCreds function in dedicated file

--- install/scripts/defineCredentials.sh
+++ install/scripts/defineCredentials.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-function replaceCreds {
-    CREDS=./creds.json
-    rm $CREDS 2> /dev/null
-    cat ./creds.sav | sed 's~GITHUB_USER_NAME_PLACEHOLDER~'"$GITU"'~' | \
-      sed 's~PERSONAL_ACCESS_TOKEN_PLACEHOLDER~'"$GITAT"'~' | \
-      sed 's~GITHUB_USER_EMAIL_PLACEHOLDER~'"$GITE"'~' | \
-      sed 's~CLUSTER_NAME_PLACEHOLDER~'"$CLN"'~' | \
-      sed 's~CLUSTER_ZONE_PLACEHOLDER~'"$CLZ"'~' | \
-      sed 's~GKE_PROJECT_PLACEHOLDER~'"$PROJ"'~' | \
-      sed 's~GITHUB_ORG_PLACEHOLDER~'"$GITO"'~' >> $CREDS
-}
-
 YLW='\033[1;33m'
 NC='\033[0m'
 
@@ -52,6 +40,7 @@ echo ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    source ./defineCredentialsUtils.sh
     replaceCreds
 fi
 
