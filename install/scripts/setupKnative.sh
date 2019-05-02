607823c0b53caf3fc19cbd19c8bf9485ef5357d5
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-05-02 09:16:10 +0200

    #250 Install knative two times during install process

--- install/scripts/setupKnative.sh
+++ install/scripts/setupKnative.sh
@@ -12,32 +12,43 @@ kubectl create namespace keptn #2> /dev/null
 verify_install_step $? "Creating container registry failed."
 
 # Install knative serving, building, eventing
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml
-verify_kubectl $? "Applying knative serving components failed."
-wait_for_crds "clusteringresses,configurations,images,podautoscalers,revisions,routes,services"
-wait_for_all_pods_in_namespace "knative-serving"
+for i in 1 2
+do
+  kubectl apply -f https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml
+  verify_kubectl $? "Applying knative serving components failed."
+  sleep 5
+  wait_for_crds "clusteringresses,configurations,images,podautoscalers,revisions,routes,services"
+  wait_for_all_pods_in_namespace "knative-serving"
 
-kubectl apply -f https://github.com/knative/build/releases/download/v0.4.0/build.yaml
-verify_kubectl $? "Applying knative building components failed."
-wait_for_crds "builds,buildtemplates,clusterbuildtemplates,images"
-wait_for_all_pods_in_namespace "knative-build"
+  kubectl apply -f https://github.com/knative/build/releases/download/v0.4.0/build.yaml
+  verify_kubectl $? "Applying knative building components failed."
+  sleep 5
+  wait_for_crds "builds,buildtemplates,clusterbuildtemplates,images"
+  wait_for_all_pods_in_namespace "knative-build"
 
-kubectl apply -f https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml
-## KNOWN ISSUE: Race condition regarding custom resource defintion ClusterChannelProvisioner when applying release.yaml - https://github.com/knative/eventing/issues/680
-print_info "KNOWN ISSUE: Applying the knative v.0.4.0 release.yaml runs into a race condition for the custom resource definition ClusterChannelProvisioner." 
-#verify_kubectl $? "Applying knative eventing components failed."
-wait_for_crds "channels,clusterchannelprovisioners,subscriptions"
-wait_for_all_pods_in_namespace "knative-eventing"
+  kubectl apply -f https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml
+  ## KNOWN ISSUE: Race condition regarding custom resource defintion ClusterChannelProvisioner when applying release.yaml - https://github.com/knative/eventing/issues/680
+  print_info "KNOWN ISSUE: Applying the knative v.0.4.0 release.yaml runs into a race condition for the custom resource definition ClusterChannelProvisioner." 
+  verify_kubectl $? "Applying knative eventing components failed."
+  sleep 5
+  wait_for_crds "channels,clusterchannelprovisioners,subscriptions"
+  wait_for_all_pods_in_namespace "knative-eventing"
 
-kubectl apply -f https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml
-verify_kubectl $? "Applying knative sources failed."
-wait_for_crds "awssqssources,containersources,cronjobsources,githubsources,kuberneteseventsources"
-wait_for_all_pods_in_namespace "knative-sources"
+  kubectl apply -f https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml
+  verify_kubectl $? "Applying knative sources failed."
+  sleep 5
+  wait_for_crds "awssqssources,containersources,cronjobsources,githubsources,kuberneteseventsources"
+  wait_for_all_pods_in_namespace "knative-sources"
 
-kubectl apply -f https://github.com/knative/serving/releases/download/v0.4.0/monitoring.yaml
-verify_kubectl $? "Applying knative monitoring components failed."
-wait_for_all_pods_in_namespace "knative-monitoring"
+  kubectl apply -f https://github.com/knative/serving/releases/download/v0.4.0/monitoring.yaml
+  verify_kubectl $? "Applying knative monitoring components failed."
+  sleep 5
+  wait_for_all_pods_in_namespace "knative-monitoring"
 
+  sleep 30
+done
+
+# Creating cluster role binding for knative
 kubectl apply -f https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml
 verify_kubectl $? "Creating cluster role for knative failed."
 
