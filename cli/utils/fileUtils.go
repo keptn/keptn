95724257633af27145ab79d3ae20636d1e245183
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-05-09 16:33:00 +0200

    #272 Get home path for windows

--- cli/utils/fileUtils.go
+++ cli/utils/fileUtils.go
@@ -1,27 +1,36 @@
 package utils
 
 import (
+	"fmt"
 	"os"
-	"os/user"
+	"runtime"
 )
 
 const keptnFolderName = ".keptn"
 
 // GetKeptnDirectory returns a path, which is used to store logs and possibly creds
 func GetKeptnDirectory() (string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return "", err
-	}
 
-	keptnDir := usr.HomeDir + string(os.PathSeparator) + keptnFolderName + string(os.PathSeparator)
+	keptnDir := userHomeDir() + string(os.PathSeparator) + keptnFolderName + string(os.PathSeparator)
 
 	if _, err := os.Stat(keptnDir); os.IsNotExist(err) {
 		err := os.MkdirAll(keptnDir, os.ModePerm)
+		fmt.Println("keptn creates the folder " + keptnDir + " to store logs and possibly creds.")
 		if err != nil {
 			return "", err
 		}
 	}
 
 	return keptnDir, nil
+}
+
+func userHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
 }
