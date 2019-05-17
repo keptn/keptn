6e2cf87a6ea4735f83ed5391373b6d78ef4e5734
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-05-17 12:42:32 +0200

    #336 Get cli dependencies

--- cli/build_cli_feature.sh
+++ cli/build_cli_feature.sh
@@ -6,19 +6,24 @@ NUMBER=$3
 DATE=$4
 
 # MAC
+env GOOS=darwin GOARCH=amd64 go get ./...
 env GOOS=darwin GOARCH=amd64 go build -o keptn
 zip keptn-macOS-${GIT_SHA}-${TYPE}-${NUMBER}-${DATE}.zip keptn
 tar -zcvf keptn-macOS-${GIT_SHA}-${TYPE}-${NUMBER}-${DATE}.tar.gz keptn
 rm keptn
 
 # Linux
+env GOOS=linux GOARCH=amd64 go get ./...
 env GOOS=linux GOARCH=amd64 go build -o keptn
 zip keptn-linux-${GIT_SHA}-${TYPE}-${NUMBER}-${DATE}.zip keptn
 tar -zcvf keptn-linux-${GIT_SHA}-${TYPE}-${NUMBER}-${DATE}.tar.gz keptn
 rm keptn
 
 # Windows
+env GOOS=windows GOARCH=amd64 go get ./...
 env GOOS=windows GOARCH=amd64 go build -o keptn.exe
 zip keptn-windows-${GIT_SHA}-${TYPE}-${NUMBER}-${DATE}.zip keptn.exe
 tar -zcvf keptn-windows-${GIT_SHA}-${TYPE}-${NUMBER}-${DATE}.tar.gz keptn.exe
 rm keptn.exe
+
+ls -lsa
