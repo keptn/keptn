80c90fd1bf3cfe3b982a7256cf02f90cc8fc3df4
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-05-17 08:30:54 +0200

    #336 Fix path in build scripts

--- travis-scripts/build_feature.sh
+++ travis-scripts/build_feature.sh
@@ -13,3 +13,5 @@ docker build . -t "${IMAGE}:${GIT_SHA}"
 docker tag "${IMAGE}:${GIT_SHA}" "${IMAGE}:${TYPE}.${NUMBER}.${DATE}"
 docker push "${IMAGE}:${GIT_SHA}"
 docker push "${IMAGE}:${TYPE}.${NUMBER}.${DATE}"
+
+cd ../..
