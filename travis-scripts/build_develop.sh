48d34364bb25491c81bcaf5a7cb3fc4344a62884
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-05-17 08:46:41 +0200

    #336 Testing build for develop branch

--- travis-scripts/build_develop.sh
+++ travis-scripts/build_develop.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-IMAGE=$1
-GIT_SHA=$2
-TYPE=$3
-NUMBER=$4
-DATE=$5
+FOLDER=$1
+IMAGE=$2
+GIT_SHA=$3
+DATE=$4
+
+cd "${FOLDER}"
 
 docker build . -t "${IMAGE}:${GIT_SHA}"
-docker tag "${IMAGE}:${GIT_SHA}" "${IMAGE}:${TYPE}.${NUMBER}.${DATE}"
+docker tag "${IMAGE}:${GIT_SHA}" "${IMAGE}:${DATE}"
+docker tag "${IMAGE}:${GIT_SHA}" "${IMAGE}:latest"
 docker push "${IMAGE}:${GIT_SHA}"
-docker push "${IMAGE}:${TYPE}.${NUMBER}.${DATE}"
+docker push "${IMAGE}:${DATE}"
+docker push "${IMAGE}:latest"
+
+cd ../..
