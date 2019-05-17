263ec4c7019c7478edb76efa539c019bb9971327
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-05-17 11:18:14 +0200

    check if rules already existing

--- install/scripts/applyAutoTaggingRules.sh
+++ install/scripts/applyAutoTaggingRules.sh
@@ -2,15 +2,38 @@
 
 source ./utils.sh
 
+# API documentation
+# https://www.dynatrace.com/support/help/extend-dynatrace/dynatrace-api/configuration/auto-tag-api/
+
 DT_TENANT=$1
 DT_API_TOKEN=$2
+
+
+DT_RULE_NAME=service
+# check if rule already exists in Dynatrace tenant
+export DT_ID=
+export DT_ID=$(curl -X GET \
+  "https://$DT_TENANT/api/config/v1/autoTags?Api-Token=$DT_API_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  | jq -r '.values[] | select(.name == "'$DT_RULE_NAME'") | .id')
+
+# if exists, then delete it
+if [ "$DT_ID" != "" ]
+then
+  echo "Removing $DT_RULE_NAME since exists. Replacing with new definition."
+  curl -X DELETE \
+  "https://$DT_TENANT/api/config/v1/autoTags/$DT_ID?Api-Token=$DT_API_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache'
+fi
 
 curl -X POST \
   "https://$DT_TENANT/api/config/v1/autoTags?api-token=$DT_API_TOKEN" \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -d '{
-  "name": "service",
+  "name": "'$DT_RULE_NAME'",
   "rules": [
     {
       "type": "SERVICE",
@@ -43,12 +66,32 @@ if [[ $? != '0' ]]; then
   exit 1
 fi
 
+
+DT_RULE_NAME=environment
+# check if rule already exists in Dynatrace tenant
+export DT_ID=
+export DT_ID=$(curl -X GET \
+  "https://$DT_TENANT/api/config/v1/autoTags?Api-Token=$DT_API_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  | jq -r '.values[] | select(.name == "'$DT_RULE_NAME'") | .id')
+
+# if exists, then delete it
+if [ "$DT_ID" != "" ]
+then
+  echo "Removing $DT_RULE_NAME since exists. Replacing with new definition."
+  curl -X DELETE \
+  "https://$DT_TENANT/api/config/v1/autoTags/$DT_ID?Api-Token=$DT_API_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache'
+fi
+
 curl -X POST \
   "https://$DT_TENANT/api/config/v1/autoTags?api-token=$DT_API_TOKEN" \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -d '{
-	"name": "environment",
+	"name": "'$DT_RULE_NAME'",
   "rules": [
     {
       "type": "SERVICE",
