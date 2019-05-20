58c1dca4352c367f26c5b881e08d2061a905d584
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-05-20 12:22:35 +0200

    #336 Refactored build script for cli

--- cli/buildCLI.sh
+++ cli/buildCLI.sh
@@ -6,14 +6,16 @@ zip keptn-macOS.zip keptn
 tar -zcvf keptn-macOS.tar.gz keptn
 rm keptn
 
-# Linux
-env GOOS=linux GOARCH=amd64 go build -o keptn
-zip keptn-linux.zip keptn
-tar -zcvf keptn-linux.tar.gz keptn
-rm keptn
+# Linux build covered by Travis CI
 
-# Windows
-env GOOS=windows GOARCH=amd64 go build -o keptn.exe
-zip keptn-windows.zip keptn.exe
-tar -zcvf keptn-windows.tar.gz keptn.exe
-rm keptn.exe
+#env GOOS=linux GOARCH=amd64 go build -o keptn
+#zip keptn-linux.zip keptn
+#tar -zcvf keptn-linux.tar.gz keptn
+#rm keptn
+
+# Windows build covered by Travis CI
+
+#env GOOS=windows GOARCH=amd64 go build -o keptn.exe
+#zip keptn-windows.zip keptn.exe
+#tar -zcvf keptn-windows.tar.gz keptn.exe
+#rm keptn.exe
