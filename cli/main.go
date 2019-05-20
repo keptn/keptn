f4b2d7180cf7bcea042e80f77dced0534fa20405
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-05-20 12:29:37 +0200

    #336 Read version info from ldflags

--- cli/main.go
+++ cli/main.go
@@ -7,10 +7,16 @@ import (
 	"github.com/keptn/keptn/cli/utils"
 )
 
+var (
+	// Version information which is passed by ldflags
+	Version = "develop"
+)
+
 func init() {
 	utils.InitLoggers(os.Stdout, os.Stdout, os.Stderr)
 }
 
 func main() {
+	cmd.Version = Version
 	cmd.Execute()
 }
