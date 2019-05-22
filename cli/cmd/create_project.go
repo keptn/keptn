ec5a937c7e82ddffb1cb1fd1a74ddae11b0fdb48
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-05-22 11:04:37 +0200

    #326 Implementation of command send

--- cli/cmd/create_project.go
+++ cli/cmd/create_project.go
@@ -109,7 +109,7 @@ Example:
 				return nil
 			}
 			if responseCE.Data != nil {
-				return websockethelper.PrintWSContent(responseCE)
+				return websockethelper.PrintWSContentCEResponse(responseCE)
 			}
 		} else {
 			fmt.Println("Skipping create project due to mocking flag set to true")
--- cli/cmd/send.go
+++ cli/cmd/send.go
@@ -15,8 +15,18 @@
 package cmd
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
+	"github.com/keptn/keptn/cli/utils"
+	"github.com/keptn/keptn/cli/utils/credentialmanager"
+	"github.com/keptn/keptn/cli/utils/websockethelper"
 	"github.com/spf13/cobra"
 )
 
@@ -30,8 +40,68 @@ var sendCmd = &cobra.Command{
 
 Example:
 	keptn send --event=new_artifact.json`,
+	SilenceUsage: true,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		eventString, err := utils.ReadFile(*eventFilePath)
+		if err != nil {
+			return err
+		}
+		var body interface{}
+		return json.Unmarshal([]byte(eventString), &body)
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("send called")
+		endPoint, apiToken, err := credentialmanager.GetCreds()
+		if err != nil {
+			return errors.New(authErrorMsg)
+		}
+		eventString, err := utils.ReadFile(*eventFilePath)
+		if err != nil {
+			return err
+		}
+
+		utils.PrintLog("Starting to send an event", utils.InfoLevel)
+
+		eventURL := endPoint
+		eventURL.Path = "event"
+		utils.PrintLog(fmt.Sprintf("Connecting to server %s", eventURL.String()), utils.VerboseLevel)
+		if !mocking {
+			bodyBytes := []byte(eventString)
+			req, err := http.NewRequest("POST", eventURL.String(), bytes.NewBuffer(bodyBytes))
+
+			mac := hmac.New(sha1.New, []byte(apiToken))
+			mac.Write(bodyBytes)
+			signatureVal := mac.Sum(nil)
+			sha1Hash := "sha1=" + fmt.Sprintf("%x", signatureVal)
+
+			// Add signature header
+			req.Header.Set("X-Keptn-Signature", sha1Hash)
+			req.Header.Set("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				utils.PrintLog("Send event was unsuccessful", utils.QuietLevel)
+				return err
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != http.StatusOK {
+				utils.PrintLog("Send event was unsuccessful", utils.QuietLevel)
+				return errors.New(resp.Status)
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+			// check for responseCE to include token
+			if body == nil || len(body) == 0 {
+				utils.PrintLog("Response is empty", utils.InfoLevel)
+				return nil
+			}
+			return websockethelper.PrintWSContentByteResponse(body)
+		} else {
+			fmt.Println("Skipping send-new artifact due to mocking flag set to true")
+		}
 		return nil
 	},
 }
