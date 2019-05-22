238b3152a9a46c735db928dce8860e342484914c
Andreas Grimmer <agrimmer@users.noreply.github.com>
2019-05-22 16:31:09 +0200

    Update uninstallKeptn.sh

--- install/scripts/uninstallKeptn.sh
+++ install/scripts/uninstallKeptn.sh
@@ -11,7 +11,7 @@ kubectl delete namespace keptn --ignore-not-found
 kubectl delete -f https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml --ignore-not-found
 kubectl delete -f https://github.com/knative/build/releases/download/v0.4.0/build.yaml --ignore-not-found
 kubectl delete -f https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml --ignore-not-found
-kubectl delete -f https://github.com/knative/eventing-sources/releases/download/v0.4.0/eventing-sources.yaml --ignore-not-found
+kubectl delete -f https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml --ignore-not-found
 kubectl delete -f https://github.com/knative/serving/releases/download/v0.4.0/monitoring.yaml --ignore-not-found
 kubectl delete -f https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml --ignore-not-found
 
