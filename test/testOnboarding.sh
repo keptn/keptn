169212cbe3132cc0859426746062dc38a698c7b1
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-05-29 15:58:29 +0200

    #327 Make tests Mac ready

--- test/testOnboarding.sh
+++ test/testOnboarding.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 source ./utils.sh
 
-GITHUB_USER=$(cat ./installer/scripts/creds.json | jq -r '.githubUserName')
-GITHUB_ORG=$(cat ./installer/scripts/creds.json | jq -r '.githubOrg')
-GITHUB_TOKEN=$(cat ./installer/scripts/creds.json | jq -r '.githubPersonalAccessToken')
-
-KEPTN_ENDPOINT=https://$(kubectl get ksvc -n keptn control -o=yaml | yq r - status.domain)
-KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -o=yaml | yq - r data.keptn-api-token | base64 --decode)
+KEPTN_ENDPOINT=https://$(kubectl get ksvc -n keptn control -ojsonpath={.status.domain})
+KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
 
 PROJECT=sockshop
 
@@ -24,21 +20,21 @@ fi
 keptn auth --endpoint=$KEPTN_ENDPOINT --api-token=$KEPTN_API_TOKEN
 verify_test_step $? "Could not authenticate at keptn API."
 
-keptn configure --org=$GITHUB_ORG --user=$GITHUB_USER --token=$GITHUB_TOKEN
+keptn configure --org=$GITHUB_ORG_NIGHTLY --user=$GITHUB_USER_NAME_NIGHTLY --token=$GITHUB_TOKEN_NIGHTLY
 verify_test_step $? "keptn config command failed."
 
 # Test keptn config result
 RETRY=0; RETRY_MAX=12;
 while [[ $RETRY -lt $RETRY_MAX ]]; do
   sleep 10
-  STORED_GITHUB_USER=$(kubectl get secret github-credentials -n keptn -o=yaml | yq - r data.user | base64 --decode)
+  STORED_GITHUB_USER=$(kubectl get secret github-credentials -n keptn -ojsonpath={.data.user} | base64 --decode)
 
   if [ "$STORED_GITHUB_USER" == "$GITHUB_USER" ]; then
       echo "Keptn config succeeded."
       break
   fi
   RETRY=$[$RETRY+1]
-  echo "Expected value user=$GITHUB_USER not yet stored in cluster. Actual value is $STORED_GITHUB_USER. Trying again in 10 seconds."
+  echo "Expected value user=$GITHUB_USER_NAME_NIGHTLY not yet stored in cluster. Actual value is $STORED_GITHUB_USER. Trying again in 10 seconds."
   sleep 10
   if [ $RETRY -eq $RETRY_MAX ]; then
     echo "keptn config failed."
@@ -63,7 +59,9 @@ keptn onboard service --project=$PROJECT --values=values_carts_db.yaml --deploym
 sleep 60
 cd ../..
 npm install newman
-yq w keptn.postman_environment.json values[0].value $GITHUB_ORG | yq  - w values[1].value $PROJECT | yq  - w values[2].value $GITHUB_CLIENT_ID_NIGHTLY |  yq  - w values[3].value $GITHUB_CLIENT_SECRET_NIGHTLY | yq - r -j > keptn.postman_environment_tmp.json
+go get gopkg.in/mikefarah/yq.v2
+
+$GOPATH/bin/yq.v2 w keptn.postman_environment.json values[0].value $GITHUB_ORG_NIGHTLY | $GOPATH/bin/yq.v2  - w values[1].value $PROJECT | $GOPATH/bin/yq.v2  - w values[2].value $GITHUB_CLIENT_ID_NIGHTLY |  $GOPATH/bin/yq.v2  - w values[3].value $GITHUB_CLIENT_SECRET_NIGHTLY | $GOPATH/bin/yq.v2 - r -j > keptn.postman_environment_tmp.json
 rm keptn.postman_environment.json
 mv keptn.postman_environment_tmp.json keptn.postman_environment.json
 node_modules/.bin/newman run keptn.postman_collection.json -e keptn.postman_environment.json
