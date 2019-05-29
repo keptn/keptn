c69b51365a55d9c6dd8bde250e7c0e070fc1121e
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-05-29 10:03:55 +0200

    #327 Use keptn install for nightly setup

--- travis-scripts/cronjob_main.sh
+++ travis-scripts/cronjob_main.sh
@@ -5,10 +5,6 @@ source ./travis-scripts/setup_functions.sh
 # prints the full command before output of the command.
 set -x
 
-install_hub
-install_yq
-install_helm
-
 setup_gcloud_nightly
 
 uninstall_keptn
@@ -18,13 +14,10 @@ create_nightly_cluster
 
 build_and_install_cli
 
-keptn install --keptn-version=develop
+keptn install --keptn-version=develop --verbose
 
 export ISTIO_INGRESS=$(kubectl describe svc istio-ingressgateway -n istio-system | grep "LoadBalancer Ingress:" | sed 's~LoadBalancer Ingress:[ \t]*~~')
 export_names
-
-# Execute unit tests
-execute_cli_tests
 
 # Execute end-to-end test
 cd test
--- travis-scripts/setup_functions.sh
+++ travis-scripts/setup_functions.sh
@@ -106,32 +106,6 @@ function export_names {
     ./test/assertEquals.sh $CONTROL_NAME control
 }
 
-function execute_cli_tests {
-
-    cd cli
-
-    dep ensure
-
-    mkdir ~/.keptn
-    ENDPOINT="$(kubectl get ksvc control -n keptn -o=yaml | yq r - status.domain)"
-    while [ "$ENDPOINT" = "null" ]; do sleep 30; ENDPOINT="$(kubectl get ksvc control -n keptn -o=yaml | yq r - status.domain)"; echo "waiting for control service"; done
-    printf "https://" > ~/.keptn/.keptnmock
-    kubectl get ksvc control -n keptn -o=yaml  | yq r - status.domain >> ~/.keptn/.keptnmock
-
-    AUTH_ENDPOINT="$(kubectl get ksvc authenticator -n keptn -o=yaml | yq r - status.domain)"
-    while [ "$AUTH_ENDPOINT" = "null" ]; do sleep 30; AUTH_ENDPOINT="$(kubectl get ksvc authenticator -n keptn -o=yaml | yq r - status.domain)"; echo "waiting for authenticator service"; done
-
-    set +x
-    SEC="$(kubectl get secret keptn-api-token  -n keptn -o=yaml | yq r - data.keptn-api-token | base64 --decode)"
-    echo "${SEC}" >> ~/.keptn/.keptnmock
-    set -x
-
-    # execute GO tests
-    go test ${gobuild_args} -timeout 240s ./...
-    verify_step $? "CLI tests failed."
-    cd ..
-}
-
 function build_and_install_cli {
     # Build CLI for end-to-end test
     cd cli/
