ff70bbfb850b136095fbb25439f9d161fa2e25bb
Johannes <johannes.braeuer@dynatrace.com>
2019-07-30 12:56:40 +0200

    #537 Refactoring travis build

---
+++ travis-scripts/build_feature.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ${IMAGE}=$1 ${FOLDER}=$2 ${GIT_SHA}=$3 ${TYPE}=$4 ${NUMBER}=$5 ${DATE}=$6
+IMAGE=$1
+FOLDER=$2
+GIT_SHA=$3
+TYPE=$4
+NUMBER=$5
+DATE=$6
+
+echo "Build keptn ${IMAGE}"
+cp MANIFEST ./${IMAGE}
+cd ./${IMAGE}
+cat MANIFEST
+docker build . -t "${IMAGE}:${GIT_SHA}"
+docker tag "${IMAGE}:${GIT_SHA}" "${API_IMAGE}:${TYPE}.${NUMBER}.${DATE}"
+docker push "${API_IMAGE}:${GIT_SHA}"
+docker push "${API_IMAGE}:${TYPE}.${NUMBER}.${DATE}"
