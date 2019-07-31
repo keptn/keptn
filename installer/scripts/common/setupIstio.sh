f98609424707443dc9b072d1806fe26efef98dec
Johannes <johannes.braeuer@dynatrace.com>
2019-07-31 08:33:02 +0200

    #537 Moved installer to mono repo

---
+++ installer/scripts/common/setupIstio.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ./common/utils.sh
+
+kubectl create namespace istio-system
+
+# Apply custom resource definitions for Istio
+kubectl apply -f ../manifests/istio/crd-10.yaml
+verify_kubectl $? "Creating istio custom resource definitions failed."
+kubectl apply -f ../manifests/istio/crd-11.yaml
+verify_kubectl $? "Creating istio custom resource definitions failed."
+kubectl apply -f ../manifests/istio/crd-12.yaml
+verify_kubectl $? "Creating istio custom resource definitions failed."
+kubectl apply -f ../manifests/istio/crd-certmanager-10.yaml
+verify_kubectl $? "Creating istio custom resource definitions failed."
+kubectl apply -f ../manifests/istio/crd-certmanager-11.yaml
+verify_kubectl $? "Creating istio custom resource definitions failed."
+wait_for_crds "virtualservices,destinationrules,serviceentries,gateways,envoyfilters,policies,meshpolicies,httpapispecbindings,httpapispecs,quotaspecbindings,quotaspecs,rules,attributemanifests"
+
+kubectl apply -f ../manifests/istio/istio-lean.yaml
+verify_kubectl $? "Creating all istio components failed."
+wait_for_all_pods_in_namespace "istio-system"
