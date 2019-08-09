dc48f00775a19397e4695c4b0180c28d253527c8
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-09 11:35:19 +0200

    #553 Refactor multiple used functions

--- cli/utils/marshalHelper.go
+++ cli/utils/marshalHelper.go
@@ -1,27 +1,8 @@
 package utils
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-
-	keptnutils "github.com/keptn/go-utils/pkg/utils"
 	"gopkg.in/yaml.v2"
 )
-
-// ReadFile reads a file and returns the content as string
-func ReadFile(fileName string) (string, error) {
-
-	fileName = keptnutils.ExpandTilde(fileName)
-	if _, err := os.Stat(fileName); os.IsNotExist(err) {
-		return "", fmt.Errorf("Cannot find file %s", fileName)
-	}
-	data, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return "", err
-	}
-	return string(data), nil
-}
 
 // UnmarshalString provides a YAML unmarsheling helper
 func UnmarshalString(data string) (interface{}, error) {
