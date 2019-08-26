b4cf6104d4d4d0b992458a3e4c31c45356557863
Florian Bacher <florian.bacher@dynatrace.com>
2019-08-26 10:54:12 +0200

    #673 added detailed error message for resource upload endpoint

--- api/handlers/service_resource.go
+++ api/handlers/service_resource.go
@@ -1,11 +1,21 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/swag"
 	keptnutils "github.com/keptn/go-utils/pkg/utils"
 	models "github.com/keptn/keptn/api/models"
 	"github.com/keptn/keptn/api/restapi/operations/service_resource"
+)
+
+type notFoundError int
+
+const (
+	projectNotFound notFoundError = iota
+	stageNotFound
+	serviceNotFound
 )
 
 // DeleteProjectProjectNameStageStageNameServiceServiceNameResourceResourceURIHandlerFunc deletes the specified resource
@@ -17,6 +27,10 @@ func DeleteProjectProjectNameStageStageNameServiceServiceNameResourceResourceURI
 
 // PostProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc creates a new resource
 func PostProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(params service_resource.PostProjectProjectNameStageStageNameServiceServiceNameResourceParams, principal *models.Principal) middleware.Responder {
+	serviceFoundErr := checkServiceAvailability(params.ProjectName, params.StageName, params.ServiceName)
+	if serviceFoundErr >= 0 {
+		return service_resource.NewPostProjectProjectNameStageStageNameServiceServiceNameResourceDefault(400).WithPayload(getNotFoundErrorMessage(serviceFoundErr, params.ProjectName, params.StageName, params.ServiceName))
+	}
 	resourceHandler := keptnutils.NewResourceHandler("configuration-service:8080")
 
 	resourcesToUpload := []*keptnutils.Resource{}
@@ -37,7 +51,11 @@ func PostProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(p
 
 // PutProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc updates a list of resources
 func PutProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(params service_resource.PutProjectProjectNameStageStageNameServiceServiceNameResourceParams, principal *models.Principal) middleware.Responder {
-	resourceHandler := keptnutils.NewResourceHandler("configuration-service.keptn.svc.cluster.local:8080")
+	serviceFoundErr := checkServiceAvailability(params.ProjectName, params.StageName, params.ServiceName)
+	if serviceFoundErr >= 0 {
+		return service_resource.NewPostProjectProjectNameStageStageNameServiceServiceNameResourceDefault(400).WithPayload(getNotFoundErrorMessage(serviceFoundErr, params.ProjectName, params.StageName, params.ServiceName))
+	}
+	resourceHandler := keptnutils.NewResourceHandler("configuration-service:8080")
 
 	resourcesToUpload := []*keptnutils.Resource{}
 	for _, resource := range params.Resources.Resources {
@@ -53,4 +71,61 @@ func PutProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(pa
 	return service_resource.NewPutProjectProjectNameStageStageNameServiceServiceNameResourceCreated().WithPayload(&models.Version{
 		Version: newVersion,
 	})
+}
+
+func getNotFoundErrorMessage(notFoundErrorcode notFoundError, project string, stage string, service string) *models.Error {
+	errorMsg := &models.Error{
+		Code: 400,
+	}
+	switch notFoundErrorcode {
+	case projectNotFound:
+		errorMsg.Message = swag.String("Project " + project + " does not exist.")
+		break
+	case stageNotFound:
+		errorMsg.Message = swag.String("Stage " + stage + " does not exist within project " + project)
+		break
+	case serviceNotFound:
+		errorMsg.Message = swag.String("Service " + service + " does not exist within stage " + stage + " of project " + project)
+		break
+	default:
+		errorMsg.Message = swag.String("Unknown error")
+		break
+	}
+
+	return errorMsg
+}
+
+func checkServiceAvailability(project string, stage string, service string) notFoundError {
+	// check if project exists
+	status, err := doGet("http://configuration-service:8080/project/" + project)
+
+	if err != nil || status == 404 {
+		return projectNotFound
+	}
+	status, err = doGet("http://configuration-service:8080/project/" + project + "/stage/" + stage)
+	if err != nil || status == 404 {
+		return stageNotFound
+	}
+	status, err = doGet("http://configuration-service:8080/project/" + project + "/stage/" + stage + "/service/" + service)
+	if err != nil || status == 404 {
+		return stageNotFound
+	}
+	return -1
+}
+
+func doGet(uri string) (int, error) {
+	req, err := http.NewRequest("GET", uri, nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if err != nil {
+		return 0, err
+	}
+	return resp.StatusCode, nil
 }
