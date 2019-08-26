df40cb39c3db86b77a72ceb3ad70ece91a40159a
Florian Bacher <florian.bacher@dynatrace.com>
2019-08-26 12:48:07 +0200

    #673 moved check of project/stage/service availability to configuration service

--- api/handlers/service_resource.go
+++ api/handlers/service_resource.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"net/http"
+	b64 "encoding/base64"
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/swag"
@@ -27,16 +27,16 @@ func DeleteProjectProjectNameStageStageNameServiceServiceNameResourceResourceURI
 
 // PostProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc creates a new resource
 func PostProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(params service_resource.PostProjectProjectNameStageStageNameServiceServiceNameResourceParams, principal *models.Principal) middleware.Responder {
-	serviceFoundErr := checkServiceAvailability(params.ProjectName, params.StageName, params.ServiceName)
-	if serviceFoundErr >= 0 {
-		return service_resource.NewPostProjectProjectNameStageStageNameServiceServiceNameResourceDefault(400).WithPayload(getNotFoundErrorMessage(serviceFoundErr, params.ProjectName, params.StageName, params.ServiceName))
-	}
 	resourceHandler := keptnutils.NewResourceHandler("configuration-service:8080")
 
 	resourcesToUpload := []*keptnutils.Resource{}
 	for _, resource := range params.Resources.Resources {
+		decodedStr, err := b64.StdEncoding.DecodeString(*resource.ResourceContent)
+		if err != nil {
+			return service_resource.NewPostProjectProjectNameStageStageNameServiceServiceNameResourceDefault(500).WithPayload(&models.Error{Code: 500, Message: swag.String("Could not base64-decode content of resource " + resource.ResourceURI)})
+		}
 		resourcesToUpload = append(resourcesToUpload, &keptnutils.Resource{
-			ResourceContent: *resource.ResourceContent,
+			ResourceContent: string(decodedStr),
 			ResourceURI:     *resource.ResourceURI,
 		})
 	}
@@ -51,16 +51,15 @@ func PostProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(p
 
 // PutProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc updates a list of resources
 func PutProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(params service_resource.PutProjectProjectNameStageStageNameServiceServiceNameResourceParams, principal *models.Principal) middleware.Responder {
-	serviceFoundErr := checkServiceAvailability(params.ProjectName, params.StageName, params.ServiceName)
-	if serviceFoundErr >= 0 {
-		return service_resource.NewPostProjectProjectNameStageStageNameServiceServiceNameResourceDefault(400).WithPayload(getNotFoundErrorMessage(serviceFoundErr, params.ProjectName, params.StageName, params.ServiceName))
-	}
 	resourceHandler := keptnutils.NewResourceHandler("configuration-service:8080")
-
 	resourcesToUpload := []*keptnutils.Resource{}
 	for _, resource := range params.Resources.Resources {
+		decodedStr, err := b64.StdEncoding.DecodeString(*resource.ResourceContent)
+		if err != nil {
+			return service_resource.NewPostProjectProjectNameStageStageNameServiceServiceNameResourceDefault(500).WithPayload(&models.Error{Code: 500, Message: swag.String("Could not base64-decode content of resource " + resource.ResourceURI)})
+		}
 		resourcesToUpload = append(resourcesToUpload, &keptnutils.Resource{
-			ResourceContent: *resource.ResourceContent,
+			ResourceContent: string(decodedStr),
 			ResourceURI:     *resource.ResourceURI,
 		})
 	}
@@ -71,61 +70,4 @@ func PutProjectProjectNameStageStageNameServiceServiceNameResourceHandlerFunc(pa
 	return service_resource.NewPutProjectProjectNameStageStageNameServiceServiceNameResourceCreated().WithPayload(&models.Version{
 		Version: newVersion,
 	})
-}
-
-func getNotFoundErrorMessage(notFoundErrorcode notFoundError, project string, stage string, service string) *models.Error {
-	errorMsg := &models.Error{
-		Code: 400,
-	}
-	switch notFoundErrorcode {
-	case projectNotFound:
-		errorMsg.Message = swag.String("Project " + project + " does not exist.")
-		break
-	case stageNotFound:
-		errorMsg.Message = swag.String("Stage " + stage + " does not exist within project " + project)
-		break
-	case serviceNotFound:
-		errorMsg.Message = swag.String("Service " + service + " does not exist within stage " + stage + " of project " + project)
-		break
-	default:
-		errorMsg.Message = swag.String("Unknown error")
-		break
-	}
-
-	return errorMsg
-}
-
-func checkServiceAvailability(project string, stage string, service string) notFoundError {
-	// check if project exists
-	status, err := doGet("http://configuration-service:8080/v1/project/" + project)
-
-	if err != nil || status == 404 {
-		return projectNotFound
-	}
-	status, err = doGet("http://configuration-service:8080/v1/project/" + project + "/stage/" + stage)
-	if err != nil || status == 404 {
-		return stageNotFound
-	}
-	status, err = doGet("http://configuration-service:8080/v1/project/" + project + "/stage/" + stage + "/service/" + service)
-	if err != nil || status == 404 {
-		return serviceNotFound
-	}
-	return -1
-}
-
-func doGet(uri string) (int, error) {
-	req, err := http.NewRequest("GET", uri, nil)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	if err != nil {
-		return 0, err
-	}
-	return resp.StatusCode, nil
 }
