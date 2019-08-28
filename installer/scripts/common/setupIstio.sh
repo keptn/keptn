0b82856b4f840ed9a8c92c06d22a6e8119127985
Florian Bacher <florian.bacher@dynatrace.com>
2019-08-28 10:22:47 +0200

    #718 install istio 1.2.5 with sidecar injector

--- installer/scripts/common/setupIstio.sh
+++ installer/scripts/common/setupIstio.sh
@@ -3,21 +3,10 @@ source ./common/utils.sh
 
 kubectl create namespace istio-system
 
-# Apply custom resource definitions for Istio
-kubectl apply -f ../manifests/istio/crd-10.yaml
-verify_kubectl $? "Creating istio custom resource definitions failed."
-kubectl apply -f ../manifests/istio/crd-11.yaml
-verify_kubectl $? "Creating istio custom resource definitions failed."
-kubectl apply -f ../manifests/istio/crd-12.yaml
-verify_kubectl $? "Creating istio custom resource definitions failed."
-kubectl apply -f ../manifests/istio/crd-certmanager-10.yaml
-verify_kubectl $? "Creating istio custom resource definitions failed."
-kubectl apply -f ../manifests/istio/crd-certmanager-11.yaml
-verify_kubectl $? "Creating istio custom resource definitions failed."
-wait_for_crds "virtualservices,destinationrules,serviceentries,gateways,envoyfilters,policies,meshpolicies,httpapispecbindings,httpapispecs,quotaspecbindings,quotaspecs,rules,attributemanifests"
-
-kubectl apply -f ../manifests/istio/istio-lean.yaml
+helm template ../manifests/istio --name istio --namespace istio-system --values ../manifests/istio/values.yaml | kubectl apply -f -
+verify_kubectl $? "Installing Istio failed."
 wait_for_deployment_in_namespace "istio-ingressgateway" "istio-system"
 wait_for_deployment_in_namespace "istio-pilot" "istio-system"
-verify_kubectl $? "Creating all istio components failed."
+wait_for_deployment_in_namespace "istio-citadel" "istio-system"
+wait_for_deployment_in_namespace "istio-sidecar-injector" "istio-system"
 wait_for_all_pods_in_namespace "istio-system"
