b0f2a99de54c9f4d27a18c0310f53307a1f97eaf
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-29 17:33:28 +0200

    #611 Enrich mesh interface

--- helm-service/controller/mesh/istio.go
+++ helm-service/controller/mesh/istio.go
@@ -1,6 +1,10 @@
 package mesh
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/keptn/keptn/helm-service/pkg/apis/networking/istio/v1alpha3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
@@ -57,4 +61,46 @@ func (*IstioMesh) GenerateVirtualService(name string, gateways []string, hosts [
 	vs := v1alpha3.VirtualService{TypeMeta: metav1.TypeMeta{Kind: "VirtualService", APIVersion: "networking.istio.io/v1alpha3"},
 		ObjectMeta: metav1.ObjectMeta{Name: name}, Spec: spec}
 	return yaml.Marshal(vs)
+}
+
+// UpdateWeights returns a virtual service with updated weights
+func (*IstioMesh) UpdateWeights(virtualService []byte, canaryWeight int32) ([]byte, error) {
+
+	vs := v1alpha3.VirtualService{}
+	err := yaml.Unmarshal(virtualService, &vs)
+	if err != nil {
+		return nil, err
+	}
+
+	primaryWeight := int32(100 - canaryWeight)
+	if primaryWeight < 0 {
+		return nil, errors.New("Invalid canary weight")
+	}
+
+	for _, httpRoute := range vs.Spec.Http {
+		for _, dst := range httpRoute.Route {
+
+			if !strings.HasPrefix(dst.Destination.Host, vs.ObjectMeta.Name) {
+				return nil, fmt.Errorf("Cannot update VirutalService because host has unexpected name %s", dst.Destination.Host)
+			}
+			if strings.HasPrefix(dst.Destination.Host, vs.ObjectMeta.Name+"-canary") {
+				dst.Weight = canaryWeight
+			}
+			if strings.HasPrefix(dst.Destination.Host, vs.ObjectMeta.Name+"-primary") {
+				dst.Weight = primaryWeight
+			}
+		}
+	}
+
+	return yaml.Marshal(vs)
+}
+
+// GetDestinationRuleSuffix returns the file name suffix of destination rules
+func (*IstioMesh) GetDestinationRuleSuffix() string {
+	return "-istio-destinationrule.yaml"
+}
+
+// GetVirtualServiceSuffix returns the file name suffix of virtual services
+func (*IstioMesh) GetVirtualServiceSuffix() string {
+	return "-istio-virtualservice.yaml"
 }
