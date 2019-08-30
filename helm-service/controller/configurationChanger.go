f2cb20c447fb3b01b1664b56cf7cc79331d0e781
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-08-30 14:21:40 +0200

    #611 Initial versions of apply config, delete release

--- helm-service/controller/configurationChanger.go
+++ helm-service/controller/configurationChanger.go
@@ -2,23 +2,34 @@ package controller
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 
 	keptnevents "github.com/keptn/go-utils/pkg/events"
 	keptnutils "github.com/keptn/go-utils/pkg/utils"
 	"github.com/keptn/keptn/helm-service/controller/helm"
 	"github.com/keptn/keptn/helm-service/controller/mesh"
+	"k8s.io/helm/pkg/chartutil"
 	"k8s.io/helm/pkg/proto/hapi/chart"
 )
+
+func init() {
+	_, err := keptnutils.ExecuteCommand("helm", []string{"init", "--client-only"})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 // ChangeConfiguration
 func ChangeConfiguration(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mesh, logger *keptnutils.Logger, configServiceURL string) error {
 
 	if len(ce.ValuesPrimary) > 0 {
-		changeValues(ce, ce.Service+"-generated", ce.ValuesPrimary, logger, configServiceURL)
+		changeValues(ce, true, ce.ValuesPrimary, logger, configServiceURL)
 	}
 	if len(ce.ValuesCanary) > 0 {
-		changeValues(ce, ce.Service, ce.ValuesCanary, logger, configServiceURL)
+		changeValues(ce, false, ce.ValuesCanary, logger, configServiceURL)
 	}
 
 	// Change canary
@@ -29,13 +40,13 @@ func ChangeConfiguration(ce *keptnevents.ConfigurationChangeEventData, mesh mesh
 	return nil
 }
 
-func changeValues(ce *keptnevents.ConfigurationChangeEventData, chartName string, newValues map[string]*chart.Value,
+func changeValues(ce *keptnevents.ConfigurationChangeEventData, generated bool, newValues map[string]*chart.Value,
 	logger *keptnutils.Logger, configServiceURL string) error {
 
 	// Read chart
-	chart, err := helm.GetChart(ce.Project, ce.Service, ce.Stage, chartName, configServiceURL)
+	chart, err := helm.GetChart(ce.Project, ce.Service, ce.Stage, helm.GetChartName(ce.Service, generated), configServiceURL)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error when reading chart %s from project %s: %s", chartName, ce.Project, err.Error()))
+		logger.Error(fmt.Sprintf("Error when reading chart %s from project %s: %s", helm.GetChartName(ce.Service, generated), ce.Project, err.Error()))
 		return err
 	}
 	// Change values
@@ -46,26 +57,60 @@ func changeValues(ce *keptnevents.ConfigurationChangeEventData, chartName string
 	// Store chart
 	chartData, err := helm.PackageChart(chart)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error when packaging modified chart %s from project %s: %s", chartName, ce.Project, err.Error()))
+		logger.Error(fmt.Sprintf("Error when packaging modified chart %s from project %s: %s", helm.GetChartName(ce.Service, generated), ce.Project, err.Error()))
 		return err
 	}
-	err = helm.StoreChart(ce.Project, ce.Service, ce.Stage, chartName, chartData, configServiceURL)
+	err = helm.StoreChart(ce.Project, ce.Service, ce.Stage, helm.GetChartName(ce.Service, generated), chartData, configServiceURL)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error when storing modified chart %s from project %s: %s", chartName, ce.Project, err.Error()))
+		logger.Error(fmt.Sprintf("Error when storing modified chart %s from project %s: %s", helm.GetChartName(ce.Service, generated), ce.Project, err.Error()))
 		return err
 	}
-	return nil
+	return ApplyConfiguration(ce, generated, logger, configServiceURL)
 }
 
 func changeCanary(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mesh, logger *keptnutils.Logger, configServiceURL string) error {
 
 	switch ce.Canary.Action {
 	case keptnevents.Discard:
-		return setCanaryWeight(ce, mesh, logger, configServiceURL, 0)
+		if err := setCanaryWeight(ce, mesh, logger, configServiceURL, 0); err != nil {
+			return err
+		}
+		if err := ApplyConfiguration(ce, true, logger, configServiceURL); err != nil {
+			return err
+		}
+		if err := DeleteRelease(ce, false, logger); err != nil {
+			return err
+		}
+
 	case keptnevents.Promote:
-		return copyCanaryToPrimary(ce, mesh, logger, configServiceURL)
+		if err := setCanaryWeight(ce, mesh, logger, configServiceURL, 100); err != nil {
+			return err
+		}
+		if err := ApplyConfiguration(ce, true, logger, configServiceURL); err != nil {
+			return err
+		}
+		if err := copyCanaryToPrimary(ce, mesh, logger, configServiceURL); err != nil {
+			return err
+		}
+		if err := ApplyConfiguration(ce, true, logger, configServiceURL); err != nil {
+			return err
+		}
+		if err := setCanaryWeight(ce, mesh, logger, configServiceURL, 0); err != nil {
+			return err
+		}
+		if err := ApplyConfiguration(ce, true, logger, configServiceURL); err != nil {
+			return err
+		}
+
 	case keptnevents.Set:
-		return setCanaryWeight(ce, mesh, logger, configServiceURL, ce.Canary.Value)
+		err := setCanaryWeight(ce, mesh, logger, configServiceURL, ce.Canary.Value)
+		if err != nil {
+			return err
+		}
+		err = ApplyConfiguration(ce, true, logger, configServiceURL)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -73,18 +118,18 @@ func changeCanary(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mesh, 
 
 func copyCanaryToPrimary(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mesh, logger *keptnutils.Logger, configServiceURL string) error {
 
-	// Set 100% to primary
+	// TODO: Implement this function
+	// TODO: In generatedChartHander adapt service
 	return nil
 }
 
 func setCanaryWeight(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mesh, logger *keptnutils.Logger, configServiceURL string,
 	canaryWeight int32) error {
 
-	chartName := ce.Service + "-generated"
 	// Read chart
-	chart, err := helm.GetChart(ce.Project, ce.Service, ce.Stage, chartName, configServiceURL)
+	chart, err := helm.GetChart(ce.Project, ce.Service, ce.Stage, helm.GetChartName(ce.Service, true), configServiceURL)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error when reading chart %s from project %s: %s", chartName, ce.Project, err.Error()))
+		logger.Error(fmt.Sprintf("Error when reading chart %s from project %s: %s", helm.GetChartName(ce.Service, true), ce.Project, err.Error()))
 		return err
 	}
 
@@ -96,7 +141,7 @@ func setCanaryWeight(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mes
 			vs, err := mesh.UpdateWeights(template.Data, canaryWeight)
 			if err != nil {
 				logger.Error(fmt.Sprintf("Error when setting new weights in VirtualService %s from chart %s and project %s: %s",
-					template.Name, chartName, ce.Project, err.Error()))
+					template.Name, helm.GetChartName(ce.Service, true), ce.Project, err.Error()))
 				return err
 			}
 			template.Data = vs
@@ -106,13 +151,68 @@ func setCanaryWeight(ce *keptnevents.ConfigurationChangeEventData, mesh mesh.Mes
 	// Store chart
 	chartData, err := helm.PackageChart(chart)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error when packaging modified chart %s from project %s: %s", chartName, ce.Project, err.Error()))
+		logger.Error(fmt.Sprintf("Error when packaging modified chart %s from project %s: %s", helm.GetChartName(ce.Service, true), ce.Project, err.Error()))
 		return err
 	}
-	err = helm.StoreChart(ce.Project, ce.Service, ce.Stage, chartName, chartData, configServiceURL)
+	err = helm.StoreChart(ce.Project, ce.Service, ce.Stage, helm.GetChartName(ce.Service, true), chartData, configServiceURL)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error when storing modified chart %s from project %s: %s", chartName, ce.Project, err.Error()))
+		logger.Error(fmt.Sprintf("Error when storing modified chart %s from project %s: %s", helm.GetChartName(ce.Service, true), ce.Project, err.Error()))
 		return err
 	}
+	return nil
+}
+
+// DeleteRelease deletes a helm reease
+func DeleteRelease(ce *keptnevents.ConfigurationChangeEventData, generated bool, logger *keptnutils.Logger) error {
+	releaseName := helm.GetReleaseName(ce.Project, ce.Service, ce.Stage, generated)
+	if _, err := keptnutils.ExecuteCommand("helm", []string{"delete", "--purge", releaseName}); err != nil {
+		logger.Error(fmt.Sprintf("Error when deleting release %s: %s", releaseName, err.Error()))
+		return err
+	}
+	return nil
+}
+
+// ApplyConfiguration applies the chart of the provided service.
+// Furthermore, this function waits until all deployments in the namespace are ready.
+func ApplyConfiguration(ce *keptnevents.ConfigurationChangeEventData, generated bool, logger *keptnutils.Logger, configServiceURL string) error {
+
+	ch, err := helm.GetChart(ce.Project, ce.Service, ce.Stage, helm.GetChartName(ce.Service, generated), configServiceURL)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error when reading chart %s: %s", helm.GetChartName(ce.Service, generated), err.Error()))
+		return err
+	}
+
+	helmChartDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error when creating temporary directory: %s", err.Error()))
+		return err
+	}
+	defer os.RemoveAll(helmChartDir)
+
+	chartPath, err := chartutil.Save(ch, helmChartDir)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error when saving chart into temporary directory %s: %s", helmChartDir, err.Error()))
+		return err
+	}
+
+	releaseName := helm.GetReleaseName(ce.Project, ce.Service, ce.Stage, generated)
+	namespace := helm.GetNamespace(ce.Project, ce.Stage, generated)
+	if _, err := keptnutils.ExecuteCommand("helm", []string{"upgrade", "--install", releaseName,
+		chartPath, "--namespace", namespace, "--wait"}); err != nil {
+		logger.Error(fmt.Sprintf("Error when upgrading chart %s in namespace %s: %s",
+			releaseName, namespace, err.Error()))
+		return err
+	}
+
+	useInClusterConfig := false
+	if os.Getenv("env") == "production" {
+		useInClusterConfig = true
+	}
+
+	if err := keptnutils.WaitForDeploymentsInNamespace(useInClusterConfig, namespace); err != nil {
+		logger.Error(fmt.Sprintf("Error when waiting for deployments in namespace %s: %s", namespace, err.Error()))
+		return err
+	}
+	logger.Info(fmt.Sprintf("Finished upgrading chart %s in namespace %s", releaseName, namespace))
 	return nil
 }
