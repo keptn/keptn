aa2e9b908cce8fa3b236c77a35e7f47a3270733e
agrimmer <andreas.grimmer@dynatrace.com>
2019-09-09 09:13:59 +0200

    #611 Add comments

--- helm-service/controller/helm/canary_level_generator.go
+++ helm-service/controller/helm/canary_level_generator.go
@@ -4,6 +4,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// CanaryLevelGenerator is a collection of functions which are specific for the level of the canary
 type CanaryLevelGenerator interface {
 	GetCanaryService(originalSvc corev1.Service, project string, stageName string) (canaryService *corev1.Service)
 	IsK8sResourceDuplicated() bool
