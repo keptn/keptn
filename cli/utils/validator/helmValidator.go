4f0b5e0c8b9e074e7f3f72dfb04a3e8b1ec53522
agrimmer <andreas.grimmer@dynatrace.com>
2019-09-17 13:02:15 +0200

    #672 Improve Helm chart checks

--- cli/utils/validator/helmValidator.go
+++ cli/utils/validator/helmValidator.go
@@ -1,13 +1,19 @@
 package validator
 
 import (
+	"fmt"
+	"strings"
+
 	"k8s.io/helm/pkg/proto/hapi/chart"
 
 	"github.com/ghodss/yaml"
 	keptnutils "github.com/keptn/go-utils/pkg/utils"
+	"github.com/keptn/keptn/cli/pkg/logging"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 )
+
+var reservedFileNameSuffixes = [...]string{"-istio-destinationrule.yaml", "-istio-virtualservice.yaml"}
 
 // ValidateHelmChart validates keptn's requirements regarding
 // the values, deployment, and service file
@@ -29,18 +35,38 @@ func ValidateHelmChart(helmChart []byte) (bool, error) {
 	return resValues && resServiecs && resDeployment, nil
 }
 
-func validateValues(ch *chart.Chart) bool {
+func validateTemplateFileNames(ch *chart.Chart) bool {
+	for _, t := range ch.Templates {
+		for _, s := range reservedFileNameSuffixes {
+			if strings.HasSuffix(t.Name, s) {
+				logging.PrintLog(fmt.Sprintf("File %s has a reserved file name suffix", t.Name), logging.QuietLevel)
+				return false
+			}
+		}
+	}
+	return true
+}
 
+func validateValues(ch *chart.Chart) bool {
 	values := make(map[string]interface{})
 	yaml.Unmarshal([]byte(ch.Values.Raw), &values)
 	_, containsImage := values["image"]
-	_, containReplicaCount := values["replicas"]
-	return containsImage && containReplicaCount
+	if !containsImage {
+		logging.PrintLog("Provided Helm chart does not contain \"image\" in values.yaml", logging.QuietLevel)
+		return false
+	}
+	_, containsReplicaCount := values["replicas"]
+	if !containsReplicaCount {
+		logging.PrintLog("Provided Helm chart does not contain \"replicas\" in values.yaml", logging.QuietLevel)
+		return false
+	}
+	return true
 }
 
 func validateServices(ch *chart.Chart) (bool, error) {
 	services, err := keptnutils.GetRenderedServices(ch)
 	if err != nil {
+		logging.PrintLog("Error rendering Helm chart", logging.QuietLevel)
 		return false, err
 	}
 	for _, svc := range services {
@@ -48,12 +74,17 @@ func validateServices(ch *chart.Chart) (bool, error) {
 			return false, nil
 		}
 	}
-	return len(services) > 0, nil
+	if len(services) == 0 {
+		logging.PrintLog("Helm chart must contain at lease one service", logging.QuietLevel)
+		return false, nil
+	}
+	return true, nil
 }
 
 func validateDeployments(ch *chart.Chart) (bool, error) {
 	deployments, err := keptnutils.GetRenderedDeployments(ch)
 	if err != nil {
+		logging.PrintLog("Error rendering Helm chart", logging.QuietLevel)
 		return false, err
 	}
 	for _, depl := range deployments {
@@ -61,25 +92,56 @@ func validateDeployments(ch *chart.Chart) (bool, error) {
 			return false, nil
 		}
 	}
-	return len(deployments) == 1, nil
+	if len(deployments) != 1 {
+		logging.PrintLog("Helm chart must contain a single deployment", logging.QuietLevel)
+		return false, nil
+	}
+	return true, nil
 }
 
 func validateService(svc *corev1.Service) bool {
-
+	if !keptnutils.IsService(svc) {
+		logging.PrintLog(fmt.Sprintf("Service %s does not have kind \"service\"", svc.Name), logging.QuietLevel)
+		return false
+	}
 	if svc.Spec.Selector == nil {
+		logging.PrintLog(fmt.Sprintf("Service %s does not contain \"selector\"", svc.Name), logging.QuietLevel)
 		return false
 	}
 	val, ok := svc.Spec.Selector["app"]
-	return keptnutils.IsService(svc) && ok && val != ""
+	if !ok || val == "" {
+		logging.PrintLog(fmt.Sprintf("Service %s does not have \"spec.selector.app\"", svc.Name), logging.QuietLevel)
+		return false
+	}
+	return true
 }
 
 func validateDeployment(depl *appsv1.Deployment) bool {
-
-	if depl.Spec.Selector == nil || depl.Spec.Selector.MatchLabels == nil ||
-		depl.Spec.Template.ObjectMeta.Labels == nil {
+	if !keptnutils.IsDeployment(depl) {
+		logging.PrintLog(fmt.Sprintf("Deployment %s does not have kind \"deployment\"", depl.Name), logging.QuietLevel)
 		return false
 	}
-	mLabel, ok1 := depl.Spec.Selector.MatchLabels["app"]
-	podLabel, ok2 := depl.Spec.Template.ObjectMeta.Labels["app"]
-	return keptnutils.IsDeployment(depl) && ok1 && ok2 && mLabel != "" && podLabel != ""
+	if depl.Spec.Selector == nil {
+		logging.PrintLog(fmt.Sprintf("Deployment %s does not contain \"selector\"", depl.Name), logging.QuietLevel)
+		return false
+	}
+	if depl.Spec.Selector.MatchLabels == nil {
+		logging.PrintLog(fmt.Sprintf("Deployment %s does not contain \"selector.matchLabels\"", depl.Name), logging.QuietLevel)
+		return false
+	}
+	if depl.Spec.Template.ObjectMeta.Labels == nil {
+		logging.PrintLog(fmt.Sprintf("Deployment %s does not contain \"spec.template.metadata.labels\"", depl.Name), logging.QuietLevel)
+		return false
+	}
+	mLabel, okmLabel := depl.Spec.Selector.MatchLabels["app"]
+	if !okmLabel || mLabel == "" {
+		logging.PrintLog(fmt.Sprintf("Deployment %s does not contain \"spec.selector.matchLabels.app\"", depl.Name), logging.QuietLevel)
+		return false
+	}
+	podLabel, okPodLabel := depl.Spec.Template.ObjectMeta.Labels["app"]
+	if !okPodLabel || podLabel == "" {
+		logging.PrintLog(fmt.Sprintf("Deployment %s does not contain \"spec.template.metadata.labels.app\"", depl.Name), logging.QuietLevel)
+		return false
+	}
+	return true
 }
