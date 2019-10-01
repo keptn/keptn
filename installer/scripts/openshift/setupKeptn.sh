89cfdeab7dfe3cf496c7af4af97809e90cb2bbe4
Florian Bacher <florian.bacher@dynatrace.com>
2019-10-01 08:25:37 +0200

    #715 inject istio into jmeter-service

--- installer/scripts/openshift/setupKeptn.sh
+++ installer/scripts/openshift/setupKeptn.sh
@@ -130,3 +130,6 @@ verify_kubectl $? "Deploying keptn api virtualservice failed."
 
 helm init
 oc adm policy  add-cluster-role-to-user cluster-admin system:serviceaccount:kube-system:default
+
+# Inject istio into jmeter-service
+kubectl get deployment -n keptn jmeter-service -oyaml | istioctl kube-inject -f - | kubectl apply -f -
