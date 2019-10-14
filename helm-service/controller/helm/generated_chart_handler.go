fab68df46fd23840b153e51878eebd6c77d9925e
Florian Bacher <florian.bacher@dynatrace.com>
2019-10-14 08:51:31 +0200

    #715 removed obsolete code

--- helm-service/controller/helm/generated_chart_handler.go
+++ helm-service/controller/helm/generated_chart_handler.go
@@ -180,15 +180,6 @@ func (c *GeneratedChartHandler) generateServices(svc *corev1.Service, project st
 
 	templates := make([]*chart.Template, 0, 0)
 
-	if createOriginalService {
-		resetService(svc)
-		data, err := yaml.Marshal(svc)
-		if err != nil {
-			return nil, err
-		}
-		templates = append(templates, &chart.Template{Name: "templates/" + svc.Name + "-service" + ".yaml", Data: data})
-	}
-
 	serviceCanary := c.canaryLevelGen.GetCanaryService(*svc, project, stageName)
 	resetService(serviceCanary)
 	data, err := yaml.Marshal(serviceCanary)
