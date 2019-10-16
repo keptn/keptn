7be6209264606aa6b2c4f28d592eea3f4b81d18a
Johannes <johannes.braeuer@dynatrace.com>
2019-10-16 15:15:30 +0200

    #949 Tests for get event evaluation-done

---
+++ cli/cmd/get_event_evaluationDone_test.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/keptn/keptn/cli/pkg/logging"
+)
+
+func init() {
+	logging.InitLoggers(os.Stdout, os.Stdout, os.Stderr)
+}
+
+// TestEvaluationDone tests the evaluation-done command.
+func TestEvaluationDone(t *testing.T) {
+
+//	credentialmanager.MockAuthCreds = true
+	buf := new(bytes.Buffer)
+	rootCmd.SetOutput(buf)
+
+	args := []string{
+		"get",
+		"event",
+		"evaluation-done",
+		fmt.Sprintf("--keptn-context=%s", "??"),
+//		"--mock",
+	}
+	rootCmd.SetArgs(args)
+	err := rootCmd.Execute()
+
+	if err != nil {
+		t.Errorf("An error occured %v", err)
+	}
+}
