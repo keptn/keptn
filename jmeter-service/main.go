6b74982c702681d3f4874a9ae7a420213a0e9d81
Florian Bacher <florian.bacher@dynatrace.com>
2019-11-12 12:53:44 +0100

    do not execute jmeter-tests if test strategy is set to 'real-user'

--- jmeter-service/main.go
+++ jmeter-service/main.go
@@ -58,6 +58,9 @@ func gotEvent(ctx context.Context, event cloudevents.Event) error {
 		return errors.New(errorMsg)
 	}
 
+	if data.TestStrategy == "real-user" {
+		logger.Info("Received 'real-user' test strategy, hence no tests are triggered")
+	}
 	go runTests(event, shkeptncontext, *data, logger)
 
 	return nil
