8f4fb43571e1f53440e0739a0ad06578f861fed8
Florian Bacher <florian.bacher@dynatrace.com>
2019-11-27 15:08:21 +0100

    #1121 fixes for using nginx with nodeport

--- installer/scripts/installIngressForApi.sh
+++ installer/scripts/installIngressForApi.sh
@@ -6,12 +6,13 @@ verify_install_step $? "Installing Keptn api-ingress failed."
 
 
 wait_for_k8s_ingress "hostname"
+
 export DOMAIN=$(kubectl get ingress api-ingress -n keptn -o json | jq -r .status.loadBalancer.ingress[0].hostname)
 if [[ $? != 0 ]]; then
     print_error "Failed to get k8s ingress gateway information." && exit 1
 fi
 
-if [[ "$DOMAIN" == "null" ]]; then
+if [[ "$DOMAIN" == "null" && "$GATEWAY_TYPE" == "LoadBalancer" ]]; then
     print_info "Could not get ingress gateway domain name. Trying to retrieve IP address instead."
 
     wait_for_k8s_ingress "ip"
@@ -22,18 +23,23 @@ if [[ "$DOMAIN" == "null" ]]; then
         exit 1
     fi
     export DOMAIN="$DOMAIN.xip.io"
+elif [[ "$DOMAIN" == "null" && "$GATEWAY_TYPE" == "NodePort" ]]; then
+    NODE_PORT=$(kubectl -n ingress-nginx get service ingress-nginx -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+    NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[*].status.addresses[?(@.type=="InternalIP")].address }')
+    export DOMAIN="$NODE_IP.xip.io:$NODE_PORT"
+    export INGRESS_HOST="$NODE_IP.xip.io"
 fi
 
 echo $DOMAIN
 
-openssl req -nodes -newkey rsa:2048 -keyout key.pem -out certificate.pem  -x509 -days 365 -subj "/CN=$DOMAIN"
+openssl req -nodes -newkey rsa:2048 -keyout key.pem -out certificate.pem  -x509 -days 365 -subj "/CN=$INGRESS_HOST"
 kubectl create secret tls sslcerts --key key.pem --cert certificate.pem -n keptn
 rm key.pem
 rm certificate.pem  
 
-# Update ingress with updates hosts
+# Update ingress with updated hosts
 cat ../manifests/keptn/api-ingress.yaml | \
-    sed 's~domain.placeholder~'"$DOMAIN"'~' > ../manifests/keptn/gen/api-ingress.yaml
+    sed 's~domain.placeholder~'"$INGRESS_HOST"'~' > ../manifests/keptn/gen/api-ingress.yaml
 
 kubectl apply -f ../manifests/keptn/gen/api-ingress.yaml
 verify_kubectl $? "Deploying Keptn ingress failed."
