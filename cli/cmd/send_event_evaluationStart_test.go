834f9a1f3cd8e0ce6253a2ef0cf6b60aa7ecd24e
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2019-11-27 09:09:35 +0100

    #1131 #1138 Start-Evalation Event: Added end date, refactored getStartEndTime function

--- cli/cmd/send_event_evaluationStart_test.go
+++ cli/cmd/send_event_evaluationStart_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 
@@ -86,4 +87,85 @@ func TestEvaluationStartTimeSpecified(t *testing.T) {
 	if err != nil {
 		t.Errorf("An error occured: %v", err)
 	}
+}
+
+
+func TestEvaluationStartAndEndTimeSpecified(t *testing.T) {
+
+	credentialmanager.MockAuthCreds = true
+	buf := new(bytes.Buffer)
+	rootCmd.SetOutput(buf)
+
+	args := []string{
+		"send",
+		"event",
+		"start-evaluation",
+		fmt.Sprintf("--project=%s", "sockshop"),
+		fmt.Sprintf("--stage=%s", "hardening"),
+		fmt.Sprintf("--service=%s", "carts"),
+		fmt.Sprintf("--start=%s", "2019-07-24T10:17:12"),
+		fmt.Sprintf("--end=%s", "2019-07-24T10:20:12"),
+		"--mock",
+	}
+	rootCmd.SetArgs(args)
+	err := rootCmd.Execute()
+
+	if err != nil {
+		t.Errorf("An error occured: %v", err)
+	}
+}
+
+func TestEvaluationStartAndEndTimeAndTimeframeSpecified(t *testing.T) {
+
+	credentialmanager.MockAuthCreds = true
+	buf := new(bytes.Buffer)
+	rootCmd.SetOutput(buf)
+
+	args := []string{
+		"send",
+		"event",
+		"start-evaluation",
+		fmt.Sprintf("--project=%s", "sockshop"),
+		fmt.Sprintf("--stage=%s", "hardening"),
+		fmt.Sprintf("--service=%s", "carts"),
+		fmt.Sprintf("--start=%s", "2019-07-24T10:17:12"),
+		fmt.Sprintf("--end=%s", "2019-07-24T10:20:12"),
+		fmt.Sprintf("--timeframe=%s", "5m"),
+		"--mock",
+	}
+	rootCmd.SetArgs(args)
+	err := rootCmd.Execute()
+
+	if err == nil {
+		t.Error("An error occured: expect an error due to too many parameters (start, end and timeframe) used at the same time")
+	}
+
+	assert.EqualValues(t, "Start and end time of evaluation time frame not set: You can not use --end together with --timeframe", err.Error())
+}
+
+func TestEvaluationStartAndEndTimeWrongOrder(t *testing.T) {
+
+	credentialmanager.MockAuthCreds = true
+	buf := new(bytes.Buffer)
+	rootCmd.SetOutput(buf)
+
+	args := []string{
+		"send",
+		"event",
+		"start-evaluation",
+		fmt.Sprintf("--project=%s", "sockshop"),
+		fmt.Sprintf("--stage=%s", "hardening"),
+		fmt.Sprintf("--service=%s", "carts"),
+		fmt.Sprintf("--start=%s", "2019-07-24T10:17:12"),
+		fmt.Sprintf("--end=%s", "2019-07-24T10:10:12"),
+		"--mock",
+	}
+	rootCmd.SetArgs(args)
+	err := rootCmd.Execute()
+
+	if err == nil {
+		t.Error("An error occured: expect an error as end time is before start time")
+	}
+
+	assert.EqualValues(t, "Start and end time of evaluation time frame not set: end date must be at least 1 minute after start date", err.Error())
 }
