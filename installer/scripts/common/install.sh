2a30bd937fb7ebf2a3ecf922cb01abb7031f6fff
Johannes <johannes.braeuer@dynatrace.com>
2019-11-28 16:35:00 +0100

    #1130 Exclude uniform when installing quality-gates

--- installer/scripts/common/install.sh
+++ installer/scripts/common/install.sh
@@ -21,10 +21,14 @@ verify_install_step $? "Installing Keptn failed."
 print_info "Installing Keptn done"
 
 # Install Keptn services
-print_info "Wear uniform"
-./common/wearUniform.sh
-verify_install_step $? "Installing Keptn's uniform failed."
-print_info "Keptn wears uniform"
+if [[ "$USE_CASE" == "all" ]]; then
+  print_info "Wear uniform"
+  ./common/wearUniform.sh
+  verify_install_step $? "Installing Keptn's uniform failed."
+  print_info "Keptn wears uniform"
+else
+  print_debug "Wear uniform is skipped since use case ${USE_CASE} does not need it." 
+fi
 
 # Install done
 print_info "Installation of Keptn complete."
