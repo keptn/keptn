1e1111d98751fad5347b4cef48e565a9b73e9a0a
agrimmer <andreas.grimmer@dynatrace.com>
2019-12-11 08:52:31 +0100

    #1124 Remove helm template falg

--- installer/scripts/common/setupIstio.sh
+++ installer/scripts/common/setupIstio.sh
@@ -3,7 +3,7 @@ source ./common/utils.sh
 
 kubectl create namespace istio-system
 
-helm template ../manifests/istio/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f -
+helm template ../manifests/istio/helm/istio-init --namespace istio-system | kubectl apply -f -
 verify_kubectl $? "Creating Istio resources failed"
 wait_for_crds "adapters.config.istio.io,attributemanifests.config.istio.io,authorizationpolicies.rbac.istio.io,clusterrbacconfigs.rbac.istio.io,destinationrules.networking.istio.io,envoyfilters.networking.istio.io,gateways.networking.istio.io,handlers.config.istio.io,httpapispecbindings.config.istio.io,httpapispecs.config.istio.io,instances.config.istio.io,meshpolicies.authentication.istio.io,policies.authentication.istio.io,quotaspecbindings.config.istio.io,quotaspecs.config.istio.io,rbacconfigs.rbac.istio.io,rules.config.istio.io,serviceentries.networking.istio.io,servicerolebindings.rbac.istio.io,serviceroles.rbac.istio.io,sidecars.networking.istio.io,templates.config.istio.io,virtualservices.networking.istio.io"
 
