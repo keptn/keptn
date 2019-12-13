982d32d74931fbd6ba82b90139e98baccaea655e
Johannes <johannes.braeuer@dynatrace.com>
2019-12-13 09:16:12 +0100

    #1199 Reference feature image

--- installer/scripts/common/utils.sh
+++ installer/scripts/common/utils.sh
@@ -155,11 +155,11 @@ function wait_for_istio_ingressgateway() {
     fi
 
     if [[ "$DOMAIN" != "" ]]; then
-      print_debug "${PROPERTY} of Istio Ingressgateway is available."
+      print_debug "${PROPERTY} of Istio ingress gateway is available."
       break
     fi
     RETRY=$[$RETRY+1]
-    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 5s for ${PROPERTY} of Istio Ingressgateway to be available ..."
+    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 5s for ${PROPERTY} of Istio ingress gateway to be available ..."
     sleep 5
   done
 }
@@ -193,7 +193,7 @@ function wait_for_k8s_ingress() {
     fi
 
     RETRY=$[$RETRY+1]
-    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 5s for domain name or IP address of ingress gateway ..."
+    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 5s for domain name or IP address of ingress gateway to be available ..."
     sleep 5
 
   done
