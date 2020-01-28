86e44124c5172597572424dfab95186577d31bed
ermin.muratovic <ermin.muratovic@gmail.com>
2020-01-28 13:48:20 +0100

    show only evaluation results that failed expanded and collapse the rest display the evaluation criteria next to the result

---
+++ bridge/client/app/_components/ktb-event-item/ktb-event-item.component.ts
@@ -0,0 +1,101 @@
+import {ChangeDetectorRef, Component, Directive, Input, OnInit} from '@angular/core';
+import {Trace} from "../../_models/trace";
+
+@Directive({
+  selector: `ktb-event-item-detail, [ktb-event-item-detail], [ktbEventItemDetail]`,
+  exportAs: 'ktbEventItemDetail',
+})
+export class KtbEventItemDetail {}
+
+@Component({
+  selector: 'ktb-event-item',
+  templateUrl: './ktb-event-item.component.html',
+  styleUrls: ['./ktb-event-item.component.scss']
+})
+export class KtbEventItemComponent implements OnInit {
+
+  public _event: Trace;
+
+  @Input()
+  get event(): Trace {
+    return this._event;
+  }
+  set event(value: Trace) {
+    if (this._event !== value) {
+      this._event = value;
+      this._changeDetectorRef.markForCheck();
+    }
+  }
+
+  constructor(private _changeDetectorRef: ChangeDetectorRef) { }
+
+  ngOnInit() {
+  }
+
+  getEventLabel(key: string): string {
+    let label = key;
+    switch(key) {
+      case "sh.keptn.internal.event.service.create": {
+        label = "Service create"
+        break;
+      }
+      case "sh.keptn.event.configuration.change": {
+        label = "Configuration change"
+        break;
+      }
+      case "sh.keptn.event.monitoring.configure": {
+        label = "Configure monitoring"
+        break;
+      }
+      case "sh.keptn.events.deployment-finished": {
+        label = "Deployment finished";
+        break;
+      }
+      case "sh.keptn.events.tests-finished": {
+        label = "Tests finished";
+        break;
+      }
+      case "sh.keptn.events.evaluation-done": {
+        label = "Evaluation done";
+        break;
+      }
+      case "sh.keptn.internal.event.get-sli": {
+        label = "Start SLI retrieval";
+        break;
+      }
+      case "sh.keptn.internal.event.get-sli.done": {
+        label = "SLI retrieval done";
+        break;
+      }
+      case "sh.keptn.events.done": {
+        label = "Done";
+        break;
+      }
+      case "sh.keptn.events.done": {
+        label = "Done";
+        break;
+      }
+
+
+      case "sh.keptn.events.done": {
+        label = "Done";
+        break;
+      }
+      case "sh.keptn.event.problem.open": {
+        label = "Problem open";
+        break;
+      }
+      case "sh.keptn.events.problem": {
+        label = "Problem detected";
+        break;
+      }
+      default: {
+        //statements;
+        break;
+      }
+    }
+
+    return label;
+  }
+
+}
