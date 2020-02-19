55d367aecdf068a478939ca06e9d86f90d231ac3
agrimmer <andreas.grimmer@dynatrace.com>
2020-02-19 11:04:13 +0100

    #1190 Utility for fetching the current version

---
+++ cli/utils/version_checker/version_fetcher.go
@@ -0,0 +1,50 @@
+package version_checker
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const versionURL = "https://get.keptn.sh/version.json"
+
+type Client struct {
+	httpClient *http.Client
+	versionUrl string
+}
+
+func newClient() *Client {
+	client := Client{
+		httpClient: &http.Client{
+			Timeout: 5 * time.Second,
+		},
+		versionUrl: versionURL,
+	}
+	return &client
+}
+
+type VersionInfo struct {
+	CLIVersionInfo CLIVersionInfo `json:"cli"`
+}
+
+type CLIVersionInfo struct {
+	Stable string `json:"stable"`
+	Beta   string `json:"beta"`
+}
+
+func (client *Client) GetCLIVersionInfo() (*CLIVersionInfo, error) {
+
+	versionInfo := &VersionInfo{}
+	resp, err := client.httpClient.Get(client.versionUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, versionInfo)
+	return &versionInfo.CLIVersionInfo, err
+}
---
+++ cli/utils/version_checker/version_fetcher_test.go
@@ -0,0 +1,61 @@
+package version_checker
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+// testingHTTPClient builds a test client with a httptest server
+func testingHTTPClient(handler http.Handler) (*http.Client, string, func()) {
+	server := httptest.NewTLSServer(handler)
+
+	cert, err := x509.ParseCertificate(server.TLS.Certificates[0].Certificate[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	certpool := x509.NewCertPool()
+	certpool.AddCert(cert)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, network, _ string) (net.Conn, error) {
+				return net.Dial(network, server.Listener.Addr().String())
+			},
+			TLSClientConfig: &tls.Config{
+				RootCAs: certpool,
+			},
+		},
+	}
+
+	return client, server.URL, server.Close
+}
+
+func TestGetCLIVersionInfo(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expect GET request")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, `{ "cli": { "stable": "0.5.2", "beta": "0.6.0.beta2" } }`)
+	})
+
+	httpClient, url, teardown := testingHTTPClient(handler)
+	defer teardown()
+
+	client := newClient()
+	client.httpClient = httpClient
+	client.versionUrl = url
+
+	cliVersionInfo, err := client.GetCLIVersionInfo()
+	assert.Equal(t, err, nil, "Received unexpected error")
+	assert.Equal(t, cliVersionInfo.Beta, "0.6.0.beta2", "Received unexpected content")
+	assert.Equal(t, cliVersionInfo.Stable, "0.5.2", "Received unexpected content")
+}
