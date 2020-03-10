91a120ae3089b3c099d80194f4fb7925e2c178d4
Florian Bacher <florian.bacher@dynatrace.com>
2020-03-10 12:54:02 +0100

    #1423 added image for updating from 0.6.0 to 0.6.1

---
+++ upgrade-060-061/scripts/upgradeKeptn.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+source ./utils.sh
+
+print_debug "Upgrading from Keptn 0.6.0 to $KEPTN_VERSION"
+
+
+manifests=(
+  "https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/logging/mongodb/pvc.yaml"
+  "https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/logging/mongodb/deployment.yaml"
+  "https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/logging/mongodb/svc.yaml"
+  "https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/keptn/core.yaml"
+  "https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/keptn/quality-gates.yaml"
+  )
+
+for manifest in "${manifests[@]}"
+do
+   :
+   if curl --head --silent --fail $manifest 2> /dev/null;
+     then
+      continue
+     else
+      print_error "Required manifest not found. Aborting upgrade."
+      exit 1
+    fi
+done
+
+
+print_debug "Check if Keptn 0.6.0 is currently installed"
+API_IMAGE=$(kubectl get deployment -n keptn api -o=jsonpath='{$.spec.template.spec.containers[:1].image}')
+
+  if [[ $API_IMAGE != 'keptn/api:0.6.0' ]]; then
+    print_error "Installed Keptn version does not match 0.6.0. aborting."
+    exit 1
+  fi
+
+# export data from 0.6.0 mongodb
+print_debug "Exporting events from previous Keptn installation."
+mongoexport --uri="mongodb://user:password@mongodb.keptn-datastore.svc.cluster.local:27017/keptn"  --collection=events  --out=events.json
+verify_install_step $? "Mongodb export failed."
+
+# delete old deployment of mongodb
+print_debug "Updating MongoDB."
+kubectl delete deployment -n keptn-datastore mongodb
+# deploy 0.6.1 mongodb
+kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/logging/mongodb/pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/logging/mongodb/deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/logging/mongodb/svc.yaml
+wait_for_deployment_in_namespace "mongodb" "keptn-datastore"
+
+# import previous data to new mongodb
+print_debug "Importing events from previous installation to updated MongoDB."
+mongoimport --uri="mongodb://user:password@mongodb.keptn-datastore.svc.cluster.local:27017/keptn" --collection events --file events.json
+
+
+print_debug "Updating mongodb-datastore."
+kubectl -n keptn-datastore set image deployment/mongodb-datastore mongodb-datastore=keptn/mongodb-datastore:$KEPTN_VERSION --record
+kubectl -n keptn-datastore set image deployment/mongodb-datastore-distributor distributor=keptn/distributor:$KEPTN_VERSION --record
+
+
+print_debug "Updating Keptn core."
+kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/keptn/core.yaml
+kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/keptn/quality-gates.yaml
+
+# check if full installation is available
+kubectl -n keptn get svc gatekeeper-service
+
+  if [[ $? == '0' ]]; then
+      print_debug "Full installation detected. Upgrading CD and CO services"
+      kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/keptn/continuous-deployment.yaml
+      kubectl apply -f https://raw.githubusercontent.com/keptn/keptn/$KEPTN_VERSION/installer/manifests/keptn/continuous-operations.yaml
+  fi
+
+# check for keptn-contrib services
+kubectl -n keptn get svc dynatrace-service
+
+  if [[ $? == '0' ]]; then
+      print_debug "Dynatrace-service detected. Upgrading to 0.6.2"
+      kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/release-0.6.2/deploy/manifests/dynatrace-service/dynatrace-service.yaml
+  fi
+
+kubectl -n keptn get svc dynatrace-sli-service
+
+  if [[ $? == '0' ]]; then
+      print_debug "Dynatrace-sli-service detected. Upgrading to 0.3.1"
+      kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/release-0.3.1/deploy/service.yaml
+  fi
+
+kubectl -n keptn get svc prometheus-service
+
+  if [[ $? == '0' ]]; then
+      print_debug "Prometheus-service detected. Upgrading to 0.3.1"
+      kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/prometheus-service/release-0.3.1/deploy/service.yaml
+  fi
+
+kubectl -n keptn get svc prometheus-sli-service
+
+  if [[ $? == '0' ]]; then
+      print_debug "Prometheus-sli-service detected. Upgrading to 0.2.1"
+      kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/prometheus-sli-service/release-0.2.1/deploy/service.yaml
+  fi
+
+kubectl -n keptn get svc servicenow-service
+
+  if [[ $? == '0' ]]; then
+      print_debug "Servicenow-service detected. Upgrading to 0.2.0"
+      kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/servicenow-service/release-0.2.0/deploy/service.yaml
+  fi
+
+
---
+++ upgrade-060-061/scripts/utils.sh
@@ -0,0 +1,200 @@
+################################################################
+# This is shared library for the keptn installation            #
+################################################################
+
+function timestamp() {
+  date +"[%Y-%m-%d %H:%M:%S]"
+}
+
+function print_info() {
+  echo "[keptn|INFO] $(timestamp) $1"
+}
+
+function print_debug() {
+  echo "[keptn|DEBUG] $(timestamp) $1"
+}
+
+function print_error() {
+  echo "[keptn|ERROR] $(timestamp) $1"
+}
+
+function verify_install_step() {
+  if [[ $1 != '0' ]]; then
+    print_error "$2"
+    print_error "Stopping keptn installation. Already created resources are not deleted; run keptn uninstall to clean-up."
+    exit 1
+  fi
+}
+
+function verify_kubectl() {
+  if [[ $1 != '0' ]]; then
+    print_error "$2"
+    print_error "Stopping keptn installation. Already created resources are not deleted; run keptn uninstall to clean-up."
+    exit 1
+  fi
+}
+
+function verify_variable() {
+  if [[ -z "$1" ]]; then
+    print_error "$2"
+    print_error "Stopping keptn installation. Already created resources are not deleted; run keptn uninstall to clean-up."
+    exit 1
+  fi
+}
+
+# Waits for a deployment in a given namespace to be available.
+function wait_for_deployment_in_namespace() {
+  DEPLOYMENT=$1; NAMESPACE=$2;
+  RETRY=0; RETRY_MAX=24; 
+
+  while [[ $RETRY -lt $RETRY_MAX ]]; do
+    DEPLOYMENT_LIST=$(eval "kubectl get deployments -n ${NAMESPACE} | awk '/$DEPLOYMENT/'" | awk '{print $1}') # list of multiple deployments when starting with the same name
+    if [[ -z "$DEPLOYMENT_LIST" ]]; then
+      RETRY=$[$RETRY+1]
+      print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 10s for deployment ${DEPLOYMENT} in namespace ${NAMESPACE} ..."
+      sleep 10
+    else
+      break
+    fi
+  done
+
+  if [[ $RETRY == $RETRY_MAX ]]; then
+    print_error "Deployment ${DEPLOYMENT} in namespace ${NAMESPACE} is not available"
+    exit 1
+  fi
+
+  RETRY=0
+
+  verify_variable "$DEPLOYMENT_LIST" "List of deployments in namespace $NAMESPACE could not be derived."
+
+  array=(${DEPLOYMENT_LIST// / })
+
+  for DEPLOYED_DEPLOYMENT in "${array[@]}" 
+  do
+    while [[ $RETRY -lt $RETRY_MAX ]]; do
+      kubectl rollout status deployment $DEPLOYMENT -n $NAMESPACE
+
+      if [[ $? == '0' ]]
+      then
+        print_debug "Deployment ${DEPLOYED_DEPLOYMENT} in ${NAMESPACE} namespace available."
+        break
+      fi
+      RETRY=$[$RETRY+1]
+      print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 10s for deployment ${DEPLOYED_DEPLOYMENT} in namespace ${NAMESPACE} ..."
+      sleep 10
+    done
+
+    if [[ $RETRY == $RETRY_MAX ]]; then
+      print_error "Deployment ${DEPLOYED_DEPLOYMENT} in namespace ${NAMESPACE} is not available"
+      exit 1
+    fi
+  done
+}
+
+# Waits for all pods in a given namespace to be up and running.
+function wait_for_all_pods_in_namespace() {
+  NAMESPACE=$1;
+  RETRY=0; RETRY_MAX=24; 
+
+  CMD="kubectl get pods -n $NAMESPACE && [[ \$(kubectl get pods -n $NAMESPACE 2>&1 | grep -c -v -E '(Running|Completed|Terminating|STATUS)') -eq 0 ]]"
+  #CMD="[[ \$(kubectl get pods -n $NAMESPACE 2>&1 | grep -c -v -E '(Running|Completed|Terminating|STATUS)') -eq 0 ]]"
+
+  while [[ $RETRY -lt $RETRY_MAX ]]; do
+    eval $CMD
+
+    if [[ $? == '0' ]]; then
+      print_debug "All pods are running in namespace ${NAMESPACE}."
+      break
+    fi
+    RETRY=$[$RETRY+1]
+    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 10s for pods to start in namespace ${NAMESPACE} ..."
+    sleep 10
+  done
+
+  if [[ $RETRY == $RETRY_MAX ]]; then
+    print_error "Pods in namespace ${NAMESPACE} are not running."
+    # show the pods that have problems
+    kubectl get pods --field-selector=status.phase!=Running -n ${NAMESPACE}
+    exit 1
+  fi
+}
+
+# Waits for all custom resource defintions to be created successfully.
+function wait_for_crds() {
+  CRDS=$1; # list of custom resource definitions
+  RETRY=0; RETRY_MAX=24;
+
+  while [[ $RETRY -lt $RETRY_MAX ]]; do
+    kubectl get $CRDS
+
+    if [[ $? == '0' ]]; then
+      print_debug "All custom resource definitions are available."
+      break
+    fi
+    RETRY=$[$RETRY+1]
+    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 10s for custom resource definitions ..."
+    sleep 10
+  done
+
+  if [[ $RETRY == $RETRY_MAX ]]; then
+    print_error "Custom resource definitions are missing."
+    exit 1
+  fi
+}
+
+# Waits for ip of Istio ingress gateway (max wait time 20sec)
+function wait_for_istio_ingressgateway() {
+  PROPERTY=$1;
+  RETRY=0; RETRY_MAX=4;
+  DOMAIN="";
+
+  while [[ $RETRY -lt $RETRY_MAX ]]; do
+    DOMAIN=$(kubectl get svc istio-ingressgateway -o json -n istio-system | jq -r .status.loadBalancer.ingress[0].${PROPERTY})
+    if [[ $DOMAIN = "null" ]]; then
+      DOMAIN=""
+    fi
+
+    if [[ "$DOMAIN" != "" ]]; then
+      print_debug "${PROPERTY} of Istio ingress gateway is available."
+      break
+    fi
+    RETRY=$[$RETRY+1]
+    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 5s for ${PROPERTY} of Istio ingress gateway to be available ..."
+    sleep 5
+  done
+}
+
+# Waits for hostname or ip of ingress gateway (max wait time 120sec)
+function wait_for_k8s_ingress() {
+  RETRY=0; RETRY_MAX=24;
+  DOMAIN="";
+
+  while [[ $RETRY -lt $RETRY_MAX ]]; do
+    # Check availability of hostname
+    DOMAIN=$(kubectl get ingress api-ingress -n keptn -o json | jq -r .status.loadBalancer.ingress[0].hostname)
+    if [[ $DOMAIN = "null" ]]; then
+      DOMAIN=""
+    fi
+
+    if [[ "$DOMAIN" != "" ]]; then
+      print_debug "Domain name of ingress gateway is available."
+      break
+    fi
+
+    # Check availability of IP
+    DOMAIN=$(kubectl get ingress api-ingress -n keptn -o json | jq -r .status.loadBalancer.ingress[0].ip)
+    if [[ $DOMAIN = "null" ]]; then
+      DOMAIN=""
+    fi
+
+    if [[ "$DOMAIN" != "" ]]; then
+      print_debug "IP address of ingress gateway is available."
+      break
+    fi
+
+    RETRY=$[$RETRY+1]
+    print_debug "Retry: ${RETRY}/${RETRY_MAX} - Wait 5s for domain name or IP address of ingress gateway to be available ..."
+    sleep 5
+
+  done
+}
