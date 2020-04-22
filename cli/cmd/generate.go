36f686b59159874c7f8910bd6fc1c28a7f5d6146
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2020-04-22 16:17:27 +0200

    #1611 Update according to Pr review

--- cli/cmd/generate.go
+++ cli/cmd/generate.go
@@ -7,8 +7,6 @@ import (
 // deleteCmd represents the delete command
 var generateCmd = &cobra.Command{
 	Use:   "generate [docs]",
-	Short: `generate is the parent command of "generate docs"`,
-	Long:  `generate is the parent command of "generate docs". "generate" without subcommand cannot be used.`,
 }
 
 func init() {
