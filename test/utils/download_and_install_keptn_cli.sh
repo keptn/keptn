9a07814539820c97ef365e49fb02080e6ecb3a84
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2020-04-24 13:43:23 +0200

    Fix typo in install keptn cli

--- test/utils/download_and_install_keptn_cli.sh
+++ test/utils/download_and_install_keptn_cli.sh
@@ -2,7 +2,7 @@
 
 echo "Download the CLI"
 # Download latest KEPTN cli for linux
-wget https://storage.googleapis.com/keptn-cli/${KEPN_CLI_VERSION}/keptn-linux.zip
+wget "https://storage.googleapis.com/keptn-cli/${KEPTN_CLI_VERSION}/keptn-linux.zip"
 unzip keptn-linux.zip
 
 sudo mv keptn /usr/local/bin/keptn
