a47929ded2cc82a478b75aed55197f3952b52234
ermin.muratovic <ermin.muratovic@gmail.com>
2020-05-28 19:42:05 +0200

    concat baseUrl and reqUrl for api request

--- bridge/server/api/index.js
+++ bridge/server/api/index.js
@@ -59,6 +59,7 @@ module.exports = (params) => {
   router.all('*', async (req, res, next) => {
     try {
       let method = req.method;
+      let url = `${apiUrl}${req.url}`;
       let data = req.params;
       const result = await axios({ method, url, data });
       return res.json(result);
