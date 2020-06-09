28f33f869646512158492c1d3293d8e18e0f0b23
ermin.muratovic <ermin.muratovic@gmail.com>
2020-06-09 10:59:54 +0200

    test KtbSelectableTileComponent only with KtbSelectableTileComponent (not the complete AppModule)

--- bridge/client/app/_components/ktb-selectable-tile/ktb-selectable-tile.component.spec.ts
+++ bridge/client/app/_components/ktb-selectable-tile/ktb-selectable-tile.component.spec.ts
@@ -1,22 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { KtbSelectableTileComponent } from './ktb-selectable-tile.component';
+import {ComponentFixture, TestBed, fakeAsync} from '@angular/core/testing';
 import {By} from "@angular/platform-browser";
 import {Component} from "@angular/core";
+
 import {AppModule} from "../../app.module";
+import {KtbSelectableTileComponent} from './ktb-selectable-tile.component';
 
 describe('KtbSelectableTileComponent', () => {
   let component: SimpleKtbSelectableTileComponent;
   let fixture: ComponentFixture<SimpleKtbSelectableTileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
-        SimpleKtbSelectableTileComponent
+        SimpleKtbSelectableTileComponent,
+        KtbSelectableTileComponent,
       ],
-      imports: [
-        AppModule,
-      ],
+      imports: [],
     })
     .compileComponents();
   }));
