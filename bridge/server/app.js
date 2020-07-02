c550d066ddefd0c541931a0c1d286888bd884d44
ermin.muratovic <ermin.muratovic@gmail.com>
2020-07-02 11:14:09 +0200

    print simply err

--- bridge/server/app.js
+++ bridge/server/app.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500).send();
-  console.error("error", err);
+  console.error(err);
   // res.json(err);
 });
 
