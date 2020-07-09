462f264d164bf78f9dd3eb31cc888ecb714c68dc
agrimmer <andreas.grimmer@dynatrace.com>
2020-07-09 13:12:50 +0200

    #1960 Store helm binary in Home-dir

--- travis-scripts/install_helm.sh
+++ travis-scripts/install_helm.sh
@@ -3,4 +3,8 @@
   # download
 curl https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz > helm.tar.gz
 tar -zxvf helm.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
+if [ ! -d "$HOME/bin" ]; then
+  mkdir "$HOME/bin"
+fi
+mv linux-amd64/helm "$HOME/bin"
+export PATH=$PATH:"$HOME/bin"
