62256b3c26723bad41fa3df83a3a26412b114be5
Florian Bacher <florian.bacher@dynatrace.com>
2020-07-13 10:39:03 +0200

    #1965 updated expose bridge test

--- test/utils/expose_keptn_bridge.sh
+++ test/utils/expose_keptn_bridge.sh
@@ -3,12 +3,13 @@
 echo "Exposing Keptn Bridge..."
 # expose Keptn's Bridge (for easier troubleshooting/debugging afterwards)
 kubectl -n keptn create secret generic bridge-credentials --from-literal="BASIC_AUTH_USERNAME=${NIGHTLY_BRIDGE_USERNAME}" --from-literal="BASIC_AUTH_PASSWORD=${NIGHTLY_BRIDGE_PASSWORD}"
-keptn configure bridge --action=expose
 # restart bridge pod to make use of the secret
 kubectl -n keptn delete pods --selector=run=bridge
 sleep 5
+kubectl patch svc bridge -n keptn -p '{"spec": {"type": "LoadBalancer"}}'
+sleep 10
+export BRIDGE_URL=http://$(kubectl -n keptn get service bridge -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 # verify that bridge is available
-export BRIDGE_URL=$(echo https://bridge.keptn.$(kubectl get cm -n keptn keptn-domain -ojsonpath={.data.app_domain}))
 curl "${BRIDGE_URL}" -k
 
 if [[ $? != '0' ]]; then
