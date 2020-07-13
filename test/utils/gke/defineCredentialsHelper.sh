8d5917d9d276a658500c02aa93652da5c39d318a
Florian Bacher <florian.bacher@dynatrace.com>
2020-07-13 12:20:17 +0200

    #1965 fixed path to creds.sav

--- test/utils/gke/defineCredentialsHelper.sh
+++ test/utils/gke/defineCredentialsHelper.sh
@@ -4,7 +4,7 @@
 function replaceCreds {
     CREDS=creds.json
     rm $CREDS 2> /dev/null
-    cat ./gke/creds.sav | sed 's~CLUSTER_NAME_PLACEHOLDER~'"$CLN"'~' | \
+    cat ./creds.sav | sed 's~CLUSTER_NAME_PLACEHOLDER~'"$CLN"'~' | \
       sed 's~CLUSTER_ZONE_PLACEHOLDER~'"$CLZ"'~' | \
       sed 's~GKE_PROJECT_PLACEHOLDER~'"$PROJ"'~' >> $CREDS
 }
