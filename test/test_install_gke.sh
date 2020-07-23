65a50e966fc6292ad9804eef04c5826d3bf469ba
Florian Bacher <florian.bacher@dynatrace.com>
2020-07-23 14:43:43 +0200

    fix integration tests (#2111)
    * fix integration tests
    
    * fix authentication in integration tests
    
    * fix log output

--- test/test_install_gke.sh
+++ test/test_install_gke.sh
@@ -21,7 +21,7 @@ keptn install --chart-repo="${KEPTN_INSTALLER_REPO}" --creds=creds.json --verbos
 verify_test_step $? "keptn install failed"
 
 # authenticate at Keptn API
-KEPTN_API_URL=$(kubectl -n keptn get service api-gateway-nginx -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+KEPTN_API_URL=http://$(kubectl -n keptn get service api-gateway-nginx -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
 
 auth_at_keptn $KEPTN_API_URL $KEPTN_API_TOKEN
