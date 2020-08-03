3cefb71f47688c4e7ead9b00d233e75aec917bd6
Ermin Muratovic <ermin.muratovic@dynatrace.com>
2020-08-03 15:25:44 +0200

    use no wrapper function for sort by Date with DateUtil.compareTraceTimes
    Co-authored-by: Sandro Schmid <18055485+sandroschmid@users.noreply.github.com>

--- bridge/client/app/_services/mock-data.service.ts
+++ bridge/client/app/_services/mock-data.service.ts
@@ -89,7 +89,7 @@ export class MockDataService {
   }
 
   public loadRoots(project: Project, service: Service) {
-    service.roots = [...this.mockRoots[project.projectName+"."+service.serviceName]||[], ...service.roots||[]].sort((a, b) => DateUtil.compareTraceTimes(a, b));
+    service.roots = [...this.mockRoots[project.projectName+"."+service.serviceName]||[], ...service.roots||[]].sort(DateUtil.compareTraceTimes);
     this._roots.next(service.roots);
     this.mockRoots[project.projectName+"."+service.serviceName] = [];
   }
