74cf4f06e306963b83e21c7ec00308db86f5335c
Florian Bacher <florian.bacher@dynatrace.com>
2020-08-05 09:31:04 +0200

    #2158 added shipyard controller

--- shipyard-controller/handlers/event.go
+++ shipyard-controller/handlers/event.go
@@ -2,24 +2,58 @@ package handlers
 
 import (
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/swag"
+
+	"github.com/keptn/keptn/shipyard-controller/common"
 	"github.com/keptn/keptn/shipyard-controller/db"
 	"github.com/keptn/keptn/shipyard-controller/models"
 	"github.com/keptn/keptn/shipyard-controller/restapi/operations"
 )
 
 func GetTriggeredEvents(params operations.GetTriggeredEventsParams) middleware.Responder {
+	var payload = &models.Events{
+		PageSize:    0,
+		NextPageKey: "0",
+		TotalCount:  0,
+		Events:      []*models.Event{},
+	}
 	em := getEventManagerInstance()
 
 	var events []models.Event
 	var err error
 
-	if params.ProjectName != "" {
-		events, err = em.GetTriggeredEventsOfProject(params.ProjectName)
-	} else {
-		events, err = em.GetAllTriggeredEvents()
+	eventFilter := db.EventFilter{
+		Type:    params.EventType,
+		Stage:   params.StageName,
+		Service: params.ServiceName,
+		ID:      params.EventID,
 	}
 
-	return nil
+	if params.ProjectName != nil && *params.ProjectName != "" {
+		events, err = em.GetTriggeredEventsOfProject(*params.ProjectName, eventFilter)
+	} else {
+		events, err = em.GetAllTriggeredEvents(eventFilter)
+	}
+
+	if err != nil {
+		return operations.NewHandleEventDefault(500).WithPayload(&models.Error{
+			Code:    500,
+			Message: swag.String(err.Error()),
+		})
+	}
+
+	paginationInfo := common.Paginate(len(events), params.PageSize, params.NextPageKey)
+
+	totalCount := len(events)
+	if paginationInfo.NextPageKey < int64(totalCount) {
+		for _, event := range events[paginationInfo.NextPageKey:paginationInfo.EndIndex] {
+			payload.Events = append(payload.Events, &event)
+		}
+	}
+
+	payload.TotalCount = float64(totalCount)
+	payload.NextPageKey = paginationInfo.NewNextPageKey
+	return operations.NewGetTriggeredEventsOK().WithPayload(payload)
 }
 
 func HandleEvent(params operations.HandleEventParams) middleware.Responder {
@@ -43,7 +77,7 @@ func getEventManagerInstance() *eventManager {
 	return eventManagerInstance
 }
 
-func (em *eventManager) GetAllTriggeredEvents() ([]models.Event, error) {
+func (em *eventManager) GetAllTriggeredEvents(filter db.EventFilter) ([]models.Event, error) {
 	projects, err := em.projectRepo.GetProjects()
 
 	if err != nil {
@@ -52,7 +86,7 @@ func (em *eventManager) GetAllTriggeredEvents() ([]models.Event, error) {
 
 	allEvents := []models.Event{}
 	for _, project := range projects {
-		events, err := em.triggeredEventRepo.GetEvents(project)
+		events, err := em.triggeredEventRepo.GetEvents(project, filter)
 		if err == nil {
 			allEvents = append(allEvents, events...)
 		}
@@ -60,6 +94,6 @@ func (em *eventManager) GetAllTriggeredEvents() ([]models.Event, error) {
 	return allEvents, nil
 }
 
-func (em *eventManager) GetTriggeredEventsOfProject(project string) ([]models.Event, error) {
-	return em.triggeredEventRepo.GetEvents(project)
+func (em *eventManager) GetTriggeredEventsOfProject(project string, filter db.EventFilter) ([]models.Event, error) {
+	return em.triggeredEventRepo.GetEvents(project, filter)
 }
