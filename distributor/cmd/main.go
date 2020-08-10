be73fb8c5051b1274e3b4acd8c488c5cc182285a
Florian Bacher <florian.bacher@dynatrace.com>
2020-08-10 14:43:43 +0200

    #2166 event polling via http

--- distributor/cmd/main.go
+++ distributor/cmd/main.go
@@ -15,14 +15,21 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/url"
+
 	"github.com/keptn/keptn/distributor/pkg/lib"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	keptnmodels "github.com/keptn/go-utils/pkg/api/models"
 	keptnapi "github.com/keptn/go-utils/pkg/api/utils"
 
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
@@ -51,6 +58,8 @@ var close = make(chan bool)
 
 var mux sync.Mutex
 
+var sentCloudEvents map[string][]string
+
 func main() {
 	var env envConfig
 	if err := envconfig.Process("", &env); err != nil {
@@ -61,17 +70,237 @@ func main() {
 	os.Exit(_main(os.Args[1:], env))
 }
 
+const connectionTypeNATS = "nats"
+const connectionTypeHTTP = "http"
+
 func _main(args []string, env envConfig) int {
 	ctx = context.Background()
 	// initialize the http client
-	uptimeTicker = time.NewTicker(10 * time.Second)
-	createRecipientConnection()
+
+	connectionType := os.Getenv("CONNECTION_TYPE")
+
+	switch connectionType {
+	case "":
+		createNATSConnection()
+		break
+	case connectionTypeNATS:
+		createNATSConnection()
+		break
+	case connectionTypeHTTP:
+		createHTTPConnection()
+		break
+	default:
+		createNATSConnection()
+	}
 
 	return 0
 }
 
-func createRecipientConnection() {
+const defaultPollingInterval = 10
 
+func createHTTPConnection() {
+	sentCloudEvents = map[string][]string{}
+	createRecipientConnection()
+
+	eventEndpoint := getHTTPPollingEndpoint()
+	eventEndpointAuthToken := os.Getenv("HTTP_EVENT_ENDPOINT_AUTH_TOKEN")
+	topics := strings.Split(os.Getenv("PUBSUB_TOPIC"), ",")
+
+	pollingInterval, err := strconv.ParseInt(os.Getenv("HTTP_POLLING_INTERVAL"), 10, 64)
+	if err != nil {
+		pollingInterval = defaultPollingInterval
+	}
+
+	pollingTicker := time.NewTicker(time.Duration(pollingInterval) * time.Second)
+
+	for {
+		<-pollingTicker.C
+		pollHTTPEventSource(eventEndpoint, eventEndpointAuthToken, topics)
+	}
+}
+
+func getHTTPPollingEndpoint() string {
+	endpoint := os.Getenv("HTTP_EVENT_ENDPOINT")
+	if !strings.HasPrefix(endpoint, "https://") && !strings.HasPrefix(endpoint, "http://") {
+		endpoint = "http://" + endpoint
+	}
+	parsedURL, _ := url.Parse(endpoint)
+
+	if parsedURL.Scheme == "" {
+		parsedURL.Scheme = "http"
+	}
+	if parsedURL.Path == "" {
+		parsedURL.Path = "v1/event/triggered/"
+	}
+
+	return parsedURL.String()
+}
+
+func pollHTTPEventSource(endpoint string, token string, topics []string) {
+	for _, topic := range topics {
+		events, err := getEventsFromEndpoint(endpoint, token, topic)
+		if err != nil {
+			fmt.Println("Could not retrieve events of type " + topic + " from " + endpoint + ": " + endpoint)
+		}
+
+		for _, event := range events {
+			alreadySent := hasEventBeenSent(event)
+
+			if alreadySent {
+				fmt.Println("CloudEvent with ID " + event.ID + " has already been sent.")
+				continue
+			}
+
+			marshal, err := json.Marshal(event)
+
+			e, err := decodeCloudEvent(marshal)
+
+			if e != nil {
+				err = sendEvent(*e)
+				if err != nil {
+					fmt.Println("Could not send CloudEvent: " + err.Error())
+				}
+				sentCloudEvents[*event.Type] = append(sentCloudEvents[*event.Type], event.ID)
+			}
+		}
+
+		// clean up list of sent events to avoid memory leaks -> if an item that has been marked as already sent
+		// is not an open .triggered event anymore, it can be removed from the list
+		cleanSentEventList(topic, events)
+	}
+}
+
+func getEventsFromEndpoint(endpoint string, token string, topic string) ([]*keptnmodels.KeptnContextExtendedCE, error) {
+	events := []*keptnmodels.KeptnContextExtendedCE{}
+	nextPageKey := ""
+
+	for {
+		url, err := url.Parse(endpoint)
+		if err != nil {
+			return nil, err
+		}
+		q := url.Query()
+		if nextPageKey != "" {
+			q.Set("nextPageKey", nextPageKey)
+			url.RawQuery = q.Encode()
+		}
+		req, err := http.NewRequest("GET", url.String()+"/"+topic, nil)
+		req.Header.Set("Content-Type", "application/json")
+		if token != "" {
+			req.Header.Add("x-token", token)
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		if resp.StatusCode == 200 {
+			received := &keptnmodels.Events{}
+			err = json.Unmarshal(body, received)
+			if err != nil {
+				return nil, err
+			}
+			events = append(events, received.Events...)
+
+			if received.NextPageKey == "" || received.NextPageKey == "0" {
+				break
+			}
+
+			nextPageKey = received.NextPageKey
+		} else {
+			var respErr keptnmodels.Error
+			err = json.Unmarshal(body, &respErr)
+			if err != nil {
+				return nil, err
+			}
+			return nil, errors.New(*respErr.Message)
+		}
+	}
+
+	return events, nil
+}
+
+func hasEventBeenSent(event *keptnmodels.KeptnContextExtendedCE) bool {
+	alreadySent := false
+
+	if event.Type == nil {
+		event.Type = stringp("")
+	}
+	if sentCloudEvents[*event.Type] == nil {
+		sentCloudEvents[*event.Type] = []string{}
+	}
+	for _, sentEvent := range sentCloudEvents[*event.Type] {
+		if sentEvent == event.ID {
+			alreadySent = true
+		}
+	}
+	return alreadySent
+}
+
+func cleanSentEventList(topic string, events []*keptnmodels.KeptnContextExtendedCE) {
+	updatedList := []string{}
+	for _, sentEvent := range sentCloudEvents[topic] {
+		found := false
+		for _, ev := range events {
+			if ev.ID == sentEvent {
+				found = true
+				break
+			}
+		}
+		if !found {
+			updatedList = append(updatedList, sentEvent)
+		}
+	}
+	sentCloudEvents[topic] = updatedList
+}
+
+func stringp(s string) *string {
+	return &s
+}
+
+func createNATSConnection() {
+	uptimeTicker = time.NewTicker(10 * time.Second)
+
+	createRecipientConnection()
+
+	natsURL := os.Getenv("PUBSUB_URL")
+	topics := strings.Split(os.Getenv("PUBSUB_TOPIC"), ",")
+	nch := lib.NewNatsConnectionHandler(natsURL, topics)
+
+	nch.MessageHandler = handleMessage
+
+	err := nch.SubscribeToTopics()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	defer func() {
+		nch.RemoveAllSubscriptions()
+		// Close connection
+		fmt.Println("Disconnected from NATS")
+	}()
+
+	for {
+		select {
+		case <-uptimeTicker.C:
+			_ = nch.SubscribeToTopics()
+		case <-close:
+			return
+
+		}
+	}
+}
+
+func createRecipientConnection() {
 	recipientURL, err := getPubSubRecipientURL(
 		os.Getenv("PUBSUB_RECIPIENT"),
 		os.Getenv("PUBSUB_RECIPIENT_PORT"),
@@ -95,35 +324,6 @@ func createRecipientConnection() {
 	if err != nil {
 		fmt.Println("failed to create client: " + err.Error())
 		os.Exit(1)
-	}
-
-	natsURL := os.Getenv("PUBSUB_URL")
-	topics := strings.Split(os.Getenv("PUBSUB_TOPIC"), ",")
-	nch := lib.NewNatsConnectionHandler(natsURL, topics)
-
-	nch.MessageHandler = handleMessage
-
-	err = nch.SubscribeToTopics()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	defer func() {
-		nch.RemoveAllSubscriptions()
-		// Close connection
-		fmt.Println("Disconnected from NATS")
-	}()
-
-	for {
-		select {
-		case <-uptimeTicker.C:
-			_ = nch.SubscribeToTopics()
-		case <-close:
-			return
-
-		}
 	}
 }
 
