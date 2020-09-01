633ba703358c5f62726f09ad144af955e90c0204
Johannes <johannes.braeuer@dynatrace.com>
2020-09-01 16:31:27 +0200

    #2061 Use another isUrl checker

--- bridge/client/app/_components/ktb-event-item/ktb-event-item.component.ts
+++ bridge/client/app/_components/ktb-event-item/ktb-event-item.component.ts
@@ -71,13 +71,12 @@ export class KtbEventItemComponent {
   }
 
   isUrl(value: string): boolean {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return pattern.test(value);
+    try {
+      new URL(value);
+    } catch (_) {
+      return false;  
+    }
+    return true;
   }
 
 }
