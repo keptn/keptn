3b27288656722d2a20fad484c6fae84d9b83ddfa
ermin.muratovic <ermin.muratovic@gmail.com>
2020-09-01 14:35:34 +0200

    consider

--- bridge/client/app/_components/ktb-event-item/ktb-event-item.component.ts
+++ bridge/client/app/_components/ktb-event-item/ktb-event-item.component.ts
@@ -70,4 +70,14 @@ export class KtbEventItemComponent {
     this.clipboard.copy(plainEvent, 'event payload');
   }
 
+  isUrl(value: string): boolean {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return pattern.test(value);
+  }
+
 }
