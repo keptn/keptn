8731af99b730acd33c70de6e117ffb406854e03c
ermin.muratovic <ermin.muratovic@gmail.com>
2020-09-17 13:49:58 +0200

    implement copy-to-clipboard component, with toggle value visibility feature

---
+++ bridge/client/app/_components/ktb-copy-to-clipboard/ktb-copy-to-clipboard.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import {KtbCopyToClipboardComponent, KtbExpandableTileHeader} from './ktb-copy-to-clipboard.component';
+import {AppModule} from '../../app.module';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+
+describe('KtbExpandableTileComponent', () => {
+  let component: KtbCopyToClipboardComponent;
+  let fixture: ComponentFixture<KtbCopyToClipboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+      ],
+      imports: [
+        AppModule,
+        HttpClientTestingModule,
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KtbCopyToClipboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
