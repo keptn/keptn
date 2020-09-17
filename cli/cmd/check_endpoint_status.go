de320c34decf83fae12511b057829c7196b59bb6
Supratik Das <rick.das08@gmail.com>
2020-09-18 03:03:20 +0530

    Add utility function to test API

---
+++ cli/cmd/check_endpoint_status.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	apiUtils "github.com/keptn/go-utils/pkg/api/utils"
+)
+
+var client = http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext:     apiUtils.ResolveXipIoWithContext,
+	},
+}
+
+func checkEndPointStatus(endPoint string) error {
+	req, err := http.NewRequest("HEAD", endPoint, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	return nil
+}
