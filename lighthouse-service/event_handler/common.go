e125ea3fca8d8f858d99f3ec806bfaa77892c897
Florian Bacher <florian.bacher@dynatrace.com>
2020-09-18 13:30:29 +0200

    #2365 fixed nil pointer

--- lighthouse-service/event_handler/common.go
+++ lighthouse-service/event_handler/common.go
@@ -40,12 +40,12 @@ func getSLOs(project string, stage string, service string) (*keptn.ServiceLevelO
 	if err != nil {
 		// check if service/stage/project actually exist
 		serviceHandler := utils.NewServiceHandler(configurationServiceURL)
-		_, err = serviceHandler.GetService(project, stage, service)
-		if strings.Contains(strings.ToLower(err.Error()), "project not found") {
+		_, err2 := serviceHandler.GetService(project, stage, service)
+		if strings.Contains(strings.ToLower(err2.Error()), "project not found") {
 			return nil, ErrProjectNotFound
-		} else if strings.Contains(strings.ToLower(err.Error()), "stage not found") {
+		} else if strings.Contains(strings.ToLower(err2.Error()), "stage not found") {
 			return nil, ErrStageNotFound
-		} else if strings.Contains(strings.ToLower(err.Error()), "service not found") {
+		} else if strings.Contains(strings.ToLower(err2.Error()), "service not found") {
 			return nil, ErrServiceNotFound
 		} else {
 			return nil, ErrSLOFileNotFound
