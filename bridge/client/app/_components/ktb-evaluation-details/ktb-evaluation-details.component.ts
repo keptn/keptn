2d3c5018de21a87a241f523778d745d0897aedbe
ermin.muratovic <ermin.muratovic@gmail.com>
2020-09-24 11:51:21 +0200

    use turquoise as highlight color

--- bridge/client/app/_components/ktb-evaluation-details/ktb-evaluation-details.component.ts
+++ bridge/client/app/_components/ktb-evaluation-details/ktb-evaluation-details.component.ts
@@ -338,8 +338,8 @@ export class KtbEvaluationDetailsComponent implements OnInit, OnDestroy {
     if(highlightIndex >= 0)
       plotBands.push({
         borderWidth: 2,
-        borderColor: '#e6be00',
-        color: 'rgba(230,190,0,.4)',
+        borderColor: '#00a1b2',
+        color: 'rgba(0,161,178,.4)',
         from: highlightIndex-0.5,
         to: highlightIndex+0.5,
         zIndex: 20
