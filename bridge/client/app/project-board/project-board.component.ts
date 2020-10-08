0f7f03524c789b7c75867e50c3a6188b899853d2
ermin.muratovic <ermin.muratovic@gmail.com>
2020-10-08 11:17:44 +0200

    fix example commands for trigger quality gate evaluation

--- bridge/client/app/project-board/project-board.component.ts
+++ bridge/client/app/project-board/project-board.component.ts
@@ -54,14 +54,12 @@ export class ProjectBoardComponent implements OnInit, OnDestroy {
   public filterEventType: string = null;
 
   public integrationsExternalDetails = null;
-  public triggerQualityGateEvaluationViaCli = `keptn send event start-evaluation
-    --project=\${PROJECT} --stage=\${STAGE} --service=\${SERVICE}
-    --start=2020-12-31T11:59:59 --timeframe=5m`;
-  public triggerQualityGateEvaluationViaApi = `curl -X POST "\${KEPTN_API_ENDPOINT}/v1/project/{PROJECT}/stage/{STAGE}/service/{SERVICE}/evaluation"
+  public triggerQualityGateEvaluationViaCli = `keptn send event start-evaluation --project=\${PROJECT} --stage=\${STAGE} --service=\${SERVICE} --start=2020-12-31T09:00:00 --timeframe=5m`;
+  public triggerQualityGateEvaluationViaApi = `curl -X POST "\${KEPTN_API_ENDPOINT}/v1/project/{PROJECT}/stage/{STAGE}/service/{SERVICE}/evaluation" \\
     -H "accept: application/json; charset=utf-8" \\
     -H "x-token: \${KEPTN_API_TOKEN}" \\
     -H "Content-Type: application/json; charset=utf-8" \\
-    -d "{\\"start\\": \\"2020-09-28T07:00:00\\", \\"timeframe\\": \\"5m\\", \\"labels\\":{\\"buildId\\":\\"build-17\\",\\"owner\\":\\"JohnDoe\\",\\"testNo\\":\\"47-11\\"}"`;
+    -d "{"start": "2020-09-28T07:00:00", "timeframe": "5m", "labels":{"buildId":"build-17","owner":"JohnDoe","testNo":"47-11"}"`;
 
   public keptnInfo: any;
 
