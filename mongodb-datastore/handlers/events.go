c514ef8c5f62ef83dd48717b0f9d8683b461fddb
Florian Bacher <florian.bacher@dynatrace.com>
2020-10-21 15:53:06 +0200

    #2266 minor fixes

--- mongodb-datastore/handlers/events.go
+++ mongodb-datastore/handlers/events.go
@@ -6,13 +6,12 @@ import (
 	"errors"
 	"fmt"
 	keptncommon "github.com/keptn/go-utils/pkg/lib/keptn"
+	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 	"strconv"
 	"sync"
 	"time"
 
 	"github.com/jeremywohl/flatten"
-
-	keptnutils "github.com/keptn/go-utils/pkg/lib"
 
 	"github.com/keptn/keptn/mongodb-datastore/models"
 	"github.com/keptn/keptn/mongodb-datastore/restapi/operations/event"
@@ -99,7 +98,7 @@ func ProcessEvent(event *models.KeptnContextExtendedCE) error {
 		return err
 	}
 
-	if event.Type == keptnutils.InternalProjectDeleteEventType {
+	if event.Type == keptnv2.GetFinishedEventType(keptnv2.ProjectDeleteTaskName) {
 		return dropProjectEvents(logger, event)
 	}
 	return insertEvent(logger, event)
