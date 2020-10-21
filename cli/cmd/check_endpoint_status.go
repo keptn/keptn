1e724032e59b41cc4e7768d867b247ad6f2b64d2
Pedro Andrade <pedro.rjandrade@gmail.com>
2020-10-21 08:22:20 -0300

    * Setup a timeout on http calls - adding 5 seconds of a timeout to avoid hanging connection
    * Remove version check for root calls
    - keeping only on version command
    
    * Add tests for http timeout
    - add tests for check endpoint status function
    
    * Does not mock the tests for checkEndpointStatus function
    - forcing the checkEndPointStatusMock as true to avoid random tests

---
+++ cli/cmd/check_endpoint_status.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"context"
+	"crypto/tls"
+	"net/http"
+	"time"
+
+	apiUtils "github.com/keptn/go-utils/pkg/api/utils"
+)
+
+var maxHTTPTimeout = 5 * time.Second
+
+var endPointErrorReasons = `
+Possible reasons:
+* The Keptn API server is currently not available. Check if your Kubernetes cluster is available.
+* Your Keptn CLI points to the wrong API server (verify using 'keptn status')`
+
+var checkEndPointStatusMock = false
+
+var client = http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext:     apiUtils.ResolveXipIoWithContext,
+	},
+}
+
+func checkEndPointStatus(endPoint string) error {
+	if checkEndPointStatusMock {
+		return nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), maxHTTPTimeout)
+	defer cancel()
+
+	req, err := http.NewRequest("HEAD", endPoint, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	return nil
+}
