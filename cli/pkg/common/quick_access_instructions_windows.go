b15494bbc1e4aaa4a115550947d526f9d56cdd96
Florian Bacher <florian.bacher@dynatrace.com>
2020-10-28 10:40:24 +0100

    #2576 added missing import

--- cli/pkg/common/quick_access_instructions_windows.go
+++ cli/pkg/common/quick_access_instructions_windows.go
@@ -1,5 +1,7 @@
 package common
 
+import "fmt"
+
 func PrintQuickAccessInstructions(keptnNamespace, keptnReleaseDocsURL string) {
 	fmt.Println("* * To quickly access Keptn, you can use a port-forward and then authenticate your Keptn CLI as described here: https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install/#authenticate-keptn-cli\n")
 	fmt.Println("* To expose Keptn on a public endpoint, please continue with the installation guidelines provided at:\n" +
