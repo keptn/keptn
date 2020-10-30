3291847d5cb9714af369d8dda3c850c3d79b56ca
Florian Bacher <florian.bacher@dynatrace.com>
2020-10-30 08:28:25 +0100

    #2576 reduce duplications

--- cli/pkg/common/quick_access_instructions_darwin.go
+++ cli/pkg/common/quick_access_instructions_darwin.go
@@ -1,13 +1,6 @@
 package common
 
-import "fmt"
-
 // PrintQuickAccessInstructions prints instructions on how to access Keptn via the CLI
 func PrintQuickAccessInstructions(keptnNamespace, keptnReleaseDocsURL string) {
-	fmt.Println("* To quickly access Keptn, you can use a port-forward and then authenticate your Keptn CLI:\n" +
-		" - kubectl -n " + keptnNamespace + " port-forward service/api-gateway-nginx 8080:80\n" +
-		" - keptn auth --endpoint=http://localhost:8080/api --api-token=$(kubectl get secret keptn-api-token -n " + keptnNamespace + " -ojsonpath={.data.keptn-api-token} | base64 --decode)\n")
-	fmt.Println("* Alternatively, follow the instructions provided at: https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install/#authenticate-keptn-cli\n")
-	fmt.Println("* To expose Keptn on a public endpoint, please continue with the installation guidelines provided at:\n" +
-		" - https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install#install-keptn\n")
+	printQuickAccessInstructionsForUnix(keptnNamespace, keptnReleaseDocsURL)
 }
--- cli/pkg/common/quick_access_instructions_windows.go
+++ cli/pkg/common/quick_access_instructions_windows.go
@@ -1,10 +1,6 @@
 package common
 
-import "fmt"
-
 // PrintQuickAccessInstructions prints instructions on how to access Keptn via the CLI
 func PrintQuickAccessInstructions(keptnNamespace, keptnReleaseDocsURL string) {
-	fmt.Println("* * To quickly access Keptn, you can use a port-forward and then authenticate your Keptn CLI as described here: https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install/#authenticate-keptn-cli\n")
-	fmt.Println("* To expose Keptn on a public endpoint, please continue with the installation guidelines provided at:\n" +
-		" - https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install#install-keptn\n")
+	printQuickAccessInstructionsForWindows(keptnNamespace, keptnReleaseDocsURL)
 }
