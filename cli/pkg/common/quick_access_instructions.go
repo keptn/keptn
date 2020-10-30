eefc003b1d60349dd1c3cc142dc42c5c04be9365
Florian Bacher <florian.bacher@dynatrace.com>
2020-10-30 08:31:15 +0100

    #2576 reduce duplications

--- cli/pkg/common/quick_access_instructions.go
+++ cli/pkg/common/quick_access_instructions.go
@@ -7,12 +7,10 @@ func printQuickAccessInstructionsForUnix(keptnNamespace, keptnReleaseDocsURL str
 		" - kubectl -n " + keptnNamespace + " port-forward service/api-gateway-nginx 8080:80\n" +
 		" - keptn auth --endpoint=http://localhost:8080/api --api-token=$(kubectl get secret keptn-api-token -n " + keptnNamespace + " -ojsonpath={.data.keptn-api-token} | base64 --decode)\n")
 	fmt.Println("* Alternatively, follow the instructions provided at: https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install/#authenticate-keptn-cli\n")
-	fmt.Println("* To expose Keptn on a public endpoint, please continue with the installation guidelines provided at:\n" +
-		" - https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install#install-keptn\n")
 	printExposeEndpointInstructions(keptnReleaseDocsURL)
 }
 
-func printQuickAccessInstructionsForWindows(keptnNamespace string, keptnReleaseDocsURL string) {
+func printQuickAccessInstructionsForWindows(keptnReleaseDocsURL string) {
 	fmt.Println("* * To quickly access Keptn, you can use a port-forward and then authenticate your Keptn CLI as described here: https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install/#authenticate-keptn-cli\n")
 	printExposeEndpointInstructions(keptnReleaseDocsURL)
 }
--- cli/pkg/common/quick_access_instructions_windows.go
+++ cli/pkg/common/quick_access_instructions_windows.go
@@ -2,5 +2,5 @@ package common
 
 // PrintQuickAccessInstructions prints instructions on how to access Keptn via the CLI
 func PrintQuickAccessInstructions(keptnNamespace, keptnReleaseDocsURL string) {
-	printQuickAccessInstructionsForWindows(keptnNamespace, keptnReleaseDocsURL)
+	printQuickAccessInstructionsForWindows(keptnReleaseDocsURL)
 }
