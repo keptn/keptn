0df22342949d7e2aacafec780a01658dc88f1e69
Florian Bacher <florian.bacher@dynatrace.com>
2020-11-02 10:55:52 +0100

    Use string instead of regex for shkeptncontext search param (#2621)
    * use string instead of regex for shkeptncontext search param
    
    * fix unit test

--- mongodb-datastore/handlers/events_test.go
+++ mongodb-datastore/handlers/events_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/keptn/keptn/mongodb-datastore/models"
 	"github.com/keptn/keptn/mongodb-datastore/restapi/operations/event"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"reflect"
 	"testing"
@@ -145,7 +144,7 @@ func Test_getSearchOptions(t *testing.T) {
 				"data.service":   "carts",
 				"source":         "test-service",
 				"type":           "test-event",
-				"shkeptncontext": primitive.Regex{Pattern: "test-context", Options: ""},
+				"shkeptncontext": "test-context",
 				"time": bson.M{
 					"$gt": "1",
 				},
