f74caf3550a0c3850cda35f8f3fad650b2a1e0d9
ankitjain28may <ankitjain28may77@gmail.com>
2020-11-06 02:55:58 +0530

    Addressed the comments

--- cli/pkg/credentialmanager/handler_linux.go
+++ cli/pkg/credentialmanager/handler_linux.go
@@ -46,7 +46,7 @@ func NewCredentialManager() (cm *CredentialManager) {
 func (cm *CredentialManager) SetCreds(endPoint url.URL, apiToken string, namespace string) error {
 	if _, err := os.Stat(passwordStoreDirectory); os.IsNotExist(err) {
 		fmt.Println("Using a file-based storage for the key because the password-store seems to be not set up.")
-		apiTokenFile := cm.apiTokenFile + "__" + kubeConfigFile.CurrentContext + "__" + namespace
+		apiTokenFile := cm.getLinuxApiTokenFile(namespace)
 		return ioutil.WriteFile(apiTokenFile, []byte(endPoint.String()+"\n"+apiToken), 0644)
 	}
 	return setCreds(pass.Pass{}, endPoint, apiToken, namespace)
@@ -69,7 +69,7 @@ func (cm *CredentialManager) GetCreds(namespace string) (url.URL, string, error)
 	// try to read credentials from password-store
 	if _, err := os.Stat(passwordStoreDirectory); os.IsNotExist(err) {
 		// password-store not found, read credentials from apiTokenFile
-		apiTokenFile := cm.apiTokenFile + "__" + kubeConfigFile.CurrentContext + "__" + namespace
+		apiTokenFile := cm.getLinuxApiTokenFile(namespace)
 		data, err := ioutil.ReadFile(apiTokenFile)
 		if err != nil {
 			return url.URL{}, "", err
@@ -106,4 +106,8 @@ func (cm *CredentialManager) GetInstallCreds() (string, error) {
 		return dataStr, nil
 	}
 	return getInstallCreds(pass.Pass{})
+}
+
+func (cm *CredentialManager) getLinuxApiTokenFile(namespace string) string {
+	return cm.apiTokenFile + "__" + kubeConfigFile.CurrentContext + "__" + namespace
 }
