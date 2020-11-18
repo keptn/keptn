7129b40c36a6595e10a1378e32ac0e3fe55b48f2
Florian Bacher <florian.bacher@dynatrace.com>
2020-11-18 16:21:24 +0100

    #2500 check from and to version
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- cli/cmd/upgrade_project.go
+++ cli/cmd/upgrade_project.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"github.com/ghodss/yaml"
+	apiutils "github.com/keptn/go-utils/pkg/api/utils"
+	keptn "github.com/keptn/go-utils/pkg/lib"
+	"github.com/keptn/keptn/cli/pkg/credentialmanager"
 	"github.com/spf13/cobra"
+	"net/url"
+	"os"
 )
 
 type upgradeProjectCmdParams struct {
@@ -11,6 +19,12 @@ type upgradeProjectCmdParams struct {
 }
 
 var upgradeProjectParams *upgradeProjectCmdParams
+
+var supportedFromVersions = []string{"0.1", "0.1.0"}
+var supportedToVersions = []string{"0.2", "0.2.0"}
+
+const defaultFromVersion = "0.1"
+const defaultToVersion = "0.2"
 
 // upgradeProjectCmd represents the project command
 var upgradeProjectCmd = &cobra.Command{
@@ -27,12 +41,78 @@ For more information about upgrading projects, go to [Manage Keptn](https://kept
 	Example:      `keptn upgrade project PROJECTNAME --shipyard --fromVersion=0.1.0 --toVersion=0.2.0`,
 	SilenceUsage: true,
 	Args: func(cmd *cobra.Command, args []string) error {
+		_, _, err := credentialmanager.NewCredentialManager().GetCreds(namespace)
+		if err != nil {
+			return errors.New(authErrorMsg)
+		}
+
+		if len(args) != 1 {
+			cmd.SilenceUsage = false
+			return errors.New("required argument PROJECTNAME not set")
+		}
+
+		if upgradeProjectParams.FromVersion == nil || *upgradeProjectParams.FromVersion == "" {
+			*upgradeProjectParams.ToVersion = defaultFromVersion
+		} else if err := checkFromVersion(upgradeProjectParams.FromVersion); err != nil {
+			return err
+		}
+
+		if upgradeProjectParams.ToVersion == nil || *upgradeProjectParams.ToVersion == "" {
+			*upgradeProjectParams.ToVersion = defaultToVersion
+		} else if err := checkToVersion(upgradeProjectParams.ToVersion); err != nil {
+			return err
+		}
 
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var endPoint url.URL
+		var apiToken string
+		var err error
+		if !mocking {
+			endPoint, apiToken, err = credentialmanager.NewCredentialManager().GetCreds(namespace)
+		} else {
+			endPointPtr, _ := url.Parse(os.Getenv("MOCK_SERVER"))
+			endPoint = *endPointPtr
+			apiToken = ""
+		}
+		if endPointErr := checkEndPointStatus(endPoint.String()); endPointErr != nil {
+			return fmt.Errorf("Error connecting to server: %s"+endPointErrorReasons,
+				endPointErr)
+		}
+
+		resourceHandler := apiutils.NewAuthenticatedResourceHandler(endPoint.String(), apiToken, "x-token", nil, endPoint.Scheme)
+		shipyardResource, err := resourceHandler.GetProjectResource(*newArtifact.Project, "shipyard.yaml")
+		if err != nil {
+			return fmt.Errorf("Error while retrieving shipyard.yaml for project %s: %s:", *newArtifact.Project, err.Error())
+		}
+
+		shipyard := &keptn.Shipyard{}
+
+		if err := yaml.Unmarshal([]byte(shipyardResource.ResourceContent), shipyard); err != nil {
+			return fmt.Errorf("Error while decoding shipyard.yaml for project %s: %s", *newArtifact.Project, err.Error())
+		}
+
 		return nil
 	},
+}
+
+func checkFromVersion(fromVersion *string) error {
+	for _, value := range supportedFromVersions {
+		if value == *fromVersion {
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid fromVersion %s. Please enter one of the following: %v", fromVersion, supportedFromVersions)
+}
+
+func checkToVersion(toVersion *string) error {
+	for _, value := range supportedToVersions {
+		if value == *toVersion {
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid toVersion %s. Please enter one of the following: %v", toVersion, supportedToVersions)
 }
 
 func init() {
@@ -44,6 +124,4 @@ func init() {
 	upgradeProjectParams.FromVersion = upgradeProjectCmd.Flags().StringP("fromVersion", "", "", "The current version of the shipyard")
 	upgradeProjectParams.ToVersion = upgradeProjectCmd.Flags().StringP("toVersion", "", "", "The new target version of the shipyard")
 	upgradeProjectCmd.MarkFlagRequired("shipyard")
-	upgradeProjectCmd.MarkFlagRequired("fromVersion")
-	upgradeProjectCmd.MarkFlagRequired("toVersion")
 }
