6d64f46f36f19214c6303d2e5c0408b02a26d75a
Florian Bacher <florian.bacher@dynatrace.com>
2020-11-25 10:16:58 +0100

    #2717 fixed scripts for checking deprecated k8s versions
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- gh-actions-scripts/check_deprecated_versions.sh
+++ gh-actions-scripts/check_deprecated_versions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
-helm template installer/manifests/keptn | pluto detect-files -owide
-pluto detect-files -owide
+ls -la $GITHUB_WORKSPACE/bin
+./helm template installer/manifests/keptn | ./pluto detect-files -owide
+./pluto detect-files -owide
 set +e
--- gh-actions-scripts/install_helm.sh
+++ gh-actions-scripts/install_helm.sh
@@ -3,9 +3,8 @@
 # download
 curl https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz > helm.tar.gz
 tar -zxvf helm.tar.gz
-if [ ! -d "$HOME/bin" ]; then
-  mkdir "$HOME/bin"
+if [ ! -d "$GITHUB_WORKSPACE/bin" ]; then
+  mkdir "$GITHUB_WORKSPACE/bin"
 fi
 
-mv linux-amd64/helm "$HOME/bin"
-export PATH=$PATH:"$HOME/bin"
+mv ./linux-amd64/helm .
