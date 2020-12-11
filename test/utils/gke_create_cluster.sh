f8ebb29895403cb41c80f63ee28e9f0c8354c311
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2020-12-11 11:02:56 +0100

    #2789 Fix gcloud create cluster
    Signed-off-by: Christian Kreuzberger <christian.kreuzberger@dynatrace.com>

--- test/utils/gke_create_cluster.sh
+++ test/utils/gke_create_cluster.sh
@@ -22,11 +22,14 @@ echo "Creating nightly cluster ${CLUSTER_NAME_NIGHTLY}"
 
 # create a new cluster (Note: disk-size reduced to 25 GB to save resources; pre-emptible nodes used for cost savings)
 gcloud beta container --project "$PROJECT_NAME" clusters create "$CLUSTER_NAME_NIGHTLY" --zone "$CLOUDSDK_COMPUTE_ZONE" \
- --release-channel "None" --cluster-version $GKE_VERSION \
- --username "admin" \
- --machine-type "n1-standard-8" --image-type "UBUNTU" --preemptible --disk-type "pd-standard" --disk-size "25" \
+ --release-channel "None" --cluster-version "$GKE_VERSION" \
+ --no-enable-basic-auth \
+ --machine-type "n1-standard-8" --image-type "UBUNTU" --num-nodes "1" --preemptible --disk-type "pd-standard" --disk-size "25" \
  --metadata disable-legacy-endpoints=true \
- --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "1" --enable-cloud-logging --enable-cloud-monitoring --enable-ip-alias --network "projects/sai-research/global/networks/default" --subnetwork "projects/sai-research/regions/$CLOUDSDK_REGION/subnetworks/default" \
+ --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+ --enable-stackdriver-kubernetes --enable-ip-alias \
+ --network "projects/$PROJECT_NAME/global/networks/default" --subnetwork "projects/$PROJECT_NAME/regions/$CLOUDSDK_REGION/subnetworks/default" \
+ --no-enable-master-authorized-networks \
  --addons $ADDONS $ISTIO_CONFIG \
  --no-enable-autoupgrade --no-enable-autorepair \
  --labels owner=travis,expiry=auto-delete
