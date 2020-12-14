69d1d13000cd364b2e6ee63f8412e186d582729f
Florian Bacher <florian.bacher@dynatrace.com>
2020-12-14 18:47:51 +0100

    #2759 fixed script errors
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/test_keptn_performance.sh
+++ test/test_keptn_performance.sh
@@ -8,14 +8,14 @@ function cleanup() {
   echo "Delete lighthouse-config configmap"
   kubectl delete configmap -n ${KEPTN_NAMESPACE} lighthouse-config
 
-  echo "Deleting project ${PROJECT}"
-  keptn delete project $PROJECT
+  echo "Deleting project ${SELF_MONITORING_PROJECT}"
+  keptn delete project $SELF_MONITORING_PROJECT
 
   echo "Uninstalling dynatrace-sli-service"
   kubectl -n ${KEPTN_NAMESPACE} delete deployment dynatrace-sli-service
 
-  echo "Removing secret dynatrace-credentials-${PROJECT}"
-  kubectl -n ${KEPTN_NAMESPACE} delete secret dynatrace-credentials-${PROJECT}
+  echo "Removing secret dynatrace-credentials-${SELF_MONITORING_PROJECT}"
+  kubectl -n ${KEPTN_NAMESPACE} delete secret dynatrace-credentials-${SELF_MONITORING_PROJECT}
 
   for project_nr in $(seq 1 ${NR_PROJECTS})
   do
@@ -26,7 +26,7 @@ function cleanup() {
 
 # test configuration
 DYNATRACE_SLI_SERVICE_VERSION=${DYNATRACE_SLI_SERVICE_VERSION:-master}
-PROJECT=${PROJECT:-keptn-selfmonitoring}
+SELF_MONITORING_PROJECT=${SELF_MONITORING_PROJECT:-keptn-selfmonitoring}
 KEPTN_NAMESPACE=${KEPTN_NAMESPACE:-keptn}
 
 NR_PROJECTS=${NR_PROJECTS:-20}
@@ -62,7 +62,7 @@ KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n ${KEPTN_NAMESPACE} -ojso
 
 
 # deploy dynatrace-sli service
-kubectl -n ${KEPTN_NAMESPACE} create secret generic dynatrace-credentials-${PROJECT} --from-literal="DT_TENANT=$QG_INTEGRATION_TEST_DT_TENANT" --from-literal="DT_API_TOKEN=$QG_INTEGRATION_TEST_DT_API_TOKEN"
+kubectl -n ${KEPTN_NAMESPACE} create secret generic dynatrace-credentials-${SELF_MONITORING_PROJECT} --from-literal="DT_TENANT=$QG_INTEGRATION_TEST_DT_TENANT" --from-literal="DT_API_TOKEN=$QG_INTEGRATION_TEST_DT_API_TOKEN"
 
 echo "Install dynatrace-sli-service from: ${DYNATRACE_SLI_SERVICE_VERSION}"
 kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/${DYNATRACE_SLI_SERVICE_VERSION}/deploy/service.yaml -n ${KEPTN_NAMESPACE}
@@ -73,13 +73,13 @@ kubectl -n ${KEPTN_NAMESPACE} set image deployment/dynatrace-sli-service dynatra
 sleep 10
 wait_for_deployment_in_namespace "dynatrace-sli-service" ${KEPTN_NAMESPACE}
 
-kubectl create configmap -n ${KEPTN_NAMESPACE} lighthouse-config-${PROJECT} --from-literal=sli-provider=dynatrace
+kubectl create configmap -n ${KEPTN_NAMESPACE} lighthouse-config-${SELF_MONITORING_PROJECT} --from-literal=sli-provider=dynatrace
 
 # create the project
 
-keptn create project $PROJECT --shipyard=./test/assets/shipyard-quality-gates-self-monitoring.yaml
+keptn create project $SELF_MONITORING_PROJECT --shipyard=./test/assets/shipyard-quality-gates-self-monitoring.yaml
 
-keptn add-resource --project=$PROJECT --resource=./test/assets/self_monitoring_sli.yaml --resourceUri=dynatrace/sli.yaml
+keptn add-resource --project=$SELF_MONITORING_PROJECT --resource=./test/assets/self_monitoring_sli.yaml --resourceUri=dynatrace/sli.yaml
 
 # create services
 
@@ -87,19 +87,19 @@ SERVICES=("bridge" "eventbroker-go" "configuration-service" "mongodb-datastore" 
 
 for SERVICE in "${SERVICES[@]}"
 do
-    keptn create service $SERVICE --project=$PROJECT
+    keptn create service $SERVICE --project=$SELF_MONITORING_PROJECT
 done
 
 for SERVICE in "${SERVICES[@]}"
 do
-    keptn add-resource --project=$PROJECT --service=$SERVICE --stage=hardening --resource=./test/assets/self_monitoring_slo.yaml --resourceUri=slo.yaml
+    keptn add-resource --project=$SELF_MONITORING_PROJECT --service=$SERVICE --stage=hardening --resource=./test/assets/self_monitoring_slo.yaml --resourceUri=slo.yaml
 done
 
 # initial evaluation
 
-SERVICE=mongodb-datastore
+SELF_MONITORING_SERVICE=mongodb-datastore
 
-keptn add-resource --project=$PROJECT --service=$SERVICE --stage=hardening --resource=./test/assets/mongodb-performance.jmx --resourceUri=jmeter/load.jmx
+keptn add-resource --project=$SELF_MONITORING_PROJECT --service=$SELF_MONITORING_SERVICE --stage=hardening --resource=./test/assets/mongodb-performance.jmx --resourceUri=jmeter/load.jmx
 
 cat << EOF > ./tmp-trigger-evaluation.json
 {
@@ -108,11 +108,11 @@ cat << EOF > ./tmp-trigger-evaluation.json
   "source": "travis-ci",
   "contenttype": "application/json",
   "data": {
-    "project": "$PROJECT",
+    "project": "$SELF_MONITORING_PROJECT",
     "stage": "hardening",
-    "service": "$SERVICE",
+    "service": "$SELF_MONITORING_SERVICE",
     "deployment": {
-      "deploymentURIsLocal": ["$SERVICE:8080"]
+      "deploymentURIsLocal": ["$SELF_MONITORING_SERVICE:8080"]
     },
     "labels": {
       "nr_projects": "0"
@@ -121,12 +121,14 @@ cat << EOF > ./tmp-trigger-evaluation.json
 }
 EOF
 
+cat tmp-trigger-evaluation.json
+
 keptn_context_id=$(send_event_json ./tmp-trigger-evaluation.json)
 rm tmp-trigger-evaluation.json
 
 # try to fetch a evaluation-done event
 echo "Getting evaluation-done event with context-id: ${keptn_context_id}"
-response=$(get_event_with_retry sh.keptn.event.evaluation.finished ${keptn_context_id} ${PROJECT})
+response=$(get_event_with_retry sh.keptn.event.evaluation.finished ${keptn_context_id} ${SELF_MONITORING_PROJECT})
 
 echo $response | jq .
 
@@ -144,38 +146,37 @@ do
     do
       send_start_evaluation_request project-${project_nr} hardening service-${service_nr}
     done
-  done
-done
 
-# do the evaluation again
-
-cat << EOF > ./tmp-trigger-evaluation.json
-{
-  "type": "sh.keptn.event.hardening.evaluation.triggered",
-  "specversion": "1.0",
-  "source": "travis-ci",
-  "contenttype": "application/json",
-  "data": {
-    "project": "$PROJECT",
-    "stage": "hardening",
-    "service": "$SERVICE",
-    "deployment": {
-      "deploymentURIsLocal": ["$SERVICE:8080"]
-    },
-    "labels": {
-      "nr_projects": "$NR_PROJECTS",
-      "nr_services_per_project": "$NR_SERVICES_PER_PROJECT",
-      "nr_evaluations_per_service": "$NR_NR_EVALUATIONS_PER_SERVICE",
+    # do the evaluation again
+    cat << EOF > ./tmp-trigger-evaluation.json
+    {
+      "type": "sh.keptn.event.hardening.evaluation.triggered",
+      "specversion": "1.0",
+      "source": "travis-ci",
+      "contenttype": "application/json",
+      "data": {
+        "project": "$SELF_MONITORING_PROJECT",
+        "stage": "hardening",
+        "service": "$SELF_MONITORING_SERVICE",
+        "deployment": {
+          "deploymentURIsLocal": ["$SELF_MONITORING_SERVICE:8080"]
+        },
+        "labels": {
+          "nr_projects": "$SELF_MONITORING_PROJECT_nr",
+          "nr_services_per_project": "$service_nr",
+          "nr_evaluations_per_service": "$NR_NR_EVALUATIONS_PER_SERVICE"
+        }
+      }
     }
-  }
-}
 EOF
 
-keptn_context_id=$(send_event_json ./tmp-trigger-evaluation.json)
-rm tmp-trigger-evaluation.json
+    keptn_context_id=$(send_event_json ./tmp-trigger-evaluation.json)
+    rm tmp-trigger-evaluation.json
 
-# try to fetch a evaluation-done event
-echo "Getting evaluation-done event with context-id: ${keptn_context_id}"
-response=$(get_event_with_retry sh.keptn.event.evaluation.finished ${keptn_context_id} ${PROJECT})
+    # try to fetch a evaluation-done event
+    echo "Getting evaluation-done event with context-id: ${keptn_context_id}"
+    response=$(get_event_with_retry sh.keptn.event.evaluation.finished ${keptn_context_id} ${SELF_MONITORING_PROJECT})
 
-echo $response | jq .
+    echo $response | jq .
+  done
+done
