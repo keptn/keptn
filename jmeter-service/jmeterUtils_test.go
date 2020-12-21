7d21e5dc82f5b2486bde34f8d373935753a837c9
Andreas Grabner <andreas.grabner@dynatrace.com>
2020-12-21 13:56:25 +0100

    JMeter Dynatrace OSS and Data File Support (#2825)
    * adding sample jmeter scripts
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * adding senddeploymentfinished helper script
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * fixing version referneces
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * refactor and looking for jmeter.conf.yaml in all levels of the config repo
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * increased loopcount to achieve longer running tests
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * adding build as label
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * fixing example output
    
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * refactoring and starting to implement #2756
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * fixing primary file issue
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * fixes based on PR comments
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    Signed-off-by: grabnerandi <grabnerandi@TAG009442636466.clients.dynatrace.org>
    
    * removed unnecessary sample files
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    
    * reverting back .conf to previous version as discussed on PR
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    
    * adding more context to temp directory as discussed in PR
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    
    * fixed two more PR comments
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    
    * temporary back to grabnerandi image while waiting for build
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    
    * updating to offically built image
    
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- jmeter-service/jmeterUtils_test.go
+++ jmeter-service/jmeterUtils_test.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	keptnapimodels "github.com/keptn/go-utils/pkg/api/models"
 	keptncommon "github.com/keptn/go-utils/pkg/lib/keptn"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"os"
+	"strings"
 	"testing"
 )
 
 func Test_executeJMeter(t *testing.T) {
 	var returnedStatus int
+	var returnedResources keptnapimodels.Resources
+
 	ts := httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Type", "application/json")
+			if strings.HasSuffix(r.URL.Path, "/resource") {
+				marshal, _ := json.Marshal(returnedResources)
+				w.Write(marshal)
+				return
+			}
+
 			w.WriteHeader(returnedStatus)
-			w.Write([]byte(`{}`))
+			w.Write([]byte(`{
+	"code": ` + fmt.Sprintf("%d", returnedStatus) + `,
+	"message": ""
+}`))
 		}),
 	)
 	defer ts.Close()
@@ -33,11 +48,12 @@ func Test_executeJMeter(t *testing.T) {
 		logger         *keptncommon.Logger
 	}
 	tests := []struct {
-		name           string
-		args           args
-		returnedStatus int
-		want           bool
-		wantErr        bool
+		name              string
+		args              args
+		returnedStatus    int
+		returnedResources []string
+		want              bool
+		wantErr           bool
 	}{
 		{
 			name: "Skip tests if 404 is returned by configuration service and mark as success",
@@ -101,6 +117,16 @@ func Test_executeJMeter(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			returnedStatus = tt.returnedStatus
+
+			returnedResources = keptnapimodels.Resources{
+				Resources: []*keptnapimodels.Resource{},
+			}
+
+			for _, res := range tt.returnedResources {
+				returnedResources.Resources = append(returnedResources.Resources, &keptnapimodels.Resource{
+					ResourceURI: &res,
+				})
+			}
 			got, err := executeJMeter(tt.args.testInfo, tt.args.workload, tt.args.resultsDir, tt.args.url, tt.args.LTN, tt.args.funcValidation, tt.args.logger)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("executeJMeter() error = %v, wantErr %v", err, tt.wantErr)
