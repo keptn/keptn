11d35e286df410fc9357e356fdf91f5c835e3cb0
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2020-12-21 16:11:36 +0100

    #2811 Added integration tests for GKE and Minishift
    Signed-off-by: Christian Kreuzberger <christian.kreuzberger@dynatrace.com>

--- test/utils/gke_authenticate_at_cluster.sh
+++ test/utils/gke_authenticate_at_cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # configure gcloud
-gcloud --quiet config set project $PROJECT_NAME
+gcloud --quiet config set project $GCLOUD_PROJECT_NAME
 gcloud --quiet config set container/cluster $CLUSTER_NAME_NIGHTLY
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 
 # get cluster credentials (this will set kubectl context)
-gcloud container clusters get-credentials "$CLUSTER_NAME_NIGHTLY" --zone "$CLOUDSDK_COMPUTE_ZONE" --project "$PROJECT_NAME"
+gcloud container clusters get-credentials "$CLUSTER_NAME_NIGHTLY" --zone "$CLOUDSDK_COMPUTE_ZONE" --project "$GCLOUD_PROJECT_NAME"
 if [[ $? != '0' ]]; then
     echo "gcloud get credentials failed"
     exit 1
