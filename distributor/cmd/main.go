ab1ae3ec150df8f2a0a48a1dacf807c22a73d1a5
Florian Bacher <florian.bacher@dynatrace.com>
2021-01-08 10:27:41 +0100

    #2841 fixed path mapping
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- distributor/cmd/main.go
+++ distributor/cmd/main.go
@@ -252,6 +252,7 @@ func getProxyHost(path string) (string, string, string) {
 	// if the endpoint is not empty, map to the correct api
 	for key, value := range externalAPIProxyMappings {
 		if strings.HasPrefix(path, key) {
+			path = strings.Replace(path, key, value, 1)
 			// special case: configuration service /resource requests with nested resource URIs need to have an escaped '/' - see https://github.com/keptn/keptn/issues/2707
 			if value == "/api/configuration-service" {
 				splitPath := strings.Split(path, "/resource/")
@@ -263,7 +264,7 @@ func getProxyHost(path string) (string, string, string) {
 					path = path + url.QueryEscape(splitPath[len(splitPath)-1])
 				}
 			}
-			return parsedKeptnURL.Scheme, parsedKeptnURL.Host, parsedKeptnURL.Path + path
+			return parsedKeptnURL.Scheme, parsedKeptnURL.Host, path
 		}
 	}
 	return "", "", ""
