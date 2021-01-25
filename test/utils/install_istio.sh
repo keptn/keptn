0ab930a6c77c5dce1d8d5dde71c1710f2cdc1b94
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2021-01-25 18:57:21 +0100

    #2824 Fix istio installation
    Signed-off-by: Christian Kreuzberger <christian.kreuzberger@dynatrace.com>

--- test/utils/install_istio.sh
+++ test/utils/install_istio.sh
@@ -6,7 +6,9 @@ source test/utils.sh
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.8.2 sh -
 cd istio-*
 export PATH=$PWD/bin:$PATH
-istioctl install --set profile=demo
+istioctl install -y # --set profile=demo
+
+verify_test_step $? "Failed to install Istio"
 
 # verify the pods within istio-system
 verify_deployment_in_namespace "istio-ingressgateway" "istio-system"
