0e8ea143e42e75799f3239cd8a49e78d414a7148
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2021-02-02 09:32:52 +0100

    #2824 Update resource collection to be compliant with docs
    Signed-off-by: Christian Kreuzberger <christian.kreuzberger@dynatrace.com>

--- test/utils/k8s_collect_resources.sh
+++ test/utils/k8s_collect_resources.sh
@@ -3,13 +3,13 @@
 NAMESPACE=${1:-"keptn"}
 
 # Gathers resource limits by deployment for keptn namespace
-
+# | Deployment                          	| Memory (requested) 	| CPU (requested) 	| Memory (limit) 	| CPU (limit)
 KUBE_GET_DEPS="kubectl get deployments -n $NAMESPACE"
 echo -e ""
-echo -e "| Pod | Container | lim.cpu | lim.mem | req.cpu | req.mem | Images |"
-echo -e "|-----|-----------|---------|---------|---------|---------|--------|"
+echo -e "| Pod | Container | Memory (requested) | CPU (requested) | Memory (limit) | CPU (limit) | Images |"
+echo -e "|-----|-----------|--------------------|-----------------|----------------|-------------|--------|"
 $KUBE_GET_DEPS | sed '1d' | awk '{print $1}' | sort | while read DEPLOYMENT; do
-  $KUBE_GET_DEPS $DEPLOYMENT -o jsonpath='{range .spec.template.spec.containers[*]}{"| "}{"'$DEPLOYMENT'"}{" | "}{.name}{" | "}--{.resources.limits.cpu}{" | "}--{.resources.limits.memory}{" | "}--{.resources.requests.cpu}{" | "}--{.resources.requests.memory}{" | "}{.image}{" | "}{"\n"}{end}' | sed -E -e 's/--([0-9])/\1/g' -e 's/--/-/g'
+  $KUBE_GET_DEPS $DEPLOYMENT -o jsonpath='{range .spec.template.spec.containers[*]}{"| "}{"'$DEPLOYMENT'"}{" | "}{.name}{" | "}--{.resources.requests.memory}{" | "}--{.resources.requests.cpu}{" | "}--{.resources.limits.memory}{" | "}--{.resources.limits.cpu}{" | "}{.image}{" | "}{"\n"}{end}' | sed -E -e 's/--([0-9])/\1/g' -e 's/--/-/g'
 done
 
 echo -e ""
