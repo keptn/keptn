28d7b8649c43554abce98e567661f09aebdc7ccf
Florian Bacher <florian.bacher@dynatrace.com>
2021-02-03 09:20:18 +0100

    #2870 improved logging in case of cluster API downtime (#2954)
    * #2870 refactored metadata handler for better testability
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #2870 added error case for unit tests
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #2870 added line break after unmatched version warning
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #2870 added proper error message in case metadata endpoint does not behave as expected; unit tests
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #2870 log the error message in case a k8s client could not be initialized
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- api/handlers/metadata.go
+++ api/handlers/metadata.go
@@ -23,46 +23,67 @@ import (
 // GetMetadataHandlerFunc returns metadata of the keptn installation
 func GetMetadataHandlerFunc(params metadata.MetadataParams, principal *models.Principal) middleware.Responder {
 
+	handler := newMetadataHandler()
+
+	return handler.getMetadata()
+}
+
+func newMetadataHandler() metadataHandler {
+	var clientSet kubernetes.Interface
 	logger := keptnutils.NewLogger("", "", "api")
-	logger.Info("API received a GET metadata event")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		logger.Debug(fmt.Sprintf("Could not get InClusterConfig, will skip k8s-deployments: %s", err.Error()))
+	} else {
+		// creates the clientset
+		clientSet, err = kubernetes.NewForConfig(config)
+		if err != nil {
+			logger.Debug("Could not create kubernetes client, will skip k8s-deployments.")
+		}
+	}
+	return metadataHandler{
+		k8sClient:       clientSet,
+		logger:          logger,
+		swaggerFilePath: "/swagger-ui/swagger.yaml",
+	}
+}
+
+type swaggerFileProvider interface {
+	getSwaggerFileContent() ([]byte, error)
+}
+
+type metadataHandler struct {
+	k8sClient kubernetes.Interface
+	swaggerFileProvider
+	logger          keptnutils.LoggerInterface
+	swaggerFilePath string
+}
+
+func (h *metadataHandler) getMetadata() middleware.Responder {
+	h.logger.Info("API received a GET metadata event")
 
 	var namespace string
 	namespace = os.Getenv("POD_NAMESPACE")
-
-	var checkDeployment bool
-	checkDeployment = true
 
 	var payload models.Metadata
 	payload.Namespace = namespace
 	payload.Keptnversion = "N/A"
 	payload.Keptnlabel = "keptn"
+	payload.Bridgeversion = "N/A"
 
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		logger.Debug("Could not get InClusterConfig, will skip k8s-deployments.")
-		checkDeployment = false
-	} else {
-		// creates the clientset
-		clientset, err := kubernetes.NewForConfig(config)
+	if h.k8sClient != nil {
+		deploymentsClient := h.k8sClient.AppsV1().Deployments(namespace)
+		bridgeDeployment, err := deploymentsClient.Get("bridge", metav1.GetOptions{})
 		if err != nil {
-			logger.Debug("Could not create kubernetes client, will skip k8s-deployments.")
-			payload.Bridgeversion = "N/A"
-			checkDeployment = false
+			// log the error, but continue
+			h.logger.Error(fmt.Sprintf("Error getting deployment info: %s", err.Error()))
 		} else {
-			deploymentsClient := clientset.AppsV1().Deployments(namespace)
-			bridgeDeployment, err := deploymentsClient.Get("bridge", metav1.GetOptions{})
-			if err != nil {
-				logger.Error(fmt.Sprintf("Error getting deployment info %s", err.Error()))
-				return nil
-			}
 			payload.Bridgeversion = strings.TrimPrefix(bridgeDeployment.Spec.Template.Spec.Containers[0].Image, "keptn/")
 		}
-	}
 
-	if checkDeployment {
 		// Load swagger.yaml from /swagger-ui/swagger.yaml
 		mapSwagger := make(map[interface{}]interface{})
-		yamlFile, err := ioutil.ReadFile("/swagger-ui/swagger.yaml")
+		yamlFile, err := ioutil.ReadFile(h.swaggerFilePath)
 
 		if err != nil {
 			fmt.Printf("yamlFile.Get err   #%v ", err)
@@ -78,8 +99,6 @@ func GetMetadataHandlerFunc(params metadata.MetadataParams, principal *models.Pr
 				payload.Keptnversion = fmt.Sprintf("%v", v)
 			}
 		}
-	} else {
-		payload.Keptnversion = "N/A"
 	}
 
 	return metadata.NewMetadataOK().WithPayload(&payload)
