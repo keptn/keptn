869f1cea3a36631393bb9845a07095d07fccc950
ermin.muratovic <ermin.muratovic@gmail.com>
2021-02-09 16:10:11 +0100

    enivornment-view reduced
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_views/ktb-environment-view/ktb-environment-view.component.ts
+++ bridge/client/app/_views/ktb-environment-view/ktb-environment-view.component.ts
@@ -13,48 +13,15 @@ export class KtbEnvironmentViewComponent implements OnInit {
   get project() {
     return this._project;
   }
-
   set project(project: Project) {
     if (this._project !== project) {
       this._project = project;
-      this._changeDetectorRef.markForCheck();
     }
   }
 
-  constructor(private dataService: DataService, private _changeDetectorRef: ChangeDetectorRef) { }
+  constructor() {
+  }
 
   ngOnInit(): void {
-    this.openApprovals$ = this.dataService.openApprovals;
   }
-
-  selectStage($event) {
-    this.problemFilterEventButton?.deselect();
-    this.evaluationFilterEventButton?.deselect();
-    this.approvalFilterEventButton?.deselect();
-    this.selectedStage = $event.stage;
-    this.filterEventType = $event.filterType;
-  }
-
-  selectFilterEvent($event) {
-    if ($event.isUserInput) {
-      this.filterEventType = $event.source.selected ? $event.value : null;
-    }
-  }
-
-  countOpenApprovals(project: Project, stage: Stage, service?: Service): number {
-    return this.getOpenApprovals(project, stage, service).length;
-  }
-
-  getOpenApprovals(project: Project, stage: Stage, service?: Service): Trace[] {
-    return this.dataService.getOpenApprovals(project, stage, service);
-  }
-
-  findProblemEvent(problemEvents: Root[], service: Service): Root {
-    return problemEvents.find(root => root?.data.service === service.serviceName);
-  }
-
-  findFailedRootEvent(failedRootEvents: Root[], service: Service): Root {
-    return failedRootEvents.find(root => root.data.service === service.serviceName);
-  }
-
 }
