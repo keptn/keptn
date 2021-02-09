de0e865d4f4000cb4e56597a86793e36727152bd
ermin.muratovic <ermin.muratovic@gmail.com>
2021-02-09 16:04:03 +0100

    rename sequence-overview to sequence-view (align with other views)
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/app.module.ts
+++ bridge/client/app/app.module.ts
@@ -83,7 +83,7 @@ import { KtbEnvironmentViewComponent } from './_views/ktb-environment-view/ktb-e
 import { KtbIntegrationViewComponent } from './_views/ktb-integration-view/ktb-integration-view.component';
 import { KtbStageOverviewComponent } from './_components/ktb-stage-overview/ktb-stage-overview.component';
 import { KtbStageDetailsComponent } from './_components/ktb-stage-details/ktb-stage-details.component';
-import { KtbSequenceViewComponent } from "./_views/ktb-sequence-overview/ktb-sequence-overview.component";
+import { KtbSequenceViewComponent } from "./_views/ktb-sequence-view/ktb-sequence-view.component";
 
 registerLocaleData(localeEn, 'en');
 
