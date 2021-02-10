58c4000de43ecf2ef4e9d962e57adc61df7d0daa
ermin.muratovic <ermin.muratovic@gmail.com>
2021-02-10 15:18:02 +0100

    store started in this.started
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_models/trace.ts
+++ bridge/client/app/_models/trace.ts
@@ -286,7 +286,7 @@ class Trace {
 
   isStarted() {
     if(!this.started) {
-      this.finished = this.traces.some(t => t.type.includes(".started"));
+      this.started = this.traces.some(t => t.type.includes(".started"));
     }
 
     return this.started;
