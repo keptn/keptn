35b495d007d88124db7700bdb34b8b22545c527b
Klaus Strießnig <k.striessnig@gmail.com>
2021-02-12 16:08:32 +0100

    refresh on select, sorting, score and criteria colouring
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_components/ktb-sli-breakdown/ktb-sli-breakdown.component.ts
+++ bridge/client/app/_components/ktb-sli-breakdown/ktb-sli-breakdown.component.ts
@@ -1,5 +1,4 @@
 import {ChangeDetectorRef, Component, Input, OnInit, ViewChild} from '@angular/core';
-import {DtOverlayConfig} from "@dynatrace/barista-components/overlay";
 import { DtSort, DtTableDataSource } from '@dynatrace/barista-components/table';
 
 @Component({
@@ -11,29 +10,17 @@ export class KtbSliBreakdownComponent implements OnInit {
 
   @ViewChild('sortable', { read: DtSort, static: true }) sortable: DtSort;
 
-  public _evaluationColor = {
-    'pass': '#7dc540',
-    'warning': '#fd8232',
-    'fail': '#dc172a',
-    'info': '#f8f8f8'
+  public evaluationState = {
+    pass: 'passed',
+    warning: 'warning',
+    fail: 'failed'
   };
 
-  public _evaluationState = {
-    'pass': 'recovered',
-    'warning': 'warning',
-    'fail': 'error'
-  };
-
-  public overlayConfig: DtOverlayConfig = {
-    pinnable: true,
-    originY: 'center',
-  };
-
-  public _indicatorResults: any;
-  public _indicatorResultsFail: any = [];
-  public _indicatorResultsWarning: any = [];
-  public _indicatorResultsPass: any = [];
-  public tableEntries: DtTableDataSource<object>;
+  private _indicatorResults: any;
+  private _indicatorResultsFail: any = [];
+  private _indicatorResultsWarning: any = [];
+  private _indicatorResultsPass: any = [];
+  public tableEntries: DtTableDataSource<object> = new DtTableDataSource();
 
   @Input()
   get indicatorResults(): any {
@@ -45,63 +32,55 @@ export class KtbSliBreakdownComponent implements OnInit {
       this._indicatorResultsFail = indicatorResults.filter(i => i.status === 'fail');
       this._indicatorResultsWarning = indicatorResults.filter(i => i.status === 'warning');
       this._indicatorResultsPass = indicatorResults.filter(i => i.status !== 'fail' && i.status !== 'warning');
+      this.updateDataSource();
       this._changeDetectorRef.markForCheck();
     }
   }
 
-  constructor(private _changeDetectorRef: ChangeDetectorRef) { }
+  constructor(private _changeDetectorRef: ChangeDetectorRef) {
+  }
 
-  ngOnInit() {
-    this.tableEntries = new DtTableDataSource(this.assembleTablesEntries(this.indicatorResults));
-
-    // Set the dtSort reference on the dataSource, so it can react to sorting.
+  ngOnInit(): void {
+    this.sortable.sort('score', 'asc');
     this.tableEntries.sort = this.sortable;
   }
 
-  formatNumber(number) {
-    let n = number;
-    if(n < 1)
-      n = Math.floor(n*1000)/1000;
-    else if(n < 100)
-      n = Math.floor(n*100)/100;
-    else if(n < 1000)
-      n = Math.floor(n*10)/10;
-    else
+  private updateDataSource() {
+    this.tableEntries.data = this.assembleTablesEntries(this.indicatorResults);
+  }
+
+  private formatNumber(value: number) {
+    let n = value;
+    if (n < 1) {
+      n = Math.floor(n * 1000) / 1000;
+    } else if (n < 100) {
+      n = Math.floor(n * 100) / 100;
+    } else if (n < 1000) {
+      n = Math.floor(n * 10) / 10;
+    } else {
       n = Math.floor(n);
+    }
 
     return n;
   }
 
-  assembleTablesEntries(indicatorRestults): any {
-    var tableEntries = [];
+  private assembleTablesEntries(indicatorResults): any {
+    const totalscore  = indicatorResults.reduce((acc, result) => acc + result.score, 0);
+    let fakeScore = 0.1;
+    return indicatorResults.map(indicatorResult =>  {
+      fakeScore += 0.1;
+      return {
+        name: indicatorResult.value.metric,
+        value: this.formatNumber(indicatorResult.value.value),
+        result: indicatorResult.status,
+        score: fakeScore,//this.round(indicatorResult.score / totalscore, 2),
+        targets: indicatorResult.targets
+      };
+    });
+  }
 
-    let totalscore  = 0;
-    indicatorRestults.forEach(result => totalscore += result.score);
-
-    
-    for (let indicatorRestult of indicatorRestults) {
-      let name = indicatorRestult.value.metric;
-      let value = this.formatNumber(indicatorRestult.value.value);
-      let result = indicatorRestult.status;
-      let score = (indicatorRestult.score / totalscore).toFixed(2);
-      let criteria = "";
-
-      for (let target of indicatorRestult.targets) {
-        criteria += target.criteria + ' '
-      }
-
-      let entry = {
-        name: name,
-        value: value,
-        result: result,
-        score: score,
-        criteria: criteria
-      }
-
-      tableEntries.push(entry);
-    } 
-
-    return tableEntries;
+  private round(value: number, places: number): number {
+    return +(Math.round(Number(`${value}e+${places}`))  + `e-${places}`);
   }
 
 }
