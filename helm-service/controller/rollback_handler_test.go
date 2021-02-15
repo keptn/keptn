8130fafabf76abcb5d3fb65842900df180522c1c
warber <bernd.warmuth@dynatrace.com>
2021-02-15 10:32:59 +0100

    #3029: added rollback_handler
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

---
+++ helm-service/controller/rollback_handler_test.go
@@ -0,0 +1,57 @@
+package controller
+
+import (
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/golang/mock/gomock"
+	keptn "github.com/keptn/go-utils/pkg/lib"
+	"github.com/keptn/go-utils/pkg/lib/v0_2_0"
+	"github.com/keptn/keptn/helm-service/mocks"
+	"github.com/keptn/keptn/helm-service/pkg/helm"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHandleRollbackEvent(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockedBaseHandler := NewMockedHandler(createKeptn(), "")
+	mockedMesh := mocks.NewMockMesh(ctrl)
+	mockedConfigurationChanger := mocks.NewMockIConfigurationChanger(ctrl)
+	testGenChart := helm.GetTestGeneratedChart()
+	testUserChart := helm.GetTestUserChart()
+
+	instance := &RollbackHandler{
+		Handler:              mockedBaseHandler,
+		mesh:                 mockedMesh,
+		configurationChanger: mockedConfigurationChanger,
+	}
+
+	eventData := v0_2_0.EventData{
+		Project: "my-project",
+		Stage:   "my-stage",
+		Service: "my-service",
+	}
+	rollbackTriggeredEventData := v0_2_0.RollbackTriggeredEventData{
+		EventData: eventData,
+	}
+
+	ce := cloudevents.NewEvent()
+	_ = ce.SetData(cloudevents.ApplicationJSON, rollbackTriggeredEventData)
+
+	mockedConfigurationChanger.EXPECT().UpdateChart(gomock.Any(), gomock.Any(), gomock.Any()).Return(&testGenChart, "version", nil)
+	instance.HandleEvent(ce)
+
+	assert.Equal(t, 1, len(mockedBaseHandler.upgradeChartInvocations))
+	assert.Equal(t, keptn.Duplicate, mockedBaseHandler.upgradeChartInvocations[0].strategy)
+	assert.Equal(t, &testGenChart, mockedBaseHandler.upgradeChartInvocations[0].ch)
+	assert.Equal(t, eventData, mockedBaseHandler.upgradeChartInvocations[0].event)
+
+	assert.Equal(t, 1, len(mockedBaseHandler.upgradeChartWithReplicasInvocations))
+	assert.Equal(t, keptn.Duplicate, mockedBaseHandler.upgradeChartWithReplicasInvocations[0].strategy)
+	assert.Equal(t, &testUserChart, mockedBaseHandler.upgradeChartWithReplicasInvocations[0].ch)
+	assert.Equal(t, eventData, mockedBaseHandler.upgradeChartWithReplicasInvocations[0].event)
+	assert.Equal(t, 0, mockedBaseHandler.upgradeChartWithReplicasInvocations[0].replicas)
+
+}
--- helm-service/go.mod
+++ helm-service/go.mod
@@ -27,4 +27,6 @@ require (
 replace (
 	github.com/Azure/go-autorest => github.com/Azure/go-autorest v13.3.2+incompatible
 	github.com/docker/distribution => github.com/docker/distribution v0.0.0-20191216044856-a8371794149d
+	github.com/keptn/go-utils => ../../go-utils
+
 )
