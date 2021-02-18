d2e8d22075861fb2ac8691c56a15e2ec15b420b3
Florian Bacher <florian.bacher@dynatrace.com>
2021-02-18 11:03:51 +0100

    #3270 extracted qg backwards compatibility into its own script
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/test_quality_gates_standalone.sh
+++ test/test_quality_gates_standalone.sh
@@ -708,40 +708,6 @@ verify_using_jq "$response" ".data.service" "${SERVICE}"
 verify_using_jq "$response" ".data.result" "pass"
 verify_using_jq "$response" ".data.evaluation.comparedEvents|contains([\"${first_event_id}\"])" "false"
 
-########################################################################################################################
-# Testcase 10: backwards compatibility for 0.7.x evaluation-done events
-########################################################################################################################
-
-# feed a legacy evaluation-done event to Keptn
-echo "Testing backwards compatibility with Kepn 0.7.x evaluation-done events"
-legacy_event_context_id=$(send_event_json ./test/assets/07x_evaluation_done_event.json)
-sleep 5
-
-# check if the event is returned when checking for sh.keptn.event.evluation.finished
-response=$(get_event sh.keptn.event.evaluation.finished ${legacy_event_context_id} "legacy-project")
-
-# print the response
-echo $response | jq .
-
-verify_using_jq "$response" ".source" "lighthouse-service"
-verify_using_jq "$response" ".specversion" "1.0"
-verify_using_jq "$response" ".type" "sh.keptn.event.evaluation.finished"
-verify_using_jq "$response" ".data.project" "legacy-project"
-verify_using_jq "$response" ".data.stage" "hardening"
-verify_using_jq "$response" ".data.service" "legacy-service"
-verify_using_jq "$response" ".data.result" "fail"
-verify_using_jq "$response" ".data.evaluation.result" "fail"
-
-number_of_events=$(curl -X GET "${KEPTN_ENDPOINT}/mongodb-datastore/event/type/sh.keptn.event.evaluation.finished?filter=shkeptncontext:${legacy_event_context_id}%20AND%20AND%20AND%20data.project:legacy-project&excludeInvalidated=true" -H  "accept: application/json" -H  "x-token: ${KEPTN_API_TOKEN}" -k 2>/dev/null | jq -r '.events | length')
-
-verify_value "number of services" $number_of_events 1
-
-send_evaluation_invalidated_event "legacy-project" "hardening" "legacy-service" "evaluation-done-id" $legacy_event_context_id
-
-number_of_events=$(curl -X GET "${KEPTN_ENDPOINT}/mongodb-datastore/event/type/sh.keptn.event.evaluation.finished?filter='shkeptncontext:${legacy_event_context_id}%20AND%20AND%20AND%20data.project:legacy-project'&excludeInvalidated=true" -H  "accept: application/json" -H  "x-token: ${KEPTN_API_TOKEN}" -k 2>/dev/null | jq -r '.events | length')
-
-verify_value "number of services" $number_of_events 0
-
 echo "Quality gates standalone tests done ✓"
 
 exit 0
