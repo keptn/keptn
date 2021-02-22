0fe60f1238d81837e072f65c791366d90b2f9c92
Florian Bacher <florian.bacher@dynatrace.com>
2021-02-22 15:11:57 +0100

    #3325 increase wait time after sending problem.open event
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/test_self_healing.sh
+++ test/test_self_healing.sh
@@ -66,7 +66,7 @@ fi
 
 echo "Sending problem.open event"
 keptn_context_id=$(send_event_json ./test/assets/self_healing_problem_open_event.json)
-sleep 10
+sleep 15
 
 #response=$(curl -X GET "${KEPTN_ENDPOINT}/mongodb-datastore/event?project=${PROJECT}&type=sh.keptn.event.remediation.finished&keptnContext=${keptn_context_id}" -H  "accept: application/json" -H  "x-token: ${KEPTN_API_TOKEN}" -k 2>/dev/null | jq -r '.events[0]')
 response=$(get_keptn_event $PROJECT $keptn_context_id sh.keptn.event.remediation.finished $KEPTN_ENDPOINT $KEPTN_API_TOKEN)
