a6a9427e713b6aa87675ff73c402d9638ed2253c
Florian Bacher <florian.bacher@dynatrace.com>
2021-03-24 11:33:48 +0100

    #3465 CLI commands for managing secrets (#3596)
    * #3465 added handler for secret CLI commands
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 added handler for secret CLI commands
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 added credential manager mock
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 improve readability
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 added log output
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 fixed update command
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 updated go utils dependency
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 spelling
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #3465 spelling
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

---
+++ cli/cmd/create_secret.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/keptn/keptn/cli/pkg/credentialmanager"
+	"github.com/keptn/keptn/cli/pkg/logging"
+	"github.com/spf13/cobra"
+)
+
+type createSecretCmdParams struct {
+	Data  []string
+	Scope *string
+}
+
+var createSecretParams *createSecretCmdParams
+
+var createSecretCommand = &cobra.Command{
+	Use:          `secret SECRET_NAME --from-literal="key1=value1"" --from-literal="key2=value2" --scope=my-scope`,
+	Short:        "Creates a new secret",
+	Example:      `keptn create secret SECRET_NAME --from-literal="key1=value1"" --from-literal="key2=value2" --scope=my-scope`,
+	SilenceUsage: true,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			cmd.SilenceUsage = false
+			return errors.New("required argument SECRET_NAME not set")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		handler, err := NewSecretCmdHandler(credentialmanager.NewCredentialManager(assumeYes))
+		if err != nil {
+			return nil
+		}
+		if err := handler.CreateSecret(args[0], createSecretParams.Data, createSecretParams.Scope); err != nil {
+			return err
+		}
+		logging.PrintLog(fmt.Sprintf("Secret %s created successfully", args[0]), logging.InfoLevel)
+		return nil
+	},
+}
+
+func init() {
+	createCmd.AddCommand(createSecretCommand)
+	createSecretParams = &createSecretCmdParams{}
+	createSecretCommand.Flags().StringArrayVar(&createSecretParams.Data, "from-literal", createSecretParams.Data, "Specify a key and literal value to insert in secret (i.e. my-key=some-value)")
+	createSecretParams.Scope = createSecretCommand.Flags().StringP("scope", "s", defaultSecretScope, "The scope of the secret")
+}
---
+++ cli/cmd/update_secret.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/keptn/keptn/cli/pkg/credentialmanager"
+	"github.com/keptn/keptn/cli/pkg/logging"
+	"github.com/spf13/cobra"
+)
+
+type updateSecretCmdParams struct {
+	Data  []string
+	Scope *string
+}
+
+var updateSecretParams *updateSecretCmdParams
+
+var updateSecretCommand = &cobra.Command{
+	Use:          `secret SECRET_NAME --from-literal="key1=value1"" --from-literal="key2=value2 --scope=my-scope"`,
+	Short:        "Updates an existing secret",
+	Example:      `keptn update secret SECRET_NAME --from-literal="key1=value1"" --from-literal="key2=value2 --scope=my-scope"`,
+	SilenceUsage: true,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			cmd.SilenceUsage = false
+			return errors.New("required argument SECRET_NAME not set")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		handler, err := NewSecretCmdHandler(credentialmanager.NewCredentialManager(assumeYes))
+		if err != nil {
+			return nil
+		}
+		if err := handler.UpdateSecret(args[0], updateSecretParams.Data, updateSecretParams.Scope); err != nil {
+			return err
+		}
+		logging.PrintLog(fmt.Sprintf("Secret %s updated successfully", args[0]), logging.InfoLevel)
+		return nil
+	},
+}
+
+func init() {
+	updateCmd.AddCommand(updateSecretCommand)
+	updateSecretParams = &updateSecretCmdParams{}
+	updateSecretCommand.Flags().StringArrayVar(&updateSecretParams.Data, "from-literal", updateSecretParams.Data, "Specify a key and literal value to insert in secret (i.e. my-key=some-value)")
+	updateSecretParams.Scope = updateSecretCommand.Flags().StringP("scope", "s", defaultSecretScope, "The scope of the secret")
+}
