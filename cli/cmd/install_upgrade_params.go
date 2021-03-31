23124c0a9771decdf8d566ce7e76fb3d64b67654
Florian Bacher <florian.bacher@dynatrace.com>
2021-03-31 14:18:32 +0200

    #3350 fixed retrieval of helm chart for execution plane
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- cli/cmd/install_upgrade_params.go
+++ cli/cmd/install_upgrade_params.go
@@ -1,6 +1,9 @@
 package cmd
 
-import "github.com/keptn/keptn/cli/pkg/version"
+import (
+	"github.com/keptn/keptn/cli/pkg/version"
+	"strings"
+)
 
 const keptnInstallerHelmRepoURL = "https://storage.googleapis.com/keptn-installer/"
 
@@ -28,10 +31,21 @@ func getKeptnHelmChartRepoURL(input *string) string {
 func getExecutionPlaneServiceChartRepoURL(input *string, serviceName string) string {
 	// Determine installer version
 	if input != nil && *input != "" {
-		return *input
+		// replace the name of the keptn chart with the name of the service name.
+		// e.g. https://keptn.sh/keptn-0.8.1.tgz => https://keptn.sh/helm-service-0.8.1.tgz
+		return getServiceChartURLFromKeptnChartURL(*input, serviceName)
 	} else if version.IsOfficialKeptnVersion(Version) {
 		version, _ := version.GetOfficialKeptnVersion(Version)
 		return keptnInstallerHelmRepoURL + serviceName + "-" + version + ".tgz"
 	}
 	return keptnInstallerHelmRepoURL + "latest/" + serviceName + "-0.1.0.tgz"
+}
+
+func getServiceChartURLFromKeptnChartURL(input string, serviceName string) string {
+	serviceChartURL := input
+	split := strings.Split(serviceChartURL, "/")
+	serviceChartURL = strings.TrimSuffix(serviceChartURL, split[len(split)-1])
+	chartName := strings.Replace(split[len(split)-1], "keptn", serviceName, 1)
+	serviceChartURL = serviceChartURL + chartName
+	return serviceChartURL
 }
