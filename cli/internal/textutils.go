c90f49e2ee1c16253ea6dcd766612e6f82a68ffa
warber <bernd.warmuth@dynatrace.com>
2021-04-01 16:36:38 +0200

    added unit test
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- cli/internal/textutils.go
+++ cli/internal/textutils.go
@@ -9,21 +9,24 @@ type node interface {
 	String() string
 }
 
-func newInnerNode() innerNode {
-	return innerNode{childs: make(map[string]node)}
+func newInnerNode() *innerNode {
+	return &innerNode{
+		sortedKeys: []string{},
+		childs:     make(map[string]node),
+	}
 }
 
 type innerNode struct {
-	childs map[string]node
+	sortedKeys []string
+	childs     map[string]node
 }
 
-func (i innerNode) String() string {
-
+func (i *innerNode) String() string {
 	var sb strings.Builder
 	var entries = []string{}
 	sb.WriteString("{")
-	for k, v := range i.childs {
-		entries = append(entries, `"`+k+`"`+":"+v.String())
+	for _, e := range i.sortedKeys {
+		entries = append(entries, `"`+e+`"`+":"+i.childs[e].String())
 	}
 
 	s := strings.Join(entries, ",")
@@ -32,15 +35,25 @@ func (i innerNode) String() string {
 	return sb.String()
 }
 
-func newLeafNode() leafnode {
-	return leafnode{}
+func (i *innerNode) AddChild(name string, c node) {
+	i.childs[name] = c
+	i.sortedKeys = append(i.sortedKeys, name)
+}
+
+func (i *innerNode) GetChild(name string) node {
+	n := i.childs[name]
+	return n
+}
+
+func newLeafNode() *leafnode {
+	return &leafnode{}
 }
 
 type leafnode struct {
 	value string
 }
 
-func (l leafnode) String() string {
+func (l *leafnode) String() string {
 	return `"` + l.value + `"`
 }
 
@@ -59,18 +72,18 @@ func JSONPathToJSONObj(input []string) (string, error) {
 		pathParts := strings.Split(pathValuePair[0], ".")
 		var currentNode = root
 		for i := 0; i < len(pathParts)-1; i++ {
-			node := currentNode.childs[pathParts[i]]
+			node := currentNode.GetChild(pathParts[i])
 			if node == nil {
 				child := newInnerNode()
-				currentNode.childs[pathParts[i]] = child
+				currentNode.AddChild(pathParts[i], child)
 				currentNode = child
 			} else {
-				currentNode = node.(innerNode)
+				currentNode = node.(*innerNode)
 			}
 		}
 		leaf := newLeafNode()
 		leaf.value = pathValuePair[1]
-		currentNode.childs[pathParts[len(pathParts)-1]] = leaf
+		currentNode.AddChild(pathParts[len(pathParts)-1], leaf)
 	}
 
 	return root.String(), nil
--- cli/internal/textutils_test.go
+++ cli/internal/textutils_test.go
@@ -1,25 +1,29 @@
 package internal
 
-//
-//func Test_StrtoJSON(t *testing.T) {
-//
-//	var tests = []struct {
-//		name string
-//		in   []string
-//		out  string
-//		err  bool
-//	}{
-//		{"1", []string{""}, "", true},
-//		{"2", []string{"a.b.c=v1"}, `{"a":{"b":{"c":"v1"}}}`, false},
-//		{"3", []string{"a.b.c=v", "a.b.d=v2"}, `{"a":{"b":{"c":"v","d":"v2"}}}`, false},
-//		//{"4", []string{"a.b.c=v", "a.b.d=v2", "b.c.d.e=v3"}, `{"a":{"b":{"c":"v","d":"v2"}},"b":{"c":{"d":{"e":"v3"}}}}`, false}, //TODO this will eventually fail (unordered map)
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			out, err := JSONPathToJSONObj(tt.in)
-//			assert.Equal(t, tt.out, out)
-//			assert.Equal(t, tt.err, err != nil)
-//		})
-//	}
-//}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_JSONPathToJSONObj(t *testing.T) {
+
+	var tests = []struct {
+		name string
+		in   []string
+		out  string
+		err  bool
+	}{
+		{"1", []string{""}, "", true},
+		{"2", []string{"a.b.c=v1"}, `{"a":{"b":{"c":"v1"}}}`, false},
+		{"3", []string{"a.b.c=v", "a.b.d=v2"}, `{"a":{"b":{"c":"v","d":"v2"}}}`, false},
+		{"4", []string{"a.b.c=v", "a.b.d=v2", "b.c.d.e=v3"}, `{"a":{"b":{"c":"v","d":"v2"}},"b":{"c":{"d":{"e":"v3"}}}}`, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			out, err := JSONPathToJSONObj(tt.in)
+			assert.Equal(t, tt.out, out)
+			assert.Equal(t, tt.err, err != nil)
+		})
+	}
+}
