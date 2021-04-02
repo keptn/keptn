0f571305670bbe4efae501b102cfcde76c1d349a
ermin.muratovic <ermin.muratovic@gmail.com>
2021-04-02 15:37:05 +0200

    use default ChromeHeadless config
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/karma-headless.conf.js
+++ bridge/karma-headless.conf.js
@@ -1,9 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
-
 module.exports = function (config) {
 
   config.set({
@@ -33,23 +30,8 @@ module.exports = function (config) {
     browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
-        base: 'Chrome',
+        base: 'ChromeHeadless',
         flags: [
-          '--headless',
-          '--no-sandbox',
-          '--disable-gpu',
-          '--enable-logging',
-          '--no-default-browser-check',
-          '--no-first-run',
-          '--disable-default-apps',
-          '--disable-popup-blocking',
-          '--disable-translate',
-          '--disable-extensions',
-          '--disable-background-timer-throttling',
-          '--disable-renderer-backgrounding',
-          '--disable-device-discovery-notifications',
-          '--remote-debugging-port=9222',
-          '--disable-web-security'
         ]
       }
     },
