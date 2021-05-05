a2c5cdcd85710202937f92ee725c73157d31a4cd
ermin.muratovic <ermin.muratovic@gmail.com>
2021-05-05 10:03:31 +0200

    use DataServiceMock for unit test
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_views/ktb-settings-view/ktb-settings-view.component.spec.ts
+++ bridge/client/app/_views/ktb-settings-view/ktb-settings-view.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { KtbSettingsViewComponent } from './ktb-settings-view.component';
 import {AppModule} from "../../app.module";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {DataService} from "../../_services/data.service";
+import {DataServiceMock} from "../../_services/data.service.mock";
 
 describe('KtbSettingsViewComponent', () => {
   let component: KtbSettingsViewComponent;
@@ -12,6 +14,9 @@ describe('KtbSettingsViewComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ KtbSettingsViewComponent ],
       imports: [ AppModule, HttpClientTestingModule ],
+      providers: [
+        {provide: DataService, useClass: DataServiceMock}
+      ]
     })
     .compileComponents();
   }));
