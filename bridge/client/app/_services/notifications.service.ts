24dfc29ec4cbd6d4e6afc1472c90ea611b09ab8c
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-05-06 09:53:36 +0200

    Prevents duplicate notifications (#3974)
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/client/app/_services/notifications.service.ts
+++ bridge/client/app/_services/notifications.service.ts
@@ -17,8 +17,15 @@ export class NotificationsService {
   }
 
   addNotification(type, message) {
-    let notification = Notification.fromJSON({ type, message });
-    this._notifications.next([...this._notifications.getValue(), notification]);
+    let notification = Notification.fromJSON({type, message});
+
+    // Check if the notification to add already exists
+    const duplicateNotifications = this._notifications.getValue().filter(n => n.type === notification.type && n.message === notification.message);
+
+    // Only show notification if it is not shown yet to prevent duplicates (issue #3896 - https://github.com/keptn/keptn/issues/3896)
+    if (duplicateNotifications.length === 0) {
+      this._notifications.next([...this._notifications.getValue(), notification]);
+    }
   }
 
   removeNotification(notification) {
