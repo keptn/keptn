a10f0377cf9a8e1dfa7980575b6427a45bc787e4
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-05-06 08:18:27 +0200

    Formatting
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/client/app/_services/notifications.service.ts
+++ bridge/client/app/_services/notifications.service.ts
@@ -19,7 +19,7 @@ export class NotificationsService {
   addNotification(type, message) {
     let notification = Notification.fromJSON({type, message});
 
-    // Check if the notification to add already exists (
+    // Check if the notification to add already exists
     const duplicateNotifications = this._notifications.getValue().filter(n => n.type === notification.type && n.message === notification.message);
 
     // Only show notification if it is not shown yet to prevent duplicates (issue #3896 - https://github.com/keptn/keptn/issues/3896)
