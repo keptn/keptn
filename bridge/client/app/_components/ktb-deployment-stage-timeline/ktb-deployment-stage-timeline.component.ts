c470f84cf356630a5b8703594f84d326ca3f5e47
Klaus Strießnig <k.striessnig@gmail.com>
2021-05-28 08:54:41 +0200

    #3761 removed unused code
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_components/ktb-deployment-stage-timeline/ktb-deployment-stage-timeline.component.ts
+++ bridge/client/app/_components/ktb-deployment-stage-timeline/ktb-deployment-stage-timeline.component.ts
@@ -1,40 +1,23 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Deployment} from '../../_models/deployment';
-import {takeUntil} from 'rxjs/operators';
-import {ActivatedRoute} from '@angular/router';
-import {Subject} from 'rxjs';
 
 @Component({
   selector: 'ktb-deployment-timeline',
   templateUrl: './ktb-deployment-stage-timeline.component.html',
   styleUrls: ['./ktb-deployment-stage-timeline.component.scss']
 })
-export class KtbDeploymentStageTimelineComponent implements OnInit, OnDestroy {
+export class KtbDeploymentStageTimelineComponent {
   @Input() deployment: Deployment;
   @Input() selectedStage: string;
   @Output() selectedStageChange: EventEmitter<string> = new EventEmitter<string>();
-  private readonly unsubscribe$ = new Subject<void>();
-  public projectName: string;
 
-  constructor(private route: ActivatedRoute) { }
-
-  public ngOnInit(): void {
-    this.route.params
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(params => {
-        this.projectName = params.projectName;
-      });
-  }
+  constructor() { }
 
   public selectStage(stage: string): void {
     if (this.selectedStage !== stage) {
       this.selectedStage = stage;
       this.selectedStageChange.emit(this.selectedStage);
     }
-  }
-
-  public ngOnDestroy(): void {
-    this.unsubscribe$.next();
   }
 
 }
