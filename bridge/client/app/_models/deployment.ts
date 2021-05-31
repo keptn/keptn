d5b80bd65b2d1978ee94780528cf06b7f5278700
Klaus Strießnig <k.striessnig@gmail.com>
2021-05-31 13:18:16 +0200

    #4049 fixed merge
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_models/deployment.ts
+++ bridge/client/app/_models/deployment.ts
@@ -26,16 +26,4 @@ export class Deployment {
   public hasRemediation(stageName?: string): boolean {
     return stageName ? this.getStage(stageName)?.remediations.length > 0 : this.stages.some(s => s.remediations.length !== 0);
   }
-
-  public getStage(stage: string): {stageName: string, remediations: Sequence[], config: string } {
-    return this.stages.find(s => s.stageName === stage);
-  }
-
-  public hasStage(stage: string): boolean {
-    return this.stages.some(s => s.stageName === stage);
-  }
-
-  public hasRemediation(stageName?: string): boolean {
-    return stageName ? this.getStage(stageName)?.remediations.length > 0 : this.stages.some(s => s.remediations.length !== 0);
-  }
 }
