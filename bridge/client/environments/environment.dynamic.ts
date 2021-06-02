dae622c49a48582452eb599ce188add46933f617
ermin.muratovic <ermin.muratovic@gmail.com>
2021-06-02 13:09:33 +0200

    provide default config in dynamic environment
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/environments/environment.dynamic.ts
+++ bridge/client/environments/environment.dynamic.ts
@@ -2,6 +2,10 @@ declare var window: any;
 
 export class DynamicEnvironment {
   public get config() {
-    return window.config;
+    return window.config || {
+      "appTitle": "keptn",
+      "logoUrl": "assets/branding/logo.png",
+      "logoInvertedUrl": "assets/branding/logo_inverted.png"
+    };
   }
 }
