8e44e09c6eb7986d0f93bd13f3789fe0ad3fb942
ermin.muratovic <ermin.muratovic@gmail.com>
2021-06-02 21:53:21 +0200

    allow to load custom stylesheet from stylesheetUrl
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_services/app.init.ts
+++ bridge/client/app/_services/app.init.ts
@@ -1,32 +1,36 @@
 import { Injectable } from '@angular/core';
-import { from } from 'rxjs';
 declare var window: any;
 
 @Injectable()
 export class AppInitService {
 
   public init() {
-    return from(
-      fetch('assets/branding/app-config.json', {
-        headers : {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }).then(response => {
+    return new Promise((resolve) => {
+      fetch('assets/branding/app-config.json').then(response => {
         return response.text();
       }).then(config => {
         try {
           if(config)
             window.config = JSON.parse(config);
-          return config;
+
+          if(window.config.stylesheetUrl) {
+            let head = document.getElementsByTagName('head')[0];
+            var link = document.createElement('link');
+            link.setAttribute('rel', 'stylesheet');
+            link.setAttribute('type', 'text/css');
+            link.setAttribute('href', window.config.stylesheetUrl);
+            link.setAttribute('media', 'all');
+            head.appendChild(link);
+          }
         } catch(err) {
           console.error("Error parsing app-config.json:", err);
-          return null;
         }
+
+        return resolve(config);
       }).catch(err => {
         console.error("Error loading app-config.json.", err);
-        return null;
-      })
-    ).toPromise();
+        return resolve(null);
+      });
+    });
   }
 }
