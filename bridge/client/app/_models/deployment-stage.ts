ca786d082c22aede01823f46c60a13586a45a1c8
Klaus Strießnig <k.striessnig@gmail.com>
2021-06-07 13:13:34 +0200

    #4056 #4055 fetch evaluations if the sequence does not have any (#4224)
    * #4056 fetch evaluations if the sequence does not have any
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * #4056 fixed missing chart-link in evaluation
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * #4056 removed unnecessary code
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_models/deployment-stage.ts
+++ bridge/client/app/_models/deployment-stage.ts
@@ -1,11 +1,15 @@
 import {Sequence} from './sequence';
+import {Trace} from './trace';
 
 export class DeploymentStage {
   public stageName: string;
   public remediations: Sequence[] = [];
+  public evaluation: Trace;
+  public evaluationContext: string;
   public config: string = null;
 
-  constructor(stageName: string) {
+  constructor(stageName: string, evaluationContext: string) {
     this.stageName = stageName;
+    this.evaluationContext = evaluationContext;
   }
 }
--- bridge/client/app/_models/project.ts
+++ bridge/client/app/_models/project.ts
@@ -90,7 +90,7 @@ export class Project {
       if (service?.deploymentContext) {
         const image = service.getImageVersion();
         const deployment = deployments.find(dp => dp.version === image && dp.shkeptncontext === service.deploymentContext);
-        const stageDetails = new DeploymentStage(stage.stageName);
+        const stageDetails = new DeploymentStage(stage.stageName, service.evaluationContext);
         if (deployment) {
           deployment.stages.push(stageDetails);
         } else {
