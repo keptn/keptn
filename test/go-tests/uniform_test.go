45438085a5a1323125572ccd76e29b0f16da2ede
warber <bernd.warmuth@dynatrace.com>
2021-06-15 16:23:47 +0200

    #4103: added integration test for (un)registering keptn integration
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- test/go-tests/uniform_test.go
+++ test/go-tests/uniform_test.go
@@ -1,14 +1,18 @@
 package go_tests
 
 import (
+	"fmt"
 	keptnmodels "github.com/keptn/go-utils/pkg/api/models"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 	"github.com/keptn/keptn/shipyard-controller/models"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"testing"
+	"time"
 )
 
+// Test_UniformRegistration directly tests the API for (un)registering Keptn integrations
+// to the Keptn control plane
 func Test_UniformRegistration(t *testing.T) {
 
 	uniformIntegration := &keptnmodels.Integration{
@@ -26,6 +30,7 @@ func Test_UniformRegistration(t *testing.T) {
 
 	// register the integration at the shipyard controller
 	resp, err := ApiPOSTRequest("/controlPlane/v1/uniform/registration", uniformIntegration)
+
 	require.Nil(t, err)
 	require.Equal(t, http.StatusOK, resp.Response().StatusCode)
 
@@ -62,4 +67,51 @@ func Test_UniformRegistration(t *testing.T) {
 	err = resp.ToJSON(&integrations)
 	require.Nil(t, err)
 	require.Empty(t, integrations)
+}
+
+// Test_RegistrationOfKeptnIntegration tests whether a deployed Keptn Integration gets correctly
+// registered/unregistered to/from the Keptn control plane
+func TestRegistrationOfKeptnIntegration(t *testing.T) {
+	// install echo integration
+	deleteEchoIntegration, err := KubeCtlApplyFromURL("https://raw.githubusercontent.com/keptn-sandbox/echo-service/d071db9beb1cb6a5f278bc2cfa5803b56322bd31/deploy/service.yaml")
+	require.Nil(t, err)
+
+	// wait for echo integration registered
+	var fetchedEchoIntegration models.Integration
+	require.Eventually(t, func() bool {
+		fetchedEchoIntegration, err = getIntegrationWithName("echo-service")
+		return err == nil
+	}, time.Second*10, time.Second*1)
+
+	require.Nil(t, err)
+	require.NotNil(t, fetchedEchoIntegration)
+	require.Equal(t, "echo-service", fetchedEchoIntegration.Name)
+	require.Equal(t, "echo-service", fetchedEchoIntegration.MetaData.KubernetesMetaData.DeploymentName)
+	require.Equal(t, GetKeptnNameSpaceFromEnv(), fetchedEchoIntegration.MetaData.KubernetesMetaData.Namespace)
+	require.Equal(t, "control-plane", fetchedEchoIntegration.MetaData.Location)
+	require.Equal(t, StringArr("sh.keptn.>"), fetchedEchoIntegration.Subscription.Topics)
+
+	// uninstall echo integration
+	err = deleteEchoIntegration()
+	require.Nil(t, err)
+
+	// wait for echo integration unregistered
+	require.Eventually(t, func() bool {
+		fetchedEchoIntegration, err = getIntegrationWithName("echo-service")
+		return err != nil
+	}, time.Second*10, time.Second*1)
+}
+
+func getIntegrationWithName(name string) (models.Integration, error) {
+	resp, _ := ApiGETRequest("/controlPlane/v1/uniform/registration")
+	integrations := []models.Integration{}
+	if err := resp.ToJSON(&integrations); err != nil {
+		return models.Integration{}, err
+	}
+	for _, r := range integrations {
+		if r.Name == "echo-service" {
+			return r, nil
+		}
+	}
+	return models.Integration{}, fmt.Errorf("No Keptn Inegration with name %s found", name)
 }
