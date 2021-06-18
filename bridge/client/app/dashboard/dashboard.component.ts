150f02a06a690cc67167c8fabe322f0b27083ad1
Klaus Strießnig <k.striessnig@gmail.com>
2021-06-18 11:46:54 +0200

    #4170 regularly update projects in dashboard (#4406)
    * #4170 regularly update projects in dashboard, allow leading underscore for variable names (TSLint)
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * #4170 fixed test
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/dashboard/dashboard.component.ts
+++ bridge/client/app/dashboard/dashboard.component.ts
@@ -1,28 +1,38 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
-import {Observable} from "rxjs";
-
-import {Project} from "../_models/project";
-
-import {DataService} from "../_services/data.service";
-import {environment} from "../../environments/environment";
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subject, timer} from 'rxjs';
+import {Project} from '../_models/project';
+import {DataService} from '../_services/data.service';
+import {environment} from '../../environments/environment';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit, OnDestroy{
   public projects$: Observable<Project[]>;
-
+  private readonly _projectTimerInterval = 30 * 1000;
   public logoInvertedUrl = environment?.config?.logoInvertedUrl;
+  private readonly unsubscribe$: Subject<void> = new Subject<void>();
 
   constructor(private _changeDetectorRef: ChangeDetectorRef, private dataService: DataService) {
     this.projects$ = this.dataService.projects;
   }
 
-  loadProjects() {
+  public ngOnInit(): void {
+    timer(this._projectTimerInterval, this._projectTimerInterval)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        this.loadProjects();
+      });
+  }
+
+  public loadProjects() {
     this.dataService.loadProjects();
   }
 
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+  }
 }
