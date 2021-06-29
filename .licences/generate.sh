88c12edd1a0eebe8805afa37126d0ca9f4c2aa32
warber <bernd.warmuth@dynatrace.com>
2021-06-29 12:57:13 +0200

    #4409: script cleanup
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- .licences/generate.sh
+++ .licences/generate.sh
@@ -3,13 +3,13 @@
 go get go.elastic.co/go-licence-detector
 
 SCRIPT_FULL_PATH=$(dirname "$0")
-cd $SCRIPT_FULL_PATH
+cd "$SCRIPT_FULL_PATH" || return
 
 MODULES="api cli configuration-service distributor helm-service jmeter-service lighthouse-service mongodb-datastore remediation-service secret-service shipyard-controller statistics-service"
 
 for MODULE in $MODULES; do
    echo "🔍 Analyzing dependencies in module $MODULE"
-   ( cd ../$MODULE; go list -m -json all | go-licence-detector -depsTemplate=../.licences/templates/dependencies.md.tmpl -depsOut=../.licences/${MODULE}-dependencies.md  -overrides=../.licences/overrides/overrides.json)
+   ( cd ../"$MODULE" || return ; go list -m -json all | go-licence-detector -depsTemplate=../.licences/templates/dependencies.md.tmpl -depsOut=../.licences/"${MODULE}"-dependencies.md  -overrides=../.licences/overrides/overrides.json)
 done
 
 echo
