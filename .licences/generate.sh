eaa672b72c0e230ad5132ecc0f72e8bc317e4d4b
warber <bernd.warmuth@dynatrace.com>
2021-06-30 08:41:16 +0200

    #4409: list dependencies in one csv formatted file
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- .licences/generate.sh
+++ .licences/generate.sh
@@ -5,12 +5,16 @@ command -v go-licence-detector >/dev/null 2>&1 || { echo >&2 "This script requir
 SCRIPT_FULL_PATH=$(dirname "$0")
 cd "$SCRIPT_FULL_PATH" || return
 
-MODULES="api cli configuration-service distributor helm-service jmeter-service lighthouse-service mongodb-datastore remediation-service secret-service shipyard-controller statistics-service"
+TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'keptndeps')
+echo "storing dependencies files in ${TMP_DIR}"
 
+MODULES="api cli configuration-service distributor helm-service jmeter-service lighthouse-service mongodb-datastore remediation-service secret-service shipyard-controller statistics-service"
 for MODULE in $MODULES; do
    echo "🔍 Analyzing dependencies in module $MODULE"
-   ( cd ../"$MODULE" || return ; go list -m -json all | go-licence-detector -depsTemplate=../.licences/templates/dependencies.md.tmpl -depsOut=../.licences/"${MODULE}"-dependencies.md  -overrides=../.licences/overrides/overrides.json)
+   ( cd ../"$MODULE" || return ; go list -m -json all | go-licence-detector -depsTemplate=../.licences/templates/dependencies.csv.tmpl -depsOut="${TMP_DIR}"/"${MODULE}"-dependencies.txt  -overrides=../.licences/overrides/overrides.json)
 done
 
+cat "$TMP_DIR"/*.txt | sort | uniq > all-dependencies.txt
+
 echo
-echo "👍 done"
+echo "👍 done. written results to ./all-dependencies.txt"
