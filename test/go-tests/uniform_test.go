19ebc6990d9ddd3f235189ca7b4f7fc43e0ebf8c
Florian Bacher <florian.bacher@dynatrace.com>
2021-07-19 12:29:24 +0200

    #4615 updated integration test
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/go-tests/uniform_test.go
+++ test/go-tests/uniform_test.go
@@ -49,7 +49,6 @@ func Test_UniformRegistration_TestAPI(t *testing.T) {
 	require.NotEmpty(t, integrations)
 	require.Len(t, integrations, 1)
 	require.Equal(t, uniformIntegration.Name, integrations[0].Name)
-	require.Equal(t, uniformIntegration.MetaData, integrations[0].MetaData)
 	require.Equal(t, uniformIntegration.MetaData.DistributorVersion, integrations[0].MetaData.DistributorVersion)
 	require.Equal(t, uniformIntegration.MetaData.KubernetesMetaData, integrations[0].MetaData.KubernetesMetaData)
 	require.Equal(t, uniformIntegration.Subscription, integrations[0].Subscription)
