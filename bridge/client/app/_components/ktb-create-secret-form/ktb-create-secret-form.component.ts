5115d32761b14d90bb3cd05f13a2babef18a0ee6
ermin.muratovic <ermin.muratovic@gmail.com>
2021-07-23 11:03:44 +0200

    fix "object might be undefined"
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-create-secret-form/ktb-create-secret-form.component.ts
+++ bridge/client/app/_components/ktb-create-secret-form/ktb-create-secret-form.component.ts
@@ -39,7 +39,7 @@ export class KtbCreateSecretFormComponent {
 
       const secret: Secret = new Secret();
       secret.setName(this.createSecretForm.get('name')?.value);
-      for (const dataGroup of this.data?.controls) {
+      for (const dataGroup of this.data?.controls || []) {
         secret.addData(dataGroup.get('key')?.value, dataGroup.get('value')?.value);
       }
 
