a19d7521dc8f2fc8e96e4ecc520739db6a7821d9
Giovanni Liva <giovanni.liva@dynatrace.com>
2021-07-27 13:20:18 +0200

    Address feedback (#4323)
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- api/restapi/configure_keptn_test.go
+++ api/restapi/configure_keptn_test.go
@@ -1,14 +1,18 @@
 package restapi
 
 import (
-	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_getEnvConfig(t *testing.T) {
+	defer os.Unsetenv("MAX_AUTH_REQUESTS_PER_SECOND")
+	defer os.Unsetenv("MAX_AUTH_REQUESTS_BURST")
 	_ = os.Setenv("MAX_AUTH_REQUESTS_PER_SECOND", "0.5")
 	_ = os.Setenv("MAX_AUTH_REQUESTS_BURST", "1")
+
 	config, err := getEnvConfig()
 	require.Nil(t, err)
 	require.Equal(t, 0.5, config.AuthRequestsPerSecond)
