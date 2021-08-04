0e1de2b01538fd3a26545c1e5dafbc72afa6920c
ermin.muratovic <ermin.muratovic@gmail.com>
2021-08-04 13:58:26 +0200

    add pause/resume/abort buttons also to list
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-root-events-list/ktb-root-events-list.component.ts
+++ bridge/client/app/_components/ktb-root-events-list/ktb-root-events-list.component.ts
@@ -7,13 +7,16 @@ import {
   OnInit, Output,
   ViewEncapsulation
 } from '@angular/core';
-import {DateUtil} from '../../_utils/date.utils';
-import {filter, map, switchMap, takeUntil} from 'rxjs/operators';
-import {ActivatedRoute} from '@angular/router';
-import {DataService} from '../../_services/data.service';
-import {Subject} from 'rxjs';
-import {Project} from '../../_models/project';
-import {Sequence} from '../../_models/sequence';
+import { DateUtil } from '../../_utils/date.utils';
+import { filter, map, switchMap, takeUntil } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../../_services/data.service';
+import { Subject } from 'rxjs';
+import { Project } from '../../_models/project';
+import { Sequence, SequenceStateControl } from '../../_models/sequence';
+import { Trace } from '../../_models/trace';
+import { KtbConfirmationDialogComponent } from '../_dialogs/ktb-confirmation-dialog/ktb-confirmation-dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'ktb-root-events-list',
@@ -33,12 +36,15 @@ export class KtbRootEventsListComponent implements OnInit, OnDestroy {
   public _selectedEvent?: Sequence;
   public loading = true;
 
+  public confirmationDialogRef?: MatDialogRef<KtbConfirmationDialogComponent>;
+
   @Output() readonly selectedEventChange = new EventEmitter<{ sequence: Sequence, stage?: string }>();
 
   @Input()
   get events(): Sequence[] {
     return this._events;
   }
+
   set events(value: Sequence[]) {
     if (this._events !== value) {
       this._events = value;
@@ -50,6 +56,7 @@ export class KtbRootEventsListComponent implements OnInit, OnDestroy {
   get selectedEvent(): Sequence | undefined {
     return this._selectedEvent;
   }
+
   set selectedEvent(value: Sequence | undefined) {
     if (this._selectedEvent !== value) {
       this._selectedEvent = value;
@@ -58,7 +65,8 @@ export class KtbRootEventsListComponent implements OnInit, OnDestroy {
   }
 
   constructor(private _changeDetectorRef: ChangeDetectorRef, public dateUtil: DateUtil,
-              private route: ActivatedRoute, private dataService: DataService) { }
+              private route: ActivatedRoute, private dataService: DataService, public dialog: MatDialog) {
+  }
 
   ngOnInit() {
     this.route.params.pipe(
@@ -80,7 +88,7 @@ export class KtbRootEventsListComponent implements OnInit, OnDestroy {
 
   selectEvent(sequence: Sequence, stage?: string): void {
     this.selectedEvent = sequence;
-    this.selectedEventChange.emit({ sequence, stage });
+    this.selectedEventChange.emit({sequence, stage});
   }
 
   identifyEvent(index: number, item: Sequence): string | undefined {
@@ -93,6 +101,30 @@ export class KtbRootEventsListComponent implements OnInit, OnDestroy {
       this._changeDetectorRef.markForCheck();
       this.dataService.loadOldSequences(this.project);
     }
+  }
+
+  triggerResumeSequence(sequence: Sequence): void {
+    this.dataService.sendSequenceControl(sequence, SequenceStateControl.RESUME);
+  }
+
+  triggerPauseSequence(sequence: Sequence): void {
+    this.dataService.sendSequenceControl(sequence, SequenceStateControl.PAUSE);
+  }
+
+  triggerAbortSequence(sequence: Sequence): void {
+    const data = {
+      sequence,
+      confirmCallback: (data: any) => {
+        this.abortSequence(data.sequence);
+      }
+    };
+    this.confirmationDialogRef = this.dialog.open(KtbConfirmationDialogComponent, {
+      data,
+    });
+  }
+
+  abortSequence(sequence: Sequence): void {
+    this.dataService.sendSequenceControl(sequence, SequenceStateControl.ABORT);
   }
 
   ngOnDestroy(): void {
