2c8e63a007778bc155b7112a22997f23d76e1b77
ermin.muratovic <ermin.muratovic@gmail.com>
2021-08-19 10:31:18 +0200

    create confirmation dialog component
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/_dialogs/ktb-confirmation-dialog/ktb-confirmation-dialog.component.spec.ts
+++ bridge/client/app/_components/_dialogs/ktb-confirmation-dialog/ktb-confirmation-dialog.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { KtbConfirmationDialogComponent } from './ktb-confirmation-dialog.component';
 import { AppModule } from '../../../app.module';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { DeleteType } from '../../../_interfaces/delete';
 
 
 describe('KtbDeletionDialogComponent', () => {
   let component: KtbConfirmationDialogComponent;
   let fixture: ComponentFixture<KtbConfirmationDialogComponent>;
+  const dialogData = {
+    sequence: {
+      shkeptncontext: 'f6a38eb6-e99d-4d14-ab4c-3e94ed288b45',
+      name: 'delivery'
+    },
+    confirmCallback: (params: any) => { }
+  };
+  const dialogRefMock = {
+    close: () => { }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [KtbConfirmationDialogComponent],
-      imports: [AppModule],
+      imports: [AppModule, MatDialogModule],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: dialogData},
+        {provide: MatDialogRef, useValue: dialogRefMock}
+      ]
     })
       .compileComponents();
   });
