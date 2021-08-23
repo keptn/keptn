a6830c12bde874701b09695782b38421f6043250
Klaus Strießnig <k.striessnig@gmail.com>
2021-08-23 12:16:33 +0200

    tests for delete-confirmation dialog
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_components/_dialogs/ktb-delete-confirmation/ktb-delete-confirmation.component.spec.ts
+++ bridge/client/app/_components/_dialogs/ktb-delete-confirmation/ktb-delete-confirmation.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { KtbDeleteConfirmationComponent } from './ktb-delete-confirmation.component';
 import { AppModule } from '../../../app.module';
 
@@ -20,4 +20,91 @@ describe('KtbDeleteConfirmationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit', fakeAsync(() => {
+    // given
+    const type = 'subscription';
+    component.name = 'My item';
+    component.type = type;
+    component.dialogState = 'confirm';
+    updateDialog();
+
+    // when
+    const spyEmit = jest.spyOn(component.confirmClicked, 'emit');
+    const deleteButton: HTMLElement | null = document.querySelector('dt-confirmation-dialog-state[name=confirm] button[uitestid=dialogDeleteButton]');
+    deleteButton?.click();
+    updateDialog();
+
+    // then
+    expect(component.dialogState).toEqual('deleting');
+    expect(document.querySelector('dt-confirmation-dialog-state[name=deleting]')?.textContent?.trim()).toEqual(`Deleting ${type} ...`);
+    expect(spyEmit).toHaveBeenCalled();
+    flush();
+  }));
+
+  it('should dismiss dialog after success and 2 seconds', fakeAsync(() => {
+    // given
+    component.name = 'My item';
+    component.type = 'subscription';
+
+    // when
+    component.dialogState = 'success';
+    updateDialog();
+    expect(document.querySelector('dt-confirmation-dialog-state[name=success]')?.textContent?.trim()).toEqual('Subscription deleted successfully!');
+    tick(2010);
+    updateDialog();
+
+    // then
+    expect(component.dialogState).toBeNull();
+    isDialogClosed();
+    flush();
+  }));
+
+  it('should reset timeout on confirm', fakeAsync(() => {
+    // given
+    component.name = 'My item';
+    component.type = 'subscription';
+    component.dialogState = 'success';
+    updateDialog();
+
+    // when
+    tick(1000);
+    component.dialogState = 'confirm';
+
+    // then
+    tick(2000);
+    expect(component.dialogState).toEqual('confirm');
+  }));
+
+  it('should dismiss dialog on cancel', fakeAsync(() => {
+    // given
+    component.name = 'My item';
+    component.type = 'subscription';
+    component.dialogState = 'confirm';
+    updateDialog();
+    // when
+    const cancelButton: HTMLElement | null = document.querySelector('dt-confirmation-dialog-state[name=confirm] button[uitestid=dialogCancelButton]');
+    cancelButton?.click();
+
+    // then
+    expect(component.dialogState).toBeNull();
+    updateDialog();
+    isDialogClosed();
+    flush();
+  }));
+
+  /**
+   * dt-confirmation-dialog has a really strange behavior. This function is used to update the dialog according to its state
+   */
+  function updateDialog() {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+  }
+
+  function isDialogClosed() {
+    for (const state of Array.from(document.querySelectorAll('dt-confirmation-dialog-state'))) {
+      expect(state.textContent).toEqual('');
+    }
+  }
 });
