4bd0e1a5d7fedc1e5841e392284e90d7d5a1aec6
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-08-24 15:53:48 +0200

    feat(bridge): Show service and image in sequence list (#4733)
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/client/app/_components/ktb-services-list/ktb-services-list.component.spec.ts
+++ bridge/client/app/_components/ktb-services-list/ktb-services-list.component.spec.ts
@@ -2,10 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { KtbServicesListComponent } from './ktb-services-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppModule } from '../../app.module';
+import { Service } from '../../_models/service';
+import { ServiceMock } from '../../_models/service.mock';
 
 describe('KtbServicesListComponent', () => {
   let component: KtbServicesListComponent;
   let fixture: ComponentFixture<KtbServicesListComponent>;
+  let service: Service;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,10 +20,65 @@ describe('KtbServicesListComponent', () => {
 
     fixture = TestBed.createComponent(KtbServicesListComponent);
     component = fixture.componentInstance;
+    service = Service.fromJSON(ServiceMock);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return an image string (<image>:<version>)', () => {
+    // when
+    const imageString = component.getImageText(service);
+
+    // then
+    expect(imageString).toEqual('mongo:4.2.2');
+  });
+
+  it('should return an image string (<image>) when no version is set', () => {
+    // given
+    service.deployedImage = 'docker.io/mongo';
+
+    // when
+    const imageString = component.getImageText(service);
+
+    // then
+    expect(imageString).toEqual('mongo:docker.io/mongo');
+  });
+
+  it('should return an empty string when Service.deployedImage is not set', () => {
+    // given
+    service.deployedImage = undefined;
+
+    // when
+    const imageString = component.getImageText(service);
+
+    // then
+    expect(imageString).toEqual('');
+  });
+
+  it('should display the service name and the deployed image', () => {
+    // given
+    component.services = [service];
+    fixture.detectChanges();
+
+    // when
+    const elem = fixture.nativeElement.querySelectorAll('.dt-table-column-serviceName p a span')[1];
+
+    // then
+    expect(elem).toBeTruthy();
+    expect(elem.textContent).toEqual('mongo:4.2.2');
+  });
+
+  it('should return a link to the service', () => {
+    // given
+    service.stage = 'dev';
+
+    // when
+    const link = component.getServiceLink(service);
+
+    // then
+    expect(link).toEqual(['service', 'carts-db', 'context', 'ff8a3e69-7e5c-48ec-b668-4e96a006a505', 'stage', 'dev']);
   });
 });
---
+++ bridge/client/app/_models/service.mock.ts
@@ -0,0 +1,52 @@
+const service = {
+  "creationDate": "1629370450682259716",
+  "deployedImage": "docker.io/mongo:4.2.2",
+  "lastEventTypes": {
+    "sh.keptn.event.deployment.finished": {
+      "eventId": "d84f1451-6970-4d5d-95a6-67b711a07169",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370497069935686",
+    },
+    "sh.keptn.event.deployment.started": {
+      "eventId": "def58304-d70d-4a9e-9199-4cb2b7ea5af6",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370467306271564",
+    },
+    "sh.keptn.event.deployment.triggered": {
+      "eventId": "87cb3fb2-3079-45b7-b2a2-155b243c6b88",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370467295058136",
+    },
+    "sh.keptn.event.dev.delivery-direct.finished": {
+      "eventId": "908af98d-574c-430c-823f-be13cdc47725",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370499290976662",
+    },
+    "sh.keptn.event.dev.delivery-direct.triggered": {
+      "eventId": "01f6bf17-b51a-4bb8-809f-934f535bc733",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370467211902383",
+    },
+    "sh.keptn.event.release.finished": {
+      "eventId": "a5f963d7-6bfc-45c2-a39b-b2560a1258fa",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370497270285274",
+    },
+    "sh.keptn.event.release.started": {
+      "eventId": "22d100b4-1920-4aff-9ec8-739c520b0106",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370497189113966",
+    },
+    "sh.keptn.event.release.triggered": {
+      "eventId": "deddbd14-19b3-4aac-901a-a7bf721af6e4",
+      "keptnContext": "ff8a3e69-7e5c-48ec-b668-4e96a006a505",
+      "time": "1629370497183275952",
+    },
+  },
+  "openRemediations": null,
+  "serviceName": "carts-db",
+}
+
+// tslint:disable-next-line:no-any
+const ServiceMock: any = service;
+export { ServiceMock };
