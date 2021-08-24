ba628bea25c7cc155201ce4b05222bfb059bc8bb
Florian Bacher <florian.bacher@dynatrace.com>
2021-08-24 15:31:03 +0200

    feat(cli): Adapt output for configure bridge command (#4435) (#4942)
    * feat(cli): Adapt output for configure bridge command (#4435)
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * feat(cli): Adapt output for configure bridge command (#4435)
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * #4435 added unit test
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- cli/cmd/configure_bridge.go
+++ cli/cmd/configure_bridge.go
@@ -3,17 +3,13 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 type configureBridgeCmdParams struct {
 	User     *string
 	Password *string
 	Read     *bool
-}
-
-type configureBridgeAPIPayload struct {
-	User     string `json:"user"`
-	Password string `json:"password"`
 }
 
 var configureBridgeParams *configureBridgeCmdParams
@@ -25,15 +21,12 @@ var bridgeCmd = &cobra.Command{
 	Example:      `keptn configure bridge --user=<user> --password=<password>`,
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Warning: From version 0.9.0 of Keptn this command is not supported anymore!")
-		fmt.Println()
-		fmt.Println("You can get your login credentials e.g. by using the following kubectl commands:")
-		fmt.Println("Username - kubectl get secret -n keptn bridge-credentials -o jsonpath=\"{.data.BASIC_AUTH_USERNAME}\" | base64 --decode")
-		fmt.Println("Password - kubectl get secret -n keptn bridge-credentials -o jsonpath=\"{.data.BASIC_AUTH_PASSWORD}\" | base64 --decode")
-		fmt.Println()
-		fmt.Println("For editing the login credentials please use: 'kubectl edit secrets -n keptn bridge-credentials'")
-		fmt.Println("In order to apply the new credentials you need to restart the Keptn bridge:")
-		fmt.Println("kubectl -n keptn rollout restart deployment bridge")
+		if isBoolFlagSet(configureBridgeParams.Read) {
+			fmt.Println(getPrintSecretCommand())
+		} else {
+			fmt.Println(getReplaceSecretCommand(*configureBridgeParams))
+		}
+
 		fmt.Println()
 		fmt.Println("The URL to your Keptn Bridge can be retrieved using 'keptn status'")
 		return nil
@@ -46,4 +39,33 @@ func init() {
 	configureBridgeParams.Read = bridgeCmd.Flags().BoolP("output", "o", false, "Print the current credentials")
 	configureBridgeParams.User = bridgeCmd.Flags().StringP("user", "u", "", "The user name to login to the bridge")
 	configureBridgeParams.Password = bridgeCmd.Flags().StringP("password", "p", "", "The password to login to the bridge")
+}
+
+func getPrintSecretCommand() string {
+	builder := strings.Builder{}
+
+	builder.WriteString("You can get your login credentials e.g. by using the following kubectl commands:\n\n")
+	builder.WriteString("Username - kubectl get secret -n keptn bridge-credentials -o jsonpath=\"{.data.BASIC_AUTH_USERNAME}\" | base64 --decode\n")
+	builder.WriteString("Password - kubectl get secret -n keptn bridge-credentials -o jsonpath=\"{.data.BASIC_AUTH_PASSWORD}\" | base64 --decode\n")
+
+	return builder.String()
+}
+
+func getReplaceSecretCommand(cmdParams configureBridgeCmdParams) string {
+	user := "${BRIDGE_USER}"
+	password := "${BRIDGE_PASSWORD}"
+	if isStringFlagSet(cmdParams.User) {
+		user = *cmdParams.User
+	}
+	if isStringFlagSet(cmdParams.Password) {
+		password = *cmdParams.Password
+	}
+
+	builder := strings.Builder{}
+
+	builder.WriteString("For editing the bridge login credentials please use the following command:\n\n")
+	builder.WriteString(fmt.Sprintf("kubectl create secret -n keptn generic bridge-credentials --from-literal=\"BASIC_AUTH_USERNAME=%s\" --from-literal=\"BASIC_AUTH_PASSWORD=%s\" -oyaml --dry-run=client | kubectl replace -f -\n", user, password))
+	builder.WriteString("kubectl -n keptn rollout restart deployment bridge")
+
+	return builder.String()
 }
