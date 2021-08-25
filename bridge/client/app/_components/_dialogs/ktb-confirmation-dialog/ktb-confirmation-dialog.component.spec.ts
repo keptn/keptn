6e9edf65d99271736bd0bf95abceba8eeb3e4edd
ermin.muratovic <ermin.muratovic@gmail.com>
2021-08-25 12:33:43 +0200

    create test for closing the confirmation dialog
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/_dialogs/ktb-confirmation-dialog/ktb-confirmation-dialog.component.spec.ts
+++ bridge/client/app/_components/_dialogs/ktb-confirmation-dialog/ktb-confirmation-dialog.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { KtbConfirmationDialogComponent } from './ktb-confirmation-dialog.component';
 import { AppModule } from '../../../app.module';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { EventService } from '../../../_services/event.service';
+import { DeleteResult } from '../../../_interfaces/delete';
 
 describe('KtbDeletionDialogComponent', () => {
   let component: KtbConfirmationDialogComponent;
@@ -33,5 +35,19 @@ describe('KtbDeletionDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call callback and close the dialog when user clicked on confirm', () => {
+    // given
+    const confirmButton = fixture.nativeElement.querySelector('.danger-button');
+    const spyClose = jest.spyOn(component.dialogRef, 'close');
+    const spyConfirmCallback = jest.spyOn(component.data, 'confirmCallback');
+
+    // when
+    confirmButton.dispatchEvent(new Event('click'));
+
+    // then
+    expect(spyClose).toHaveBeenCalled();
+    expect(spyConfirmCallback).toHaveBeenCalled();
   });
 });
