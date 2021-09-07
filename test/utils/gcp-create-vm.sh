999733065f018c4974c94578006e1490ccfe8a57
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2021-09-07 09:57:34 +0200

    ci: make use of env variables, only run tutorial pipeline on manual trigger
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- test/utils/gcp-create-vm.sh
+++ test/utils/gcp-create-vm.sh
@@ -8,20 +8,21 @@ gcloud --quiet config set compute/zone "${CLOUDSDK_COMPUTE_ZONE}"
 
 echo "Creating VM Instance..."
 gcloud beta compute instances create "$VM_INSTANCE_NAME" \
---zone=us-east1-b \
---machine-type=e2-standard-2 \
---subnet=default \
---network-tier=PREMIUM \
---no-restart-on-failure \
---maintenance-policy=TERMINATE \
---preemptible --no-service-account \
---no-scopes \
---image=ubuntu-2004-focal-v20210825 \
---image-project=ubuntu-os-cloud \
---boot-disk-size=10GB \
---boot-disk-type=pd-balanced \
---boot-disk-device-name=gh-nightly-tutorial-runner \
---shielded-secure-boot \
---shielded-vtpm \
---shielded-integrity-monitoring \
---reservation-affinity=any
+  --zone="${CLOUDSDK_COMPUTE_ZONE}" \
+  --machine-type=e2-standard-2 \
+  --subnet=default \
+  --network-tier=PREMIUM \
+  --no-restart-on-failure \
+  --maintenance-policy=TERMINATE \
+  --preemptible \
+  --no-service-account \
+  --no-scopes \
+  --image=ubuntu-2004-focal-v20210825 \
+  --image-project=ubuntu-os-cloud \
+  --boot-disk-size=10GB \
+  --boot-disk-type=pd-balanced \
+  --boot-disk-device-name="$VM_INSTANCE_NAME" \
+  --shielded-secure-boot \
+  --shielded-vtpm \
+  --shielded-integrity-monitoring \
+  --reservation-affinity=any
