9bcf62b424dca4d7017e8e10f501a4de95d8b8b8
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2021-09-09 15:09:36 +0200

    test: more setup and config work
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- bridge/cypress/plugins/index.ts
+++ bridge/cypress/plugins/index.ts
@@ -1,7 +1,11 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
 
-/**
- * @type {Cypress.PluginConfig}
- */
-export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {};
+export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--window-size=1920,1080');
+      launchOptions.args.push('--force-device-scale-factor=1');
+    }
+  });
+};
--- bridge/cypress/support/index.ts
+++ bridge/cypress/support/index.ts
@@ -16,6 +16,8 @@
 // When a command from ./commands is ready to use, import with `import './commands'` syntax
 // import './commands';
 
+import 'cypress-xpath';
+
 Cypress.on('window:before:load', (window) => {
   cy.spy(window.console, 'error');
   cy.spy(window.console, 'warn');
