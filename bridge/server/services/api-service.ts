eccf8ec7e64fa08d402041938b00387eb0f34bca
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-09-15 15:48:03 +0200

    New signature for getServiceResource
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/server/services/api-service.ts
+++ bridge/server/services/api-service.ts
@@ -105,7 +105,7 @@ export class ApiService {
     return this.axios.get<Resource>(`${this.baseUrl}/configuration-service/v1/project/${projectName}/resource/shipyard.yaml`);
   }
 
-  public getServiceResource(projectName: string, stageName: string, serviceName: string, nextPageKey?: string): Promise<AxiosResponse<ResourceResponse>> {
+  public getServiceResource(projectName: string, stageName: string, serviceName: string, nextPageKey?: string | undefined): Promise<AxiosResponse<ResourceResponse>> {
     const url = `${this.baseUrl}/configuration-service/v1/project/${projectName}/stage/${stageName}/service/${serviceName}/resource`;
     const params: { [key: string]: string } = {};
     if (nextPageKey) {
