de7c76e96fe0494b7dc5fb92709668520a2fdd0a
RealAnna <anna.reale@dynatrace.com>
2021-09-17 10:20:09 +0200

    fixed tests time format
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- shipyard-controller/handler/evaluationmanager_test.go
+++ shipyard-controller/handler/evaluationmanager_test.go
@@ -89,8 +89,8 @@ func TestEvaluationManager_CreateEvaluation(t *testing.T) {
 				service: "test-service",
 				params: &operations.CreateEvaluationParams{
 					Labels:    map[string]string{"foo": "bar"},
-					Start:     "2030-01-02T15:00:00",
-					End:       "2020-01-02T16:00:00",
+					Start:     "2030-01-02T15:00:00.000Z",
+					End:       "2020-01-02T16:00:00.000Z",
 					Timeframe: "",
 				},
 			},
@@ -118,8 +118,8 @@ func TestEvaluationManager_CreateEvaluation(t *testing.T) {
 				service: "test-service",
 				params: &operations.CreateEvaluationParams{
 					Labels:    map[string]string{"foo": "bar"},
-					Start:     "2020-01-02T15:00:00",
-					End:       "2020-01-02T16:00:00",
+					Start:     "2020-01-02T15:00:00.000Z",
+					End:       "2020-01-02T16:00:00.000Z",
 					Timeframe: "",
 				},
 			},
