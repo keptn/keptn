f9ef7ea56bff1098850ff51fdc00b873befb377e
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-09-17 16:19:31 +0200

    Add unit tests
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/client/app/_interceptors/http-error-interceptor.spec.ts
+++ bridge/client/app/_interceptors/http-error-interceptor.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpHeaders } from '@angular/common/http';
 import { HttpErrorInterceptor } from './http-error-interceptor';
 import { Overlay } from '@angular/cdk/overlay';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { RETRY_ON_HTTP_ERROR } from '../_utils/app.utils';
+import { ApiService } from '../_services/api.service';
+import { NotificationsService } from '../_services/notifications.service';
+import { DtToast } from '@dynatrace/barista-components/toast';
 
 describe('HttpErrorInterceptorService', () => {
+  let httpErrorInterceptor: HttpErrorInterceptor;
+  let httpMock: HttpTestingController;
+  let apiService: ApiService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,11 +25,61 @@ describe('HttpErrorInterceptorService', () => {
         },
         {provide: RETRY_ON_HTTP_ERROR, useValue: false},
       ],
-    }).compileComponents();
+    });
+
+    httpErrorInterceptor = TestBed.inject(HttpErrorInterceptor);
+    httpMock = TestBed.inject(HttpTestingController);
+    apiService = TestBed.inject(ApiService);
   });
 
   it('should be an instance', () => {
-    const interceptor = TestBed.inject(HttpErrorInterceptor);
-    expect(interceptor).toBeTruthy();
+    expect(httpErrorInterceptor).toBeTruthy();
+  });
+
+  it('should show an generic error when unauthorized', async () => {
+    // given
+    const spy = jest.spyOn(TestBed.inject(NotificationsService), 'addNotification');
+
+
+    await apiService.getMetadata().subscribe();
+
+    const testRequest: TestRequest = httpMock.expectOne('./api/v1/metadata');
+    const errorEvent: ErrorEvent = new ErrorEvent('', {error: {}});
+    testRequest.error(errorEvent, {status: 401});
+
+    // then
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should show a toast when oauth is redirected', async () => {
+    // given
+    const spy = jest.spyOn(TestBed.inject(DtToast), 'create');
+
+    await apiService.getMetadata().subscribe();
+
+    const testRequest: TestRequest = httpMock.expectOne('./api/v1/metadata');
+    const errorEvent: ErrorEvent = new ErrorEvent('', {error: {}});
+    // @ts-ignore
+    const headers = new HttpHeaders({'keptn-auth-type': 'OAUTH'});
+    testRequest.error(errorEvent, {headers, status: 401});
+
+    // then
+    expect(spy).toHaveBeenCalledWith('Login required. Redirecting to login.');
+  });
+
+  it('should show a error notification when basic auth is unauthorized', async () => {
+    // given
+    const spy = jest.spyOn(TestBed.inject(NotificationsService), 'addNotification');
+
+    await apiService.getMetadata().subscribe();
+
+    const testRequest: TestRequest = httpMock.expectOne('./api/v1/metadata');
+    const errorEvent: ErrorEvent = new ErrorEvent('', {error: {}});
+    // @ts-ignore
+    const headers = new HttpHeaders({'keptn-auth-type': 'BASIC'});
+    testRequest.error(errorEvent, {headers, status: 401});
+
+    // then
+    expect(spy).toHaveBeenCalledWith('error', 'Login credentials invalid. Please check your provided username and password.');
   });
 });
