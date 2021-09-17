bef7c8b573e33881dcf0cca4474a70b9928d3488
Adam Gardner <adam.gardner@dynatrace.com>
2021-09-18 09:48:46 +1000

    keptn create secret CLI command
    Update `keptn create secret` CLI helper text to indicate that it can also be used to create secrets.

--- cli/cmd/create.go
+++ cli/cmd/create.go
@@ -6,8 +6,8 @@ import (
 
 // createCmd implements the create command
 var createCmd = &cobra.Command{
-	Use:   "create [project | service]",
-	Short: `Creates a new project or service`,
+	Use:   "create [project | service | secret]",
+	Short: `Creates a new project, service or secret`,
 }
 
 func init() {
