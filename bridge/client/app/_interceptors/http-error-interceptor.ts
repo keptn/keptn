082ae06e798f9d08a708f6f9b1cde2af545c3d76
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-09-17 14:19:49 +0200

    fix(bridge): Show proper error messages if not OAUTH is configured and prevent login loop(#5086)
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/client/app/_interceptors/http-error-interceptor.ts
+++ bridge/client/app/_interceptors/http-error-interceptor.ts
@@ -1,20 +1,23 @@
 import { Inject, Injectable } from '@angular/core';
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {catchError, retryWhen} from 'rxjs/operators';
-import {genericRetryStrategy} from './http-generic-retry-strategy';
-import {DtToast} from '@dynatrace/barista-components/toast';
-import {Location} from '@angular/common';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retryWhen } from 'rxjs/operators';
+import { genericRetryStrategy } from './http-generic-retry-strategy';
+import { DtToast } from '@dynatrace/barista-components/toast';
+import { Location } from '@angular/common';
 import { RETRY_ON_HTTP_ERROR } from '../_utils/app.utils';
+import { NotificationsService } from '../_services/notifications.service';
+import { NotificationType } from '../_models/notification';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpErrorInterceptor implements HttpInterceptor {
   private isReloading = false;
 
   constructor(private readonly toast: DtToast,
               private readonly location: Location,
+              private readonly notificationService: NotificationsService,
               @Inject(RETRY_ON_HTTP_ERROR) private hasRetry: boolean) {
   }
 
@@ -27,17 +30,25 @@ export class HttpErrorInterceptor implements HttpInterceptor {
         catchError((error: HttpErrorResponse) => {
 
           if (error.status === 401) {
-            if (!this.isReloading) {
+            const authType = error.headers.get('keptn-auth-type');
+            if (authType === 'OAUTH' && !this.isReloading) {
               this.isReloading = true;
               this.toast.create('Login required. Redirecting to login.');
               // Wait for few moments to let user see the toast message and navigate to external login route
               setTimeout(
                 () => window.location.href = this.location.prepareExternalUrl('/login'),
-                1000
+                1000,
               );
+            } else if (authType === 'BASIC') {
+              this.notificationService.addNotification(NotificationType.Error, 'Login credentials invalid. Please check your provided username and password.');
+            } else {
+              if (error.error === 'incorrect api key auth') {
+                this.notificationService.addNotification(NotificationType.Error, 'Could not authorize API token. Please check the provided API token.');
+              } else {
+                this.notificationService.addNotification(NotificationType.Error, 'Could not authorize. ' + error.error);
+              }
             }
-          }
-          else if (error.error instanceof ErrorEvent) {
+          } else if (error.error instanceof ErrorEvent) {
             // A client-side or network error occurred. Handle it accordingly.
             console.error('An error occurred:', error.error.message);
             this.toast.create(`${error.error.message}`);
@@ -49,7 +60,7 @@ export class HttpErrorInterceptor implements HttpInterceptor {
           }
 
           return throwError(error);
-        })
+        }),
       );
   }
 }
