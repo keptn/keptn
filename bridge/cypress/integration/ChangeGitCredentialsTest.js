4b58e93886987f6464cae061ee9814ae799fcf0d
Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
2021-09-20 16:50:38 +0200

    committing the test to change verify credential change
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>

---
+++ bridge/cypress/integration/ChangeGitCredentialsTest.js
@@ -0,0 +1,9 @@
+import BasePage from '../support/pageobjects/BasePage'
+import SettingsPage from '../support/pageobjects/SettingsPage'
+
+describe('Changing git credentials', () => {
+    it('The test changes git credentials and makes sure they changed successfully', () => {
+        
+    })
+})
+        
