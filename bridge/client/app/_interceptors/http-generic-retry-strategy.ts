625bc4800614c60dfdaf4e5d92e1944c8fabe312
Klaus Strießnig <k.striessnig@gmail.com>
2021-09-22 16:16:07 +0200

    chore(bridge): adapt retry-mechanism (#4867)
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    Signed-off-by: Dan Rollo <danrollo@gmail.com>

--- bridge/client/app/_interceptors/http-generic-retry-strategy.ts
+++ bridge/client/app/_interceptors/http-generic-retry-strategy.ts
@@ -1,36 +1,37 @@
-import { timer, throwError, Observable } from 'rxjs';
+import { Observable, throwError, timer } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export interface RetryParams {
-  maxAttempts?: number;
-  scalingDuration?: number;
-  shouldRetry?: ({status}: {status: number}) => boolean;
+  maxAttempts: number;
+  scalingDuration: number;
+  shouldRetry: ({status}: { status: number }) => boolean;
 }
 
 /**
- * Avoid retry for status codes which are part of a logical flow
- *
- *  - 401 : Unauthorized. Login flow get triggered with this status.
+ * Retry for the following status codes
+ *  - 500: Internal Server Error
+ *  - 502: Bad Gateway
+ *  - 503: Service Unavailable
+ *  - 504: Gateway Timeout
  */
-const avoidRetryFor = [401];
+const retryFor = [500, 502, 503, 504];
 
 const defaultParams: RetryParams = {
   maxAttempts: 3,
   scalingDuration: 1000,
-  shouldRetry: ({status}) => (status >= 400 && !avoidRetryFor.includes(status))
+  shouldRetry: ({status}) => (retryFor.includes(status)),
 };
 
-// tslint:disable-next-line:no-any
-export const genericRetryStrategy = (params: RetryParams = {}) => (attempts: Observable<any>) => attempts.pipe(
+export const genericRetryStrategy = (params?: RetryParams) => (attempts: Observable<HttpErrorResponse>) => attempts.pipe(
   mergeMap((error, i) => {
-    const { maxAttempts, scalingDuration, shouldRetry } = { ...defaultParams, ...params };
+    const {maxAttempts, scalingDuration, shouldRetry} = {...defaultParams, ...params};
     const retryAttempt = i + 1;
-    // @ts-ignore
+
     if (retryAttempt > maxAttempts || !shouldRetry(error)) {
       return throwError(error);
     }
-    // @ts-ignore
     return timer(retryAttempt * scalingDuration);
-  })
+  }),
 );
 
