3de549b791d3e3f7ac7c1331c0564a84cb6f413a
ermin.muratovic <ermin.muratovic@gmail.com>
2021-09-23 15:20:37 +0200

    adjust tests to use Secret model
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-create-secret-form/ktb-create-secret-form.component.spec.ts
+++ bridge/client/app/_components/ktb-create-secret-form/ktb-create-secret-form.component.spec.ts
@@ -4,6 +4,7 @@ import { KtbCreateSecretFormComponent } from './ktb-create-secret-form.component
 import { AppModule } from '../../app.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DataService } from '../../_services/data.service';
+import { Secret } from '../../_models/secret';
 
 describe('KtbCreateSecretFormComponent', () => {
   let component: KtbCreateSecretFormComponent;
@@ -32,40 +33,38 @@ describe('KtbCreateSecretFormComponent', () => {
     // given
     const createButton = fixture.nativeElement.querySelector('[uitestid=keptn-secret-create-button]');
     const spy = jest.spyOn(dataService, 'addSecret');
+    const secret: Secret = new Secret();
+    secret.name = 'test';
+    secret.scope = component.scopes[0];
+    secret.data.push({key: 'testKey', value: 'testValue'});
 
     // when
-    component.getFormControl('name')?.setValue('test');
-    component.data?.controls[0].get('key')?.setValue('testKey');
-    component.data?.controls[0].get('value')?.setValue('testValue');
+    component.getFormControl('name')?.setValue(secret.name);
+    component.data?.controls[0].get('key')?.setValue(secret.data[0].key);
+    component.data?.controls[0].get('value')?.setValue(secret.data[0].value);
     createButton.click();
 
     // then
-    expect(spy).toHaveBeenCalled();
-    const argument = spy.mock.calls[0][0];
-    expect(argument.name).toEqual('test');
-    expect(argument.scope).toEqual(component.scopes[0]);
-    expect(argument.data[0].key).toEqual('testKey');
-    expect(argument.data[0].value).toEqual('testValue');
+    expect(spy).toHaveBeenCalledWith(secret);
   });
 
   it('should create secret with selected scope', () => {
     // given
     const createButton = fixture.nativeElement.querySelector('[uitestid=keptn-secret-create-button]');
     const spy = jest.spyOn(dataService, 'addSecret');
+    const secret: Secret = new Secret();
+    secret.name = 'test';
+    secret.scope = component.scopes[1];
+    secret.data.push({key: 'testKey', value: 'testValue'});
 
     // when
-    component.getFormControl('name')?.setValue('test');
-    component.getFormControl('scope')?.setValue(component.scopes[1]);
-    component.data?.controls[0].get('key')?.setValue('testKey');
-    component.data?.controls[0].get('value')?.setValue('testValue');
+    component.getFormControl('name')?.setValue(secret.name);
+    component.getFormControl('scope')?.setValue(secret.scope);
+    component.data?.controls[0].get('key')?.setValue(secret.data[0].key);
+    component.data?.controls[0].get('value')?.setValue(secret.data[0].value);
     createButton.click();
 
     // then
-    expect(spy).toHaveBeenCalled();
-    const argument = spy.mock.calls[0][0];
-    expect(argument.name).toEqual('test');
-    expect(argument.scope).toEqual(component.scopes[1]);
-    expect(argument.data[0].key).toEqual('testKey');
-    expect(argument.data[0].value).toEqual('testValue');
+    expect(spy).toHaveBeenCalledWith(secret);
   });
 });
