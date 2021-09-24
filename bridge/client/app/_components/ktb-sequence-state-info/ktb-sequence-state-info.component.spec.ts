b74a0498a0a988540629cf8955f2c076fb9a8f04
Ermin Muratovic <ermin.muratovic@dynatrace.com>
2021-09-24 13:10:58 +0200

    feat(bridge): Align the way how sequence states are displayed #5150 (#5376)
    * create ktb-sequence-state-info component
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * show state info and allow to show latest or all stages
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * fix overflow stages-list
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * handle click on stage correctly
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * adjust isWaiting check for project dashboard (not traces loaded there)
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * add unit tests for component ktb-sequence-state-info
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * check spy only once
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * remove empty constructor
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * make getStages() return string[]
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * remove unused imports
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * getProject synchronous
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * click on stage in sequence-state-list should redirect to sequence detail page
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * do route.navigate
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * make state info not to small, but smaller than default
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * remove unused import
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * show service name and shortImageName if available
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * remove image name from state info
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * ignore undefined type
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * show sequence-state-info without stages
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * adjust getProject in tests
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * load project async
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * use waitForAsync
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * use async and make await complete with take(1)
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    Signed-off-by: Dan Rollo <danrollo@gmail.com>

---
+++ bridge/client/app/_components/ktb-sequence-state-info/ktb-sequence-state-info.component.spec.ts
@@ -0,0 +1,108 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { KtbSequenceStateInfoComponent } from './ktb-sequence-state-info.component';
+import { AppModule } from '../../app.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DataService } from '../../_services/data.service';
+import { DataServiceMock } from '../../_services/data.service.mock';
+import { Project } from '../../_models/project';
+import { filter, take } from 'rxjs/operators';
+
+describe('KtbSequenceStateInfoComponent', () => {
+  let component: KtbSequenceStateInfoComponent;
+  let fixture: ComponentFixture<KtbSequenceStateInfoComponent>;
+  let dataService: DataService;
+  const projectName = 'sockshop';
+  let project: Project;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [],
+      imports: [
+        AppModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        {
+          provide: DataService,
+          useClass: DataServiceMock,
+        }
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(KtbSequenceStateInfoComponent);
+    component = fixture.componentInstance;
+    dataService = fixture.debugElement.injector.get(DataService);
+    project = await dataService.getProject(projectName).pipe(
+      filter((p: Project | undefined): p is Project => !!p),
+      take(1)
+    ).toPromise();
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show sequence info', () => {
+    // given
+    dataService.loadSequences(project);
+    component.sequence = project.sequences[11];
+    fixture.detectChanges();
+
+    // then
+    const sequenceName = fixture.nativeElement.querySelector('[uitestid=keptn-sequence-info-sequenceName]');
+    const serviceName = fixture.nativeElement.querySelector('[uitestid=keptn-sequence-info-serviceName]');
+    const status = fixture.nativeElement.querySelector('[uitestid=keptn-sequence-info-status]');
+
+    expect(sequenceName.textContent).toEqual('delivery');
+    expect(serviceName.textContent).toEqual('carts');
+    expect(status.textContent).toEqual('succeeded');
+  });
+
+  it('should show sequence info with 3 stages', () => {
+    // given
+    dataService.loadSequences(project);
+    component.sequence = project.sequences[11];
+    fixture.detectChanges();
+
+    // then
+    const stageDetails = fixture.nativeElement.querySelectorAll('[uitestid=keptn-sequence-info-stageDetails] ktb-stage-badge');
+    expect(stageDetails.length).toEqual(3);
+
+    const firstStage = stageDetails[0].querySelector('dt-tag');
+    const secondStage = stageDetails[1].querySelector('dt-tag');
+    const thirdStage = stageDetails[2].querySelector('dt-tag');
+    expect(firstStage.textContent).toEqual('dev');
+    expect(secondStage.textContent).toEqual('staging');
+    expect(thirdStage.textContent).toEqual('production');
+  });
+
+  it('should show sequence info without stages if showStages is false', () => {
+    // given
+    dataService.loadSequences(project);
+    component.sequence = project.sequences[11];
+    component.showStages = false;
+    fixture.detectChanges();
+
+    // then
+    const stageDetails = fixture.nativeElement.querySelectorAll('[uitestid=keptn-sequence-info-stageDetails] ktb-stage-badge');
+    expect(stageDetails.length).toEqual(0);
+  });
+
+  it('should trigger click callback on stage', () => {
+    // given
+    dataService.loadSequences(project);
+    const sequence = project.sequences[11]
+    component.sequence = sequence;
+    const spy = jest.spyOn(component, 'stageClick');
+    fixture.detectChanges();
+
+    // when
+    const stageDetails = fixture.nativeElement.querySelectorAll('[uitestid=keptn-sequence-info-stageDetails] ktb-stage-badge');
+    stageDetails[0].click();
+
+    // then
+    expect(spy).toHaveBeenCalledWith(sequence, sequence.getStages()[0]);
+  });
+});
