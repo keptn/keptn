e890a5f4e04bd0646f2c621cb8f1faf4538607e5
Klaus Strießnig <k.striessnig@gmail.com>
2021-09-29 15:07:44 +0200

    removed code smells, adapted ESLint config
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_utils/test.utils.ts
+++ bridge/client/app/_utils/test.utils.ts
@@ -8,10 +8,14 @@ export class TestUtils {
       value: dataTransfer,
     });
     Object.defineProperty(event.constructor.prototype, 'preventDefault', {
-      value: () => {},
+      value: () => {
+        return;
+      },
     });
     Object.defineProperty(event.constructor.prototype, 'stopPropagation', {
-      value: () => {},
+      value: () => {
+        return;
+      },
     });
     return event;
   }
