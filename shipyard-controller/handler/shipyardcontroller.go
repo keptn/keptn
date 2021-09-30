9f2cf30efda3ea3ccd4bd4e795ac3393f77c57f5
Florian Bacher <florian.bacher@dynatrace.com>
2021-09-30 11:26:47 +0200

    removed commented code
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- shipyard-controller/handler/shipyardcontroller.go
+++ shipyard-controller/handler/shipyardcontroller.go
@@ -601,16 +601,7 @@ func (sc *shipyardController) handleFinishedEvent(event models.Event) error {
 		log.Error(msg)
 		return errors.New(msg)
 	}
-	//for _, startedEvent := range startedEvents {
-	//	if *event.Source == *startedEvent.Source {
-	//		err = sc.eventRepo.DeleteEvent(eventScope.Project, startedEvent.ID, common.StartedEvent)
-	//		if err != nil {
-	//			msg := "could not delete '.started' event with ID " + startedEvent.ID + ": " + err.Error()
-	//			log.Error(msg)
-	//			return errors.New(msg)
-	//		}
-	//	}
-	//}
+
 	// check if this was the last '.started' event
 	if len(startedEvents) == 1 {
 		triggeredEventType, err := keptnv2.ReplaceEventTypeKind(*event.Type, string(common.TriggeredEvent))
