debd9e3cc248b7d11e4bf34aa574d479be154afa
Bernd Warmuth <72415058+warber@users.noreply.github.com>
2021-09-30 13:17:40 +0200

    fix(configuration-service): deprecate get default resources endpoints (#5443)
    * marked endpoints as deprecated and rerun swagger code gen (v0.27)
    
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>
    
    * marked rest of endpoints as deprecated and rerun swagger code gen (v0.27)
    
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>
    Signed-off-by: Dan Rollo <danrollo@gmail.com>

--- configuration-service/restapi/operations/project/post_project_parameters.go
+++ configuration-service/restapi/operations/project/post_project_parameters.go
@@ -6,17 +6,20 @@ package project
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/validate"
 
 	"github.com/keptn/keptn/configuration-service/models"
 )
 
 // NewPostProjectParams creates a new PostProjectParams object
-// no default values defined in spec.
+//
+// There are no default values defined in the spec.
 func NewPostProjectParams() PostProjectParams {
 
 	return PostProjectParams{}
@@ -54,6 +57,11 @@ func (o *PostProjectParams) BindRequest(r *http.Request, route *middleware.Match
 		} else {
 			// validate body object
 			if err := body.Validate(route.Formats); err != nil {
+				res = append(res, err)
+			}
+
+			ctx := validate.WithOperationRequest(context.Background())
+			if err := body.ContextValidate(ctx, route.Formats); err != nil {
 				res = append(res, err)
 			}
 
--- configuration-service/restapi/operations/stage_resource/get_project_project_name_stage_stage_name_resource.go
+++ configuration-service/restapi/operations/stage_resource/get_project_project_name_stage_stage_name_resource.go
@@ -29,7 +29,7 @@ func NewGetProjectProjectNameStageStageNameResource(ctx *middleware.Context, han
 	return &GetProjectProjectNameStageStageNameResource{Context: ctx, Handler: handler}
 }
 
-/*GetProjectProjectNameStageStageNameResource swagger:route GET /project/{projectName}/stage/{stageName}/resource Stage Resource getProjectProjectNameStageStageNameResource
+/* GetProjectProjectNameStageStageNameResource swagger:route GET /project/{projectName}/stage/{stageName}/resource Stage Resource getProjectProjectNameStageStageNameResource
 
 Get list of stage resources
 
@@ -42,17 +42,15 @@ type GetProjectProjectNameStageStageNameResource struct {
 func (o *GetProjectProjectNameStageStageNameResource) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	route, rCtx, _ := o.Context.RouteInfo(r)
 	if rCtx != nil {
-		r = rCtx
+		*r = *rCtx
 	}
 	var Params = NewGetProjectProjectNameStageStageNameResourceParams()
-
 	if err := o.Context.BindValidRequest(r, route, &Params); err != nil { // bind params
 		o.Context.Respond(rw, r, route.Produces, route, err)
 		return
 	}
 
 	res := o.Handler.Handle(Params) // actually handle the request
-
 	o.Context.Respond(rw, r, route.Produces, route, res)
 
 }
