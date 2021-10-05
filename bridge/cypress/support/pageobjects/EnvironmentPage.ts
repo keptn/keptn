7d7e9f4b1da0073d196b8ed3b7bc75c79b1a8397
carpe-github <63784298+carpe-github@users.noreply.github.com>
2021-10-05 14:02:31 +0200

    test(bridge): initial integration tests (#5360)
    * committing the test to change verify credential change
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    * T* - adding 2 new tests '
    ProjectCreateTest.js
    KeptnSecretAddDeleteTest.js
    and page objects
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    * Adding 4 tests with mocked backend:
    C:\workspace\carpe-github-fork-keptn\keptn\bridge\cypress\integration\ChangeGitCredentialsTest.js
    C:\workspace\carpe-github-fork-keptn\keptn\bridge\cypress\integration\KeptnSecretAddDeleteTest.js
    C:\workspace\carpe-github-fork-keptn\keptn\bridge\cypress\integration\ProjectCreateTest.js
    C:\workspace\carpe-github-fork-keptn\keptn\bridge\cypress\integration\ProjectDeleteTest.js
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    * Fixing Prettier code style issues - 2nd try
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    * Fixing issues detected by eslint.
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    * Fixing eslint issues.
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    * Fixing code smell issues.
    
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
    
    Co-authored-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>

---
+++ bridge/cypress/support/pageobjects/EnvironmentPage.ts
@@ -0,0 +1,4 @@
+/// <reference types="cypress" />
+
+class EnvironmentPage {}
+export default EnvironmentPage;
---
+++ bridge/cypress/support/pageobjects/SecretsPage.ts
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+class SecretsPage {
+  clickAddSecret(): void {
+    cy.get('.dt-button > .dt-button-label').contains('Add secret').click();
+  }
+
+  addSecret(SECRET_NAME: string, SECRET_KEY: string, SECRET_VALUE: string): void {
+    this.clickAddSecret();
+    cy.get('input[uitestid="keptn-secret-name-input"]')
+      .type(SECRET_NAME)
+      .get('input[uitestid="keptn-secret-key-input"]')
+      .type(SECRET_KEY)
+      .get('input[uitestid="keptn-secret-value-input"]')
+      .type(SECRET_VALUE)
+      .get('button > span')
+      .contains('Add secret')
+      .click();
+  }
+
+  deleteSecret(SECRET_NAME: string | number | RegExp): void {
+    cy.get('dt-row.dt-row > dt-cell').contains(SECRET_NAME).next().children('button').click();
+    cy.get('span.dt-button-label').contains('Delete').click();
+  }
+}
+
+export default SecretsPage;
