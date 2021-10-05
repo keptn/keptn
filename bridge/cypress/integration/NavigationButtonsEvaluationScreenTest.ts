a9cc08df33cb492dab58eccdb36d3b3b581a15d6
Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
2021-10-05 14:16:01 +0200

    committing the test template
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>

---
+++ bridge/cypress/integration/NavigationButtonsEvaluationScreenTest.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+//import BasePage from '../support/pageobjects/BasePage';
+
+describe('Test Navigation Buttons In Evaluation Screen', () => {
+  it('The test clicks on Navigation buttons and make sure the pages are aopen respectivily ', () => {
+    //  const basePage = new BasePage();
+    //basePage.gotoServicesPage();
+  });
+});
