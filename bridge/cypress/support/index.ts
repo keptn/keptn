af4a7af3c5a1d386c934a89bc417fef1ed6898a9
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-10-06 09:53:24 +0200

    Remove comment
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/cypress/support/index.ts
+++ bridge/cypress/support/index.ts
@@ -12,9 +12,7 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
 // When a command from ./commands is ready to use, import with `import './commands'` syntax
-import './commands';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'cypress-xpath';
