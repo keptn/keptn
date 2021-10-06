276a5d797c26bc0d060f08e895cca754bfecd0a3
RealAnna <anna.reale@dynatrace.com>
2021-10-06 09:06:36 +0200

    remediation endpoint
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- remediation-service/main.go
+++ remediation-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	api "github.com/keptn/go-utils/pkg/api/utils"
 	"github.com/keptn/keptn/go-sdk/pkg/sdk"
 	"github.com/keptn/keptn/remediation-service/handler"
 	"log"
@@ -10,7 +11,7 @@ const getActionTriggeredEventType = "sh.keptn.event.get-action.triggered"
 const serviceName = "remediation-service"
 
 func main() {
-
+	go api.RunHealthEndpoint("8080")
 	log.Fatal(sdk.NewKeptn(
 		serviceName,
 		sdk.WithTaskHandler(
