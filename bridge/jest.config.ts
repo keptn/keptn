99f0fa4c6cda41dcc947af34e16bcc62af1ea9ee
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2021-10-07 09:52:21 +0200

    Exclude cypress from jest runs
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/jest.config.ts
+++ bridge/jest.config.ts
@@ -1,5 +1,6 @@
 module.exports = {
   rootDir: './',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/cypress'],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   collectCoverage: true,
