cd90acb6533f0a779ecaa8f6aea99f888f5f101a
championshuttler <shivams2799@gmail.com>
2021-10-12 19:43:13 +0530

    Remove unused printQuickAccessInstructionsForWindows func

--- cli/pkg/common/quick_access_instructions.go
+++ cli/pkg/common/quick_access_instructions.go
@@ -17,12 +17,6 @@ func printQuickAccessInstructionsForUnix(keptnNamespace, keptnReleaseDocsURL str
 	printExposeEndpointInstructions(keptnReleaseDocsURL)
 }
 
-func printQuickAccessInstructionsForWindows(keptnReleaseDocsURL string) {
-	fmt.Println("* To quickly access Keptn, you can use a port-forward and then authenticate your Keptn CLI as described here: https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install/#authenticate-keptn-cli\n")
-
-	printExposeEndpointInstructions(keptnReleaseDocsURL)
-}
-
 func printExposeEndpointInstructions(keptnReleaseDocsURL string) {
 	fmt.Println("* To expose Keptn on a public endpoint, please continue with the installation guidelines provided at:\n" +
 		" - https://keptn.sh/docs/" + keptnReleaseDocsURL + "/operate/install#install-keptn\n")
