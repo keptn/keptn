2691f8ec7528a19421d104613951bd5819d59940
Giovanni Liva <giovanni.liva@dynatrace.com>
2021-10-14 07:42:45 +0200

    fix(shipyard-controller): Correctly handle time format in evaluation manager (#5633)

--- shipyard-controller/handler/evaluationmanager_test.go
+++ shipyard-controller/handler/evaluationmanager_test.go
@@ -127,6 +127,114 @@ func TestEvaluationManager_CreateEvaluation(t *testing.T) {
 			wantErr:      &models.Error{Code: evaluationErrSendEventFailed},
 			wantEvents:   []eventTypeWithPayload{},
 		},
+		{
+			name: "time w/ timezone w/o millis",
+			fields: fields{
+				EventSender: &keptnfake.EventSender{},
+				ServiceAPI: &db_mock.ServicesDbOperationsMock{GetServiceFunc: func(project string, stage string, service string) (*models.ExpandedService, error) {
+					return &models.ExpandedService{}, nil
+				}},
+			},
+			args: args{
+				project: "test-project",
+				stage:   "test-stage",
+				service: "test-service",
+				params: &operations.CreateEvaluationParams{
+					Labels:    map[string]string{"foo": "bar"},
+					Start:     "2020-01-02T15:00:00Z",
+					End:       "2020-01-02T16:00:00Z",
+					Timeframe: "",
+				},
+			},
+			wantResponse: false,
+			wantErr:      &models.Error{Code: evaluationErrInvalidTimeframe},
+			wantEvents:   []eventTypeWithPayload{},
+		},
+		{
+			name: "time w/ timezone w/o millis",
+			fields: fields{
+				EventSender: &keptnfake.EventSender{},
+				ServiceAPI: &db_mock.ServicesDbOperationsMock{GetServiceFunc: func(project string, stage string, service string) (*models.ExpandedService, error) {
+					return &models.ExpandedService{}, nil
+				}},
+			},
+			args: args{
+				project: "test-project",
+				stage:   "test-stage",
+				service: "test-service",
+				params: &operations.CreateEvaluationParams{
+					Labels:    map[string]string{"foo": "bar"},
+					Start:     "2020-01-02T15:00:00Z",
+					End:       "2020-01-02T16:00:00Z",
+					Timeframe: "",
+				},
+			},
+			wantResponse: false,
+			wantErr:      &models.Error{Code: evaluationErrInvalidTimeframe},
+			wantEvents:   []eventTypeWithPayload{},
+		},
+		{
+			name: "time w/ timezone w/o millis w/ offset",
+			fields: fields{
+				EventSender: &keptnfake.EventSender{},
+				ServiceAPI: &db_mock.ServicesDbOperationsMock{GetServiceFunc: func(project string, stage string, service string) (*models.ExpandedService, error) {
+					return &models.ExpandedService{}, nil
+				}},
+			},
+			args: args{
+				project: "test-project",
+				stage:   "test-stage",
+				service: "test-service",
+				params: &operations.CreateEvaluationParams{
+					Labels:    map[string]string{"foo": "bar"},
+					Start:     "2020-01-02T15:00:00Z-0700",
+					End:       "2020-01-02T16:00:00Z-0700",
+					Timeframe: "",
+				},
+			},
+			wantResponse: false,
+			wantErr:      &models.Error{Code: evaluationErrInvalidTimeframe},
+			wantEvents:   []eventTypeWithPayload{},
+		},
+		{
+			name: "time w/o timezone w/o millis",
+			fields: fields{
+				EventSender: &keptnfake.EventSender{},
+				ServiceAPI: &db_mock.ServicesDbOperationsMock{GetServiceFunc: func(project string, stage string, service string) (*models.ExpandedService, error) {
+					return &models.ExpandedService{}, nil
+				}},
+			},
+			args: args{
+				project: "test-project",
+				stage:   "test-stage",
+				service: "test-service",
+				params: &operations.CreateEvaluationParams{
+					Labels:    map[string]string{"foo": "bar"},
+					Start:     "2020-01-02T15:00:00",
+					End:       "2020-01-02T16:00:00",
+					Timeframe: "",
+				},
+			},
+			wantResponse: true,
+			wantErr:      nil,
+			wantEvents: []eventTypeWithPayload{
+				{
+					eventType: keptnv2.GetTriggeredEventType("test-stage." + keptnv2.EvaluationTaskName),
+					payload: &keptnv2.EvaluationTriggeredEventData{
+						EventData: keptnv2.EventData{
+							Project: "test-project",
+							Stage:   "test-stage",
+							Service: "test-service",
+							Labels:  map[string]string{"foo": "bar"},
+						},
+						Evaluation: keptnv2.Evaluation{
+							Start: "2020-01-02T15:00:00.000Z",
+							End:   "2020-01-02T16:00:00.000Z",
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
