89f268a2fcc4200f1a2803875ed546eacf827f75
Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>
2021-10-14 21:02:21 +0200

    Remove redundant loop which had testing purpose.
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>

--- bridge/cypress/integration/navigation-buttons-evaluation-screen-test.ts
+++ bridge/cypress/integration/navigation-buttons-evaluation-screen-test.ts
@@ -68,12 +68,6 @@ describe('Test Navigation Buttons In Evaluation Screen', () => {
     cy.visit('/');
     basePage.clickProjectTile('dynatrace');
 
-    for (let i = 0; i < 50; i++) {
-      basePage.goToServicesPage().clickOnServicePanelByName('items').clickOnServiceInnerPanelByName('items');
-      basePage.gotoSettingsPage();
-      cy.log('i =' + i);
-    }
-
     basePage
       .goToServicesPage()
       .clickOnServicePanelByName('items')
