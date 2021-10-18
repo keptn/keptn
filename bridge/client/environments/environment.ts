5593482c3183f37383d9981678748a74a5a7306a
Ermin Muratovic <ermin.muratovic@dynatrace.com>
2021-10-18 11:59:54 +0200

    feat(bridge): Add ability to configure feature flags (#5211) (#5708)
    * feat(bridge): create FeatureFlagsService and read feature flags from environment
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * add TODO for example featureFlag
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * make featureFlags in service public
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * remove TODO and add ignore flag
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * prettier fix
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * Remove example feature flag
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    
    * Remove example flag
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>

--- bridge/client/environments/environment.ts
+++ bridge/client/environments/environment.ts
@@ -1,19 +1,9 @@
 import { DynamicEnvironment } from './environment.dynamic';
 
 class Environment extends DynamicEnvironment {
-  public appTitle?: string;
-  public logoUrl?: string;
-  public logoInvertedUrl?: string;
-  public production: boolean;
-  public appConfigUrl: string;
-  public baseUrl: string;
-  public pollingIntervalMillis?: number;
-
   constructor() {
     super();
-    this.production = false;
-    this.appConfigUrl = 'assets/default-branding/app-config.json';
-    this.baseUrl = '/';
+    this.featureFlags = {};
   }
 }
 
