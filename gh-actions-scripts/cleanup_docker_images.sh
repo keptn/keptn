3794817055be14a1f898006884aa5424753e99b4
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2021-10-18 14:07:43 +0200

    debugging
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- gh-actions-scripts/cleanup_docker_images.sh
+++ gh-actions-scripts/cleanup_docker_images.sh
@@ -130,10 +130,10 @@ function delete_tag() {
       -H "Authorization: JWT ${DOCKER_API_TOKEN}" \
       -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
       "https://hub.docker.com/v2/$DOCKER_ORG/api/manifests/0.9.3-dev.202110040932")
+  echo "Image Digest that will be deleted: ${image_digest}"
 
   exit 2
 
-  echo "Image Digest that will be deleted: ${image_digest}"
   curl -H "Authorization: JWT ${DOCKER_API_TOKEN}" -X DELETE "https://hub.docker.com/v2/$DOCKER_ORG/$REPO/manifests/${image_digest}"
 
 #  response=$(curl -s -o /dev/null -i -X DELETE \
