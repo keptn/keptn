d650cd71de3f4e3653091c60e91cb8306d80166c
Florian Bacher <florian.bacher@dynatrace.com>
2021-10-20 16:03:43 +0200

    fix(distributor): Send event once for each matching subscription (#5681) (#5714)
    * added integration test for overlapping subscriptions
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * fix(distributor): Send event once for each matching subscription (#5681)
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * fix lift remark
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * added unit test
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * added --quiet flag to CLI invocations
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * fix overlapping event subscriptions for remote exec mode
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * fixed merge conflicts, use subscription IDs for ceCache keys
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * nats-receiver: use subscription IDs for ceCache keys
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * pr review
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/go-tests/webhook_test.go
+++ test/go-tests/webhook_test.go
@@ -21,6 +21,7 @@ metadata:
   name: "shipyard-echo-service"
 spec:
   stages:
+    - name: "otherstage"
     - name: "dev"
       sequences:
         - name: "othersequence"
@@ -113,6 +114,29 @@ spec:
     - type: "sh.keptn.event.mytask.triggered"
       subscriptionID: ${mytask-sub-id}
       sendFinished: false
+      envFrom: 
+        - name: "secretKey"
+          secretRef:
+            name: "my-webhook-k8s-secret"
+            key: "my-key"
+      requests:
+        - "curl --header 'x-token: {{.env.secretKey}}' http://shipyard-controller:8080/v1/project/{{.data.project}}"
+        - "curl http://shipyard-controller:8080/v1/project/{{.data.project}}/stage/{{.data.stage}}"`
+
+const webhookWithOverlappingSubscriptionsYaml = `apiVersion: webhookconfig.keptn.sh/v1alpha1
+kind: WebhookConfig
+metadata:
+  name: webhook-configuration
+spec:
+  webhooks:
+    - type: "sh.keptn.event.mytask.triggered"
+      subscriptionID: ${mytask-sub-id}
+      sendFinished: true
+      requests:
+        - "curl http://shipyard-controller:8080/v1/project/{{.data.project}}/stage/{{.data.stage}}"
+    - type: "sh.keptn.event.mytask.triggered"
+      subscriptionID: ${mytask-sub-2-id}
+      sendFinished: true
       envFrom: 
         - name: "secretKey"
           secretRef:
@@ -246,6 +270,89 @@ func Test_Webhook(t *testing.T) {
 		require.Equal(t, string(keptnv2.ResultFailed), decodedEvent["result"])
 		require.Nil(t, decodedEvent["failedtask"])
 	})
+}
+
+func Test_Webhook_OverlappingSubscriptions(t *testing.T) {
+	projectName := "webhooks-subscription-overlap"
+	serviceName := "myservice"
+	stageName := "dev"
+	sequencename := "mysequence"
+	taskName := "mytask"
+
+	shipyardFilePath, err := CreateTmpShipyardFile(webhookShipyard)
+	require.Nil(t, err)
+	defer os.Remove(shipyardFilePath)
+
+	t.Logf("creating project %s", projectName)
+	err = CreateProject(projectName, shipyardFilePath, true)
+	require.Nil(t, err)
+
+	t.Logf("creating service %s", serviceName)
+	output, err := ExecuteCommand(fmt.Sprintf("keptn create service %s --project=%s", serviceName, projectName))
+
+	require.Nil(t, err)
+	require.Contains(t, output, "created successfully")
+
+	// create a secret that should be referenced in the webhook
+	_, err = ApiPOSTRequest("/secrets/v1/secret", map[string]interface{}{
+		"name":  "my-webhook-k8s-secret",
+		"scope": "keptn-webhook-service",
+		"data": map[string]string{
+			"my-key": "my-value",
+		},
+	})
+	require.Nil(t, err)
+
+	// create subscriptions for the webhook-service
+	webhookYamlWithSubscriptionIDs := webhookWithOverlappingSubscriptionsYaml
+	subscriptionID, err := CreateSubscription(t, "webhook-service", models.EventSubscription{
+		Event: keptnv2.GetTriggeredEventType(taskName),
+		Filter: models.EventSubscriptionFilter{
+			Projects: []string{projectName},
+			Stages:   []string{stageName},
+		},
+	})
+	require.Nil(t, err)
+
+	webhookYamlWithSubscriptionIDs = strings.Replace(webhookYamlWithSubscriptionIDs, "${mytask-sub-id}", subscriptionID, -1)
+
+	// create a second subscription that overlaps with the previously created one
+	subscriptionID2, err := CreateSubscription(t, "webhook-service", models.EventSubscription{
+		Event: keptnv2.GetTriggeredEventType(taskName),
+		Filter: models.EventSubscriptionFilter{
+			Projects: []string{projectName},
+			Stages:   []string{stageName, "otherstage"},
+		},
+	})
+	require.Nil(t, err)
+
+	webhookYamlWithSubscriptionIDs = strings.Replace(webhookYamlWithSubscriptionIDs, "${mytask-sub-2-id}", subscriptionID2, -1)
+
+	// wait some time to make sure the webhook service has pulled the updated subscription
+	<-time.After(20 * time.Second) // sorry :(
+
+	// now, let's add a webhook.yaml file to our service
+	webhookFilePath, err := CreateTmpFile("webhook.yaml", webhookYamlWithSubscriptionIDs)
+	require.Nil(t, err)
+	defer os.Remove(webhookFilePath)
+
+	t.Log("Adding webhook.yaml to our service")
+	_, err = ExecuteCommand(fmt.Sprintf("keptn add-resource --project=%s --service=%s --resource=%s --resourceUri=webhook/webhook.yaml --all-stages", projectName, serviceName, webhookFilePath))
+
+	require.Nil(t, err)
+
+	t.Logf("triggering sequence %s in stage %s", sequencename, stageName)
+	keptnContextID, _ := TriggerSequence(projectName, serviceName, stageName, sequencename, nil)
+
+	var taskFinishedEvent []*models.KeptnContextExtendedCE
+	require.Eventually(t, func() bool {
+		taskFinishedEvent, err = GetEventsOfType(keptnContextID, projectName, stageName, keptnv2.GetFinishedEventType(taskName))
+		if err != nil || taskFinishedEvent == nil || len(taskFinishedEvent) != 2 {
+			return false
+		}
+		return true
+	}, 30*time.Second, 3*time.Second)
+
 }
 
 func Test_WebhookWithDisabledFinishedEvents(t *testing.T) {
