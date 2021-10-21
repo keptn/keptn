e7090e5cc12c081f6c3a7cf8ca4df013577fe7d9
Florian Bacher <florian.bacher@dynatrace.com>
2021-10-21 12:43:56 +0200

    fix(cli): Use information from project endpoint to determine the first stage of a project (#5587) (#5767)
    * use cached shipyard file as fallback
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * use response of project endpoint instead of retrieving the shipyard for determining first stage
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * Revert "use cached shipyard file as fallback"
    
    This reverts commit ed81ebe6
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * adapted error message
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    Signed-off-by: Gevorg Iskandaryan <gevorg.iskandaryan@dynatrace.com>

--- cli/cmd/trigger_delivery_test.go
+++ cli/cmd/trigger_delivery_test.go
@@ -27,8 +27,8 @@ type DockerImg struct {
 	Tag   string
 }
 
-// TestNewArtifact tests the new-artifact command.
-func TestDelivery(t *testing.T) {
+// TestTriggerDelivery tests the trigger delivery command.
+func TestTriggerDelivery(t *testing.T) {
 
 	credentialmanager.MockAuthCreds = true
 	checkEndPointStatusMock = true
@@ -39,10 +39,7 @@ func TestDelivery(t *testing.T) {
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Type", "application/json")
 			w.WriteHeader(200)
-			if strings.Contains(r.RequestURI, "shipyard.yaml") {
-				w.Write([]byte(shipyardResourceMockResponse))
-				return
-			} else if strings.Contains(r.RequestURI, "v1/event") {
+			if strings.Contains(r.RequestURI, "v1/event") {
 				defer r.Body.Close()
 				bytes, err := ioutil.ReadAll(r.Body)
 				if err != nil {
@@ -72,6 +69,75 @@ func TestDelivery(t *testing.T) {
 
 	cmd := fmt.Sprintf("trigger delivery --project=%s --service=%s --stage=%s --sequence=%s "+
 		"--image=%s --tag=%s --values=a.b.c=d --mock --values=c.d=e", "sockshop", "carts", "dev", "artifact-delivery", "docker.io/keptnexamples/carts", "0.9.1")
+	_, err := executeActionCommandC(cmd)
+
+	if err != nil {
+		t.Errorf(unexpectedErrMsg, err)
+	}
+
+	select {
+	case <-receivedEvent:
+		t.Log("event has been sent successfully")
+		break
+	case <-time.After(5 * time.Second):
+		t.Error("event was not sent")
+	}
+}
+
+// TestTriggerDelivery tests the trigger delivery command.
+func TestTriggerDeliveryNoStageProvided(t *testing.T) {
+
+	credentialmanager.MockAuthCreds = true
+	checkEndPointStatusMock = true
+
+	receivedEvent := make(chan bool)
+	mocking = true
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(200)
+			if strings.Contains(r.RequestURI, "v1/project") {
+				project := &apimodels.Project{
+					ProjectName: "sockshop",
+					Stages: []*apimodels.Stage{
+						{
+							StageName: "dev",
+						},
+					},
+				}
+				marshal, _ := json.Marshal(project)
+				w.Write(marshal)
+			}
+			if strings.Contains(r.RequestURI, "v1/event") {
+				defer r.Body.Close()
+				bytes, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					t.Errorf("could not read received event payload: %s", err.Error())
+				}
+				event := &apimodels.KeptnContextExtendedCE{}
+				if err := json.Unmarshal(bytes, event); err != nil {
+					t.Errorf("could not decode received event: %s", err.Error())
+				}
+				if *event.Type != keptnv2.GetTriggeredEventType("dev.artifact-delivery") {
+					t.Errorf("did not receive correct event: %s", err.Error())
+				}
+				go func() {
+					receivedEvent <- true
+				}()
+			} else if strings.Contains(r.RequestURI, "/v1/metadata") {
+				defer r.Body.Close()
+				w.Write([]byte(metadataMockResponse))
+				return
+			}
+			return
+		}),
+	)
+	defer ts.Close()
+
+	os.Setenv("MOCK_SERVER", ts.URL)
+
+	cmd := fmt.Sprintf("trigger delivery --project=%s --service=%s --sequence=%s "+
+		"--image=%s --tag=%s --values=a.b.c=d --mock --values=c.d=e", "sockshop", "carts", "artifact-delivery", "docker.io/keptnexamples/carts", "0.9.1")
 	_, err := executeActionCommandC(cmd)
 
 	if err != nil {
