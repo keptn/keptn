f32244e7f44d5d7493308293fc7554e1e9855a3f
Florian Bacher <florian.bacher@dynatrace.com>
2021-10-28 10:25:50 +0200

    chore: Cancel integration tests when mismatch between CLI and kube context is detected (#5734)
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

---
+++ test/go-tests/init_test.go
@@ -0,0 +1,54 @@
+package go_tests
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	if err := setup(); err != nil {
+		os.Exit(-1)
+	}
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setup() error {
+	match, err := endpointsMatch()
+	if err != nil {
+		return fmt.Errorf("could not compare endpoints of kubectl context and keptn CLI: %s", err.Error())
+	}
+	if !match {
+		return errors.New("endpoint mismatch between CLI and kubectl detected")
+	}
+	return nil
+}
+
+func endpointsMatch() (bool, error) {
+	_, keptnAPIURL, err := GetApiCredentials()
+	if err != nil {
+		return false, err
+	}
+	statusCmdOutput, err := ExecuteCommand("keptn status")
+	if err != nil {
+		return false, err
+	}
+	statusOutputLines := strings.Split(statusCmdOutput, "\n")
+
+	var apiURLFromStatusCommand string
+	for _, line := range statusOutputLines {
+		if strings.Contains(line, "Successfully authenticated") {
+			endpointLineSplit := strings.Split(line, " ")
+			apiURLFromStatusCommand = endpointLineSplit[len(endpointLineSplit)-1]
+			break
+		}
+	}
+
+	if apiURLFromStatusCommand != keptnAPIURL {
+		return false, nil
+	}
+	return true, nil
+}
