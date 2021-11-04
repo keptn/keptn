7a9b7116fc8ada02a492a2dfe96ac6e5328051d5
Luis Marsano <luis.marsano@gmail.com>
2021-11-04 02:13:56 -0400

    fix(installer): set registry through global variable
    fix registry duplication
    
    Signed-off-by: Luis Marsano <luis.marsano@gmail.com>

--- installer/airgapped/install_keptn.sh
+++ installer/airgapped/install_keptn.sh
@@ -11,6 +11,10 @@ KEPTN_HELM_CHART=${2}
 KEPTN_HELM_SERVICE_HELM_CHART=${3}
 KEPTN_JMETER_SERVICE_HELM_CHART=${4}
 
+if [[ $TARGET_INTERNAL_DOCKER_REGISTRY ]]
+then GLOBAL_IMAGE_REGISTRY="global.imageRegistry=${TARGET_INTERNAL_DOCKER_REGISTRY%/},"
+else GLOBAL_IMAGE_REGISTRY=
+fi
 KEPTN_NAMESPACE=${KEPTN_NAMESPACE:-"keptn"}
 KEPTN_SERVICE_TYPE=${KEPTN_SERVICE_TYPE:-"ClusterIP"}
 
@@ -22,25 +26,25 @@ kubectl create namespace "${KEPTN_NAMESPACE}"
 
 helm upgrade keptn "${KEPTN_HELM_CHART}" --install --create-namespace -n "${KEPTN_NAMESPACE}" --wait \
 --set="control-plane.apiGatewayNginx.type=${KEPTN_SERVICE_TYPE},continuous-delivery.enabled=true,\
-control-plane.mongo.image.registry=${TARGET_INTERNAL_DOCKER_REGISTRY%/},\
+${GLOBAL_IMAGE_REGISTRY}\
 control-plane.nats.nats.image=${TARGET_INTERNAL_DOCKER_REGISTRY}nats:2.1.9-alpine3.12,\
 control-plane.nats.reloader.image=${TARGET_INTERNAL_DOCKER_REGISTRY}connecteverything/nats-server-config-reloader:0.6.0,\
 control-plane.nats.exporter.image=${TARGET_INTERNAL_DOCKER_REGISTRY}synadia/prometheus-nats-exporter:0.5.0,\
-control-plane.apiGatewayNginx.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}nginxinc/nginx-unprivileged,\
+control-plane.apiGatewayNginx.image.repository=nginxinc/nginx-unprivileged,\
 control-plane.apiGatewayNginx.image.tag=1.21.3-alpine,\
-control-plane.remediationService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/remediation-service,\
-control-plane.apiService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/api,\
-control-plane.bridge.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/bridge2,\
-control-plane.distributor.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/distributor,\
-control-plane.shipyardController.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/shipyard-controller,\
-control-plane.configurationService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/configuration-service,\
-control-plane.mongodbDatastore.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/mongodb-datastore,\
-control-plane.statisticsService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/statistics-service,\
-control-plane.lighthouseService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/lighthouse-service,\
-control-plane.secretService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/secret-service,\
-control-plane.approvalService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/approval-service,\
-control-plane.webhookService.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/webhook-service,\
-continuous-delivery.distributor.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/distributor"
+control-plane.remediationService.image.repository=keptn/remediation-service,\
+control-plane.apiService.image.repository=keptn/api,\
+control-plane.bridge.image.repository=keptn/bridge2,\
+control-plane.distributor.image.repository=keptn/distributor,\
+control-plane.shipyardController.image.repository=keptn/shipyard-controller,\
+control-plane.configurationService.image.repository=keptn/configuration-service,\
+control-plane.mongodbDatastore.image.repository=keptn/mongodb-datastore,\
+control-plane.statisticsService.image.repository=keptn/statistics-service,\
+control-plane.lighthouseService.image.repository=keptn/lighthouse-service,\
+control-plane.secretService.image.repository=keptn/secret-service,\
+control-plane.approvalService.image.repository=keptn/approval-service,\
+control-plane.webhookService.image.repository=keptn/webhook-service,\
+continuous-delivery.distributor.image.repository=keptn/distributor"
 
 if [[ $? -ne 0 ]]; then
   echo "Installing Keptn failed."
@@ -54,8 +58,9 @@ echo "Installing Keptn Helm-Service Helm Chart in Namespace ${KEPTN_NAMESPACE}"
 echo "-----------------------------------------------------------------------"
 
 helm upgrade helm-service "${KEPTN_HELM_SERVICE_HELM_CHART}" --install -n "${KEPTN_NAMESPACE}" \
---set="helmservice.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/helm-service,\
-distributor.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/distributor"
+--set="${GLOBAL_IMAGE_REGISTRY}\
+helmservice.image.repository=keptn/helm-service,\
+distributor.image.repository=keptn/distributor"
 
 if [[ $? -ne 0 ]]; then
   echo "Installing helm-service failed."
@@ -69,8 +74,9 @@ echo "Installing Keptn JMeter-Service Helm Chart in Namespace ${KEPTN_NAMESPACE}
 echo "-----------------------------------------------------------------------"
 
 helm upgrade jmeter-service "${KEPTN_JMETER_SERVICE_HELM_CHART}" --install -n "${KEPTN_NAMESPACE}" \
---set="jmeterservice.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/jmeter-service,\
-distributor.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}keptn/distributor"
+--set="${GLOBAL_IMAGE_REGISTRY}\
+jmeterservice.image.repository=keptn/jmeter-service,\
+distributor.image.repository=keptn/distributor"
 
 if [[ $? -ne 0 ]]; then
   echo "Installing jmeter-service failed."
