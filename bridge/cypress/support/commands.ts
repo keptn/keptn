58c5deb78b5467d3e2b304447fcbed8b0589f984
Klaus Strießnig <k.striessnig@gmail.com>
2021-11-05 13:27:40 +0100

    fix(bridge): Possible fix for flaky clicks in UI tests (#5909)
    * fix(bridge): Possible fix for flaky clicks in UI tests (#5909)
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * another possible fix
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * increase interval time
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * another possible fix
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * another possible fix
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * increase waiting time
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/cypress/support/commands.ts
+++ bridge/cypress/support/commands.ts
@@ -1,15 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import 'cypress-file-upload';
+/* eslint-enable import/no-extraneous-dependencies */
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Chainable<Subject> {
-    byTestId<E extends Node = HTMLElement>(id: string): Cypress.Chainable<JQuery<E>>;
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable<Subject> {
+      byTestId<E extends Node = HTMLElement>(id: string): Cypress.Chainable<JQuery<E>>;
+    }
   }
 }
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 const byTestId = (testId: string) => cy.get(`[uitestid="${testId}"]`);
-
+/* eslint-enable @typescript-eslint/explicit-function-return-type */
 // Commands have to be added by hooking them to Cypress
 Cypress.Commands.add('byTestId', byTestId);
