31242c3ac4c7b5a2136e1964670adb0b9e4fecf3
RealAnna <anna.reale@dynatrace.com>
2021-11-08 12:03:36 +0100

    feat: removed duplicate import
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- lighthouse-service/main.go
+++ lighthouse-service/main.go
@@ -11,8 +11,6 @@ import (
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/kelseyhightower/envconfig"
-
-	keptnapi "github.com/keptn/go-utils/pkg/api/utils"
 	keptncommon "github.com/keptn/go-utils/pkg/lib/keptn"
 	"github.com/keptn/keptn/lighthouse-service/event_handler"
 )
