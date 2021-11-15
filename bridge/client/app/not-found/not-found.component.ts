e9db24480c9319c46654ba6dcccf74134d2c6273
ermin.muratovic <ermin.muratovic@gmail.com>
2021-11-15 12:25:27 +0100

    add go to dashboard button and adjust message
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/not-found/not-found.component.ts
+++ bridge/client/app/not-found/not-found.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
 @Component({
+  selector: 'ktb-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
+  public routerUrl: string;
   public logoInvertedUrl = environment?.config?.logoInvertedUrl;
+
+  constructor(private router: Router) {
+    this.routerUrl = this.router.url;
+  }
 }
