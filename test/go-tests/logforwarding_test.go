51ffde2619adde4345a41a78fb9ffabc013976ef
Florian Bacher <florian.bacher@dynatrace.com>
2021-11-16 07:55:34 +0100

    test: Log output for failing LogForwarding tests (#6011)
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/go-tests/logforwarding_test.go
+++ test/go-tests/logforwarding_test.go
@@ -51,7 +51,7 @@ func Test_LogForwarding(t *testing.T) {
 	require.NotEmpty(t, keptnContextID)
 
 	// verify state
-	require.Eventually(t, func() bool {
+	require.Eventuallyf(t, func() bool {
 		states, resp, err := GetState(projectName)
 		if err != nil {
 			return false
@@ -72,7 +72,7 @@ func Test_LogForwarding(t *testing.T) {
 			return false
 		}
 		return true
-	}, 100*time.Second, 2*time.Second)
+	}, 100*time.Second, 2*time.Second, GetDiagnostics("shipyard-controller"))
 
 	// retrieve the integration for the lighthouse service
 	integrations, _, err := getIntegrations()
