f3941eadfb1cb8b3315ed65d788c2194a2f080d9
RealAnna <anna.reale@dynatrace.com>
2021-11-18 13:52:58 +0100

    feat(shipyard-controller): using opaqu key
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- shipyard-controller/handler/gracefulshutdownmiddleware.go
+++ shipyard-controller/handler/gracefulshutdownmiddleware.go
@@ -11,7 +11,7 @@ func GracefulShutdownMiddleware(wg *sync.WaitGroup) gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		wg.Add(1)
-		ctx := context.WithValue(c.Request.Context(), "wg", wg)
+		ctx := context.WithValue(c.Request.Context(), gracefulShutdownKey, wg)
 		c.Request.WithContext(ctx)
 		c.Next()
 		wg.Done()
