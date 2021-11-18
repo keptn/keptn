bcb386402f98778852302ad4c91a45ff43569355
RealAnna <anna.reale@dynatrace.com>
2021-11-18 13:48:14 +0100

    feat(shipyard-controller): added  handler middleware
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

---
+++ shipyard-controller/handler/gracefulshutdownmiddleware.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"sync"
+)
+
+// GracefulShutdownMiddleware syncronise active handlers to enable graceful shutdown
+func GracefulShutdownMiddleware(wg *sync.WaitGroup) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		wg.Add(1)
+		ctx := context.WithValue(c.Request.Context(), "wg", wg)
+		c.Request.WithContext(ctx)
+		c.Next()
+		wg.Done()
+	}
+}
