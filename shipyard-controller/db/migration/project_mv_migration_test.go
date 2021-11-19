a67d618937e1c07b71f66bdb56f144f04d87187f
warber <bernd.warmuth@dynatrace.com>
2021-11-19 12:04:34 +0100

    enhanced unit tests
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- shipyard-controller/db/migration/project_mv_migration_test.go
+++ shipyard-controller/db/migration/project_mv_migration_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/keptn/keptn/shipyard-controller/db"
 	"github.com/keptn/keptn/shipyard-controller/models"
 	logger "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tryvium-travels/memongo"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -37,7 +38,7 @@ func setupLocalMongoDB() func() {
 	return func() { mongoServer.Stop() }
 }
 
-func Test_MigrateKeys(t *testing.T) {
+func Test_MigratorRunsOnOldData(t *testing.T) {
 	defer setupLocalMongoDB()()
 
 	project := &models.ExpandedProject{
@@ -57,19 +58,65 @@ func Test_MigrateKeys(t *testing.T) {
 		},
 	}
 
-	projectRepo := db.NewMongoDBKeyEncodingProjectsRepo(db.GetMongoDBConnectionInstance())
-
+	// insert old data
+	projectRepo := db.NewMongoDBProjectsRepo(db.GetMongoDBConnectionInstance())
 	err := projectRepo.CreateProject(project)
 	require.Nil(t, err)
 
+	// migrate data
 	projectMVMigrator := NewProjectMVMigrator(db.GetMongoDBConnectionInstance())
-	projectMVMigrator.MigrateKeys()
+	err = projectMVMigrator.MigrateKeys()
+	require.Nil(t, err)
 
+	// get data using not encoding repo
+	insertedProject, err := projectRepo.GetProject("test-project")
+	require.Nil(t, err)
+	assert.NotContains(t, insertedProject.Stages[0].Services[0].LastEventTypes, "sh.keptn.event.get-sli.start")
+	assert.NotContains(t, insertedProject.Stages[0].Services[0].LastEventTypes, `sh.keptn.event.get-s\u022e\u022eli.started`)
+
+	// get data using encoding aware repo
+	encodingAwareProjectRepo := db.NewMongoDBKeyEncodingProjectsRepo(db.GetMongoDBConnectionInstance())
+	insertedProject, err = encodingAwareProjectRepo.GetProject("test-project")
+	require.Nil(t, err)
+	assert.Contains(t, insertedProject.Stages[0].Services[0].LastEventTypes, "sh.keptn.event.get-sli.start")
+	assert.Contains(t, insertedProject.Stages[0].Services[0].LastEventTypes, `sh.keptn.event.get-s\u022e\u022eli.started`)
+}
+
+func Test_MigratorRunsOnAlreadyMigratedData(t *testing.T) {
+	defer setupLocalMongoDB()()
+
+	project := &models.ExpandedProject{
+		ProjectName: "test-project",
+		Stages: []*models.ExpandedStage{
+			{
+				Services: []*models.ExpandedService{
+					{
+						LastEventTypes: map[string]models.EventContext{
+							`sh.keptn.event.get-sli.start`:               {},
+							`sh.keptn.event.get-s\u022e\u022eli.started`: {},
+						},
+						ServiceName: "test-service",
+					},
+				},
+			},
+		},
+	}
+
+	// insert correctly formatted data
+	projectRepo := db.NewMongoDBKeyEncodingProjectsRepo(db.GetMongoDBConnectionInstance())
+	err := projectRepo.CreateProject(project)
+	require.Nil(t, err)
+
+	// migrate data
+	projectMVMigrator := NewProjectMVMigrator(db.GetMongoDBConnectionInstance())
+	err = projectMVMigrator.MigrateKeys()
+	require.Nil(t, err)
+
+	// getting data
 	insertedProject, err := projectRepo.GetProject("test-project")
 	require.Nil(t, err)
 
-	for k, _ := range insertedProject.Stages[0].Services[0].LastEventTypes {
-		fmt.Println(k)
-	}
+	assert.Contains(t, insertedProject.Stages[0].Services[0].LastEventTypes, "sh.keptn.event.get-sli.start")
+	assert.Contains(t, insertedProject.Stages[0].Services[0].LastEventTypes, `sh.keptn.event.get-s\u022e\u022eli.started`)
 
 }
