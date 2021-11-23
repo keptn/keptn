4fb9193d5f63e87b2c142951196af20eb8dc783d
ermin.muratovic <ermin.muratovic@gmail.com>
2021-11-23 13:07:41 +0100

    remove unused imports
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-webhook-settings/ktb-webhook-settings.component.spec.ts
+++ bridge/client/app/_components/ktb-webhook-settings/ktb-webhook-settings.component.spec.ts
@@ -6,8 +6,6 @@ import { AbstractControl } from '@angular/forms';
 import { WebhookConfigMock } from '../../_services/_mockData/webhook-config.mock';
 import { DataService } from '../../_services/data.service';
 import { DataServiceMock } from '../../_services/data.service.mock';
-import { Secret } from '../../_models/secret';
-import { SecretScope } from '../../../../shared/interfaces/secret-scope';
 
 describe('KtbWebhookSettingsComponent', () => {
   let component: KtbWebhookSettingsComponent;
