6dd18334c882d5da748bef8c7f6f513c05618944
Florian Bacher <florian.bacher@dynatrace.com>
2021-12-01 12:01:34 +0100

    fix(mongodb-datastore): Ensure MongoDB Client is not nil before retrieving database (#6251) (#6255)
    * fix(mongodb-datastore): Ensure MongoDB Client is not nil before retrieving database (#6251)
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * cleanup test
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- mongodb-datastore/db/mongodb_connection.go
+++ mongodb-datastore/db/mongodb_connection.go
@@ -19,7 +19,7 @@ var mongoConnectionOnce sync.Once
 
 // MongoDBConnection takes care of establishing a connection to the mongodb
 type MongoDBConnection struct {
-	Client *mongo.Client
+	client *mongo.Client
 }
 
 func GetMongoDBConnectionInstance() *MongoDBConnection {
@@ -33,6 +33,14 @@ func getDatabaseName() string {
 	return os.Getenv("MONGODB_DATABASE")
 }
 
+func (m *MongoDBConnection) GetClient() (*mongo.Client, error) {
+	err := m.EnsureDBConnection()
+	if err != nil {
+		return nil, err
+	}
+	return m.client, nil
+}
+
 // EnsureDBConnection makes sure a connection to the mongodb is established
 func (m *MongoDBConnection) EnsureDBConnection() error {
 	mutex.Lock()
@@ -41,10 +49,10 @@ func (m *MongoDBConnection) EnsureDBConnection() error {
 	// attention: not calling the cancel() function likely causes memory leaks
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	if m.Client == nil {
+	if m.client == nil {
 		fmt.Println("No MongoDB client has been initialized yet. Creating a new one.")
 		return m.connectMongoDBClient()
-	} else if err = m.Client.Ping(ctx, nil); err != nil {
+	} else if err = m.client.Ping(ctx, nil); err != nil {
 		fmt.Println("MongoDB client lost connection. Attempt reconnect.")
 		return m.connectMongoDBClient()
 	}
@@ -58,7 +66,7 @@ func (m *MongoDBConnection) connectMongoDBClient() error {
 	if err != nil {
 		return fmt.Errorf("failed to create mongo client: %v", err)
 	}
-	m.Client, err = mongo.NewClient(options.Client().ApplyURI(connectionString))
+	m.client, err = mongo.NewClient(options.Client().ApplyURI(connectionString))
 	if err != nil {
 		err := fmt.Errorf("failed to create mongo client: %v", err)
 		return err
@@ -67,7 +75,7 @@ func (m *MongoDBConnection) connectMongoDBClient() error {
 
 	defer cancel()
 
-	err = m.Client.Connect(ctx)
+	err = m.client.Connect(ctx)
 	if err != nil {
 		err := fmt.Errorf("failed to connect client to MongoDB: %v", err)
 		return err
