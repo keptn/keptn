0d5ff4002f9066f7ade3addd22da530c38b803f3
Klaus Strießnig <k.striessnig@gmail.com>
2021-12-06 08:30:38 +0100

    fix(bridge): Fixed environment screen update issues (#6271)
    * fix(bridge): Fixed environment screen update issues (#6271)
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * fixed adding/removing of services
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * fixed UI test
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>
    
    * strict equals
    
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_models/stage.ts
+++ bridge/client/app/_models/stage.ts
@@ -14,6 +14,43 @@ export class Stage extends st {
     return stage;
   }
 
+  public update(newStage: Stage): void {
+    this.parentStages = newStage.parentStages;
+    for (const newService of newStage.services) {
+      const existingService = this.services.find((service) => service.serviceName === newService.serviceName);
+      if (existingService) {
+        // update existing service
+        Object.assign(existingService, newService);
+      } else {
+        // add new service
+        this.services.push(newService);
+      }
+    }
+    // remove deleted services
+    for (let i = 0; i < this.services.length; ) {
+      if (!newStage.services.some((service) => service.serviceName === this.services[i].serviceName)) {
+        this.services.splice(i, 1);
+        --i;
+      }
+      ++i;
+    }
+    this.services.sort(this.compareServices());
+  }
+
+  private compareServices() {
+    return (a: Service, b: Service): number => {
+      if (!a.latestSequence && !b.latestSequence) {
+        return new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime();
+      } else if (!a.latestSequence) {
+        return 1;
+      } else if (!b.latestSequence) {
+        return -1;
+      } else {
+        return new Date(b.latestSequence.time).getTime() - new Date(a.latestSequence.time).getTime();
+      }
+    };
+  }
+
   public getServicesWithOpenApprovals(): Service[] {
     return this.services.filter((s) => s.getOpenApprovals().length > 0);
   }
