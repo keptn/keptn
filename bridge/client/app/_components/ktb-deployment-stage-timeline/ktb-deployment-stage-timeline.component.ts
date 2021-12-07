37f5ae39ab65011e1dfd4ecd25c28208294c5039
Klaus Strießnig <k.striessnig@gmail.com>
2021-12-07 15:32:24 +0100

    removed unused code
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_components/ktb-deployment-stage-timeline/ktb-deployment-stage-timeline.component.ts
+++ bridge/client/app/_components/ktb-deployment-stage-timeline/ktb-deployment-stage-timeline.component.ts
@@ -1,27 +1,20 @@
-import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Deployment } from '../../_models/deployment';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ktb-deployment-timeline[deployment]',
   templateUrl: './ktb-deployment-stage-timeline.component.html',
   styleUrls: ['./ktb-deployment-stage-timeline.component.scss'],
 })
-export class KtbDeploymentStageTimelineComponent implements OnDestroy {
+export class KtbDeploymentStageTimelineComponent {
   @Input() deployment?: Deployment;
   @Input() selectedStage?: string;
   @Output() selectedStageChange: EventEmitter<string> = new EventEmitter<string>();
-  private unsubscribe$: Subject<void> = new Subject();
 
   public selectStage(stage: string): void {
     if (this.selectedStage !== stage) {
       this.selectedStage = stage;
       this.selectedStageChange.emit(this.selectedStage);
     }
-  }
-
-  public ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
   }
 }
