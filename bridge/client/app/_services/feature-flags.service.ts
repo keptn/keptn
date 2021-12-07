9afb2bd8591dd47562c503840fc261049b2c322f
Klaus Strießnig <k.striessnig@gmail.com>
2021-12-07 08:05:00 +0100

    unsubscribe feature flags
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_services/feature-flags.service.ts
+++ bridge/client/app/_services/feature-flags.service.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { DataService } from './data.service';
 import { KeptnInfo } from '../_models/keptn-info';
-import { filter } from 'rxjs/operators';
+import { filter, takeUntil } from 'rxjs/operators';
 import { IClientFeatureFlags } from '../../../shared/interfaces/feature-flags';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FeatureFlagsService {
+export class FeatureFlagsService implements OnDestroy {
   public featureFlags?: IClientFeatureFlags;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(dataService: DataService) {
-    dataService.keptnInfo.pipe(filter((info): info is KeptnInfo => !!info)).subscribe((info) => {
-      this.featureFlags = info.bridgeInfo.featureFlags;
-    });
+    dataService.keptnInfo
+      .pipe(
+        filter((info): info is KeptnInfo => !!info),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe((info) => {
+        this.featureFlags = info.bridgeInfo.featureFlags;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
