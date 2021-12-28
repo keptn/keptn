da559832460a61d6f57f730696636c4bc986bfdd
Florian Bacher <florian.bacher@dynatrace.com>
2021-12-28 14:50:03 +0100

    service resource handler implementation
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- resource-service/handler/service_resource_handler.go
+++ resource-service/handler/service_resource_handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/keptn/keptn/resource-service/models"
+	"net/http"
 )
 
 type IServiceResourceHandler interface {
@@ -33,13 +35,38 @@ func NewServiceResourceHandler(serviceResourceManager IResourceManager) *Service
 // @Param	project				path	string	true	"The name of the project"
 // @Param	stage				path	string	true	"The name of the stage"
 // @Param	service				path	string	true	"The name of the service"
-// @Param   resources     body    models.CreateResourcesParams     true        "List of resources"
+// @Param   resources     body    models.CreateResourcesPayload     true        "List of resources"
 // @Success 204 {string} "ok"
 // @Failure 400 {object} models.Error "Invalid payload"
 // @Failure 500 {object} models.Error "Internal error"
 // @Router /project/{project}/stage/{stage}/service/{service}/resource [post]
 func (ph *ServiceResourceHandler) CreateServiceResources(c *gin.Context) {
+	params := &models.CreateResourcesParams{
+		Project: models.Project{ProjectName: c.Param(pathParamProjectName)},
+		Stage:   &models.Stage{StageName: c.Param(pathParamStageName)},
+		Service: &models.Service{ServiceName: c.Param(pathParamServiceName)},
+	}
 
+	createResources := &models.CreateResourcesPayload{}
+	if err := c.ShouldBindJSON(createResources); err != nil {
+		SetBadRequestErrorResponse(c, "Invalid request format")
+		return
+	}
+
+	params.CreateResourcesPayload = *createResources
+
+	if err := params.Validate(); err != nil {
+		SetBadRequestErrorResponse(c, err.Error())
+		return
+	}
+
+	err := ph.ServiceResourceManager.CreateResources(*params)
+	if err != nil {
+		OnAPIError(c, err)
+		return
+	}
+
+	c.String(http.StatusNoContent, "")
 }
 
 // GetServiceResources godoc
@@ -60,7 +87,32 @@ func (ph *ServiceResourceHandler) CreateServiceResources(c *gin.Context) {
 // @Failure 500 {object} models.Error "Internal error"
 // @Router /project/{project}/stage/{stage}/service/{service}/resource [get]
 func (ph *ServiceResourceHandler) GetServiceResources(c *gin.Context) {
+	params := &models.GetResourcesParams{
+		Project: models.Project{ProjectName: c.Param(pathParamProjectName)},
+		Stage:   &models.Stage{StageName: c.Param(pathParamStageName)},
+		Service: &models.Service{ServiceName: c.Param(pathParamServiceName)},
+	}
 
+	getResources := &models.GetResourcesQuery{PageSize: 20}
+	if err := c.ShouldBindQuery(getResources); err != nil {
+		SetBadRequestErrorResponse(c, "Invalid request format")
+		return
+	}
+
+	params.GetResourcesQuery = *getResources
+
+	if err := params.Validate(); err != nil {
+		SetBadRequestErrorResponse(c, err.Error())
+		return
+	}
+
+	resources, err := ph.ServiceResourceManager.GetResources(*params)
+	if err != nil {
+		OnAPIError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, resources)
 }
 
 // UpdateServiceResources godoc
@@ -73,13 +125,38 @@ func (ph *ServiceResourceHandler) GetServiceResources(c *gin.Context) {
 // @Param	project				path	string	true	"The name of the project"
 // @Param	stage				path	string	true	"The name of the stage"
 // @Param	service				path	string	true	"The name of the service"
-// @Param   resources     body    models.UpdateResourcesParams     true        "List of resources"
+// @Param   resources     body    models.UpdateResourcesPayload     true        "List of resources"
 // @Success 201 {string} "ok"
 // @Failure 400 {object} models.Error "Invalid payload"
 // @Failure 500 {object} models.Error "Internal error"
 // @Router /project/{project}/stage/{stage}/service/{service}/resource [put]
 func (ph *ServiceResourceHandler) UpdateServiceResources(c *gin.Context) {
+	params := &models.UpdateResourcesParams{
+		Project: models.Project{ProjectName: c.Param(pathParamProjectName)},
+		Stage:   &models.Stage{StageName: c.Param(pathParamStageName)},
+		Service: &models.Service{ServiceName: c.Param(pathParamServiceName)},
+	}
 
+	updateResources := &models.UpdateResourcesPayload{}
+	if err := c.ShouldBindJSON(updateResources); err != nil {
+		SetBadRequestErrorResponse(c, "Invalid request format")
+		return
+	}
+
+	params.UpdateResourcesPayload = *updateResources
+
+	if err := params.Validate(); err != nil {
+		SetBadRequestErrorResponse(c, err.Error())
+		return
+	}
+
+	err := ph.ServiceResourceManager.UpdateResources(*params)
+	if err != nil {
+		OnAPIError(c, err)
+		return
+	}
+
+	c.String(http.StatusNoContent, "")
 }
 
 // GetServiceResource godoc
@@ -99,7 +176,32 @@ func (ph *ServiceResourceHandler) UpdateServiceResources(c *gin.Context) {
 // @Failure 500 {object} models.Error "Internal error"
 // @Router /project/{project}/stage/{stage}/service/{service}/resource/{resourceURI} [get]
 func (ph *ServiceResourceHandler) GetServiceResource(c *gin.Context) {
+	params := &models.GetResourceParams{
+		Project:     models.Project{ProjectName: c.Param(pathParamProjectName)},
+		Stage:       &models.Stage{StageName: c.Param(pathParamStageName)},
+		Service:     &models.Service{ServiceName: c.Param(pathParamServiceName)},
+		ResourceURI: c.Param(pathParamResourceURI),
+	}
+	getResources := &models.GetResourceQuery{}
+	if err := c.ShouldBindQuery(getResources); err != nil {
+		SetBadRequestErrorResponse(c, "Invalid request format")
+		return
+	}
 
+	params.GetResourceQuery = *getResources
+
+	if err := params.Validate(); err != nil {
+		SetBadRequestErrorResponse(c, err.Error())
+		return
+	}
+
+	resource, err := ph.ServiceResourceManager.GetResource(*params)
+	if err != nil {
+		OnAPIError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, resource)
 }
 
 // UpdateServiceResource godoc
@@ -113,13 +215,38 @@ func (ph *ServiceResourceHandler) GetServiceResource(c *gin.Context) {
 // @Param	stage				path	string	true	"The name of the stage"
 // @Param	service				path	string	true	"The name of the service"
 // @Param	resourceURI				path	string	true	"The path of the resource file"
-// @Param   resources     body    models.UpdateResourceParams     true        "resource"
+// @Param   resources     body    models.UpdateResourcePayload     true        "resource"
 // @Success 200 {string} "ok"
 // @Failure 400 {object} models.Error "Invalid payload"
 // @Failure 500 {object} models.Error "Internal error"
 // @Router /project/{project}/stage/{stage}/service/{service}/resource/{resourceURI} [put]
 func (ph *ServiceResourceHandler) UpdateServiceResource(c *gin.Context) {
+	params := &models.UpdateResourceParams{
+		Project:     models.Project{ProjectName: c.Param(pathParamProjectName)},
+		Stage:       &models.Stage{StageName: c.Param(pathParamStageName)},
+		Service:     &models.Service{ServiceName: c.Param(pathParamServiceName)},
+		ResourceURI: c.Param(pathParamResourceURI),
+	}
+	updateResource := &models.UpdateResourcePayload{}
+	if err := c.ShouldBindJSON(updateResource); err != nil {
+		SetBadRequestErrorResponse(c, "Invalid request format")
+		return
+	}
 
+	params.UpdateResourcePayload = *updateResource
+
+	if err := params.Validate(); err != nil {
+		SetBadRequestErrorResponse(c, err.Error())
+		return
+	}
+
+	err := ph.ServiceResourceManager.UpdateResource(*params)
+	if err != nil {
+		OnAPIError(c, err)
+		return
+	}
+
+	c.String(http.StatusNoContent, "")
 }
 
 // DeleteServiceResource godoc
@@ -138,5 +265,23 @@ func (ph *ServiceResourceHandler) UpdateServiceResource(c *gin.Context) {
 // @Failure 500 {object} models.Error "Internal error"
 // @Router /project/{project}/stage/{stage}/service/{service}/resource/{resourceURI} [delete]
 func (ph *ServiceResourceHandler) DeleteServiceResource(c *gin.Context) {
+	params := &models.DeleteResourceParams{
+		Project:     models.Project{ProjectName: c.Param(pathParamProjectName)},
+		Stage:       &models.Stage{StageName: c.Param(pathParamStageName)},
+		Service:     &models.Service{ServiceName: c.Param(pathParamServiceName)},
+		ResourceURI: c.Param(pathParamResourceURI),
+	}
 
+	if err := params.Validate(); err != nil {
+		SetBadRequestErrorResponse(c, err.Error())
+		return
+	}
+
+	err := ph.ServiceResourceManager.DeleteResource(*params)
+	if err != nil {
+		OnAPIError(c, err)
+		return
+	}
+
+	c.String(http.StatusNoContent, "")
 }
