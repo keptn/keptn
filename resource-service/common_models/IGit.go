d6a2ac00f53607988c559eab7fb23fc922d3d181
RealAnna <anna.reale@dynatrace.com>
2021-12-28 15:29:10 +0100

    made checkout branch test
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

---
+++ resource-service/common_models/IGit.go
@@ -0,0 +1,29 @@
+package common_models
+
+const masterBranch = "master"
+
+// GitCredentials contains git credentials info
+type GitCredentials struct {
+	User      string `json:"user,omitempty"`
+	Token     string `json:"token,omitempty"`
+	RemoteURI string `json:"remoteURI,omitempty"`
+}
+
+type GitContext struct {
+	Project     string //TODO would it make sense to store branch ? no
+	Credentials *GitCredentials
+}
+
+// IGit provides functions to interact with the git repository of a project
+//go:generate moq -pkg common_mock -skip-ensure -out ./fake/git_mock.go . IGit
+type IGit interface {
+	ProjectExists(gitContext GitContext) bool
+	CloneRepo(gitContext GitContext) (bool, error)
+	StageAndCommitAll(gitContext GitContext, message string) error
+	Push(gitContext GitContext) error
+	Pull(gitContext GitContext) error
+	CreateBranch(gitContext GitContext, branch string, sourceBranch string) error
+	CheckoutBranch(gitContext GitContext, branch string) error
+	GetFileRevision(gitContext GitContext, revision string, file string) ([]byte, error)
+	GetDefaultBranch(gitContext GitContext) (string, error)
+}
