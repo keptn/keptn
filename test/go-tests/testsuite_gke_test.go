e818a3493e2e4d5ff1440c90b65a205abc77edeb
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-01-04 16:34:01 +0100

    Revert "try to have a common testsuite"
    This reverts commit 076c4549ffac0396bfe1a011578b26e910e55a88.
    
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- test/go-tests/testsuite_gke_test.go
+++ test/go-tests/testsuite_gke_test.go
@@ -6,7 +6,29 @@ import (
 
 func Test_GKE(t *testing.T) {
 	// Common Tests
-	t.Run("TestCommon", TestCommon)
+	t.Run("Test_LogIngestion", Test_LogIngestion)
+	t.Run("Test_LogForwarding", Test_LogForwarding)
+	t.Run("Test_SequenceState", Test_SequenceState)
+	t.Run("Test_SequenceState_RetrieveMultipleSequence", Test_SequenceState_RetrieveMultipleSequence)
+	t.Run("Test_SequenceState_SequenceNotFound", Test_SequenceState_SequenceNotFound)
+	t.Run("Test_SequenceState_InvalidShipyard", Test_SequenceState_InvalidShipyard)
+	t.Run("Test_SequenceState_CannotRetrieveShipyard", Test_SequenceState_CannotRetrieveShipyard)
+	t.Run("Test_SequenceQueue", Test_SequenceQueue)
+	t.Run("Test_SequenceQueue_TriggerMultiple", Test_SequenceQueue_TriggerMultiple)
+	t.Run("Test_SequenceQueue_TriggerAndDeleteProject", Test_SequenceQueue_TriggerAndDeleteProject)
+	t.Run("Test_SequenceLoopIntegrationTest", Test_SequenceLoopIntegrationTest)
+	t.Run("Test_SelfHealing", Test_SelfHealing)
+	t.Run("Test_ResourceServiceBasic", Test_ResourceServiceBasic)
+	t.Run("Test_ManageSecrets_CreateUpdateAndDeleteSecret", Test_ManageSecrets_CreateUpdateAndDeleteSecret)
+	t.Run("Test_Webhook", Test_Webhook)
+	t.Run("Test_Webhook_OverlappingSubscriptions", Test_Webhook_OverlappingSubscriptions)
+	t.Run("Test_WebhookWithDisabledFinishedEvents", Test_WebhookWithDisabledFinishedEvents)
+	t.Run("Test_SequenceTimeout", Test_SequenceTimeout)
+	t.Run("Test_SequenceTimeoutDelayedTask", Test_SequenceTimeoutDelayedTask)
+	t.Run("Test_SequenceControl_Abort", Test_SequenceControl_Abort)
+	t.Run("Test_SequenceControl_AbortQueuedSequence", Test_SequenceControl_AbortQueuedSequence)
+	t.Run("Test_SequenceControl_PauseAndResume", Test_SequenceControl_PauseAndResume)
+	t.Run("Test_SequenceControl_PauseAndResume_2", Test_SequenceControl_PauseAndResume_2)
 
 	// Platform-specific Tests
 	t.Run("Test_QualityGates", Test_QualityGates)
