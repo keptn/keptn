9345a56570c2f9945e9fbd6ca0fa2d2b433c6ef0
Florian Bacher <florian.bacher@dynatrace.com>
2022-01-13 12:58:31 +0100

    test: Move Keptn CLI authentication to go tests (#6573) (#6576)
    * test: Move Keptn CLI authentication to go tests (#6573)
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * removed unnecessary test outcome entry
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- test/go-tests/testsuite_k3d_test.go
+++ test/go-tests/testsuite_k3d_test.go
@@ -5,9 +5,6 @@ import (
 )
 
 func Test_K3D(t *testing.T) {
-	// Common Tests
-	t.Run("Test_ResourceServiceBasic", Test_ResourceServiceBasic)
-
 	// Platform-specific Tests
 	t.Run("TestAirgappedImagesAreSetCorrectly", TestAirgappedImagesAreSetCorrectly)
 }
