9635fac6b20cc5c52c3b0f742e15e8abbe49c661
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-01-17 10:44:29 +0100

    ci: Add pipeline and shell script to check docker image digests nightly and manually
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

---
+++ gh-actions-scripts/check-image-digests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+DIGEST_FILE="digests.csv"
+
+readarray -t tags <<< "$(gh release list --limit 1000 | awk '{print $1}')"
+
+for tag in "${tags[@]}"; do
+  readarray -t release_assets <<< "$(gh release view "$tag" --json assets --jq ".assets[].name")"
+
+  echo "Checking if release $tag has docker image digests attached..."
+  if [[ "${release_assets[*]}" =~ $DIGEST_FILE ]]; then
+    echo "Release $tag has docker image digests attached. Downloading digests file..."
+    gh release download --pattern="$DIGEST_FILE"
+
+    while IFS=, read -r artifact release_digest; do
+      echo "Pulling docker image $artifact:$tag..."
+      docker pull "keptn/$artifact:$tag"
+      docker_digest=$(docker inspect "keptn/$artifact:$tag" | jq -r '.[0].RepoDigests[0]' | cut -d'@' -f2)
+
+      echo "Checking image digest..."
+      if [[ "$docker_digest" != "$release_digest" ]]; then
+        echo "CAUTION: Docker image digest does not match released digest!"
+        exit 1
+      else
+        echo "Image digest for $artifact:$tag matches released digest. Continuing..."
+      fi
+    done < $DIGEST_FILE
+  fi
+done
