0d8c10aee884930b9dab0f0c16955eacec108e14
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-01-19 09:25:42 +0100

    ci: Fix digest checker pipeline (#6619)
    Co-authored-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- gh-actions-scripts/check-image-digests.sh
+++ gh-actions-scripts/check-image-digests.sh
@@ -9,7 +9,10 @@ for tag in "${tags[@]}"; do
   echo "Checking if release $tag has docker image digests attached..."
   if [[ "${release_assets[*]}" =~ $DIGEST_FILE ]]; then
     echo "Release $tag has docker image digests attached. Downloading digests file..."
-    gh release download --pattern="$DIGEST_FILE"
+    gh release download "$tag" --pattern="$DIGEST_FILE"
+    echo "Digests from release:"
+    cat "$DIGEST_FILE"
+    echo ""
 
     while IFS=, read -r artifact release_digest; do
       echo "Pulling docker image $artifact:$tag..."
@@ -19,10 +22,14 @@ for tag in "${tags[@]}"; do
       echo "Checking image digest..."
       if [[ "$docker_digest" != "$release_digest" ]]; then
         echo "CAUTION: Docker image digest does not match released digest!"
+        echo "Digest from dockerhub: $docker_digest"
+        echo "Digest from release:   $release_digest"
         exit 1
       else
         echo "Image digest for $artifact:$tag matches released digest. Continuing..."
       fi
     done < $DIGEST_FILE
+    rm "$DIGEST_FILE"
   fi
+  echo "============="
 done
