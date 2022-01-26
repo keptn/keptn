d21e659985a23fac0323cfba612010e733e4a070
Klaus Strießnig <k.striessnig@gmail.com>
2022-01-26 09:05:58 +0100

    improved equality check for notifications
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_services/notifications.service.ts
+++ bridge/client/app/_services/notifications.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ComponentInfo, Notification, NotificationType } from '../_models/notification';
+import { isEqual } from 'lodash-es';
 
 @Injectable({
   providedIn: 'root',
@@ -21,9 +22,7 @@ export class NotificationsService {
     const notification = new Notification(type, message, componentInfo, time);
     const notifications = this._notifications.getValue();
     // Check if the notification to add already exists
-    const duplicateNotifications = notifications.filter(
-      (n) => n.severity === notification.severity && n.message === notification.message
-    );
+    const duplicateNotifications = notifications.filter((n) => isEqual(n, notification));
 
     // Only show notification if it is not shown yet to prevent duplicates (issue #3896 - https://github.com/keptn/keptn/issues/3896)
     if (duplicateNotifications.length === 0) {
