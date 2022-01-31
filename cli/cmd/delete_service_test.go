d26c112a33c0193d01bb0e67246d9b4a026dac70
odubajDT <ondrej.dubaj@dynatrace.com>
2022-01-31 11:08:25 +0100

    minor fix in unit tests
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- cli/cmd/delete_service_test.go
+++ cli/cmd/delete_service_test.go
@@ -2,27 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"testing"
-
-	"github.com/keptn/keptn/cli/pkg/credentialmanager"
-
-	"github.com/keptn/keptn/cli/pkg/logging"
 )
-
-func init() {
-	logging.InitLoggers(os.Stdout, os.Stdout, os.Stderr)
-}
-
-func TestDeleteServiceCmd(t *testing.T) {
-	credentialmanager.MockAuthCreds = true
-
-	cmd := fmt.Sprintf("delete secret myservice --mock --project=sockshop")
-	_, err := executeActionCommandC(cmd)
-	if err != nil {
-		t.Errorf(unexpectedErrMsg, err)
-	}
-}
 
 // TestDeleteServiceUnknownCommand
 func TestDeleteServiceUnknownCommand(t *testing.T) {
