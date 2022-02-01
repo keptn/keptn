385cd80bbee07f14ece914c280784c58a248b055
RealAnna <89971034+RealAnna@users.noreply.github.com>
2022-02-01 15:50:46 +0100

    test: Reduce graceful shutdown test load (#6730)

--- test/go-tests/test_graceful_shutdown.go
+++ test/go-tests/test_graceful_shutdown.go
@@ -30,73 +30,36 @@ spec:
             - name: "deployment"
               properties:
                 deploymentstrategy: "direct"
-            - name: "release"
-
-    - name: "staging"
-      sequences:
-        - name: "delivery"
-          triggeredOn:
-            - event: "dev.delivery.finished"
-          tasks:
-            - name: "deployment"
-              properties:
-                deploymentstrategy: "blue_green_service"
-            - name: "test"
-              properties:
-                teststrategy: "performance"
-            - name: "evaluation"
-            - name: "release"
-        - name: "rollback"
-          triggeredOn:
-            - event: "staging.delivery.finished"
-              selector:
-                match:
-                  result: "fail"
-          tasks:
-            - name: "rollback"
-
-        - name: "delivery-direct"
-          triggeredOn:
-            - event: "dev.delivery-direct.finished"
-          tasks:
-            - name: "deployment"
-              properties:
-                deploymentstrategy: "direct"
-            - name: "release"
-`
+            - name: "release"`
 
 func Test_GracefulShutdown(t *testing.T) {
 	repoLocalDir, err := filepath.Abs("../")
 	require.Nil(t, err)
 	t.Log("Current local dir is : ", repoLocalDir)
 
-	projectName := "tinypodtato"
+	keptnProjectName := "tinypodtato"
 	serviceName := "helloservice"
 	serviceChartLocalDir := repoLocalDir + "/helm-charts/helloservice.tgz"
 	serviceJmeterDir := repoLocalDir + "/jmeter"
 	serviceHealthCheckEndpoint := "/metrics"
 	shipyardPod := "shipyard-controller"
 
-	t.Logf("Creating a new project %s", projectName)
+	t.Logf("Creating a new project %s", keptnProjectName)
 	shipyardFilePath, err := CreateTmpShipyardFile(tinyShipyard)
 	require.Nil(t, err)
-	projectName, err = CreateProject(projectName, shipyardFilePath, true)
+	keptnProjectName, err = CreateProject(keptnProjectName, shipyardFilePath, true)
 	require.Nil(t, err)
 
-	t.Logf("Creating service %s in project %s", serviceName, projectName)
-	_, err = ExecuteCommandf("keptn create service %s --project %s", serviceName, projectName)
+	t.Logf("Creating service %s in project %s", serviceName, keptnProjectName)
+	_, err = ExecuteCommandf("keptn create service %s --project %s", serviceName, keptnProjectName)
 	require.Nil(t, err)
 
-	t.Logf("Adding resource for service %s in project %s", serviceName, projectName)
-	_, err = ExecuteCommandf("keptn add-resource --project %s --service=%s --all-stages --resource=%s --resourceUri=%s", projectName, serviceName, serviceChartLocalDir, "helm/helloservice.tgz")
+	t.Logf("Adding resource for service %s in project %s", serviceName, keptnProjectName)
+	_, err = ExecuteCommandf("keptn add-resource --project %s --service=%s --stage=%s --resource=%s --resourceUri=%s", keptnProjectName, serviceName, "dev", serviceChartLocalDir, "helm/helloservice.tgz")
 	require.Nil(t, err)
 
 	t.Log("Adding jmeter config in staging")
-	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", projectName, serviceName, "staging", serviceJmeterDir+"/jmeter.conf.yaml", "jmeter/jmeter.conf.yaml")
-	require.Nil(t, err)
-
-	t.Log("Adding load test resources for jmeter in staging")
-	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", projectName, serviceName, "staging", serviceJmeterDir+"/load.jmx", "jmeter/load.jmx")
+	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", keptnProjectName, serviceName, "dev", serviceJmeterDir+"/jmeter.conf.yaml", "jmeter/jmeter.conf.yaml")
 	require.Nil(t, err)
 
 	///////////////////////////////////////
@@ -104,7 +67,7 @@ func Test_GracefulShutdown(t *testing.T) {
 	///////////////////////////////////////
 
 	t.Logf("Trigger delivery of helloservice:v0.1.0")
-	_, err = ExecuteCommandf("keptn trigger delivery --project=%s --service=%s --image=%s --tag=%s --sequence=%s", projectName, serviceName, "ghcr.io/podtato-head/podtatoserver", "v0.1.0", "delivery")
+	_, err = ExecuteCommandf("keptn trigger delivery --project=%s --service=%s --image=%s --tag=%s --sequence=%s", keptnProjectName, serviceName, "ghcr.io/podtato-head/podtatoserver", "v0.1.0", "delivery")
 	require.Nil(t, err)
 
 	waitAndKill(t, shipyardPod, 35)
@@ -115,25 +78,12 @@ func Test_GracefulShutdown(t *testing.T) {
 	err = WaitForPodOfDeployment(shipyardPod)
 	require.Nil(t, err)
 
-	//keptnkubeutils.WaitForDeploymentToBeRolledOut(false, serviceName, GetKeptnNameSpaceFromEnv())
-
 	t.Log("Verify Direct delivery of helloservice in stage dev")
-	err = VerifyDirectDeployment(serviceName, projectName, "dev", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
+	err = VerifyDirectDeployment(serviceName, keptnProjectName, "dev", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
 	logError(err, t, shipyardPod)
 
 	t.Log("Verify network access to public URI of helloservice in stage dev")
-	cartPubURL, err := GetPublicURLOfService(serviceName, projectName, "dev")
-	logError(err, t, shipyardPod)
-
-	err = WaitForURL(cartPubURL+serviceHealthCheckEndpoint, time.Minute)
-	logError(err, t, shipyardPod)
-
-	t.Log("Verify delivery of helloservice:v0.1.0 in stage staging")
-	err = VerifyBlueGreenDeployment(serviceName, projectName, "staging", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
-	logError(err, t, shipyardPod)
-
-	t.Log("Verify network access to public URI of helloservice in stage staging")
-	cartPubURL, err = GetPublicURLOfService(serviceName, projectName, "staging")
+	cartPubURL, err := GetPublicURLOfService(serviceName, keptnProjectName, "dev")
 	logError(err, t, shipyardPod)
 
 	err = WaitForURL(cartPubURL+serviceHealthCheckEndpoint, time.Minute)
@@ -144,6 +94,10 @@ func Test_GracefulShutdown(t *testing.T) {
 func waitAndKill(t *testing.T, keptnServiceName string, waitFor int) {
 	t.Logf("Sleeping %d seconds...\n", waitFor)
 	time.Sleep(time.Duration(waitFor) * time.Second)
+
+	t.Logf("this is the state of %s before shutdown", keptnServiceName)
+	GetDiagnostics(keptnServiceName, "")
+
 	t.Logf("Killing %s Pod", keptnServiceName)
 	err := RestartPod(keptnServiceName)
 	logError(err, t, keptnServiceName)
