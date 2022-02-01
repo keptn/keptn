ca9bebee092e50ce50e0724d04c204a3039e4a52
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-02-01 09:27:38 +0100

    test: Integration Tests on static GKE clusters (#6632)
    Co-authored-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- test/go-tests/test_backuprestore.go
+++ test/go-tests/test_backuprestore.go
@@ -71,37 +71,37 @@ spec:
 
 func Test_BackupRestore(t *testing.T) {
 	repoLocalDir := "../assets/podtato-head"
-	keptnProjectName := "backup-restore"
+	projectName := "backup-restore"
 	serviceName := "helloservice"
 	serviceChartLocalDir := path.Join(repoLocalDir, "helm-charts", "helloservice.tgz")
 	serviceJmeterDir := path.Join(repoLocalDir, "jmeter")
 	keptnNamespace := GetKeptnNameSpaceFromEnv()
 	serviceHealthCheckEndpoint := "/metrics"
 
-	t.Logf("Creating a new project %s without a GIT Upstream", keptnProjectName)
+	t.Logf("Creating a new project %s without a GIT Upstream", projectName)
 	shipyardFilePath, err := CreateTmpShipyardFile(testingShipyard)
 	require.Nil(t, err)
-	err = CreateProject(keptnProjectName, shipyardFilePath, true)
+	projectName, err = CreateProject(projectName, shipyardFilePath, true)
 	require.Nil(t, err)
 
-	t.Logf("Creating service %s in project %s", serviceName, keptnProjectName)
-	_, err = ExecuteCommandf("keptn create service %s --project %s", serviceName, keptnProjectName)
+	t.Logf("Creating service %s in project %s", serviceName, projectName)
+	_, err = ExecuteCommandf("keptn create service %s --project %s", serviceName, projectName)
 	require.Nil(t, err)
 
-	t.Logf("Adding resource for service %s in project %s", serviceName, keptnProjectName)
-	_, err = ExecuteCommandf("keptn add-resource --project %s --service=%s --all-stages --resource=%s --resourceUri=%s", keptnProjectName, serviceName, serviceChartLocalDir, "helm/helloservice.tgz")
+	t.Logf("Adding resource for service %s in project %s", serviceName, projectName)
+	_, err = ExecuteCommandf("keptn add-resource --project %s --service=%s --all-stages --resource=%s --resourceUri=%s", projectName, serviceName, serviceChartLocalDir, "helm/helloservice.tgz")
 	require.Nil(t, err)
 
 	t.Log("Adding jmeter config in prod")
-	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", keptnProjectName, serviceName, "prod", serviceJmeterDir+"/jmeter.conf.yaml", "jmeter/jmeter.conf.yaml")
+	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", projectName, serviceName, "prod", serviceJmeterDir+"/jmeter.conf.yaml", "jmeter/jmeter.conf.yaml")
 	require.Nil(t, err)
 
 	t.Log("Adding load test resources for jmeter in prod")
-	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", keptnProjectName, serviceName, "prod", serviceJmeterDir+"/load.jmx", "jmeter/load.jmx")
+	_, err = ExecuteCommandf("keptn add-resource --project=%s --service=%s --stage=%s --resource=%s --resourceUri=%s", projectName, serviceName, "prod", serviceJmeterDir+"/load.jmx", "jmeter/load.jmx")
 	require.Nil(t, err)
 
 	t.Logf("Trigger delivery before backup of helloservice:v0.1.0")
-	_, err = ExecuteCommandf("keptn trigger delivery --project=%s --service=%s --image=%s --tag=%s --sequence=%s", keptnProjectName, serviceName, "ghcr.io/podtato-head/podtatoserver", "v0.1.0", "delivery")
+	_, err = ExecuteCommandf("keptn trigger delivery --project=%s --service=%s --image=%s --tag=%s --sequence=%s", projectName, serviceName, "ghcr.io/podtato-head/podtatoserver", "v0.1.0", "delivery")
 	require.Nil(t, err)
 
 	t.Logf("Sleeping for 60s...")
@@ -109,26 +109,26 @@ func Test_BackupRestore(t *testing.T) {
 	t.Logf("Continue to work...")
 
 	t.Logf("Verify Direct delivery before backup of %s in stage dev", serviceName)
-	err = VerifyDirectDeployment(serviceName, keptnProjectName, "dev", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
+	err = VerifyDirectDeployment(serviceName, projectName, "dev", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
 	require.Nil(t, err)
 
 	t.Log("Verify network access to public URI of helloservice in stage dev")
-	cartPubURL, err := GetPublicURLOfService(serviceName, keptnProjectName, "dev")
+	cartPubURL, err := GetPublicURLOfService(serviceName, projectName, "dev")
 	require.Nil(t, err)
 	err = WaitForURL(cartPubURL+serviceHealthCheckEndpoint, time.Minute)
 	require.Nil(t, err)
 
 	t.Logf("Verify Direct delivery before backup of %s in stage prod", serviceName)
-	err = VerifyDirectDeployment(serviceName, keptnProjectName, "prod", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
+	err = VerifyDirectDeployment(serviceName, projectName, "prod", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
 	require.Nil(t, err)
 
-	sequenceStates, _, err := GetState(keptnProjectName)
+	sequenceStates, _, err := GetState(projectName)
 	require.Nil(t, err)
 	require.NotEmpty(t, sequenceStates.States)
-	VerifySequenceEndsUpInState(t, keptnProjectName, &keptnapimodels.EventContext{KeptnContext: &sequenceStates.States[0].Shkeptncontext}, 2*time.Minute, []string{models.SequenceFinished})
+	VerifySequenceEndsUpInState(t, projectName, &keptnapimodels.EventContext{KeptnContext: &sequenceStates.States[0].Shkeptncontext}, 2*time.Minute, []string{models.SequenceFinished})
 
 	t.Log("Verify network access to public URI of helloservice in stage prod")
-	cartPubURL, err = GetPublicURLOfService(serviceName, keptnProjectName, "prod")
+	cartPubURL, err = GetPublicURLOfService(serviceName, projectName, "prod")
 	require.Nil(t, err)
 	err = WaitForURL(cartPubURL+serviceHealthCheckEndpoint, time.Minute)
 	require.Nil(t, err)
@@ -184,7 +184,7 @@ func Test_BackupRestore(t *testing.T) {
 	//deleting testing project
 
 	t.Logf("Deleting testing project")
-	_, err = ExecuteCommandf("keptn delete project %s", keptnProjectName)
+	_, err = ExecuteCommandf("keptn delete project %s", projectName)
 	require.Nil(t, err)
 
 	t.Logf("Sleeping for 15s...")
@@ -212,7 +212,7 @@ func Test_BackupRestore(t *testing.T) {
 	t.Logf("Continue to work...")
 
 	t.Logf("Trigger delivery after restore of helloservice:v0.1.0")
-	_, err = ExecuteCommandf("keptn trigger delivery --project=%s --service=%s --image=%s --tag=%s --sequence=%s", keptnProjectName, serviceName, "ghcr.io/podtato-head/podtatoserver", "v0.1.0", "delivery")
+	_, err = ExecuteCommandf("keptn trigger delivery --project=%s --service=%s --image=%s --tag=%s --sequence=%s", projectName, serviceName, "ghcr.io/podtato-head/podtatoserver", "v0.1.0", "delivery")
 	require.Nil(t, err)
 
 	t.Logf("Sleeping for 60s...")
@@ -220,21 +220,21 @@ func Test_BackupRestore(t *testing.T) {
 	t.Logf("Continue to work...")
 
 	t.Logf("Verify Direct delivery after restore of %s in stage dev", serviceName)
-	err = VerifyDirectDeployment(serviceName, keptnProjectName, "dev", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
+	err = VerifyDirectDeployment(serviceName, projectName, "dev", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
 	require.Nil(t, err)
 
 	t.Log("Verify network access to public URI of helloservice in stage dev")
-	cartPubURL, err = GetPublicURLOfService(serviceName, keptnProjectName, "dev")
+	cartPubURL, err = GetPublicURLOfService(serviceName, projectName, "dev")
 	require.Nil(t, err)
 	err = WaitForURL(cartPubURL+serviceHealthCheckEndpoint, time.Minute)
 	require.Nil(t, err)
 
 	t.Logf("Verify Direct delivery after restore of %s in stage prod", serviceName)
-	err = VerifyDirectDeployment(serviceName, keptnProjectName, "prod", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
+	err = VerifyDirectDeployment(serviceName, projectName, "prod", "ghcr.io/podtato-head/podtatoserver", "v0.1.0")
 	require.Nil(t, err)
 
 	t.Log("Verify network access to public URI of helloservice in stage prod")
-	cartPubURL, err = GetPublicURLOfService(serviceName, keptnProjectName, "prod")
+	cartPubURL, err = GetPublicURLOfService(serviceName, projectName, "prod")
 	require.Nil(t, err)
 	err = WaitForURL(cartPubURL+serviceHealthCheckEndpoint, time.Minute)
 	require.Nil(t, err)
--- test/go-tests/test_selfhealing.go
+++ test/go-tests/test_selfhealing.go
@@ -62,7 +62,7 @@ func Test_SelfHealing(t *testing.T) {
 
 	_, err = ExecuteCommand(fmt.Sprintf("kubectl delete configmap -n %s lighthouse-config-%s", GetKeptnNameSpaceFromEnv(), projectName))
 	t.Logf("creating project %s", projectName)
-	err = CreateProject(projectName, shipyardFilePath, true)
+	projectName, err = CreateProject(projectName, shipyardFilePath, true)
 	require.Nil(t, err)
 
 	t.Logf("creating service %s", serviceName)
