1bbef854bbfd05776586a83bb576c02d6af21d39
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-02-01 09:10:16 +0100

    show distributor many times, also add container name and deployment name
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- gh-actions-scripts/generate-k8s-resource-stats.sh
+++ gh-actions-scripts/generate-k8s-resource-stats.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 # Print complete yaml of the keptn installer
-helm template ../dist/keptn-installer/keptn-*.tgz --dry-run > "$HELM_TEMPLATE"
+helm template ../dist/keptn-installer/keptn-*.tgz --dry-run --name-template=keptn > "$HELM_TEMPLATE"
 
 # yq: Extract container name and resource limits/requests for all resources
 # jq: Bring yq result into nice format that can be read by the tablemark-cli
-# unique_by: Remove duplicated distributor resource stats
 yq eval-all '[
-  .spec.template.spec.containers.[] |
-  {
-    "name": .name,
-    "resources": .resources
-  }
+{
+  "name": .metadata.name,
+  "container": .spec.template.spec.containers.[]
+} |
+{
+  "name": .name,
+  "container": .container.name,
+  "resources": .container.resources
+}
 ]' "$HELM_TEMPLATE" -o=json | \
 jq '[
-  .[] | {
+.[] | {
     name: .name,
+    container_name: .container,
     cpu_request: .resources.requests.cpu,
     cpu_limit: .resources.limits.cpu,
     mem_request: .resources.requests.memory,
     mem_limit: .resources.limits.memory
   }
-] |
-unique_by(.name)' > "$RESOURCE_JSON"
+]' > "$RESOURCE_JSON"
 
 # Generate markdown table from JSON
-npx tablemark-cli@v2.0.0 "$RESOURCE_JSON" -c "Name" -c "CPU Request" -c "CPU Limit" -c "RAM Request" -c "RAM Limit" > "$RESOURCE_MARKDOWN"
+npx tablemark-cli@v2.0.0 "$RESOURCE_JSON" -c "Name" -c "Container Name" -c "Image" -c "CPU Request" -c "CPU Limit" -c "RAM Request" -c "RAM Limit" > "$RESOURCE_MARKDOWN"
 
 # Attach resource stats to release notes
 {
