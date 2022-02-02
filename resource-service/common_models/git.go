8b113dc8209ebe8425f2639367196755f694b74b
odubajDT <ondrej.dubaj@dynatrace.com>
2022-02-02 12:04:18 +0100

    feat: Added new auth methods support
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- resource-service/common_models/git.go
+++ resource-service/common_models/git.go
@@ -1,15 +1,17 @@
 package common_models
 
 import (
-	kerrors "github.com/keptn/keptn/resource-service/errors"
 	"net/url"
+
+	kerrors "github.com/keptn/keptn/resource-service/errors"
 )
 
 // GitCredentials contains git credentials info
 type GitCredentials struct {
-	User      string `json:"user,omitempty"`
-	Token     string `json:"token,omitempty"`
-	RemoteURI string `json:"remoteURI,omitempty"`
+	User       string `json:"user,omitempty"`
+	Token      string `json:"token,omitempty"`
+	RemoteURI  string `json:"remoteURI,omitempty"`
+	PrivateKey string `json:"privateKey,omitempty"`
 }
 
 type GitContext struct {
