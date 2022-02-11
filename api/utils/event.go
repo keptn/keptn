9249e663ed029568629125789bc6d80e60f3b2b7
agardnerit <adam@agardner.net>
2022-02-11 18:25:57 +1000

    Sign commit to keep DCO happy
    Signed-off-by: agardnerit <adam@agardner.net>

--- api/utils/event.go
+++ api/utils/event.go
@@ -9,7 +9,6 @@ import (
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/google/uuid"
-
 	keptnutils "github.com/keptn/go-utils/pkg/lib/keptn"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 )
