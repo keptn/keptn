a4575de5d07428fa8ced4707a4ddc5df5cc1809c
Florian Bacher <florian.bacher@dynatrace.com>
2022-02-21 07:36:34 +0100

    removed unsupported query parameter from resource list GET endpoints
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- resource-service/handler/stage_resource_handler.go
+++ resource-service/handler/stage_resource_handler.go
@@ -80,7 +80,6 @@ func (ph *StageResourceHandler) CreateStageResources(c *gin.Context) {
 // @Produce  json
 // @Param	projectName					path	string	true	"The name of the project"
 // @Param	stageName					path	string	true	"The name of the stage"
-// @Param gitCommitID              query string false "The commit ID to be checked out"
 // @Param pageSize              query int false "The number of items to return"
 // @Param nextPageKey              query string false "Pointer to the next set of items"
 // @Success 200 {object} models.GetResourcesResponse
