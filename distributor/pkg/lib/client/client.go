3ccbd77f32d95bf3540817a9d59f89591e88a3fb
Florian Bacher <florian.bacher@dynatrace.com>
2022-02-22 13:23:09 +0100

    fix(distributor): Increase timout of http client to 30s (#6948) (#6954)
    * fix(distributor): Increase timout of http client to 30s
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * make http timeout configurable
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * make http timeout configurable
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- distributor/pkg/lib/client/client.go
+++ distributor/pkg/lib/client/client.go
@@ -100,7 +100,7 @@ func (g *SimpleClientGetter) Get() (*http.Client, error) {
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: !g.envConfig.VerifySSL}, //nolint:gosec
 		},
-		Timeout: 5 * time.Second,
+		Timeout: g.envConfig.GetAPIProxyHTTPTimeout(),
 	}
 	return c, nil
 }
