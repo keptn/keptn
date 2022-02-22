6fd4299d87a38ac3d4a9a81391125e1bd4f8dbba
Klaus Strießnig <k.striessnig@gmail.com>
2022-02-22 10:18:02 +0100

    adapted scope slice creation, fixed missing import for styles
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- secret-service/pkg/backend/secretbackend_k8s.go
+++ secret-service/pkg/backend/secretbackend_k8s.go
@@ -301,12 +301,9 @@ func (k K8sSecretBackend) GetScopes() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	scopeArray := make([]string, len(scopes.Scopes))
-
-	i := 0
+	scopeArray := make([]string, 0, len(scopes.Scopes))
 	for scope := range scopes.Scopes {
-		scopeArray[i] = scope
-		i++
+		scopeArray = append(scopeArray, scope)
 	}
 	sort.Strings(scopeArray)
 	return scopeArray, nil
