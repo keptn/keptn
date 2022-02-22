58870312478617dc053e1a6b0539cd49beee03b9
ermin.muratovic <ermin.muratovic@gmail.com>
2022-02-22 23:21:03 +0100

    add UI test for sorting of sli-breakdown and that errors are displayed correctly
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/cypress/integration/sli-breakdown.spec.ts
+++ bridge/cypress/integration/sli-breakdown.spec.ts
@@ -28,6 +28,13 @@ describe('evaluations', () => {
     cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(2).should('have.text', '88 (+1000%) ');
     cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(3).should('have.text', '1');
     cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(7).should('have.text', '33.33');
+
+    cy.byTestId('keptn-sli-breakdown-row-request_throughput')
+      .find('dt-cell')
+      .eq(2)
+      .find('.error')
+      .should('have.length', 1);
+    cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(2).find('.error').should('have.length', 0);
   });
 
   it('should show more details when expanding sli breakdown in service screen', () => {
@@ -66,5 +73,63 @@ describe('evaluations', () => {
     cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(1).should('contain.text', 'Absolute change:');
     cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(1).should('contain.text', 'Relative change:');
     cy.byTestId('keptn-sli-breakdown-row-go_routines').find('dt-cell').eq(1).should('contain.text', 'Compared with:');
+  });
+
+  it('should sort elements correctly', () => {
+    cy.intercept('GET', '/api/project/sockshop/serviceStates', {
+      statusCode: 200,
+      fixture: 'get.sockshop.service.states.mock.json',
+    });
+    cy.intercept('GET', '/api/project/sockshop/deployment/da740469-9920-4e0c-b304-0fd4b18d17c2', {
+      statusCode: 200,
+      fixture: 'get.sockshop.service.carts.deployment.mock.json',
+    });
+    cy.intercept('GET', 'api/mongodb-datastore/event/type/sh.keptn.event.evaluation.finished?*', {
+      statusCode: 200,
+      fixture: 'get.sockshop.service.carts.evaluations.mock.json',
+    });
+
+    cy.visit('/project/sockshop/service/carts/context/da740469-9920-4e0c-b304-0fd4b18d17c2/stage/staging');
+
+    // sort name asc
+    let nameColumnHeader = cy
+      .byTestId('keptn-sli-breakdown')
+      .find('dt-header-row')
+      .first()
+      .find('dt-header-cell')
+      .eq(1);
+    nameColumnHeader.click();
+    nameColumnHeader.should('have.class', 'dt-header-cell');
+
+    nameColumnHeader.find('.dt-sort-header-container').first().should('have.class', 'dt-sort-header-sorted');
+    nameColumnHeader.find('dt-icon').first().invoke('attr', 'ng-reflect-name').should('equal', 'sorter2-up');
+
+    cy.byTestId('keptn-sli-breakdown').find('dt-row').eq(0).find('dt-cell').eq(1).should('have.text', 'go_routines');
+    cy.byTestId('keptn-sli-breakdown')
+      .find('dt-row')
+      .eq(1)
+      .find('dt-cell')
+      .eq(1)
+      .should('have.text', 'http_response_time_seconds_main_page_sum');
+
+    // sort name desc
+    nameColumnHeader = cy.byTestId('keptn-sli-breakdown').find('dt-header-row').first().find('dt-header-cell').eq(1);
+    nameColumnHeader.click();
+
+    nameColumnHeader.find('.dt-sort-header-container').first().should('have.class', 'dt-sort-header-sorted');
+    nameColumnHeader.find('dt-icon').first().invoke('attr', 'ng-reflect-name').should('equal', 'sorter2-down');
+
+    cy.byTestId('keptn-sli-breakdown')
+      .find('dt-row')
+      .eq(0)
+      .find('dt-cell')
+      .eq(1)
+      .should('have.text', 'request_throughput');
+    cy.byTestId('keptn-sli-breakdown')
+      .find('dt-row')
+      .eq(1)
+      .find('dt-cell')
+      .eq(1)
+      .should('have.text', 'http_response_time_seconds_main_page_sum');
   });
 });
