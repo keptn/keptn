e36397a7f9b2d131c6aa8efe0d18c3cf33d6972b
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2022-02-23 08:32:48 +0100

    Unsubscribe from timer
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/client/app/_components/ktb-trigger-sequence/ktb-trigger-sequence.component.ts
+++ bridge/client/app/_components/ktb-trigger-sequence/ktb-trigger-sequence.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Inject, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { DataService } from '../../_services/data.service';
 import { AppUtils, POLLING_INTERVAL_MILLIS } from '../../_utils/app.utils';
 import { Timeframe } from '../../_models/timeframe';
@@ -17,6 +17,8 @@ import {
   TriggerSequenceData,
 } from '../../_models/trigger-sequence';
 import { Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 export class ShowErrorStateMatcher implements ErrorStateMatcher {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -45,7 +47,7 @@ export class JsonErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './ktb-trigger-sequence.component.html',
   styleUrls: ['./ktb-trigger-sequence.component.scss'],
 })
-export class KtbTriggerSequenceComponent implements OnInit {
+export class KtbTriggerSequenceComponent implements OnInit, OnDestroy {
   public TRIGGER_SEQUENCE = TRIGGER_SEQUENCE;
   public TRIGGER_EVALUATION_TIME = TRIGGER_EVALUATION_TIME;
   public state: TRIGGER_SEQUENCE | 'ENTRY' = 'ENTRY';
@@ -58,6 +60,7 @@ export class KtbTriggerSequenceComponent implements OnInit {
   public showErrorStateMatcher = new ShowErrorStateMatcher();
   public jsonErrorStateMatcher = new JsonErrorStateMatcher();
   public isLoading = false;
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
   public deliveryFormData: DeliverySequenceFormData = {
     image: undefined,
@@ -92,6 +95,11 @@ export class KtbTriggerSequenceComponent implements OnInit {
     private router: Router
   ) {}
 
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
   public ngOnInit(): void {
     if (!this.projectName) {
       throw new Error('Project name is required');
@@ -101,19 +109,21 @@ export class KtbTriggerSequenceComponent implements OnInit {
       this.selectedStage = this.stage;
     }
 
-    AppUtils.createTimer(0, this.pollingInterval).subscribe(() => {
-      if (this.projectName) {
-        this.dataService.getServiceNames(this.projectName).subscribe((services) => {
-          this.services = services;
-        });
-        this.dataService.getStageNames(this.projectName).subscribe((stages) => {
-          this.stages = stages;
-        });
-        this.dataService.getCustomSequenceNames(this.projectName).subscribe((customSequences) => {
-          this.customSequences = customSequences;
-        });
-      }
-    });
+    AppUtils.createTimer(0, this.pollingInterval)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        if (this.projectName) {
+          this.dataService.getServiceNames(this.projectName).subscribe((services) => {
+            this.services = services;
+          });
+          this.dataService.getStageNames(this.projectName).subscribe((stages) => {
+            this.stages = stages;
+          });
+          this.dataService.getCustomSequenceNames(this.projectName).subscribe((customSequences) => {
+            this.customSequences = customSequences;
+          });
+        }
+      });
   }
 
   public setFormState(): void {
