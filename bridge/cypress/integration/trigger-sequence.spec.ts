e87bd5c0cffb71f693521d87e7443a3b396c604d
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2022-03-01 11:01:05 +0100

    Refactoring of cypress test to page objects
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/cypress/integration/trigger-sequence.spec.ts
+++ bridge/cypress/integration/trigger-sequence.spec.ts
@@ -1,3 +1,7 @@
+import EnvironmentPage from '../support/pageobjects/EnvironmentPage';
+
+const environmentPage = new EnvironmentPage();
+
 describe('Trigger a sequence', () => {
   beforeEach(() => {
     cy.intercept('/api/v1/metadata', { fixture: 'metadata.mock' });
@@ -62,201 +66,154 @@ describe('Trigger a sequence', () => {
 
   it('should navigate through all forms and close it from everywhere properly', () => {
     // Opening of triggering component
-    cy.byTestId('keptn-trigger-button-open').click();
-    cy.byTestId('keptn-trigger-button-open').should('not.exist');
-    cy.byTestId('keptn-trigger-entry-h2').should('have.text', 'Trigger a new sequence for project sockshop');
-    cy.byTestId('keptn-trigger-button-next').should('be.disabled');
+    environmentPage.openTriggerSequence();
+    environmentPage.assertOpenTriggerSequenceExists(false);
+    environmentPage.assertTriggerEntryH2('have.text', 'Trigger a new sequence for project sockshop');
+    environmentPage.assertTriggerNextPageEnabled(false);
 
     // Closing of triggering component from entry
-    cy.byTestId('keptn-trigger-button-close').click();
-    cy.byTestId('keptn-trigger-entry-h2').should('not.exist');
-    cy.byTestId('keptn-trigger-button-open').should('exist');
+    environmentPage.clickTriggerClose();
+    environmentPage.assertTriggerEntryH2('not.exist');
+    environmentPage.assertOpenTriggerSequenceExists(true);
 
     // Delivery navigations
-    cy.byTestId('keptn-trigger-button-open').click();
-    selectDelivery();
+    environmentPage.openTriggerSequence();
+    environmentPage.selectTriggerDelivery();
     testNavigationFirstPart('keptn-trigger-delivery-h2', 'Trigger a delivery for carts in dev');
-    selectDelivery();
+    environmentPage.selectTriggerDelivery();
     testNavigationSecondPart('keptn-trigger-delivery-h2');
 
     // Evaluation navigations
-    cy.byTestId('keptn-trigger-button-open').click();
-    selectEvaluation();
+    environmentPage.openTriggerSequence();
+    environmentPage.selectTriggerEvaluation();
     testNavigationFirstPart('keptn-trigger-evaluation-h2', ' Trigger an evaluation for carts in dev ');
-    selectEvaluation();
+    environmentPage.selectTriggerEvaluation();
     testNavigationSecondPart('keptn-trigger-evaluation-h2');
 
     // Custom sequence navigations
-    cy.byTestId('keptn-trigger-button-open').click();
-    selectCustomSequence();
+    environmentPage.openTriggerSequence();
+    environmentPage.selectTriggerCustomSequence();
     testNavigationFirstPart('keptn-trigger-custom-h2', ' Trigger a custom sequence for carts in dev ');
-    selectCustomSequence();
+    environmentPage.selectTriggerCustomSequence();
     testNavigationSecondPart('keptn-trigger-custom-h2');
   });
 
   it('should trigger a delivery sequence', () => {
-    cy.byTestId('keptn-trigger-button-open').click();
-    cy.byTestId('keptn-trigger-button-next').should('be.disabled');
-    selectDelivery();
-    cy.byTestId('keptn-trigger-button-next').should('be.enabled').click();
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-delivery-labels').type('key1=val1');
-    cy.byTestId('keptn-trigger-delivery-values').type('{"key2');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-delivery-values-error').should('exist');
-    cy.byTestId('keptn-trigger-delivery-values').type('": "val2"}');
-    cy.byTestId('keptn-trigger-delivery-values-error').should('not.exist');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-delivery-image').type('docker.io/keptn');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-delivery-tag').type('v0.1.2');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled').click();
+    environmentPage.openTriggerSequence();
+    environmentPage.assertTriggerNextPageEnabled(false);
+    environmentPage.selectTriggerDelivery();
+    environmentPage.assertTriggerNextPageEnabled(true).click();
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.typeTriggerDeliveryLabels('key1=val1');
+    environmentPage.typeTriggerDeliveryValues('{"key2');
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.assertTriggerDeliveryValuesErrorExists(true);
+    environmentPage.typeTriggerDeliveryValues('": "val2"}');
+    environmentPage.assertTriggerDeliveryValuesErrorExists(false);
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.typeTriggerDeliveryImage('docker.io/keptn');
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.typeTriggerDeliveryTag('v0.1.2');
+    environmentPage.assertTriggerSequenceEnabled(true).click();
     cy.url().should('include', '/project/sockshop/sequence/6c98fbb0-4c40-4bff-ba9f-b20556a57c8a/stage/dev');
   });
 
   it('should trigger an evaluation sequence with a timeframe', () => {
-    cy.byTestId('keptn-trigger-button-open').click();
-    cy.byTestId('keptn-trigger-button-next').should('be.disabled');
-    selectEvaluation();
-    cy.byTestId('keptn-trigger-button-next').should('be.enabled').click();
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-evaluation-type').children().first().click();
-    cy.byTestId('keptn-trigger-evaluation-labels').type('key1=val1');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-button-starttime').click();
-    selectDateTime(0);
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-time-input-hours').type('0');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled');
-    cy.byTestId('keptn-time-input-minutes').type('1');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled');
-    cy.byTestId('keptn-time-input-seconds').type('15');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled');
-    cy.byTestId('keptn-time-input-millis').type('0');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled');
-    cy.byTestId('keptn-time-input-micros').type('0');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled').click();
+    environmentPage.openTriggerSequence();
+    environmentPage.assertTriggerNextPageEnabled(false);
+    environmentPage.selectTriggerEvaluation();
+    environmentPage.assertTriggerNextPageEnabled(true).click();
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.selectTriggerEvaluationType(0);
+    environmentPage.typeTriggerEvaluationLabels('key1=val1');
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.clickTriggerStartTime();
+    environmentPage.selectTriggerDateTime(0);
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.typeTriggerEvaluationTimeInput('hours', '0');
+    environmentPage.assertTriggerSequenceEnabled(true);
+    environmentPage.typeTriggerEvaluationTimeInput('minutes', '1');
+    environmentPage.assertTriggerSequenceEnabled(true);
+    environmentPage.typeTriggerEvaluationTimeInput('seconds', '15');
+    environmentPage.assertTriggerSequenceEnabled(true);
+    environmentPage.typeTriggerEvaluationTimeInput('millis', '0');
+    environmentPage.assertTriggerSequenceEnabled(true);
+    environmentPage.typeTriggerEvaluationTimeInput('micros', '0');
+    environmentPage.assertTriggerSequenceEnabled(true).click();
     cy.url().should('include', '/project/sockshop/sequence/6c98fbb0-4c40-4bff-ba9f-b20556a57c8a/stage/dev');
   });
 
   it('should trigger an evaluation sequence with a start end date', () => {
-    cy.byTestId('keptn-trigger-button-open').click();
-    cy.byTestId('keptn-trigger-button-next').should('be.disabled');
-    selectEvaluation();
-    cy.byTestId('keptn-trigger-button-next').should('be.enabled').click();
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-evaluation-type').children().eq(1).click();
-    cy.byTestId('keptn-trigger-evaluation-labels').type('key1=val1');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
+    environmentPage.openTriggerSequence();
+    environmentPage.assertTriggerNextPageEnabled(false);
+    environmentPage.selectTriggerEvaluation();
+    environmentPage.assertTriggerNextPageEnabled(true).click();
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.selectTriggerEvaluationType(1);
+    environmentPage.typeTriggerEvaluationLabels('key1=val1');
+    environmentPage.assertTriggerSequenceEnabled(false);
 
     // End before start date error
-    cy.byTestId('keptn-trigger-button-starttime').click();
-    selectDateTime(1);
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-button-endtime').click();
-    selectDateTime(0);
-    cy.byTestId('keptn-trigger-evaluation-date-error').should('exist');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
+    environmentPage.clickTriggerStartTime();
+    environmentPage.selectTriggerDateTime(1);
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.clickTriggerEndTime();
+    environmentPage.selectTriggerDateTime(0);
+    environmentPage.assertTriggerEvaluationDateErrorExists(true);
+    environmentPage.assertTriggerSequenceEnabled(false);
 
     // Correct date order
-    cy.byTestId('keptn-trigger-button-starttime').click();
-    selectDateTime(0);
-    cy.byTestId('keptn-trigger-button-endtime').click();
-    selectDateTime(1);
-    cy.byTestId('keptn-trigger-evaluation-date-error').should('not.exist');
+    environmentPage.clickTriggerStartTime();
+    environmentPage.selectTriggerDateTime(0);
+    environmentPage.clickTriggerEndTime();
+    environmentPage.selectTriggerDateTime(1);
+    environmentPage.assertTriggerEvaluationDateErrorExists(false);
 
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled').click();
+    environmentPage.assertTriggerSequenceEnabled(true).click();
     cy.url().should('include', '/project/sockshop/sequence/6c98fbb0-4c40-4bff-ba9f-b20556a57c8a/stage/dev');
   });
 
   it('should trigger a custom sequence', () => {
-    cy.byTestId('keptn-trigger-button-open').click();
-    cy.byTestId('keptn-trigger-button-next').should('be.disabled');
-    selectCustomSequence();
-    cy.byTestId('keptn-trigger-button-next').should('be.enabled').click();
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-custom-labels').type('key1=val1');
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-custom-sequence').click();
-    cy.wait(500);
-    cy.get('dt-option').eq(0).click();
-    cy.byTestId('keptn-trigger-button-trigger').should('be.enabled').click();
+    environmentPage.openTriggerSequence();
+    environmentPage.assertTriggerNextPageEnabled(false);
+    environmentPage.selectTriggerCustomSequence();
+    environmentPage.assertTriggerNextPageEnabled(true).click();
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.typeTriggerCustomLabels('key1=val1');
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.selectTriggerCustomSequenceType(0);
+    environmentPage.assertTriggerSequenceEnabled(true).click();
     cy.url().should('include', '/project/sockshop/sequence/6c98fbb0-4c40-4bff-ba9f-b20556a57c8a/stage/dev');
   });
 
   it('should open the trigger form from the sequence screen', () => {
     cy.visit('/project/sockshop/sequence');
-    cy.byTestId('keptn-trigger-button-open').should('exist').click();
+    environmentPage.assertOpenTriggerSequenceExists(true).click();
     cy.url().should('include', '/project/sockshop');
-    cy.byTestId('keptn-trigger-entry-h2')
-      .should('exist')
-      .should('have.text', 'Trigger a new sequence for project sockshop');
+    environmentPage.assertTriggerEntryH2('exist');
+    environmentPage.assertTriggerEntryH2('have.text', 'Trigger a new sequence for project sockshop');
   });
 
   it('should have the selected stage preselected', () => {
-    testStageSelection(0, 'dev');
-    testStageSelection(1, 'staging');
-    testStageSelection(2, 'production');
+    environmentPage.assertTriggerStageSelection(0, 'dev');
+    environmentPage.assertTriggerStageSelection(1, 'staging');
+    environmentPage.assertTriggerStageSelection(2, 'production');
   });
 
-  function selectDelivery(): void {
-    cy.byTestId('keptn-trigger-sequence-selection').children().first().click();
-    selectServiceAndStage();
-  }
-
-  function selectEvaluation(): void {
-    cy.byTestId('keptn-trigger-sequence-selection').children().eq(1).click();
-    selectServiceAndStage();
-  }
-
-  function selectCustomSequence(): void {
-    cy.wait(500);
-    cy.byTestId('keptn-trigger-sequence-selection').children().eq(2).click();
-    selectServiceAndStage();
-  }
-
-  function selectServiceAndStage(): void {
-    cy.byTestId('keptn-trigger-service-selection').click();
-    cy.wait(500);
-    cy.get('dt-option').eq(0).click();
-    cy.byTestId('keptn-trigger-stage-selection').click();
-    cy.wait(500);
-    cy.get('dt-option').eq(0).click();
-  }
-
   function testNavigationFirstPart(h2Selector: string, expectedText: string): void {
-    cy.byTestId('keptn-trigger-button-next').should('be.enabled').click();
+    environmentPage.assertTriggerNextPageEnabled(true).click();
     cy.byTestId(h2Selector).should('have.text', expectedText);
-    cy.byTestId('keptn-trigger-button-trigger').should('be.disabled');
-    cy.byTestId('keptn-trigger-button-close').click();
+    environmentPage.assertTriggerSequenceEnabled(false);
+    environmentPage.clickTriggerClose();
     cy.byTestId(h2Selector).should('not.exist');
-    cy.byTestId('keptn-trigger-button-open').should('exist');
-    cy.byTestId('keptn-trigger-button-open').click();
+    environmentPage.assertOpenTriggerSequenceExists(true).click();
   }
 
   function testNavigationSecondPart(h2Selector: string): void {
-    cy.byTestId('keptn-trigger-button-next').click();
-    cy.byTestId('keptn-trigger-button-back').click();
-    cy.byTestId('keptn-trigger-entry-h2').should('exist');
+    environmentPage.clickTriggerNext();
+    environmentPage.clickTriggerBack();
+    environmentPage.assertTriggerEntryH2('exist');
     cy.byTestId(h2Selector).should('not.exist');
-    cy.byTestId('keptn-trigger-button-close').click();
-  }
-
-  function selectDateTime(calElement: number): void {
-    cy.get('.dt-calendar-table-cell').eq(calElement).click();
-    cy.byTestId('keptn-datetime-picker-submit').should('be.enabled');
-    cy.get('[uitestid="keptn-datetime-picker-time"] [uitestid="keptn-time-input-hours"]').type('1');
-    cy.byTestId('keptn-datetime-picker-submit').should('be.disabled');
-    cy.get('[uitestid="keptn-datetime-picker-time"] [uitestid="keptn-time-input-minutes"]').type('15');
-    cy.byTestId('keptn-datetime-picker-submit').should('be.disabled');
-    cy.get('[uitestid="keptn-datetime-picker-time"] [uitestid="keptn-time-input-seconds"]').type('0');
-    cy.byTestId('keptn-datetime-picker-submit').should('be.enabled').click();
-  }
-
-  function testStageSelection(elem: number, expectedText: string): void {
-    cy.get('.stage-list .ktb-selectable-tile').eq(elem).find('h2').click();
-    cy.byTestId('keptn-trigger-button-open').click();
-    cy.get('[uitestid="keptn-trigger-stage-selection"] .dt-select-value-text > span').should('have.text', expectedText);
-    cy.byTestId('keptn-trigger-button-close').click();
+    environmentPage.clickTriggerClose();
   }
 });
--- bridge/cypress/support/pageobjects/EnvironmentPage.ts
+++ bridge/cypress/support/pageobjects/EnvironmentPage.ts
@@ -1,6 +1,139 @@
 /// <reference types="cypress" />
 
+import Chainable = Cypress.Chainable;
+
 class EnvironmentPage {
-  STAGE_HEADER_LOC = 'div > h2';
+  public openTriggerSequence(): void {
+    cy.byTestId('keptn-trigger-button-open').click();
+  }
+
+  public selectTriggerDelivery(): void {
+    cy.byTestId('keptn-trigger-sequence-selection').children().first().click();
+    this.selectTriggerServiceAndStage();
+  }
+
+  public selectTriggerEvaluation(): void {
+    cy.byTestId('keptn-trigger-sequence-selection').children().eq(1).click();
+    this.selectTriggerServiceAndStage();
+  }
+
+  public selectTriggerCustomSequence(): void {
+    cy.wait(500);
+    cy.byTestId('keptn-trigger-sequence-selection').children().eq(2).click();
+    this.selectTriggerServiceAndStage();
+  }
+
+  public selectTriggerServiceAndStage(): void {
+    cy.byTestId('keptn-trigger-service-selection').click();
+    cy.wait(500);
+    cy.get('dt-option').eq(0).click();
+    cy.byTestId('keptn-trigger-stage-selection').click();
+    cy.wait(500);
+    cy.get('dt-option').eq(0).click();
+  }
+
+  public selectTriggerDateTime(calElement: number): void {
+    cy.get('.dt-calendar-header-button-prev-month').click();
+    cy.get('.dt-calendar-table-cell').eq(calElement).click();
+    cy.byTestId('keptn-datetime-picker-submit').should('be.enabled');
+    cy.get('[uitestid="keptn-datetime-picker-time"] [uitestid="keptn-time-input-hours"]').type('1');
+    cy.byTestId('keptn-datetime-picker-submit').should('be.disabled');
+    cy.get('[uitestid="keptn-datetime-picker-time"] [uitestid="keptn-time-input-minutes"]').type('15');
+    cy.byTestId('keptn-datetime-picker-submit').should('be.disabled');
+    cy.get('[uitestid="keptn-datetime-picker-time"] [uitestid="keptn-time-input-seconds"]').type('0');
+    cy.byTestId('keptn-datetime-picker-submit').should('be.enabled').click();
+  }
+
+  public clickTriggerClose(): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-close').click();
+  }
+
+  public clickTriggerNext(): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-next').click();
+  }
+
+  public clickTriggerBack(): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-back').click();
+  }
+
+  public clickTriggerStartTime(): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-starttime').click();
+  }
+
+  public clickTriggerEndTime(): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-endtime').click();
+  }
+
+  public typeTriggerDeliveryLabels(value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-delivery-labels').type(value);
+  }
+
+  public typeTriggerDeliveryValues(value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-delivery-values').type(value);
+  }
+
+  public typeTriggerDeliveryImage(value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-delivery-image').type(value);
+  }
+
+  public typeTriggerDeliveryTag(value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-delivery-tag').type(value);
+  }
+
+  public typeTriggerEvaluationLabels(value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-evaluation-labels').type(value);
+  }
+
+  public typeTriggerEvaluationTimeInput(inputName: string, value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId(`keptn-time-input-${inputName}`).type(value);
+  }
+
+  public typeTriggerCustomLabels(value: string): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-custom-labels').type(value);
+  }
+
+  public selectTriggerEvaluationType(elIndex: number): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-evaluation-type').children().eq(elIndex).click();
+  }
+
+  public selectTriggerCustomSequenceType(elIndex: number): void {
+    cy.byTestId('keptn-trigger-custom-sequence').click();
+    cy.wait(500);
+    cy.get('dt-option').eq(elIndex).click();
+  }
+
+  public assertTriggerStageSelection(elem: number, expectedText: string): void {
+    cy.get('.stage-list .ktb-selectable-tile').eq(elem).find('h2').click();
+    cy.byTestId('keptn-trigger-button-open').click();
+    cy.get('[uitestid="keptn-trigger-stage-selection"] .dt-select-value-text > span').should('have.text', expectedText);
+    cy.byTestId('keptn-trigger-button-close').click();
+  }
+
+  public assertTriggerSequenceEnabled(enabled: boolean): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-trigger').should(enabled ? 'be.enabled' : 'be.disabled');
+  }
+
+  public assertOpenTriggerSequenceExists(exists: boolean): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-open').should(exists ? 'exist' : 'not.exist');
+  }
+
+  public assertTriggerNextPageEnabled(enabled: boolean): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-button-next').should(enabled ? 'be.enabled' : 'be.disabled');
+  }
+
+  public assertTriggerEntryH2(chainer: string, value: string | undefined = undefined): Chainable<JQuery<HTMLElement>> {
+    if (value) {
+      return cy.byTestId('keptn-trigger-entry-h2').should(chainer, value);
+    }
+    return cy.byTestId('keptn-trigger-entry-h2').should(chainer);
+  }
+
+  public assertTriggerDeliveryValuesErrorExists(exists: boolean): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-delivery-values-error').should(exists ? 'exist' : 'not.exist');
+  }
+
+  public assertTriggerEvaluationDateErrorExists(exists: boolean): Chainable<JQuery<HTMLElement>> {
+    return cy.byTestId('keptn-trigger-evaluation-date-error').should(exists ? 'exist' : 'not.exist');
+  }
 }
 export default EnvironmentPage;
