dd9d222a5c953389b2c3d672be0a73762e4e5ec6
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-03-07 11:09:55 +0100

    better debug output
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- gh-actions-scripts/dependencies-and-licenses.sh
+++ gh-actions-scripts/dependencies-and-licenses.sh
@@ -21,15 +21,43 @@ MODULES=(
 )
 
 for MODULE in "${MODULES[@]}"; do
-   echo "🔍 Analyzing dependencies in module $MODULE"
-   ( cd ./"$MODULE" || return ; go mod tidy > /dev/null 2>&1; go list -m -json all | go-licence-detector -depsTemplate=../.dependencies/templates/dependencies.csv.tmpl -depsOut="${TMP_DIR}"/"${MODULE}"-dependencies.txt  -overrides=../.dependencies/overrides/overrides.json )
+  echo "🔍 Analyzing dependencies in module $MODULE"
+  cd ./"$MODULE" || return
+
+  echo "go mod tidy..."
+  go mod tidy > /dev/null 2>&1
+
+  echo "Getting list of dependencies..."
+  go list -m -json all | \
+    go-licence-detector \
+      -depsTemplate=../.dependencies/templates/dependencies.csv.tmpl \
+      -depsOut="${TMP_DIR}"/"${MODULE}"-dependencies.txt \
+      -overrides=../.dependencies/overrides/overrides.json
 done
 
 echo "🔍 Analyzing dependencies in go-utils"
-( cd ../go-utils || return ; go mod tidy > /dev/null 2>&1; go list -m -json all | go-licence-detector -depsTemplate=../keptn/.dependencies/templates/dependencies.csv.tmpl -depsOut="${TMP_DIR}"/go-utils-dependencies.txt )
+cd ../go-utils || return
+
+echo "go mod tidy..."
+go mod tidy > /dev/null 2>&1
+
+echo "Getting list of dependencies..."
+go list -m -json all | \
+  go-licence-detector \
+    -depsTemplate=../keptn/.dependencies/templates/dependencies.csv.tmpl \
+    -depsOut="${TMP_DIR}"/go-utils-dependencies.txt
 
 echo "🔍 Analyzing dependencies in kubernetes-utils"
-( cd ../kubernetes-utils || return ; go mod tidy > /dev/null 2>&1; go list -m -json all | go-licence-detector -depsTemplate=../keptn/.dependencies/templates/dependencies.csv.tmpl -depsOut="${TMP_DIR}"/kubernetes-utils-dependencies.txt )
+cd ../kubernetes-utils || return
+
+echo "go mod tidy..."
+go mod tidy > /dev/null 2>&1
+
+echo "Getting list of dependencies..."
+go list -m -json all | \
+  go-licence-detector \
+    -depsTemplate=../keptn/.dependencies/templates/dependencies.csv.tmpl \
+    -depsOut="${TMP_DIR}"/kubernetes-utils-dependencies.txt
 
 cat "$TMP_DIR"/*.txt | sort | uniq > dependencies-and-licenses-go.txt
 
