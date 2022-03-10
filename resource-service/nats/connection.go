8e64eaf2af90e2eb18bc90782aacaaa7f5ff7161
warber <bernd.warmuth@dynatrace.com>
2022-03-10 14:25:48 +0100

    set nats max reconnect to -1
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- resource-service/nats/connection.go
+++ resource-service/nats/connection.go
@@ -104,7 +104,7 @@ func (nch *NatsConnectionHandler) renewNatsConnection() error {
 
 	nch.natsConnection.Close()
 	logger.Infof("Connecting to NATS server at %s ...", nch.natsURL)
-	nch.natsConnection, err = nats.Connect(nch.natsURL)
+	nch.natsConnection, err = nats.Connect(nch.natsURL, nats.MaxReconnects(-1))
 
 	if err != nil {
 		return errors.New("failed to create NATS connection: " + err.Error())
