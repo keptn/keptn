d36e15555e5d4142b207d7cac47746fd4700d646
odubajDT <ondrej.dubaj@dynatrace.com>
2022-03-15 11:44:59 +0100

    added unit tests to evaluation_manager
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- shipyard-controller/handler/evaluationmanager_test.go
+++ shipyard-controller/handler/evaluationmanager_test.go
@@ -76,6 +76,68 @@ func TestEvaluationManager_CreateEvaluation(t *testing.T) {
 			},
 		},
 		{
+			name: "everything ok with timeframe - send evaluation.triggered event",
+			fields: fields{
+				EventSender: &keptnfake.EventSender{},
+				ServiceAPI: &db_mock.ProjectMVRepoMock{GetServiceFunc: func(project string, stage string, service string) (*models.ExpandedService, error) {
+					return &models.ExpandedService{}, nil
+				}},
+			},
+			args: args{
+				project: "test-project",
+				stage:   "test-stage",
+				service: "test-service",
+				params: &models.CreateEvaluationParams{
+					Labels:    map[string]string{"foo": "bar"},
+					Start:     "2020-01-02T15:00:00.000Z",
+					End:       "",
+					Timeframe: "5m",
+				},
+			},
+			wantResponse: true,
+			wantErr:      nil,
+			wantEvents: []eventTypeWithPayload{
+				{
+					eventType: keptnv2.GetTriggeredEventType("test-stage." + keptnv2.EvaluationTaskName),
+					payload: &keptnv2.EvaluationTriggeredEventData{
+						EventData: keptnv2.EventData{
+							Project: "test-project",
+							Stage:   "test-stage",
+							Service: "test-service",
+							Labels:  map[string]string{"foo": "bar"},
+						},
+						Evaluation: keptnv2.Evaluation{
+							Start: "2020-01-02T15:00:00.000Z",
+							End:   "2020-01-02T15:05:00.000Z",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "non-existing service",
+			fields: fields{
+				EventSender: &keptnfake.EventSender{},
+				ServiceAPI: &db_mock.ProjectMVRepoMock{GetServiceFunc: func(project string, stage string, service string) (*models.ExpandedService, error) {
+					return nil, errors.New("service not found")
+				}},
+			},
+			args: args{
+				project: "test-project",
+				stage:   "test-stage",
+				service: "no-test-service",
+				params: &models.CreateEvaluationParams{
+					Labels:    map[string]string{"foo": "bar"},
+					Start:     "2020-01-02T15:00:00.000Z",
+					End:       "2020-01-02T16:00:00.000Z",
+					Timeframe: "",
+				},
+			},
+			wantResponse: false,
+			wantErr:      &models.Error{Code: evaluationErrServiceNotAvailable},
+			wantEvents:   []eventTypeWithPayload{},
+		},
+		{
 			name: "invalid timeframe",
 			fields: fields{
 				EventSender: &keptnfake.EventSender{},
