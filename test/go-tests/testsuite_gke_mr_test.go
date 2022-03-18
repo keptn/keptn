2d8565f5a478eadb737fe1c4955a7b725a1db5d6
warber <bernd.warmuth@dynatrace.com>
2022-03-18 09:42:14 +0100

    cleanup
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- test/go-tests/testsuite_gke_mr_test.go
+++
@@ -1,9 +0,0 @@
-package go_tests
-
-import "testing"
-
-// Test_GKE_MR contains tests that are run against a keptn installation
-// that has multiple replicas of its components in place
-func Test_GKE_MR(t *testing.T) {
-	t.Run("Test_ResourceService_MR", Test_ResourceServiceBasic)
-}
