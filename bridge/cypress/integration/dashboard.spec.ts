b0847d0ec5c1f25c6f9773698c4377df08960640
ermin.muratovic <ermin.muratovic@gmail.com>
2022-03-22 17:48:30 +0100

    create UI test to check if dashboard loads projects correctly also if version.json was not able to be loaded
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

---
+++ bridge/cypress/integration/dashboard.spec.ts
@@ -0,0 +1,33 @@
+import DashboardPage from '../support/pageobjects/DashboardPage';
+
+import * as projectsResponse from '../fixtures/projects.mock.json';
+
+describe('Bridge Dashboard', () => {
+  const dashboardPage = new DashboardPage();
+
+  beforeEach(() => {
+    cy.intercept('/api/v1/metadata', { fixture: 'metadata' });
+    cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', { fixture: 'projects.mock' }).as(
+      'projects'
+    );
+    cy.intercept('/api/controlPlane/v1/sequence/sockshop?pageSize=5', { fixture: 'sequences.sockshop' }).as(
+      'sequences'
+    );
+    cy.intercept('/api/bridgeInfo', { fixture: 'bridgeInfo.mock' });
+  });
+
+  it('should load also if version.json is not available', () => {
+    cy.intercept('/api/bridgeInfo', { fixture: 'bridgeInfoVersionCheck.mock' }).as('bridgeInfo');
+    cy.intercept('/api/v1/metadata', { fixture: 'metadata' });
+    cy.intercept('/api/version.json', { statusCode: 500 }).as('version.json');
+
+    // versioncheck was accepted by user
+    localStorage.setItem('keptn_versioncheck', JSON.stringify({ enabled: true, time: 1647880061049 }));
+
+    dashboardPage.visitDashboard();
+    cy.visit('/');
+    cy.wait('@projects', { timeout: 15000 });
+    cy.wait('@sequences', { timeout: 15000 });
+    dashboardPage.assertProjects(projectsResponse.projects);
+  });
+});
