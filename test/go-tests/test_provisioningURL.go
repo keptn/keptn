899c5408cfe85326abbff9eceeb94516efe89757
odubajDT <ondrej.dubaj@dynatrace.com>
2022-03-29 14:28:07 +0200

    refactor test
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- test/go-tests/test_provisioningURL.go
+++ test/go-tests/test_provisioningURL.go
@@ -48,62 +48,31 @@ data:
     ]
   mockserver.properties: |-
     ###############################
-
     # MockServer & Proxy Settings #
-
     ###############################
-
-
     # Socket & Port Settings
-
-
     # socket timeout in milliseconds (default 120000)
-
     mockserver.maxSocketTimeout=120000
-
-
     # Certificate Generation
-
-
     # dynamically generated CA key pair (if they don't already exist in
     specified directory)
-
     mockserver.dynamicallyCreateCertificateAuthorityCertificate=true
-
     # save dynamically generated CA key pair in working directory
-
     mockserver.directoryToSaveDynamicSSLCertificate=.
-
     # certificate domain name (default "localhost")
-
     mockserver.sslCertificateDomainName=localhost
-
     # comma separated list of ip addresses for Subject Alternative Name domain
     names (default empty list)
-
     mockserver.sslSubjectAlternativeNameDomains=www.example.com,www.another.com
-
     # comma separated list of ip addresses for Subject Alternative Name ips
     (default empty list)
-
     mockserver.sslSubjectAlternativeNameIps=127.0.0.1
-
-
     # CORS
-
-
     # enable CORS for MockServer REST API
-
     mockserver.enableCORSForAPI=true
-
     # enable CORS for all responses
-
     mockserver.enableCORSForAllResponses=true
-
-
     # Json Initialization
-
-
     mockserver.initializationJsonPath=/config/initializerJson.json
 `
 
