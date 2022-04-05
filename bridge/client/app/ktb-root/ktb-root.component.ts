08b70ce95eff3f670e5f686d0dcfceee048aaeae
Klaus Strießnig <k.striessnig@gmail.com>
2022-04-05 18:10:14 +0200

    removed empty file
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/ktb-root/ktb-root.component.ts
+++ bridge/client/app/ktb-root/ktb-root.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ktb-root',
   templateUrl: './ktb-root.component.html',
-  styleUrls: ['./ktb-root.component.scss'],
+  styleUrls: [],
 })
 export class KtbRootComponent {}
