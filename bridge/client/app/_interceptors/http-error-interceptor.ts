b2caa258a7e9c90dce9de80c86418c26f8090954
ermin.muratovic <ermin.muratovic@gmail.com>
2022-04-07 14:25:31 +0200

    return NEVER or EMPTY Observable in case of 401 and 403
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_interceptors/http-error-interceptor.ts
+++ bridge/client/app/_interceptors/http-error-interceptor.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable, throwError, of } from 'rxjs';
+import { Observable, throwError, NEVER, EMPTY } from 'rxjs';
 import { catchError, retryWhen } from 'rxjs/operators';
 import { genericRetryStrategy, RetryParams } from './http-generic-retry-strategy';
 import { Location } from '@angular/common';
@@ -27,61 +27,73 @@ export class HttpErrorInterceptor implements HttpInterceptor {
       : { maxAttempts: 3, scalingDuration: 0, shouldRetry: (): boolean => false };
     return next.handle(request).pipe(
       retryWhen(genericRetryStrategy(params)),
-      catchError((error) => {
+      catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          this._handleUnauthorizedError(error);
-          return of(error);
-        } else if (error.status === 403) {
+          return this._handleUnauthorizedError(error);
+        }
+
+        if (error.status === 403) {
           this.notificationService.addNotification(
             NotificationType.ERROR,
             'You do not have the permissions to perform this action.'
           );
-          return of(error);
-        } else if (error.status === 409 && error.url?.endsWith('/api/secrets/v1/secret')) {
-          // Special case for already existing secrets - for unit test has to be before instanceof ErrorEvent
-          return throwError(error);
-        } else if (error.error instanceof ErrorEvent) {
-          // A client-side or network error occurred. Handle it accordingly.
-          this.notificationService.addNotification(NotificationType.ERROR, error.error.message);
-        } else {
-          // The backend returned an unsuccessful response code.
-          // The response body may contain clues as to what went wrong,
-          this.notificationService.addNotification(NotificationType.ERROR, error.message);
+          return EMPTY;
         }
 
+        if (error.status === 409 && error.url?.endsWith('/api/secrets/v1/secret')) {
+          // Special case for already existing secrets - for unit test has to be before instanceof ErrorEvent
+          return throwError(error);
+        }
+
+        if (error.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          this.notificationService.addNotification(NotificationType.ERROR, error.error.message);
+          return throwError(error);
+        }
+
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        this.notificationService.addNotification(NotificationType.ERROR, error.message);
         return throwError(error);
       })
     );
   }
 
-  private _handleUnauthorizedError(error: HttpErrorResponse): void {
+  private _handleUnauthorizedError(error: HttpErrorResponse): Observable<HttpEvent<unknown>> {
     const authType = error.headers.get('keptn-auth-type');
-    if (authType === 'OAUTH') {
-      if (!this.isReloading) {
-        if (window.location.href.indexOf('/oauth/login') === -1) {
-          this.isReloading = true;
-          this.notificationService.addNotification(NotificationType.INFO, 'Login required. Redirecting to login.');
-          // Wait for few moments to let user see the toast message and navigate to external login route
-          setTimeout(() => (window.location.href = this.location.prepareExternalUrl('/oauth/login')), 1000);
-        }
-      }
-    } else if (authType === 'BASIC') {
-      if (!this.isAuthorizedErrorShown) {
-        this.isAuthorizedErrorShown = true;
-        this.notificationService.addNotification(
-          NotificationType.ERROR,
-          'Login credentials invalid. Please check your provided username and password.'
-        );
-      }
-    } else if (!this.isAuthorizedErrorShown) {
-      let errorInfo;
-      if (error.error === 'incorrect api key auth') {
-        errorInfo = 'Could not authorize API token. Please check the configured API token.';
-      } else {
-        errorInfo = 'Could not authorize.';
-      }
-      this.isAuthorizedErrorShown = true;
-      this.notificationService.addNotification(NotificationType.ERROR, ' ' + errorInfo);
+
+    if (this.isAuthorizedErrorShown) {
+      return EMPTY;
     }
+
+    if (this.isReloading) {
+      return NEVER;
+    }
+
+    if (authType === 'OAUTH') {
+      this.isReloading = true;
+      this.notificationService.addNotification(NotificationType.INFO, 'Login required. Redirecting to login.');
+      // Wait for few moments to let user see the toast message and navigate to external login route
+      setTimeout(() => (window.location.href = this.location.prepareExternalUrl('/oauth/login')), 1000);
+      return NEVER;
+    } else if (authType === 'BASIC') {
+      this.isAuthorizedErrorShown = true;
+      this.notificationService.addNotification(
+        NotificationType.ERROR,
+        'Login credentials invalid. Please check your provided username and password.'
+      );
+      return EMPTY;
+    }
+
+    let errorInfo;
+    if (error.error === 'incorrect api key auth') {
+      errorInfo = 'Could not authorize API token. Please check the configured API token.';
+    } else {
+      errorInfo = 'Could not authorize.';
+    }
+    this.isAuthorizedErrorShown = true;
+    this.notificationService.addNotification(NotificationType.ERROR, ' ' + errorInfo);
+
+    return EMPTY;
   }
 }
