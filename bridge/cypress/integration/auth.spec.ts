695a24812b1ff8b23fa9d1d6ede0c16572d4da96
ermin.muratovic <ermin.muratovic@gmail.com>
2022-04-11 14:17:50 +0200

    add UI tests for BASIC auth and invalid api token
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

---
+++ bridge/cypress/integration/auth.spec.ts
@@ -0,0 +1,81 @@
+import BasePage from '../support/pageobjects/BasePage';
+
+describe('Test auth errors', () => {
+  const basePage = new BasePage();
+
+  beforeEach(() => {
+    cy.intercept('/api/v1/metadata', { fixture: 'metadata' });
+  });
+
+  it('should show error for 401 response', () => {
+    cy.intercept('/api/bridgeInfo', { statusCode: 401 });
+    cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', {
+      statusCode: 401,
+    });
+
+    cy.visit('/');
+    basePage.notificationErrorVisible('Could not authorize.');
+  });
+
+  it('should show error for invalid token', () => {
+    cy.intercept('/api/bridgeInfo', {
+      statusCode: 401,
+      body: 'incorrect api key auth',
+    });
+    cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', {
+      statusCode: 401,
+      body: 'incorrect api key auth',
+    });
+
+    cy.visit('/');
+    basePage.notificationErrorVisible('Could not authorize API token. Please check the configured API token.');
+  });
+});
+
+describe('Test BASIC auth', () => {
+  const basePage = new BasePage();
+
+  beforeEach(() => {
+    cy.intercept('/api/v1/metadata', { fixture: 'metadata' });
+  });
+
+  it('should show error for 401 response', () => {
+    cy.intercept('/api/bridgeInfo', { statusCode: 401, headers: { 'keptn-auth-type': 'BASIC' } });
+    cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', {
+      statusCode: 401,
+      headers: { 'keptn-auth-type': 'BASIC' },
+    });
+
+    cy.visit('/');
+    basePage.notificationErrorVisible('Login credentials invalid. Please check your provided username and password.');
+  });
+});
+
+describe('Test OAuth', () => {
+  const basePage = new BasePage();
+
+  beforeEach(() => {
+    cy.intercept('/api/v1/metadata', { fixture: 'metadata' });
+  });
+
+  it('should redirect to login page for 401 response', () => {
+    cy.intercept('/api/bridgeInfo', { statusCode: 401, headers: { 'keptn-auth-type': 'OAUTH' } });
+    cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', {
+      statusCode: 401,
+      headers: { 'keptn-auth-type': 'OAUTH' },
+    });
+
+    cy.visit('/');
+    basePage.notificationInfoVisible('Login required. Redirecting to login.');
+
+    cy.url().should('include', '/oauth/login');
+  });
+
+  it('should show message for 403 response', () => {
+    cy.intercept('/api/bridgeInfo', { fixture: 'bridgeInfo.mock' });
+    cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', { statusCode: 403 });
+
+    cy.visit('/');
+    basePage.notificationErrorVisible('You do not have the permissions to perform this action.');
+  });
+});
