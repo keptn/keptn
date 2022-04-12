503d3ef585bdee5bdea9369d665abc9782f70a42
odubajDT <93584209+odubajDT@users.noreply.github.com>
2022-04-12 15:58:26 +0200

    fix: Disconnect MongoDB client before reconnecting (#7431)

--- shipyard-controller/db/mongodb_connection.go
+++ shipyard-controller/db/mongodb_connection.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	keptnmongoutils "github.com/keptn/go-utils/pkg/common/mongoutils"
+	logger "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -17,6 +18,9 @@ var mutex = &sync.Mutex{}
 var mongoDBConnectionInstance *MongoDBConnection
 
 var mongoConnectionOnce sync.Once
+
+const clientCreationFailed = "failed to create mongo client: %v"
+const clientConnectionFailed = "failed to connect client to MongoDB: %v"
 
 // MongoDBConnection takes care of establishing a connection to the mongodb
 type MongoDBConnection struct {
@@ -36,40 +40,58 @@ func getDatabaseName() string {
 
 // EnsureDBConnection makes sure a connection to the mongodb is established
 func (m *MongoDBConnection) EnsureDBConnection() error {
+	// Mutex is neccessary for not creating multiple clients after restarting the pod
+	mutex.Lock()
+	defer mutex.Unlock()
 	var err error
 	// attention: not calling the cancel() function likely causes memory leaks
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if m.Client == nil {
-		fmt.Println("No MongoDB client has been initialized yet. Creating a new one.")
+		logger.Info("No MongoDB client has been initialized yet. Creating a new one.")
 		return m.connectMongoDBClient()
 	} else if err = m.Client.Ping(ctx, nil); err != nil {
-		fmt.Println("MongoDB client lost connection. Attempt reconnect.")
+		logger.Info("MongoDB client lost connection. Attempting reconnect.")
+		ctxDisconnect, cancelDisconnect := context.WithTimeout(context.TODO(), 30*time.Second)
+		defer cancelDisconnect()
+		err2 := m.Client.Disconnect(ctxDisconnect)
+		if err2 != nil {
+			logger.Errorf("failed to disconnect client from MongoDB: %v", err2)
+		}
+		m.Client = nil
 		return m.connectMongoDBClient()
 	}
 	return nil
 }
 
 func (m *MongoDBConnection) connectMongoDBClient() error {
-	var err error
-
 	connectionString, _, err := keptnmongoutils.GetMongoConnectionStringFromEnv()
 	if err != nil {
-		return fmt.Errorf("failed to create mongo client: %v", err)
+		logger.Errorf(clientCreationFailed, err)
+		return fmt.Errorf(clientCreationFailed, err)
 	}
-	m.Client, err = mongo.NewClient(options.Client().ApplyURI(connectionString))
+	clientOptions := options.Client()
+	clientOptions = clientOptions.ApplyURI(connectionString)
+	clientOptions = clientOptions.SetConnectTimeout(30 * time.Second)
+	m.Client, err = mongo.NewClient(clientOptions)
 	if err != nil {
-		err := fmt.Errorf("failed to create mongo client: %v", err)
-		return err
+		logger.Errorf(clientCreationFailed, err)
+		return fmt.Errorf(clientCreationFailed, err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	err = m.Client.Connect(ctx)
 	if err != nil {
-		err := fmt.Errorf("failed to connect client to MongoDB: %v", err)
-		return err
+		logger.Errorf(clientConnectionFailed, err)
+		return fmt.Errorf(clientConnectionFailed, err)
 	}
+	if err = m.Client.Ping(ctx, nil); err != nil {
+		logger.Errorf(clientConnectionFailed, err)
+		return fmt.Errorf(clientConnectionFailed, err)
+	}
+
+	logger.Info("Successfully connected to MongoDB")
 	return nil
 }
--- statistics-service/db/mongodb_connection.go
+++ statistics-service/db/mongodb_connection.go
@@ -3,10 +3,12 @@ package db
 import (
 	"context"
 	"fmt"
-	keptnmongoutils "github.com/keptn/go-utils/pkg/common/mongoutils"
 	"sync"
 	"time"
 
+	keptnmongoutils "github.com/keptn/go-utils/pkg/common/mongoutils"
+
+	logger "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -15,6 +17,9 @@ var databaseName string
 
 var mutex = &sync.Mutex{}
 
+const clientCreationFailed = "failed to create mongo client: %v"
+const clientConnectionFailed = "failed to connect client to MongoDB: %v"
+
 // MongoDBConnection takes care of establishing a connection to the mongodb
 type MongoDBConnection struct {
 	Client *mongo.Client
@@ -22,6 +27,7 @@ type MongoDBConnection struct {
 
 // EnsureDBConnection makes sure a connection to the mongodb is established
 func (m *MongoDBConnection) EnsureDBConnection() error {
+	// Mutex is neccessary for not creating multiple clients after restarting the pod
 	mutex.Lock()
 	defer mutex.Unlock()
 	var err error
@@ -29,10 +35,17 @@ func (m *MongoDBConnection) EnsureDBConnection() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if m.Client == nil {
-		fmt.Println("No MongoDB client has been initialized yet. Creating a new one.")
+		logger.Info("No MongoDB client has been initialized yet. Creating a new one.")
 		return m.connectMongoDBClient()
 	} else if err = m.Client.Ping(ctx, nil); err != nil {
-		fmt.Println("MongoDB client lost connection. Attempt reconnect.")
+		logger.Info("MongoDB client lost connection. Attempting reconnect.")
+		ctxDisconnect, cancelDisconnect := context.WithTimeout(context.TODO(), 30*time.Second)
+		defer cancelDisconnect()
+		err2 := m.Client.Disconnect(ctxDisconnect)
+		if err2 != nil {
+			logger.Errorf("failed to disconnect client from MongoDB: %v", err2)
+		}
+		m.Client = nil
 		return m.connectMongoDBClient()
 	}
 	return nil
@@ -42,22 +55,31 @@ func (m *MongoDBConnection) connectMongoDBClient() error {
 	var err error
 	connectionString, dbName, err := keptnmongoutils.GetMongoConnectionStringFromEnv()
 	if err != nil {
-		err := fmt.Errorf("failed to create mongo client: %v", err)
-		return err
+		logger.Errorf(clientCreationFailed, err)
+		return fmt.Errorf(clientCreationFailed, err)
 	}
 	databaseName = dbName
-	m.Client, err = mongo.NewClient(options.Client().ApplyURI(connectionString))
+	clientOptions := options.Client()
+	clientOptions = clientOptions.ApplyURI(connectionString)
+	clientOptions = clientOptions.SetConnectTimeout(30 * time.Second)
+	m.Client, err = mongo.NewClient(clientOptions)
 	if err != nil {
-		err := fmt.Errorf("failed to create mongo client: %v", err)
-		return err
+		logger.Errorf(clientCreationFailed, err)
+		return fmt.Errorf(clientCreationFailed, err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	err = m.Client.Connect(ctx)
 	if err != nil {
-		err := fmt.Errorf("failed to connect client to MongoDB: %v", err)
-		return err
+		logger.Errorf(clientConnectionFailed, err)
+		return fmt.Errorf(clientConnectionFailed, err)
 	}
+	if err = m.Client.Ping(ctx, nil); err != nil {
+		logger.Errorf(clientConnectionFailed, err)
+		return fmt.Errorf(clientConnectionFailed, err)
+	}
+
+	logger.Info("Successfully connected to MongoDB")
 	return nil
 }
