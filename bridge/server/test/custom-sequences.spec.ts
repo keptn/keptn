9658cc848dbc4a45f3f7658a0eb707b016bc533f
Klaus Strießnig <k.striessnig@gmail.com>
2022-04-13 15:33:42 +0200

    added unit test for shipyard with stage without sequences
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/server/test/custom-sequences.spec.ts
+++ bridge/server/test/custom-sequences.spec.ts
@@ -3,6 +3,7 @@ import MockAdapter from 'axios-mock-adapter';
 import { ShipyardResponse } from '../fixtures/shipyard-response.mock';
 import { setupServer } from '../.jest/setupServer';
 import { Express } from 'express';
+import { ShipyardEmptySequenceResponse } from '../fixtures/shipyard-empty-sequence-response';
 
 let axiosMock: MockAdapter;
 
@@ -28,6 +29,19 @@ describe('Test /project/:projectName/customSequences', () => {
       dev: ['delivery-direct'],
       staging: ['delivery-direct', 'rollback'],
       production: ['delivery-direct', 'remediation', 'rollback'],
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should return empty sequences if stage does not have any sequences', async () => {
+    const projectName = 'sockshop';
+    axiosMock
+      .onGet(`${global.baseUrl}/configuration-service/v1/project/${projectName}/resource/shipyard.yaml`)
+      .reply(200, ShipyardEmptySequenceResponse);
+    const response = await request(app).get(`/api/project/${projectName}/customSequences`);
+    expect(response.body).toEqual({
+      dev: ['rollback'],
+      staging: [],
     });
     expect(response.statusCode).toBe(200);
   });
