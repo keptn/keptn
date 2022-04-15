91a6fdde1600978e8b26a1cfb1605751874bc48b
RealAnna <anna.reale@dynatrace.com>
2022-04-15 10:16:44 +0200

    feat:  fixed api
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- test/ZD/API_test.go
+++ test/ZD/API_test.go
@@ -40,43 +40,43 @@ func TestAPIs(t *testing.T) {
 
 func (suite *TestSuiteEnv) Test_API_Service() {
 
-	cli := &http.Client{
-		Timeout: time.Second * 1,
-	}
 	apiURL := suite.keptnAPIURL + "/v1"
 
-	apitest.New("Test api-service auth").EnableNetworking(cli).Post(apiURL + "/auth").Headers(map[string]string{"x-token": suite.token}).
+	apitest.New("Test api-service auth").EnableNetworking(getClient(1)).Post(apiURL + "/auth").Headers(map[string]string{"x-token": suite.token}).
 		Expect(suite.T()).Status(http.StatusOK).End()
 
-	apitest.New("Test api-service metadata").EnableNetworking(cli).Get(apiURL + "/metadata").Headers(map[string]string{"x-token": suite.token}).
+	apitest.New("Test api-service metadata").EnableNetworking(getClient(1)).Get(apiURL + "/metadata").Headers(map[string]string{"x-token": suite.token}).
 		Expect(suite.T()).Status(http.StatusOK).End()
 
 }
 
 func (suite *TestSuiteEnv) Test_Configuration_Service() {
 
-	cli := &http.Client{
-		Timeout: time.Second * 1,
-	}
 	apiURL := suite.keptnAPIURL + "/configuration-service/v1"
 
-	// can add Body(`{"name": "jon", "id": "1234"}`) or .Assert(jsonpath.Equal(`$.key`, value )) Debug()
-	apitest.New("Test configuration-service: not existing project").EnableNetworking(cli).
+	req := apitest.New("Test configuration-service: not existing project").EnableNetworking(getClient(1)).Debug().
 		Get(apiURL + "/project/unexisting-project/resource").
-		Headers(map[string]string{"x-token": suite.token}).
-		Expect(suite.T()).Status(http.StatusNotFound).Assert(jsonpath.Equal(`$.message`, "Project does not exist")).End()
+		Headers(map[string]string{"x-token": suite.token}).Expect(suite.T()).Status(http.StatusNotFound)
 
+	if res, err := testutils.CompareServiceNameWithDeploymentName("configuration-service", "configuration-service"); err == nil && res {
+		req.Assert(jsonpath.Equal(`$.message`, "Project does not exist")).End()
+	} else {
+		req.Assert(jsonpath.Equal(`$.message`, "Could not find credentials for upstream repository")).End()
+	}
+}
+
+func getClient(sec time.Duration) *http.Client {
+	return &http.Client{
+		Timeout: time.Second * sec,
+	}
 }
 
 func (suite *TestSuiteEnv) Test_ControlPlane() {
 
-	cli := &http.Client{
-		Timeout: time.Second * 1,
-	}
 	apiURL := suite.keptnAPIURL + "/controlPlane/v1"
 
 	// can add Body(`{"name": "jon", "id": "1234"}`) or .Assert(jsonpath.Equal(`$.key`, value )) Debug().
-	apitest.New("Test control-plane: check uniform").EnableNetworking(cli).Debug().
+	apitest.New("Test control-plane: check uniform").EnableNetworking(getClient(1)).Debug().
 		Get(apiURL+"/uniform/registration").Query("name", "lighthouse-service").
 		Headers(map[string]string{"x-token": suite.token}).
 		Expect(suite.T()).Status(http.StatusOK).Assert(jsonpath.Equal(`$[0].name`, "lighthouse-service")).End()
@@ -85,12 +85,9 @@ func (suite *TestSuiteEnv) Test_ControlPlane() {
 
 func (suite *TestSuiteEnv) Test_MongoDB() {
 
-	cli := &http.Client{
-		Timeout: time.Second * 1,
-	}
 	apiURL := suite.keptnAPIURL + "/mongodb-datastore"
 
-	apitest.New("Test mongo-datastore: not existing project").EnableNetworking(cli).Debug().
+	apitest.New("Test mongo-datastore: not existing project").EnableNetworking(getClient(1)).Debug().
 		Get(apiURL+"/event").Query("project", "keptn").Query("pageSize", "20").
 		Headers(map[string]string{"x-token": suite.token}).
 		Expect(suite.T()).Status(http.StatusOK).Body(`{"events":[], "pageSize":20}`).End()
--- test/ZD/ZeroDowntime_test.go
+++ test/ZD/ZeroDowntime_test.go
@@ -2,7 +2,9 @@ package ZD
 
 import (
 	"context"
+	"fmt"
 	"github.com/benbjohnson/clock"
+	testutils "github.com/keptn/keptn/test/go-tests"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"sync"
@@ -12,6 +14,9 @@ import (
 
 const apiProbeInterval = 1 * time.Second
 const deliveryInterval = 10 * time.Second
+
+var chartLatestVersion = "https://github.com/keptn/helm-charts-dev/blob/1efe3dab77da9ea3cf2b7dd5eff4b2fac6f76633/packages/keptn-0.15.0-dev-PR-7266.tgz?raw=true"
+var chartPreviousVersion = "https://github.com/keptn/helm-charts-dev/blob/5b4fbc630895a2a71721763110376b452f4c2c67/packages/keptn-0.15.0-dev-PR-7266.tgz?raw=true"
 
 var FailedSequences uint64 = 0
 var PassedSequences uint64 = 0
@@ -29,7 +34,9 @@ func TestZeroDowntime(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	//start update
-	go rollingUpgrade(cancel, t)
+	//go t.Run("Rolling Updates", func(t *testing.T) {
+	go rollingUpgrade(cancel, 2, t)
+	//})
 
 	go func() {
 		for {
@@ -64,15 +71,33 @@ func TestZeroDowntime(t *testing.T) {
 
 }
 
-func rollingUpgrade(cancel context.CancelFunc, t *testing.T) {
+func rollingUpgrade(cancel context.CancelFunc, nrOfUpgrades int, t *testing.T) {
 	defer cancel()
 	t.Log("rolling")
 	time.Sleep(40 * time.Second)
 
 	//TODO setup helm upgrade here
-	//_, err := ExecuteCommand(fmt.Sprintf(`helm upgrade -n %s keptn %s --wait --set=control-plane.apiGatewayNginx.type=LoadBalancer --set=control-plane.common.strategy.rollingUpdate.maxUnavailable=0 --set control-plane.resourceService.enabled=true --set control-plane.resourceService.env.DIRECTORY_STAGE_STRUCTURE=true --set control-plane.distributor.image.repository=docker.io/keptndev/distributor --set control-plane.distributor.image.tag=0.14.1-dev-PR-7308.202204010740 --set control-plane.lighthouseService.image.repository=docker.io/warber/lighthouse-service --set control-plane.lighthouseService.image.tag=%s`, GetKeptnNameSpaceFromEnv(), chartURL, lighthouseVersion))
-	//assert.Nil(t, err)
+	for i := 0; i < nrOfUpgrades; i++ {
+		chartURL := ""
+		//lighthouseVersion := ""
+		var err error
+		if i%2 == 0 {
+			//lighthouseVersion = "v1"
+			chartURL = chartLatestVersion
+			//_, err = ExecuteCommand(fmt.Sprintf("kubectl -n %s set image deployment.v1.apps/lighthouse-service lighthouse-service=keptndev/lighthouse-service:0.14.0-dev", GetKeptnNameSpaceFromEnv()))
+		} else {
+			//lighthouseVersion = "v2"
+			chartURL = chartPreviousVersion
+			//_, err = ExecuteCommand(fmt.Sprintf("kubectl -n %s set image deployment.v1.apps/lighthouse-service lighthouse-service=keptndev/lighthouse-service:0.14.0-dev-PR-7266.202203280650", GetKeptnNameSpaceFromEnv()))
+		}
+		t.Logf("Upgrading Keptn to %s", chartURL)
+		_, err = testutils.ExecuteCommand(fmt.Sprintf("helm upgrade -n %s keptn %s --wait --set=control-plane.apiGatewayNginx.type=LoadBalancer --set=control-plane.common.strategy.rollingUpdate.maxUnavailable=0 --set control-plane.resourceService.enabled=true --set control-plane.resourceService.env.DIRECTORY_STAGE_STRUCTURE=true", testutils.GetKeptnNameSpaceFromEnv(), chartURL))
+		//_, err = testutils.ExecuteCommand(fmt.Sprintf(`helm upgrade -n %s keptn %s --wait --set=control-plane.apiGatewayNginx.type=LoadBalancer --set=control-plane.common.strategy.rollingUpdate.maxUnavailable=0 --set control-plane.resourceService.enabled=true --set control-plane.resourceService.env.DIRECTORY_STAGE_STRUCTURE=true --set control-plane.distributor.image.repository=docker.io/keptndev/distributor --set control-plane.distributor.image.tag=0.14.1-dev-PR-7308.202204010740 --set control-plane.lighthouseService.image.repository=docker.io/warber/lighthouse-service --set control-plane.lighthouseService.image.tag=%s`, testutils.GetKeptnNameSpaceFromEnv(), chartURL, lighthouseVersion))
+		if err != nil {
+			t.Logf("Encountered error when upgrading keptn: %v", err)
 
-	//	chartLatestVersion := "https://github.com/keptn/helm-charts-dev/blob/1efe3dab77da9ea3cf2b7dd5eff4b2fac6f76633/packages/keptn-0.15.0-dev-PR-7266.tgz?raw=true"
-	//	chartPreviousVersion := "https://github.com/keptn/helm-charts-dev/blob/5b4fbc630895a2a71721763110376b452f4c2c67/packages/keptn-0.15.0-dev-PR-7266.tgz?raw=true"
+		}
+		<-time.After(5 * time.Second)
+	}
+
 }
