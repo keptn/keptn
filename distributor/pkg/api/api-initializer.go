189df868ad789550cdff44459af742fd933d10d3
RealAnna <anna.reale@dynatrace.com>
2022-05-05 16:53:41 +0200

    added test to api creation logic
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- distributor/pkg/api/api-initializer.go
+++ distributor/pkg/api/api-initializer.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// Initializer implements both methods of creating a new keptn API with internal or remote control plane
+// Initializer implements both methods of creating a new keptn API with internal or remote execution plane
 type Initializer struct {
 	Remote   func(baseURL string, options ...func(*keptnapi.APISet)) (*keptnapi.APISet, error)
 	Internal func(client *http.Client, apiMappings ...InClusterAPIMappings) (*InternalAPISet, error)
