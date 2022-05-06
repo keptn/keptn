b023ee46fb741b5590dcb34b940f93dca29955ca
Florian Bacher <florian.bacher@dynatrace.com>
2022-05-06 12:37:31 +0200

    additional unit tests
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- api/handlers/event_test.go
+++ api/handlers/event_test.go
@@ -283,6 +283,43 @@ func TestEventHandler_PostEvent_SetDefaultSource(t *testing.T) {
 	require.Equal(t, testEvent.Type, mockPublisher.PublishCalls()[0].Event.Type)
 }
 
+func TestEventHandler_PostEvent_ReplaceInvalidWithDefaultSource(t *testing.T) {
+	mockPublisher := &handlers_mock.EventPublisherMock{
+		PublishFunc: func(event apimodels.KeptnContextExtendedCE) error {
+			return nil
+		},
+	}
+	eh := &EventHandler{
+		EventPublisher: mockPublisher,
+	}
+
+	topicName := "my-topic"
+
+	testEvent := models.KeptnContextExtendedCE{
+		Contenttype:    "application/json",
+		Data:           map[string]interface{}{},
+		Extensions:     nil,
+		ID:             "",
+		Shkeptncontext: "",
+		Source:         stringp(":my-source"),
+		Specversion:    "1.0",
+		Time:           strfmt.DateTime{},
+		Type:           &topicName,
+	}
+
+	got, err := eh.PostEvent(testEvent)
+
+	require.Nil(t, err)
+	require.NotNil(t, got)
+
+	require.Len(t, mockPublisher.PublishCalls(), 1)
+	require.Equal(t, *got.KeptnContext, mockPublisher.PublishCalls()[0].Event.Shkeptncontext)
+	require.NotEmpty(t, mockPublisher.PublishCalls()[0].Event.ID)
+	require.Equal(t, defaultEventSource, *mockPublisher.PublishCalls()[0].Event.Source)
+	require.Equal(t, testEvent.Data, mockPublisher.PublishCalls()[0].Event.Data)
+	require.Equal(t, testEvent.Type, mockPublisher.PublishCalls()[0].Event.Type)
+}
+
 func TestEventHandler_PostEvent_SendFails(t *testing.T) {
 	mockPublisher := &handlers_mock.EventPublisherMock{
 		PublishFunc: func(event apimodels.KeptnContextExtendedCE) error {
