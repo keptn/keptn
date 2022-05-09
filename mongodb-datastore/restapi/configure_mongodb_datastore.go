13dc0d59779e726c2d22e67632356c6a68d769e0
RealAnna <anna.reale@dynatrace.com>
2022-05-09 16:18:51 +0200

    removed smell
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- mongodb-datastore/restapi/configure_mongodb_datastore.go
+++ mongodb-datastore/restapi/configure_mongodb_datastore.go
@@ -124,7 +124,7 @@ func configureTLS(tlsConfig *tls.Config) {
 // This function can be called multiple times, depending on the number of serving schemes.
 // scheme value will be set accordingly: "http", "https" or "unix"
 func configureServer(s *http.Server, scheme, addr string) {
-
+	// Make all necessary changes to the Server configuration here.
 }
 
 // The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
