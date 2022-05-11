b28ecff27fe93cff16d522eb84a69dd9a5dad76f
RealAnna <anna.reale@dynatrace.com>
2022-05-11 13:39:30 +0200

    fix: according to review
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- mongodb-datastore/handlers/config.go
+++ mongodb-datastore/handlers/config.go
@@ -12,16 +12,13 @@ const (
 
 type envConfig struct {
 	// Port on which to listen for cloudevents
-	Port                    int    `envconfig:"RCV_PORT" default:"8080"`
-	Path                    string `envconfig:"RCV_PATH" default:"/"`
-	ConfigurationServiceURL string `envconfig:"CONFIGURATION_SERVICE" default:"http://configuration-service:8080"`
-	K8SDeploymentName       string `envconfig:"K8S_DEPLOYMENT_NAME" default:""`
-	K8SDeploymentVersion    string `envconfig:"K8S_DEPLOYMENT_VERSION" default:""`
-	K8SDeploymentComponent  string `envconfig:"K8S_DEPLOYMENT_COMPONENT" default:""`
-	K8SPodName              string `envconfig:"K8S_POD_NAME" default:""`
-	K8SNamespace            string `envconfig:"K8S_NAMESPACE" default:""`
-	K8SNodeName             string `envconfig:"K8S_NODE_NAME" default:""`
-	LogLevel                string `envconfig:"LOG_LEVEL" default:"info"`
+	K8SDeploymentName      string `envconfig:"K8S_DEPLOYMENT_NAME" default:""`
+	K8SDeploymentVersion   string `envconfig:"K8S_DEPLOYMENT_VERSION" default:""`
+	K8SDeploymentComponent string `envconfig:"K8S_DEPLOYMENT_COMPONENT" default:""`
+	K8SPodName             string `envconfig:"K8S_POD_NAME" default:""`
+	K8SNamespace           string `envconfig:"K8S_NAMESPACE" default:""`
+	K8SNodeName            string `envconfig:"K8S_NODE_NAME" default:""`
+	LogLevel               string `envconfig:"LOG_LEVEL" default:"info"`
 }
 
 func GetMongoDBConnectionString() (string, string, error) {
