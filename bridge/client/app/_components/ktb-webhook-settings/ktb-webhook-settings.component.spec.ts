152eadcda03cdce7552c0d1210ba63c5c7e63aa1
TannerGilbert <gilberttanner.work@gmail.com>
2022-05-12 12:10:25 +0200

    Fixed formating issues
    Signed-off-by: TannerGilbert <gilberttanner.work@gmail.com>

--- bridge/client/app/_components/ktb-webhook-settings/ktb-webhook-settings.component.spec.ts
+++ bridge/client/app/_components/ktb-webhook-settings/ktb-webhook-settings.component.spec.ts
@@ -113,9 +113,7 @@ describe('KtbWebhookSettingsComponent', () => {
 
   it('should be valid URL when it starts with a secret', () => {
     const urlControl: AbstractControl = component.webhookConfigForm.get('url') as AbstractControl;
-    const urls = [
-      '{{.secret.gremlin.url}}',
-    ];
+    const urls = ['{{.secret.gremlin.url}}'];
 
     for (const url of urls) {
       urlControl.setValue(url);
