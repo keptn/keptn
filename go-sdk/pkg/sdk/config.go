fb434b26a83b0290abe56784cdc27ee7020ee620
warber <bernd.warmuth@dynatrace.com>
2022-05-12 08:05:05 +0200

    sdk changed eventbroker env var name
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- go-sdk/pkg/sdk/config.go
+++ go-sdk/pkg/sdk/config.go
@@ -7,13 +7,13 @@ import (
 
 type envConfig struct {
 	ConfigurationServiceURL string `envconfig:"CONFIGURATION_SERVICE" default:"configuration-service:8080"`
+	EventBrokerURL          string `envconfig:"EVENTBROKER" default:"nats://keptn-nats"`
 	Location                string `envconfig:"LOCATION" default:"control-plane"`
 	Version                 string `envconfig:"VERSION" default:""`
 	K8sDeploymentName       string `envconfig:"K8S_DEPLOYMENT_NAME" default:""`
 	K8sNamespace            string `envconfig:"K8S_NAMESPACE" default:""`
 	K8sPodName              string `envconfig:"K8S_POD_NAME" default:""`
 	K8sNodeName             string `envconfig:"K8S_NODE_NAME" default:""`
-	EventBrokerURL          string `envconfig:"EVENT_BROKER_URL" default:"nats://keptn-nats"`
 }
 
 func newEnvConfig() envConfig {
