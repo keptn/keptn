54a7f975a2b91a56d2d15a945aa61679aa1adebd
Elisabeth Lang <elisabeth.lang@dynatrace.com>
2022-05-13 10:52:41 +0200

    (feat)bridge: Show automatic provisioning message
    Signed-off-by: Elisabeth Lang <elisabeth.lang@dynatrace.com>

--- bridge/server/api/index.ts
+++ bridge/server/api/index.ts
@@ -35,6 +35,7 @@ const apiRouter = (params: {
   const projectsPageSize = EnvironmentUtils.getNumber(process.env.PROJECTS_PAGE_SIZE);
   const servicesPageSize = EnvironmentUtils.getNumber(process.env.SERVICES_PAGE_SIZE);
   const keptnInstallationType = process.env.KEPTN_INSTALLATION_TYPE;
+  const automaticProvisioningMsg = process.env.AUTOMATIC_PROVISIONING_MSG;
   const dataService = new DataService(apiUrl, apiToken);
 
   // bridgeInfo endpoint: Provide certain metadata for Bridge
@@ -53,6 +54,7 @@ const apiRouter = (params: {
       servicesPageSize,
       authType,
       ...(user && { user }),
+      automaticProvisioningMsg,
     };
 
     try {
