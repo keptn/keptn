ecf1de049f0517926bda32e6061acf365da32d26
odubajDT <ondrej.dubaj@dynatrace.com>
2022-05-13 09:27:31 +0200

    remediation service working with go-sdk
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- remediation-service/main.go
+++ remediation-service/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/keptn/keptn/go-sdk/pkg/sdk"
 	"github.com/keptn/keptn/remediation-service/handler"
 	"github.com/sirupsen/logrus"
-	"log"
 )
 
 const getActionTriggeredEventType = "sh.keptn.event.get-action.triggered"
 const serviceName = "remediation-service"
+const envVarLogLevel = "LOG_LEVEL"
 
 func main() {
+	if os.Getenv(envVarLogLevel) != "" {
+		logLevel, err := logrus.ParseLevel(os.Getenv(envVarLogLevel))
+		if err != nil {
+			logrus.WithError(err).Error("could not parse log level provided by 'LOG_LEVEL' env var")
+			logrus.SetLevel(logrus.InfoLevel)
+		} else {
+			logrus.SetLevel(logLevel)
+		}
+	}
+
 	log.Fatal(sdk.NewKeptn(
 		serviceName,
 		sdk.WithTaskHandler(
