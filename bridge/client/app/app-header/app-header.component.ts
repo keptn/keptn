09082a8c93c62db3f0ef8f19588a4be185824fdf
ermin.muratovic <ermin.muratovic@gmail.com>
2022-05-19 17:07:09 +0200

    drop query params on project change
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/app-header/app-header.component.ts
+++ bridge/client/app/app-header/app-header.component.ts
@@ -206,7 +206,7 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
   }
 
   setProject(): void {
-    const urlPieces = this.router.url.split('/');
+    const urlPieces = this.router.url.split('?')[0].split('/');
     if (urlPieces[1] === 'project') {
       this.selectedProject = urlPieces[2];
 
