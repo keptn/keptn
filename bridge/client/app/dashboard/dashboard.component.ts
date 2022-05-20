a42f86c049c68ac600ca90c57edd7ba07364c56b
René Panzar <rene.panzar@dynatrace.com>
2022-05-20 11:14:39 +0200

    feat(bridge): Removes projects polling on dashboard #7796
    - Removes polling for projects
    - Removes dependency on POLLING_INTERVAL_MILLIS
    - Adapts spec
    
    Signed-off-by: René Panzar <rene.panzar@dynatrace.com>

--- bridge/client/app/dashboard/dashboard.component.ts
+++ bridge/client/app/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { DtOverlay } from '@dynatrace/barista-components/overlay';
 import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 import { Project } from '../_models/project';
 import { DataService } from '../_services/data.service';
-import { environment } from '../../environments/environment';
-import { takeUntil } from 'rxjs/operators';
-import { DtOverlay } from '@dynatrace/barista-components/overlay';
-import { AppUtils, POLLING_INTERVAL_MILLIS } from '../_utils/app.utils';
 
 @Component({
   selector: 'ktb-dashboard',
@@ -13,30 +12,19 @@ import { AppUtils, POLLING_INTERVAL_MILLIS } from '../_utils/app.utils';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-  public projects$: Observable<Project[] | undefined>;
+  public readonly projects$: Observable<Project[] | undefined> = this.dataService.projects;
   public logoInvertedUrl = environment?.config?.logoInvertedUrl;
   public isQualityGatesOnly = false;
   private readonly unsubscribe$: Subject<void> = new Subject<void>();
 
-  constructor(
-    private dataService: DataService,
-    private ngZone: NgZone,
-    private _dtOverlay: DtOverlay,
-    @Inject(POLLING_INTERVAL_MILLIS) private initialDelayMillis: number
-  ) {
-    this.projects$ = this.dataService.projects;
-  }
+  constructor(private dataService: DataService, private ngZone: NgZone, private _dtOverlay: DtOverlay) {}
 
   public ngOnInit(): void {
     this.dataService.isQualityGatesOnly.pipe(takeUntil(this.unsubscribe$)).subscribe((isQualityGatesOnly) => {
       this.isQualityGatesOnly = isQualityGatesOnly;
     });
 
-    AppUtils.createTimer(this.initialDelayMillis, this.initialDelayMillis)
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(() => {
-        this.loadProjects();
-      });
+    this.loadProjects();
   }
 
   public loadProjects(): void {
