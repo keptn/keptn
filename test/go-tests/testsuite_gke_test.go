f6bb098fe3380c26b923e8cfe52aae523bc3898e
RealAnna <89971034+RealAnna@users.noreply.github.com>
2022-05-23 10:22:34 +0200

    fix(shipyard-controller): Allow parallel sequence execution if the service is different (#7825)

--- test/go-tests/testsuite_gke_test.go
+++ test/go-tests/testsuite_gke_test.go
@@ -17,6 +17,7 @@ func Test_GKE(t *testing.T) {
 	t.Run("Test_LogForwarding", Test_LogForwarding)
 	t.Run("Test_SequenceState", Test_SequenceState)
 	t.Run("Test_SequenceStateParallelStages", Test_SequenceStateParallelStages)
+	t.Run("Test_SequenceStateParallelServices", Test_SequenceStateParallelServices)
 	t.Run("Test_SequenceState_RetrieveMultipleSequence", Test_SequenceState_RetrieveMultipleSequence)
 	t.Run("Test_SequenceState_SequenceNotFound", Test_SequenceState_SequenceNotFound)
 	t.Run("Test_SequenceState_InvalidShipyard", Test_SequenceState_InvalidShipyard)
