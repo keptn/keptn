dcc76a736192e3b31f8fcc61758caf23c39a02f8
ermin.muratovic <ermin.muratovic@gmail.com>
2022-05-24 13:49:39 +0200

    remove polling from service settings
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-service-settings-list/ktb-service-settings-list.component.ts
+++ bridge/client/app/_components/ktb-service-settings-list/ktb-service-settings-list.component.ts
@@ -1,26 +1,19 @@
-import { Component, Inject, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { DataService } from '../../_services/data.service';
 import { DtTableDataSource } from '@dynatrace/barista-components/table';
-import { AppUtils, POLLING_INTERVAL_MILLIS } from '../../_utils/app.utils';
 
 @Component({
   selector: 'ktb-service-settings-list',
   templateUrl: './ktb-service-settings-list.component.html',
 })
-export class KtbServiceSettingsListComponent implements OnDestroy {
+export class KtbServiceSettingsListComponent {
   public projectName?: string;
   public isLoading = false;
   public dataSource: DtTableDataSource<string> = new DtTableDataSource<string>();
-  private _timer: Subscription = Subscription.EMPTY;
 
-  constructor(
-    private router: ActivatedRoute,
-    private dataService: DataService,
-    @Inject(POLLING_INTERVAL_MILLIS) private initialDelayMillis: number
-  ) {
+  constructor(private router: ActivatedRoute, private dataService: DataService) {
     this.router.paramMap
       .pipe(
         map((params) => params.get('projectName')),
@@ -30,20 +23,12 @@ export class KtbServiceSettingsListComponent implements OnDestroy {
         this.projectName = projectName;
         this.isLoading = true;
 
-        this._timer.unsubscribe();
-
-        this._timer = AppUtils.createTimer(0, initialDelayMillis).subscribe(() => {
-          if (this.projectName) {
-            this.dataService.getServiceNames(this.projectName).subscribe((services) => {
-              this.dataSource = new DtTableDataSource<string>(services);
-              this.isLoading = false;
-            });
-          }
-        });
+        if (this.projectName) {
+          this.dataService.getServiceNames(this.projectName).subscribe((services) => {
+            this.dataSource = new DtTableDataSource<string>(services);
+            this.isLoading = false;
+          });
+        }
       });
-  }
-
-  public ngOnDestroy(): void {
-    this._timer.unsubscribe();
   }
 }
