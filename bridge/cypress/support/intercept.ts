1835ebb1703e080d10cb9edcd1eb39303cba3f16
Klaus Strießnig <k.striessnig@gmail.com>
2022-05-25 08:54:45 +0200

    fixed UI test, added new UI tests
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/cypress/support/intercept.ts
+++ bridge/cypress/support/intercept.ts
@@ -287,3 +287,47 @@ export function interceptSecrets(): void {
     },
   });
 }
+
+export function interceptEvaluationBoardDynatrace(): void {
+  cy.intercept('api/mongodb-datastore/event?keptnContext=*&type=sh.keptn.event.evaluation.triggered&pageSize=1', {
+    fixture: 'service/get.evaluation.triggered.mock.json',
+  });
+
+  cy.intercept(
+    'api/mongodb-datastore/event?keptnContext=*&type=sh.keptn.event.evaluation.finished&source=lighthouse-service',
+    {
+      fixture: 'service/get.event2.data.json',
+    }
+  );
+  cy.intercept('api/controlPlane/v1/project/dynatrace/stage/quality-gate/service/items', {
+    fixture: 'get.service.items.mock.json',
+  });
+}
+
+export function interceptEvaluationBoard(): void {
+  interceptMain();
+  cy.intercept('api/mongodb-datastore/event/type/sh.keptn.event.evaluation.finished?*', {
+    fixture: 'service/get.eval.data.json',
+  });
+
+  cy.intercept('api/mongodb-datastore/event?keptnContext=*&type=sh.keptn.event.evaluation.triggered&pageSize=1', {
+    fixture: 'service/get.evaluation.triggered-with-deployment.mock.json',
+  });
+
+  cy.intercept(
+    'api/mongodb-datastore/event?keptnContext=*&type=sh.keptn.event.evaluation.finished&source=lighthouse-service',
+    {
+      fixture: 'service/get.event2.data.json',
+    }
+  );
+  cy.intercept('api/controlPlane/v1/project/dynatrace/stage/quality-gate/service/items', {
+    fixture: 'get.service.items.mock.json',
+  });
+}
+
+export function interceptEvaluationBoardWithoutDeployment(): void {
+  interceptEvaluationBoard();
+  cy.intercept('api/mongodb-datastore/event?keptnContext=*&type=sh.keptn.event.evaluation.triggered&pageSize=1', {
+    fixture: 'service/get.evaluation.triggered.mock.json',
+  });
+}
