b7af118ff67862d10d4a8d6d9892ca219b119988
warber <bernd.warmuth@dynatrace.com>
2022-05-31 16:37:56 +0200

    fix: use custom implementation using apihandler internally
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- cp-common/api/internal.go
+++ cp-common/api/internal.go
@@ -15,7 +15,7 @@ import (
 type InternalAPISet struct {
 	apimap                 InClusterAPIMappings
 	httpClient             *http.Client
-	apiHandler             *api.APIHandler
+	apiHandler             *InternalAPIHandler
 	authHandler            *api.AuthHandler
 	eventHandler           *api.EventHandler
 	logHandler             *api.LogHandler
@@ -68,10 +68,17 @@ func NewInternal(client *http.Client, apiMappings ...InClusterAPIMappings) (*Int
 	as := &InternalAPISet{}
 	as.httpClient = client
 
-	as.apiHandler = &api.APIHandler{
-		BaseURL:    apimap[ShipyardController],
-		HTTPClient: &http.Client{Transport: wrapOtelTransport(getClientTransport(as.httpClient.Transport))},
-		Scheme:     "http",
+	as.apiHandler = &InternalAPIHandler{
+		shippyApiHandler: &api.APIHandler{
+			BaseURL:    apimap[ShipyardController],
+			HTTPClient: &http.Client{Transport: wrapOtelTransport(getClientTransport(as.httpClient.Transport))},
+			Scheme:     "http",
+		},
+		apiServiceApiHandler: &api.APIHandler{
+			BaseURL:    apimap[ApiService],
+			HTTPClient: &http.Client{Transport: wrapOtelTransport(getClientTransport(as.httpClient.Transport))},
+			Scheme:     "http",
+		},
 	}
 
 	as.authHandler = &api.AuthHandler{
@@ -217,4 +224,41 @@ func getClientTransport(rt http.RoundTripper) http.RoundTripper {
 	}
 	return rt
 
+}
+
+type InternalAPIHandler struct {
+	shippyApiHandler     *api.APIHandler
+	apiServiceApiHandler *api.APIHandler
+}
+
+func (i *InternalAPIHandler) SendEvent(event models.KeptnContextExtendedCE) (*models.EventContext, *models.Error) {
+	return i.apiServiceApiHandler.SendEvent(event)
+}
+
+func (i *InternalAPIHandler) TriggerEvaluation(project string, stage string, service string, evaluation models.Evaluation) (*models.EventContext, *models.Error) {
+	return i.shippyApiHandler.TriggerEvaluation(project, stage, service, evaluation)
+}
+
+func (i *InternalAPIHandler) CreateProject(project models.CreateProject) (string, *models.Error) {
+	return i.shippyApiHandler.CreateProject(project)
+}
+
+func (i *InternalAPIHandler) UpdateProject(project models.CreateProject) (string, *models.Error) {
+	return i.shippyApiHandler.UpdateProject(project)
+}
+
+func (i *InternalAPIHandler) DeleteProject(project models.Project) (*models.DeleteProjectResponse, *models.Error) {
+	return i.shippyApiHandler.DeleteProject(project)
+}
+
+func (i *InternalAPIHandler) CreateService(project string, service models.CreateService) (string, *models.Error) {
+	return i.shippyApiHandler.CreateService(project, service)
+}
+
+func (i *InternalAPIHandler) DeleteService(project string, service string) (*models.DeleteServiceResponse, *models.Error) {
+	return i.DeleteService(project, service)
+}
+
+func (i *InternalAPIHandler) GetMetadata() (*models.Metadata, *models.Error) {
+	return i.apiServiceApiHandler.GetMetadata()
 }
--- cp-common/go.mod
+++ cp-common/go.mod
@@ -2,7 +2,7 @@ module github.com/keptn/keptn/cp-common
 
 go 1.18
 
-require github.com/keptn/go-utils v0.15.1-0.20220517082831-2052e1404e4c
+require github.com/keptn/go-utils v0.15.1-0.20220531134033-88e29eae7a0b
 
 require (
 	github.com/benbjohnson/clock v1.3.0
