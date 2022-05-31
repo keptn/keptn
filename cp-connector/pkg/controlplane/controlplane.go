f0a7d4c9a94c9b8cd3413c09af175d818894fcb2
warber <bernd.warmuth@dynatrace.com>
2022-05-31 08:48:02 +0200

    added constructor methods for backwards compatibility
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- cp-connector/pkg/controlplane/controlplane.go
+++ cp-connector/pkg/controlplane/controlplane.go
@@ -5,13 +5,19 @@ import (
 	"errors"
 	"fmt"
 	"github.com/keptn/go-utils/pkg/api/models"
+	api "github.com/keptn/go-utils/pkg/api/utils"
 	"github.com/keptn/keptn/cp-connector/pkg/eventmatcher"
 	"github.com/keptn/keptn/cp-connector/pkg/eventsource"
 	"github.com/keptn/keptn/cp-connector/pkg/logforwarder"
 	"github.com/keptn/keptn/cp-connector/pkg/logger"
+	natseventsource "github.com/keptn/keptn/cp-connector/pkg/nats"
 	"github.com/keptn/keptn/cp-connector/pkg/subscriptionsource"
 	"github.com/keptn/keptn/cp-connector/pkg/types"
 )
+
+// EventSenderKey can be used to get the event sender from the context
+// Deprecated: use own value of EventSenderKeyType
+var EventSenderKey = EventSenderKeyType{}
 
 type EventSender = types.EventSender
 type EventSenderKeyType = types.EventSenderKeyType
@@ -155,4 +161,22 @@ func subjects(subscriptions []models.EventSubscription) []string {
 		ret = append(ret, s.Event)
 	}
 	return ret
+}
+
+// NewNATSEventSource creates a new NATSEventSource
+// Deprecated: use eventsource.New
+func NewNATSEventSource(natsConnector natseventsource.NATS) *eventsource.NATSEventSource {
+	return eventsource.New(natsConnector)
+}
+
+// NewLogForwarder creates a new logging forwarder
+// Deprecated: use logforwarder.New
+func NewLogForwarder(logApi api.LogsV1Interface) *logforwarder.LogForwardingHandler {
+	return logforwarder.New(logApi)
+}
+
+// NewUniformSubscriptionSource creates a new subscription source
+// Deprecated: use subscriptionsource.New
+func NewUniformSubscriptionSource(uniformAPI api.UniformV1Interface, options ...func(source *subscriptionsource.UniformSubscriptionSource)) *subscriptionsource.UniformSubscriptionSource {
+	return subscriptionsource.New(uniformAPI, options...)
 }
