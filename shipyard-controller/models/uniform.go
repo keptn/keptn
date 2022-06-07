d277a83915c65aaaf42544cd371d7f8a972e8494
Florian Bacher <florian.bacher@dynatrace.com>
2022-06-07 13:10:22 +0200

    fix(shipyard-controller): Update registration info based on integration name/namespace (#8006)
    * fix(shipyard-controller): Update registration info based on integration name/namespace
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * remove redundant check
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * adapted logged integration info
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- shipyard-controller/models/uniform.go
+++ shipyard-controller/models/uniform.go
@@ -1,11 +1,13 @@
 package models
 
 type GetUniformIntegrationsParams struct {
-	Name    string `form:"name" json:"name"`
-	ID      string `form:"id" json:"id"`
-	Project string `form:"project" json:"project"`
-	Stage   string `form:"stage" json:"stage"`
-	Service string `form:"service" json:"service"`
+	Name      string `form:"name" json:"name"`
+	ID        string `form:"id" json:"id"`
+	Namespace string `form:"namespace" json:"namespace"`
+	HostName  string `form:"hostname" json:"hostname"`
+	Project   string `form:"project" json:"project"`
+	Stage     string `form:"stage" json:"stage"`
+	Service   string `form:"service" json:"service"`
 }
 
 type RegisterResponse struct {
