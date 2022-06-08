3adad0c706936d299770a3f551e30a1347196115
ermin.muratovic <ermin.muratovic@gmail.com>
2022-06-08 13:55:51 +0200

    fix(bridge): don't set filteredSequences initially to empty array
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_views/ktb-sequence-view/ktb-sequence-view.component.ts
+++ bridge/client/app/_views/ktb-sequence-view/ktb-sequence-view.component.ts
@@ -102,7 +102,7 @@ export class KtbSequenceViewComponent implements OnInit, OnDestroy {
     services: [],
   };
 
-  public filteredSequences: Sequence[] = [];
+  public filteredSequences?: Sequence[];
   public loading = false;
   public selectedEventId?: string;
 
