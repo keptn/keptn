0190fe44d084090c6548a48099b9c8595a5e4e7f
odubajDT <ondrej.dubaj@dynatrace.com>
2022-06-14 13:48:29 +0200

    polishing secret migrator
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- shipyard-controller/db/migration/project_credentials_migration.go
+++ shipyard-controller/db/migration/project_credentials_migration.go
@@ -1,11 +1,8 @@
 package migration
 
 import (
-	"encoding/json"
 	"fmt"
-	"strings"
 
-	apimodels "github.com/keptn/go-utils/pkg/api/models"
 	"github.com/keptn/keptn/shipyard-controller/common"
 	"github.com/keptn/keptn/shipyard-controller/db"
 )
@@ -13,13 +10,13 @@ import (
 func NewProjectCredentialsMigrator(dbConnection *db.MongoDBConnection, secretStore common.SecretStore) *ProjectCredentialsMigrator {
 	return &ProjectCredentialsMigrator{
 		projectRepo: db.NewMongoDBProjectCredentialsRepo(dbConnection),
-		SecretStore: secretStore,
+		secretRepo:  db.NewMongoDBSecretCredentialsRepo(secretStore),
 	}
 }
 
 type ProjectCredentialsMigrator struct {
 	projectRepo *db.MongoDBProjectCredentialsRepo
-	SecretStore common.SecretStore
+	secretRepo  *db.MongoDBSecretCredentialsRepo
 }
 
 func (s *ProjectCredentialsMigrator) Transform() error {
@@ -48,86 +45,14 @@ func (s *ProjectCredentialsMigrator) updateProjects(projects []*db.ExpandedProje
 }
 
 func (s *ProjectCredentialsMigrator) updateSecrets(projects []*db.ExpandedProjectOld) error {
+	if projects == nil {
+		return nil
+	}
 	for _, project := range projects {
-		secret, err := s.SecretStore.GetSecret("git-credentials-" + project.ProjectName)
+		err := s.secretRepo.UpdateSecret(project)
 		if err != nil {
-			return fmt.Errorf("failed to get git-credentials secret during migration for project %s", project.ProjectName)
-		}
-		if secret != nil {
-			if marshalledSecret, ok := secret["git-credentials"]; ok {
-				//try to unmarshall to new format
-				newSecretObj := &apimodels.GitAuthCredentials{}
-				if err := json.Unmarshal(marshalledSecret, newSecretObj); err == nil && newSecretObj != nil {
-					continue
-				}
-
-				//try to unmarshall to old format
-				secretObj := &GitOldCredentials{}
-				if err := json.Unmarshal(marshalledSecret, secretObj); err != nil {
-					return fmt.Errorf("failed to unmarshal git-credentials secret during migration for project %s", project.ProjectName)
-				}
-
-				newSecret := transformSecret(secretObj)
-
-				credsEncoded, err := json.Marshal(newSecret)
-				if err != nil {
-					return fmt.Errorf("could not store git credentials during migration for project %s: %s", project.ProjectName, err.Error())
-				}
-
-				if err := s.SecretStore.UpdateSecret("git-credentials-"+project.ProjectName, map[string][]byte{
-					"git-credentials": credsEncoded,
-				}); err != nil {
-					return fmt.Errorf("could not store git credentials during migration for project %s: %s", project.ProjectName, err.Error())
-				}
-			}
+			return fmt.Errorf("could not transform git credentials for project %s: %w", project.ProjectName, err)
 		}
 	}
-
 	return nil
-}
-
-func transformSecret(oldSecret *GitOldCredentials) *apimodels.GitAuthCredentials {
-	newSecret := apimodels.GitAuthCredentials{
-		RemoteURL: oldSecret.RemoteURI,
-		User:      oldSecret.User,
-	}
-
-	//if project is using ssh auth
-	if strings.HasPrefix(oldSecret.RemoteURI, "ssh://") {
-		newSecret.SshAuth = &apimodels.SshGitAuth{
-			PrivateKey:     oldSecret.GitPrivateKey,
-			PrivateKeyPass: oldSecret.GitPrivateKeyPass,
-		}
-	} else { //project is using https auth
-		newSecret.HttpsAuth = &apimodels.HttpsGitAuth{
-			Token:           oldSecret.Token,
-			InsecureSkipTLS: oldSecret.InsecureSkipTLS,
-			Certificate:     oldSecret.GitPemCertificate,
-		}
-
-		if oldSecret.GitProxyURL != "" {
-			newSecret.HttpsAuth.Proxy = &apimodels.ProxyGitAuth{
-				User:     oldSecret.GitProxyUser,
-				Password: oldSecret.GitProxyPassword,
-				URL:      oldSecret.GitProxyURL,
-				Scheme:   oldSecret.GitProxyScheme,
-			}
-		}
-	}
-
-	return &newSecret
-}
-
-type GitOldCredentials struct {
-	User              string `json:"user,omitempty"`
-	Token             string `json:"token,omitempty"`
-	RemoteURI         string `json:"remoteURI,omitempty"`
-	GitPrivateKey     string `json:"privateKey,omitempty"`
-	GitPrivateKeyPass string `json:"privateKeyPass,omitempty"`
-	GitProxyURL       string `json:"gitProxyUrl,omitempty"`
-	GitProxyScheme    string `json:"gitProxyScheme,omitempty"`
-	GitProxyUser      string `json:"gitProxyUser,omitempty"`
-	GitProxyPassword  string `json:"gitProxyPassword,omitempty"`
-	GitPemCertificate string `json:"gitPemCertificate,omitempty"`
-	InsecureSkipTLS   bool   `json:"insecureSkipTLS,omitempty"`
 }
---
+++ shipyard-controller/db/mongodb_secret_credentials_repo.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	apimodels "github.com/keptn/go-utils/pkg/api/models"
+	"github.com/keptn/keptn/shipyard-controller/common"
+)
+
+type MongoDBSecretCredentialsRepo struct {
+	SecretStore common.SecretStore
+}
+
+func NewMongoDBSecretCredentialsRepo(secretStore common.SecretStore) *MongoDBSecretCredentialsRepo {
+	return &MongoDBSecretCredentialsRepo{
+		SecretStore: secretStore,
+	}
+}
+
+func (s *MongoDBSecretCredentialsRepo) UpdateSecret(project *ExpandedProjectOld) error {
+	secret, err := s.SecretStore.GetSecret("git-credentials-" + project.ProjectName)
+	if err != nil {
+		return fmt.Errorf("failed to get git-credentials secret during migration for project %s", project.ProjectName)
+	}
+	if secret != nil {
+		if marshalledSecret, ok := secret["git-credentials"]; ok {
+			//try to unmarshall to new format
+			newSecretObj := &apimodels.GitAuthCredentials{}
+			if err := json.Unmarshal(marshalledSecret, newSecretObj); err == nil && newSecretObj != nil {
+				return nil
+			}
+
+			//try to unmarshall to old format
+			secretObj := &GitOldCredentials{}
+			if err := json.Unmarshal(marshalledSecret, secretObj); err != nil {
+				return fmt.Errorf("failed to unmarshal git-credentials secret during migration for project %s", project.ProjectName)
+			}
+
+			newSecret := transformSecret(secretObj)
+
+			credsEncoded, err := json.Marshal(newSecret)
+			if err != nil {
+				return fmt.Errorf("could not store git credentials during migration for project %s: %s", project.ProjectName, err.Error())
+			}
+
+			if err := s.SecretStore.UpdateSecret("git-credentials-"+project.ProjectName, map[string][]byte{
+				"git-credentials": credsEncoded,
+			}); err != nil {
+				return fmt.Errorf("could not store git credentials during migration for project %s: %s", project.ProjectName, err.Error())
+			}
+		}
+	}
+
+	return nil
+}
+
+func transformSecret(oldSecret *GitOldCredentials) *apimodels.GitAuthCredentials {
+	newSecret := apimodels.GitAuthCredentials{
+		RemoteURL: oldSecret.RemoteURI,
+		User:      oldSecret.User,
+	}
+
+	//if project is using ssh auth
+	if strings.HasPrefix(oldSecret.RemoteURI, "ssh://") {
+		newSecret.SshAuth = &apimodels.SshGitAuth{
+			PrivateKey:     oldSecret.GitPrivateKey,
+			PrivateKeyPass: oldSecret.GitPrivateKeyPass,
+		}
+	} else { //project is using https auth
+		newSecret.HttpsAuth = &apimodels.HttpsGitAuth{
+			Token:           oldSecret.Token,
+			InsecureSkipTLS: oldSecret.InsecureSkipTLS,
+			Certificate:     oldSecret.GitPemCertificate,
+		}
+
+		if oldSecret.GitProxyURL != "" {
+			newSecret.HttpsAuth.Proxy = &apimodels.ProxyGitAuth{
+				User:     oldSecret.GitProxyUser,
+				Password: oldSecret.GitProxyPassword,
+				URL:      oldSecret.GitProxyURL,
+				Scheme:   oldSecret.GitProxyScheme,
+			}
+		}
+	}
+
+	return &newSecret
+}
+
+type GitOldCredentials struct {
+	User              string `json:"user,omitempty"`
+	Token             string `json:"token,omitempty"`
+	RemoteURI         string `json:"remoteURI,omitempty"`
+	GitPrivateKey     string `json:"privateKey,omitempty"`
+	GitPrivateKeyPass string `json:"privateKeyPass,omitempty"`
+	GitProxyURL       string `json:"gitProxyUrl,omitempty"`
+	GitProxyScheme    string `json:"gitProxyScheme,omitempty"`
+	GitProxyUser      string `json:"gitProxyUser,omitempty"`
+	GitProxyPassword  string `json:"gitProxyPassword,omitempty"`
+	GitPemCertificate string `json:"gitPemCertificate,omitempty"`
+	InsecureSkipTLS   bool   `json:"insecureSkipTLS,omitempty"`
+}
