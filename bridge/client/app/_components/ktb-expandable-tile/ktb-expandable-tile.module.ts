e5b453ec32aff3db8dfe2002db5ec951e1db8383
ermin.muratovic <ermin.muratovic@gmail.com>
2022-06-15 14:11:06 +0200

    add icon module
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-expandable-tile/ktb-expandable-tile.module.ts
+++ bridge/client/app/_components/ktb-expandable-tile/ktb-expandable-tile.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { KtbExpandableTileComponent, KtbExpandableTileHeaderDirective } from './ktb-expandable-tile.component';
 import { DtExpandablePanelModule } from '@dynatrace/barista-components/expandable-panel';
 import { DtShowMoreModule } from '@dynatrace/barista-components/show-more';
+import { DtIconModule } from '@dynatrace/barista-components/icon';
 
 @NgModule({
   declarations: [KtbExpandableTileComponent, KtbExpandableTileHeaderDirective],
-  imports: [CommonModule, DtExpandablePanelModule, DtShowMoreModule],
+  imports: [
+    CommonModule,
+    DtExpandablePanelModule,
+    DtIconModule.forRoot({
+      svgIconLocation: `assets/icons/{{name}}.svg`,
+    }),
+    DtShowMoreModule,
+  ],
   exports: [KtbExpandableTileComponent, KtbExpandableTileHeaderDirective],
 })
 export class KtbExpandableTileModule {}
