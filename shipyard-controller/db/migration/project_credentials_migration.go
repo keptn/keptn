cbd427883d61f0318a74c515afdd378048faadc6
odubajDT <ondrej.dubaj@dynatrace.com>
2022-06-15 13:57:36 +0200

    tests for migrator
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- shipyard-controller/db/migration/project_credentials_migration.go
+++ shipyard-controller/db/migration/project_credentials_migration.go
@@ -8,55 +8,51 @@ import (
 	"github.com/keptn/keptn/shipyard-controller/models"
 )
 
-type ProjectCredentialsMigrator interface {
-	Transform() error
-}
-
-func NewProjectCredentialsMigrator(dbConnection *db.MongoDBConnection, secretStore common.SecretStore) *projectCredentialsMigrator {
-	return &projectCredentialsMigrator{
+func NewProjectCredentialsMigrator(dbConnection *db.MongoDBConnection, secretStore common.SecretStore) *ProjectCredentialsMigrator {
+	return &ProjectCredentialsMigrator{
 		projectRepo: db.NewMongoDBProjectCredentialsRepo(dbConnection),
 		secretRepo:  db.NewMongoDBSecretCredentialsRepo(secretStore),
 	}
 }
 
-type projectCredentialsMigrator struct {
+type ProjectCredentialsMigrator struct {
 	projectRepo db.MongoDBProjectCredentialsRepo
 	secretRepo  db.MongoDBSecretCredentialsRepo
 }
 
-func (s *projectCredentialsMigrator) Transform() error {
+func (s *ProjectCredentialsMigrator) Transform() error {
 	projects, err := s.projectRepo.GetOldCredentialsProjects()
 	if err != nil {
-		return fmt.Errorf("could not transform git credentials to new format: %w", err)
+		return fmt.Errorf("could not transform git credentials to new format: %s", err.Error())
 	}
-	if err := s.updateSecrets(projects); err != nil {
+	if err := s.UpdateSecrets(projects); err != nil {
 		return err
 	}
-	return s.updateProjects(projects)
+	return s.UpdateProjects(projects)
 
 }
 
-func (s *projectCredentialsMigrator) updateProjects(projects []*models.ExpandedProjectOld) error {
+func (s *ProjectCredentialsMigrator) UpdateProjects(projects []*models.ExpandedProjectOld) error {
 	if projects == nil {
 		return nil
 	}
 	for _, project := range projects {
 		err := s.projectRepo.UpdateProject(project)
 		if err != nil {
-			return fmt.Errorf("could not transform git credentials for project %s: %w", project.ProjectName, err)
+			return fmt.Errorf("could not transform git credentials for project %s: %s", project.ProjectName, err.Error())
 		}
 	}
 	return nil
 }
 
-func (s *projectCredentialsMigrator) updateSecrets(projects []*models.ExpandedProjectOld) error {
+func (s *ProjectCredentialsMigrator) UpdateSecrets(projects []*models.ExpandedProjectOld) error {
 	if projects == nil {
 		return nil
 	}
 	for _, project := range projects {
 		err := s.secretRepo.UpdateSecret(project)
 		if err != nil {
-			return fmt.Errorf("could not transform git credentials for project %s: %w", project.ProjectName, err)
+			return fmt.Errorf("could not transform git credentials for project %s: %s", project.ProjectName, err.Error())
 		}
 	}
 	return nil
--- shipyard-controller/db/migration/project_credentials_migration_test.go
+++ shipyard-controller/db/migration/project_credentials_migration_test.go
@@ -1,1 +1,192 @@
 package migration
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/keptn/keptn/shipyard-controller/db"
+	db_mock "github.com/keptn/keptn/shipyard-controller/db/mock"
+	"github.com/keptn/keptn/shipyard-controller/models"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProjectCredentialsMigration_Transform(t *testing.T) {
+	tests := []struct {
+		name        string
+		projectRepo db.MongoDBProjectCredentialsRepo
+		secretRepo  db.MongoDBSecretCredentialsRepo
+		want        error
+	}{
+		{
+			name: "valid",
+			secretRepo: db_mock.MongoDBSecretCredentialsRepoMock{
+				UpdateSecretFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			projectRepo: db_mock.MongoDBProjectCredentialsRepoMock{
+				GetOldCredentialsProjectsFunc: func() ([]*models.ExpandedProjectOld, error) {
+					return []*models.ExpandedProjectOld{
+						{
+							CreationDate:     "date",
+							LastEventContext: nil,
+							ProjectName:      "project",
+							Shipyard:         "shippy",
+							ShipyardVersion:  "ship",
+							Stages:           nil,
+							GitRemoteURI:     "http://some-url",
+							GitUser:          "user",
+							InsecureSkipTLS:  false,
+						},
+						{
+							CreationDate:     "date",
+							LastEventContext: nil,
+							ProjectName:      "project2",
+							Shipyard:         "shippy",
+							ShipyardVersion:  "ship",
+							Stages:           nil,
+							GitRemoteURI:     "http://some-url2",
+							GitUser:          "user",
+							InsecureSkipTLS:  false,
+						},
+					}, nil
+				},
+				UpdateProjectFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "no projects",
+			secretRepo: db_mock.MongoDBSecretCredentialsRepoMock{
+				UpdateSecretFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			projectRepo: db_mock.MongoDBProjectCredentialsRepoMock{
+				GetOldCredentialsProjectsFunc: func() ([]*models.ExpandedProjectOld, error) {
+					return []*models.ExpandedProjectOld{}, nil
+				},
+				UpdateProjectFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "nil projects",
+			secretRepo: db_mock.MongoDBSecretCredentialsRepoMock{
+				UpdateSecretFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			projectRepo: db_mock.MongoDBProjectCredentialsRepoMock{
+				GetOldCredentialsProjectsFunc: func() ([]*models.ExpandedProjectOld, error) {
+					return nil, nil
+				},
+				UpdateProjectFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "get projects err",
+			projectRepo: db_mock.MongoDBProjectCredentialsRepoMock{
+				GetOldCredentialsProjectsFunc: func() ([]*models.ExpandedProjectOld, error) {
+					return nil, fmt.Errorf("some err")
+				},
+			},
+			want: fmt.Errorf("could not transform git credentials to new format: some err"),
+		},
+		{
+			name: "update project err",
+			secretRepo: db_mock.MongoDBSecretCredentialsRepoMock{
+				UpdateSecretFunc: func(project *models.ExpandedProjectOld) error {
+					return nil
+				},
+			},
+			projectRepo: db_mock.MongoDBProjectCredentialsRepoMock{
+				GetOldCredentialsProjectsFunc: func() ([]*models.ExpandedProjectOld, error) {
+					return []*models.ExpandedProjectOld{
+						{
+							CreationDate:     "date",
+							LastEventContext: nil,
+							ProjectName:      "project",
+							Shipyard:         "shippy",
+							ShipyardVersion:  "ship",
+							Stages:           nil,
+							GitRemoteURI:     "http://some-url",
+							GitUser:          "user",
+							InsecureSkipTLS:  false,
+						},
+						{
+							CreationDate:     "date",
+							LastEventContext: nil,
+							ProjectName:      "project2",
+							Shipyard:         "shippy",
+							ShipyardVersion:  "ship",
+							Stages:           nil,
+							GitRemoteURI:     "http://some-url2",
+							GitUser:          "user",
+							InsecureSkipTLS:  false,
+						},
+					}, nil
+				},
+				UpdateProjectFunc: func(project *models.ExpandedProjectOld) error {
+					return fmt.Errorf("some err")
+				},
+			},
+			want: fmt.Errorf("could not transform git credentials for project project: some err"),
+		},
+		{
+			name: "update secret err",
+			secretRepo: db_mock.MongoDBSecretCredentialsRepoMock{
+				UpdateSecretFunc: func(project *models.ExpandedProjectOld) error {
+					return fmt.Errorf("some err")
+				},
+			},
+			projectRepo: db_mock.MongoDBProjectCredentialsRepoMock{
+				GetOldCredentialsProjectsFunc: func() ([]*models.ExpandedProjectOld, error) {
+					return []*models.ExpandedProjectOld{
+						{
+							CreationDate:     "date",
+							LastEventContext: nil,
+							ProjectName:      "project",
+							Shipyard:         "shippy",
+							ShipyardVersion:  "ship",
+							Stages:           nil,
+							GitRemoteURI:     "http://some-url",
+							GitUser:          "user",
+							InsecureSkipTLS:  false,
+						},
+						{
+							CreationDate:     "date",
+							LastEventContext: nil,
+							ProjectName:      "project2",
+							Shipyard:         "shippy",
+							ShipyardVersion:  "ship",
+							Stages:           nil,
+							GitRemoteURI:     "http://some-url2",
+							GitUser:          "user",
+							InsecureSkipTLS:  false,
+						},
+					}, nil
+				},
+			},
+			want: fmt.Errorf("could not transform git credentials for project project: some err"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := ProjectCredentialsMigrator{
+				secretRepo:  tt.secretRepo,
+				projectRepo: tt.projectRepo,
+			}
+			err := repo.Transform()
+			require.Equal(t, tt.want, err)
+		})
+	}
+}
