def6005955628baa7fe917bf28fe56eb94534305
RealAnna <anna.reale@dynatrace.com>
2022-06-15 13:04:53 +0200

    chore: Update cp-connector ref in go-sdk
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- go-sdk/internal/config/config.go
+++ go-sdk/internal/config/config.go
@@ -8,26 +8,26 @@ import (
 )
 
 type EnvConfig struct {
-	APIProxyHTTPTimeout     string   `envConfig:"API_PROXY_HTTP_TIMEOUT" default:"30"`
-	ConfigurationServiceURL string   `envConfig:"CONFIGURATION_SERVICE" default:"configuration-service:8080"`
-	EventBrokerURL          string   `envConfig:"EVENTBROKER" default:"nats://keptn-nats"`
-	PubSubTopic             string   `envConfig:"PUBSUB_TOPIC" default:""`
-	HealthEndpointPort      string   `envConfig:"HEALTH_ENDPOINT_PORT" default:"8080"`
-	HealthEndpointEnabled   bool     `envConfig:"HEALTH_ENDPOINT_ENABLED" default:"true"`
-	KeptnAPIEndpoint        string   `envConfig:"KEPTN_API_ENDPOINT" default:""`
-	KeptnAPIToken           string   `envConfig:"KEPTN_API_TOKEN" default:""`
-	Location                string   `envConfig:"LOCATION" default:"control-plane"`
-	K8sDeploymentVersion    string   `envConfig:"K8S_DEPLOYMENT_VERSION" default:""`
-	K8sDeploymentName       string   `envConfig:"K8S_DEPLOYMENT_NAME" default:""`
-	K8sNamespace            string   `envConfig:"K8S_NAMESPACE" default:""`
-	K8sPodName              string   `envConfig:"K8S_POD_NAME" default:""`
-	K8sNodeName             string   `envConfig:"K8S_NODE_NAME" default:""`
-	OAuthClientID           string   `envConfig:"OAUTH_CLIENT_ID" default:""`
-	OAuthClientSecret       string   `envConfig:"OAUTH_CLIENT_SECRET" default:""`
-	OAuthScopes             []string `envConfig:"OAUTH_SCOPES" default:""`
-	OAuthDiscovery          string   `envConfig:"OAUTH_DISCOVERY" default:""`
-	OauthTokenURL           string   `envConfig:"OAUTH_TOKEN_URL" default:""`
-	VerifySSL               bool     `envConfig:"HTTP_SSL_VERIFY" default:"true"`
+	APIProxyHTTPTimeout     string   `envconfig:"API_PROXY_HTTP_TIMEOUT" default:"30"`
+	ConfigurationServiceURL string   `envconfig:"CONFIGURATION_SERVICE" default:"configuration-service:8080"`
+	EventBrokerURL          string   `envconfig:"EVENTBROKER" default:"nats://keptn-nats"`
+	PubSubTopic             string   `envconfig:"PUBSUB_TOPIC" default:""`
+	HealthEndpointPort      string   `envconfig:"HEALTH_ENDPOINT_PORT" default:"8080"`
+	HealthEndpointEnabled   bool     `envconfig:"HEALTH_ENDPOINT_ENABLED" default:"true"`
+	KeptnAPIEndpoint        string   `envconfig:"KEPTN_API_ENDPOINT" default:""`
+	KeptnAPIToken           string   `envconfig:"KEPTN_API_TOKEN" default:""`
+	Location                string   `envconfig:"LOCATION" default:"control-plane"`
+	K8sDeploymentVersion    string   `envconfig:"K8S_DEPLOYMENT_VERSION" default:""`
+	K8sDeploymentName       string   `envconfig:"K8S_DEPLOYMENT_NAME" default:""`
+	K8sNamespace            string   `envconfig:"K8S_NAMESPACE" default:""`
+	K8sPodName              string   `envconfig:"K8S_POD_NAME" default:""`
+	K8sNodeName             string   `envconfig:"K8S_NODE_NAME" default:""`
+	OAuthClientID           string   `envconfig:"OAUTH_CLIENT_ID" default:""`
+	OAuthClientSecret       string   `envconfig:"OAUTH_CLIENT_SECRET" default:""`
+	OAuthScopes             []string `envconfig:"OAUTH_SCOPES" default:""`
+	OAuthDiscovery          string   `envconfig:"OAUTH_DISCOVERY" default:""`
+	OauthTokenURL           string   `envconfig:"OAUTH_TOKEN_URL" default:""`
+	VerifySSL               bool     `envconfig:"HTTP_SSL_VERIFY" default:"true"`
 }
 
 type ConnectionType string
