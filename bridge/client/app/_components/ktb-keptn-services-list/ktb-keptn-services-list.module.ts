f61723e73bd08258952707352c7b941bd5a4d6e0
ermin.muratovic <ermin.muratovic@gmail.com>
2022-06-20 12:55:52 +0200

    add missing modules
    Signed-off-by: ermin.muratovic <ermin.muratovic@gmail.com>

--- bridge/client/app/_components/ktb-keptn-services-list/ktb-keptn-services-list.module.ts
+++ bridge/client/app/_components/ktb-keptn-services-list/ktb-keptn-services-list.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KtbKeptnServicesListComponent } from './ktb-keptn-services-list.component';
+import { KtbLoadingModule } from '../ktb-loading/ktb-loading.module';
+import { DtTableModule } from '@dynatrace/barista-components/table';
+import { KtbExpandableTileModule } from '../ktb-expandable-tile/ktb-expandable-tile.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DtIconModule } from '@dynatrace/barista-components/icon';
+import { KtbPipeModule } from '../../_pipes/ktb-pipe.module';
+import { DtOverlayModule } from '@dynatrace/barista-components/overlay';
+import { KtbUniformSubscriptionsModule } from '../ktb-uniform-subscriptions/ktb-uniform-subscriptions.module';
 
 @NgModule({
   declarations: [KtbKeptnServicesListComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    DtTableModule,
+    DtIconModule.forRoot({
+      svgIconLocation: `assets/icons/{{name}}.svg`,
+    }),
+    DtOverlayModule,
+    FlexLayoutModule,
+    KtbExpandableTileModule,
+    KtbLoadingModule,
+    KtbPipeModule,
+    KtbUniformSubscriptionsModule,
+  ],
   exports: [KtbKeptnServicesListComponent],
 })
 export class KtbKeptnServicesListModule {}
