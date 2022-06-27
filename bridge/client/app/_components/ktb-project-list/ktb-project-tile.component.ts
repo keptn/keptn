6d1c05d9ad4047b5bdb941562f27abdc149aab52
Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
2022-06-27 10:24:29 +0200

    feat(bridge): RX-ify the dashboard component (#8167)
    * feat(bridge): RX-ify the dashboard component
    - Refactor dashboard to be a full smart component, which fetches all relevant data for its dumb child components
    - Dumbify project list, project tile and sequences state list
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Refactor property signatures to remove a code smell
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Remove getter and setters
    - set change detection to "on push"
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Refactoring
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Add unit test
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Remove function
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Improve unit test
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    
    * - Improve unit test
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>

--- bridge/client/app/_components/ktb-project-list/ktb-project-tile.component.ts
+++ bridge/client/app/_components/ktb-project-list/ktb-project-tile.component.ts
@@ -1,44 +1,15 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Project } from '../../_models/project';
-import { filter, takeUntil } from 'rxjs/operators';
-import { DataService } from '../../_services/data.service';
-import { IMetadata } from '../../_interfaces/metadata';
-import { Subject } from 'rxjs';
+import { Sequence } from '../../_models/sequence';
 
 @Component({
   selector: 'ktb-project-tile',
   templateUrl: './ktb-project-tile.component.html',
   styleUrls: ['./ktb-project-tile.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class KtbProjectTileComponent implements OnDestroy {
-  public _project?: Project;
-  public supportedShipyardVersion?: string | null;
-  private unsubscribe$ = new Subject<void>();
-
-  @Input()
-  get project(): Project | undefined {
-    return this._project;
-  }
-
-  set project(value: Project | undefined) {
-    if (this._project !== value) {
-      this._project = value;
-    }
-  }
-
-  constructor(private dataService: DataService) {
-    this.dataService.keptnMetadata
-      .pipe(
-        takeUntil(this.unsubscribe$),
-        filter((metadata): metadata is IMetadata | null => metadata !== undefined)
-      )
-      .subscribe((metadata) => {
-        this.supportedShipyardVersion = metadata === null ? null : metadata.shipyardversion;
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+export class KtbProjectTileComponent {
+  @Input() project?: Project;
+  @Input() sequences: Sequence[] = [];
+  @Input() supportedShipyardVersion: string | undefined;
 }
