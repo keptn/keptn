68d668f096ccb3f841f59921848e55cd419dca5f
RealAnna <anna.reale@dynatrace.com>
2022-06-28 10:18:21 +0200

    do not return error code
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- shipyard-controller/handler/projecthandler.go
+++ shipyard-controller/handler/projecthandler.go
@@ -423,8 +423,7 @@ func (ph *ProjectHandler) DeleteProject(c *gin.Context) {
 		err := ph.RepositoryProvisioner.DeleteRepository(projectName, namespace)
 		if err != nil {
 			// a failure to clean up the provisioned repo should not prevent the project delete
-			log.Errorf(err.Error())
-			SetFailedDependencyErrorResponse(c, UnableProvisionDeleteGeneric)
+			log.Errorf("Automatic Provisioning error: %s", err.Error())
 		}
 	}
 
