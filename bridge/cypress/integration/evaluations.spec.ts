3e39f2018f9a7227c56bc1841a40dd2fd263f5cb
ermin.muratovic <ermin.muratovic@dynatrace.com>
2022-06-29 17:14:48 +0200

    create mock data for evaluation usecase with keySlis
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>

--- bridge/cypress/integration/evaluations.spec.ts
+++ bridge/cypress/integration/evaluations.spec.ts
@@ -7,7 +7,7 @@ describe('evaluations', () => {
     cy.intercept('/api/controlPlane/v1/project?disableUpstreamSync=true&pageSize=50', { fixture: 'projects.mock' });
   });
 
-  it('should load the heatmap with sli breakdown in service screen', () => {
+  xit('should load the heatmap with sli breakdown in service screen', () => {
     cy.intercept('GET', '/api/project/sockshop/serviceStates', {
       statusCode: 200,
       fixture: 'get.sockshop.service.states.mock.json',
@@ -30,7 +30,7 @@ describe('evaluations', () => {
     cy.byTestId('keptn-evaluation-details-chartHeatmap').should('exist');
   });
 
-  it('should truncate score to 2 decimals', () => {
+  xit('should truncate score to 2 decimals', () => {
     cy.intercept('GET', '/api/project/sockshop/serviceStates', {
       statusCode: 200,
       fixture: 'get.sockshop.service.states.mock.json',
@@ -51,5 +51,24 @@ describe('evaluations', () => {
     cy.visit('/project/sockshop/service/carts/context/da740469-9920-4e0c-b304-0fd4b18d17c2/stage/staging');
 
     cy.byTestId('keptn-evaluation-details-scoreInfo').should('have.text', '33.99');
+  });
+
+  it('should show key sli info', () => {
+    cy.intercept('GET', '/api/project/sockshop/serviceStates', {
+      statusCode: 200,
+      fixture: 'get.sockshop.service.states.mock.json',
+    });
+    cy.intercept('GET', '/api/project/sockshop/deployment/da740469-9920-4e0c-b304-0fd4b18d17c2', {
+      statusCode: 200,
+      fixture: 'get.sockshop.service.carts.deployment.mock.json',
+    });
+    cy.intercept('GET', 'api/mongodb-datastore/event/type/sh.keptn.event.evaluation.finished?*', {
+      statusCode: 200,
+      fixture: 'get.sockshop.service.carts.evaluations.keysli.mock.json',
+    });
+
+    cy.visit('/project/sockshop/service/carts/context/da740469-9920-4e0c-b304-0fd4b18d17c2/stage/staging');
+    cy.byTestId('keptn-service-view-service-carts').should('exist');
+    cy.byTestId('keptn-evaluation-details-chartHeatmap').should('exist');
   });
 });
