0ef790328bdd689c5b367d0e69225498ae389860
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-06-29 13:16:02 +0200

    revert debugging changes
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- test/zero-downtime/testsuite_zerodowntime_test.go
+++ test/zero-downtime/testsuite_zerodowntime_test.go
@@ -30,11 +30,11 @@ spec:
 
 type ZeroDowntimeEnv struct {
 	quit              chan struct{}
-	NrOfUpgrades      int           `envconfig:"NUMBER_OF_UPGRADES"  default:"2"`
-	EnvInstallVersion string        `envconfig:"INSTALL_HELM_CHART" ` //for local run you can add a default ref to this string here e.g. default:"https://github.com/keptn/helm-charts-dev/raw/69eea439a26a99ecc163e296860dbb5d43e41600/packages/keptn-0.15.1-dev.tgz"`
-	EnvUpgradeVersion string        `envconfig:"UPGRADE_HELM_CHART" ` //for local run you can add a default ref to this string here e.g. default:"https://github.com/keptn/helm-charts-dev/raw/gh-pages/packages/keptn-0.15.0-dev.tgz"
+	NrOfUpgrades      int           `envconfig:"NUMBER_OF_UPGRADES" default:"2"`
+	EnvInstallVersion string        `envconfig:"INSTALL_HELM_CHART"` //for local run you can add a default ref to this string here e.g. default:"https://github.com/keptn/helm-charts-dev/raw/69eea439a26a99ecc163e296860dbb5d43e41600/packages/keptn-0.15.1-dev.tgz"`
+	EnvUpgradeVersion string        `envconfig:"UPGRADE_HELM_CHART"` //for local run you can add a default ref to this string here e.g. default:"https://github.com/keptn/helm-charts-dev/raw/gh-pages/packages/keptn-0.15.0-dev.tgz"
 	ApiProbeInterval  time.Duration `envconfig:"API_PROBES_INTERVAL" default:"15s"`
-	SequencesInterval time.Duration `envconfig:"SEQUENCES_INTERVAL"  default:"30s"`
+	SequencesInterval time.Duration `envconfig:"SEQUENCES_INTERVAL" default:"30s"`
 	Wg                *sync.WaitGroup
 
 	//api test fields
