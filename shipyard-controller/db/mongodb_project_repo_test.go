2d20f6f891436b81ef168193420addbe55ee4a47
Florian Bacher <florian.bacher@dynatrace.com>
2022-07-05 06:56:36 +0200

    fix(shipyard-controller): Return `ErrProjectNotFound` instead of `nil, nil` when project is not in the db (#8266)
    * fix(shipyard-controller): Return ErrProjectNotFound instead of nil, nil when project is not in the db
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * adapt to changes in mongodb repo
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * added unit tests
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * reduce cyclomatic complexity
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * adapt error message
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>
    
    * adapt error message
    
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

---
+++ shipyard-controller/db/mongodb_project_repo_test.go
@@ -0,0 +1,77 @@
+package db
+
+import (
+	"fmt"
+	apimodels "github.com/keptn/go-utils/pkg/api/models"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMongoDBProjectsRepo_InsertAndRetrieve(t *testing.T) {
+	r := NewMongoDBProjectsRepo(GetMongoDBConnectionInstance())
+
+	err := r.CreateProject(&apimodels.ExpandedProject{
+		ProjectName: "my-project",
+	})
+
+	require.Nil(t, err)
+
+	prj, err := r.GetProject("my-project")
+
+	require.Nil(t, err)
+	require.NotNil(t, prj)
+
+	err = r.DeleteProject("my-project")
+	require.Nil(t, err)
+
+	prj, err = r.GetProject("my-project")
+
+	require.ErrorIs(t, err, ErrProjectNotFound)
+	require.Nil(t, prj)
+}
+
+func TestMongoDBProjectsRepo_InsertAndRetrieveMultiple(t *testing.T) {
+	r := NewMongoDBProjectsRepo(GetMongoDBConnectionInstance())
+
+	nrProjects := 5
+
+	for i := 0; i < nrProjects; i++ {
+		projectName := fmt.Sprintf("project-%d", i)
+		err := r.CreateProject(&apimodels.ExpandedProject{
+			ProjectName: projectName,
+		})
+
+		require.Nil(t, err)
+	}
+
+	prj, err := r.GetProjects()
+
+	require.Nil(t, err)
+	require.NotNil(t, prj)
+	require.Len(t, prj, 5)
+}
+
+func TestMongoDBProjectsRepo_UpdateProject(t *testing.T) {
+	r := NewMongoDBProjectsRepo(GetMongoDBConnectionInstance())
+
+	err := r.CreateProject(&apimodels.ExpandedProject{
+		ProjectName: "my-project",
+	})
+
+	require.Nil(t, err)
+
+	updatedProject := &apimodels.ExpandedProject{
+		ProjectName: "my-project",
+		Shipyard:    "shipyard-content",
+	}
+	err = r.UpdateProject(updatedProject)
+
+	require.Nil(t, err)
+
+	prj, err := r.GetProject("my-project")
+
+	require.Nil(t, err)
+	require.NotNil(t, prj)
+
+	require.Equal(t, updatedProject, prj)
+}
