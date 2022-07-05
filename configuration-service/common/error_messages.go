1a65d527566749f19123968ee02f8762f8154610
warber <bernd.warmuth@dynatrace.com>
2022-07-05 08:37:03 +0200

    chore: Removed configuration-service module
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- configuration-service/common/error_messages.go
+++
@@ -1,5 +0,0 @@
-package common
-
-const gitCredentialsFail = "failed to get git credentials from client"
-const setUpstreamFail = "failed to set upstream and push for project '%s'"
-const unmarshalGitCredentialsFail = "failed to unmarshal git credentials"
