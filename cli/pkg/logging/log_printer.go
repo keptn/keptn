2cb068955f18bcb8ff959a1877fd854c38502e90
warber <bernd.warmuth@dynatrace.com>
2022-07-05 12:05:32 +0200

    feat: Removed (un)install/upgrade commands
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- cli/pkg/logging/log_printer.go
+++ cli/pkg/logging/log_printer.go
@@ -2,7 +2,6 @@ package logging
 
 import (
 	"fmt"
-	"strings"
 )
 
 // LogLevel specifies the used logging level
@@ -26,32 +25,4 @@ func PrintLog(message string, printInLevel LogLevelType) {
 	if LogLevel <= printInLevel {
 		fmt.Println(message)
 	}
-}
-
-// PrintLogStringLevel prints the log according to the log level that is set in the flags
-func PrintLogStringLevel(message string, printInLevel string) {
-
-	lev := GetLogLevel(printInLevel)
-	if lev < 0 {
-		PrintLog("Received unknown log level: "+printInLevel, InfoLevel)
-	} else {
-		PrintLog(message, lev)
-	}
-}
-
-// GetLogLevel parses a string and returns the appropriate LogLevelType
-func GetLogLevel(logLevel string) LogLevelType {
-
-	if strings.ToLower(logLevel) == "info" {
-		return InfoLevel
-	} else if strings.ToLower(logLevel) == "debug" ||
-		strings.ToLower(logLevel) == "verbose" ||
-		strings.ToLower(logLevel) == "v" {
-		return VerboseLevel
-	} else if strings.ToLower(logLevel) == "error" ||
-		strings.ToLower(logLevel) == "quiet" ||
-		strings.ToLower(logLevel) == "q" {
-		return QuietLevel
-	}
-	return -1
 }
