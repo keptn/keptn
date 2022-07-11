9512b6b04fc4785caf0a10e70f1f4c5fa7fe32a7
Paolo Chila <paolo.chila@dynatrace.com>
2022-07-11 15:59:09 +0200

    refactor(api): split package processor api and resource task processing
    Signed-off-by: Paolo Chila <paolo.chila@dynatrace.com>

--- api/importer/package_processor.go
+++ api/importer/package_processor.go
@@ -69,47 +69,63 @@ func (ipp *ImportPackageProcessor) Process(project string, ip ImportPackage) err
 
 		switch task.Type {
 		case apiTaskType:
-			if task.APITask == nil {
-				return fmt.Errorf("malformed task of type api: %+v", task)
-			}
-			apiTaskExecution, err := mapAPITask(project, ip, task)
-			if err != nil {
-				return fmt.Errorf("error setting up API task ID %s: %w", task.ID, err)
-			}
-			_, err = ipp.executor.ExecuteAPI(apiTaskExecution)
-			if err != nil {
-				return fmt.Errorf("execution of task %s failed: %w", task.ID, err)
+			if err = ipp.processAPITask(project, ip, task); err != nil {
+				return err
 			}
 		case resourceTaskType:
-			if task.ResourceTask == nil {
-				return fmt.Errorf("malformed task of type resource: %+v", task)
+			if err = ipp.processResourceTask(project, ip, task); err != nil {
+				return err
 			}
-			var stages []string
-			if task.ResourceTask.Stage == "" {
-				stages, err = ipp.stageRetriever.GetStages(project)
-				if err != nil {
-					return fmt.Errorf("error retrieving stages for project %s: %w", project, err)
-				}
-			} else {
-				stages = []string{task.Stage}
-			}
-
-			for _, stage := range stages {
-				resourcePush, err := mapResourcePush(project, stage, ip, task)
-				if err != nil {
-					return fmt.Errorf("error setting up resource push for task ID %s: %w", task.ID, err)
-				}
-				_, err = ipp.executor.PushResource(resourcePush)
-				if err != nil {
-					return fmt.Errorf("resource task id %s failed: %w", task.ID, err)
-				}
-			}
-
 		default:
 			return fmt.Errorf("task of type %s not implemented", task.Type)
 		}
 	}
 
+	return nil
+}
+
+func (ipp *ImportPackageProcessor) processResourceTask(
+	project string, ip ImportPackage, task *model.ManifestTask,
+) error {
+	if task.ResourceTask == nil {
+		return fmt.Errorf("malformed task of type resource: %+v", task)
+	}
+	var stages []string
+	var err error
+	if task.ResourceTask.Stage == "" {
+		stages, err = ipp.stageRetriever.GetStages(project)
+		if err != nil {
+			return fmt.Errorf("error retrieving stages for project %s: %w", project, err)
+		}
+	} else {
+		stages = []string{task.Stage}
+	}
+
+	for _, stage := range stages {
+		resourcePush, err := mapResourcePush(project, stage, ip, task)
+		if err != nil {
+			return fmt.Errorf("error setting up resource push for task ID %s: %w", task.ID, err)
+		}
+		_, err = ipp.executor.PushResource(resourcePush)
+		if err != nil {
+			return fmt.Errorf("resource task id %s failed: %w", task.ID, err)
+		}
+	}
+	return nil
+}
+
+func (ipp *ImportPackageProcessor) processAPITask(project string, ip ImportPackage, task *model.ManifestTask) error {
+	if task.APITask == nil {
+		return fmt.Errorf("malformed task of type api: %+v", task)
+	}
+	apiTaskExecution, err := mapAPITask(project, ip, task)
+	if err != nil {
+		return fmt.Errorf("error setting up API task ID %s: %w", task.ID, err)
+	}
+	_, err = ipp.executor.ExecuteAPI(apiTaskExecution)
+	if err != nil {
+		return fmt.Errorf("execution of task %s failed: %w", task.ID, err)
+	}
 	return nil
 }
 
