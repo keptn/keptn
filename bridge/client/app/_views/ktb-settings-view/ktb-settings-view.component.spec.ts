11efec15af2733aa5a616090369c4f180fcc636d
ermin.muratovic <ermin.muratovic@dynatrace.com>
2022-07-12 12:15:26 +0200

    fix unit tests
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>

--- bridge/client/app/_views/ktb-settings-view/ktb-settings-view.component.spec.ts
+++ bridge/client/app/_views/ktb-settings-view/ktb-settings-view.component.spec.ts
@@ -3,6 +3,7 @@ import { By } from '@angular/platform-browser';
 import { KtbSettingsViewModule } from './ktb-settings-view.module';
 import { KtbSettingsViewComponent } from './ktb-settings-view.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('KtbSettingsViewComponent', () => {
   let component: KtbSettingsViewComponent;
@@ -10,7 +11,7 @@ describe('KtbSettingsViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [KtbSettingsViewModule, HttpClientTestingModule],
+      imports: [KtbSettingsViewModule, HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(KtbSettingsViewComponent);
