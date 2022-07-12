28130fefcb772d3742a14ba0e4ed83f7fccf07d6
RealAnna <anna.reale@dynatrace.com>
2022-07-12 11:20:28 +0200

    chore: fix webhook test
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- test/zero-downtime/webhook_test.go
+++ test/zero-downtime/webhook_test.go
@@ -1,12 +1,9 @@
 package zero_downtime
 
 import (
-	"fmt"
 	"github.com/benbjohnson/clock"
 	testutils "github.com/keptn/keptn/test/go-tests"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"os"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -63,79 +60,10 @@ Loop:
 
 }
 
-const webhookShipyard = `--- 
-apiVersion: "spec.keptn.sh/0.2.3"
-kind: Shipyard
-metadata:
-  name: "shipyard-echo-service"
-spec:
-  stages:
-    - name: "otherstage"
-    - name: "dev"
-      sequences:
-        - name: "othersequence"
-          tasks:
-            - name: "othertask"
-        - name: "sequencewithunknowntask"
-          tasks:
-            - name: "unknowntask"
-        - name: "unallowedsequence"
-          tasks:
-            - name: "unallowedtask"
-        - name: "failedsequence"
-          tasks:
-            - name: "failedtask"
-        - name: "loopbacksequence"
-          tasks:
-            - name: "loopback"
-        - name: "loopbacksequence2"
-          tasks:
-            - name: "loopback2"
-        - name: "loopbacksequence3"
-          tasks:
-            - name: "loopback3"
-        - name: "mysequence"
-          tasks:
-            - name: "mytask"`
-
 // 1 Pass 7 Fails
 func (suite *TestSuiteWebhook) Test_Webhook() {
-	projectName := "webhooks" + suite.env.gedId()
-	serviceName := "myservice"
-
-	//test considered failed by default so that we can use require
 	suite.env.failSequence()
-
-	shipyardFilePath, err := testutils.CreateTmpShipyardFile(webhookShipyard)
-	require.Nil(suite.T(), err)
-
-	suite.T().Logf("creating project %s", projectName)
-	projectName, err = testutils.CreateProject(projectName, shipyardFilePath)
-	require.Nil(suite.T(), err)
-
-	suite.T().Logf("creating service %s", serviceName)
-	output, err := testutils.ExecuteCommand(fmt.Sprintf("keptn create service %s --project=%s", serviceName, projectName))
-
-	require.Nil(suite.T(), err)
-	require.Contains(suite.T(), output, "created successfully")
-
-	// create a secret that should be referenced in the webhook yaml
-	_, _ = testutils.ApiPOSTRequest("/secrets/v1/secret", map[string]interface{}{
-		"name":  "my-webhook-k8s-secret",
-		"scope": "keptn-webhook-service",
-		"data": map[string]string{
-			"my-key": "my-value",
-		},
-	}, 3)
-
-	defer func() {
-		err := os.Remove(shipyardFilePath)
-		if err != nil {
-			suite.T().Logf("Could not delete tmp file: %s", err.Error())
-		}
-	}()
-	testutils.Test_Webhook(suite.T(), testutils.WebhookYamlBeta, projectName, serviceName)
-
+	testutils.Test_Webhook(suite.T())
 	//if test returns then it's passed
 	suite.env.passFailedSequence()
 }
