8815fd3a33c76e73182124a15bca42973f8750f2
ermin.muratovic <ermin.muratovic@dynatrace.com>
2022-07-21 12:45:13 +0200

    add missing MatDialogModule
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>

--- bridge/client/app/_views/ktb-project-view/ktb-project-view-common.module.ts
+++ bridge/client/app/_views/ktb-project-view/ktb-project-view-common.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { KtbProjectViewComponent } from './ktb-project-view.component';
-import { DtEmptyStateModule } from '@dynatrace/barista-components/empty-state';
-import { KtbPipeModule } from '../../_pipes/ktb-pipe.module';
 import { RouterModule } from '@angular/router';
+import { FlexModule } from '@angular/flex-layout';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DtEmptyStateModule } from '@dynatrace/barista-components/empty-state';
 import { DtButtonModule } from '@dynatrace/barista-components/button';
 import { DtIconModule } from '@dynatrace/barista-components/icon';
-import { FlexModule } from '@angular/flex-layout';
-import { KtbLoadingModule } from '../../_components/ktb-loading/ktb-loading.module';
 import { DtMenuModule } from '@dynatrace/barista-components/menu';
 import { DtOverlayModule } from '@dynatrace/barista-components/overlay';
+import { KtbPipeModule } from '../../_pipes/ktb-pipe.module';
+import { KtbProjectViewComponent } from './ktb-project-view.component';
+import { KtbLoadingModule } from '../../_components/ktb-loading/ktb-loading.module';
 
 @NgModule({
   declarations: [KtbProjectViewComponent],
@@ -23,6 +24,7 @@ import { DtOverlayModule } from '@dynatrace/barista-components/overlay';
     DtMenuModule,
     DtOverlayModule,
     FlexModule,
+    MatDialogModule,
     KtbLoadingModule,
     KtbPipeModule,
     RouterModule,
