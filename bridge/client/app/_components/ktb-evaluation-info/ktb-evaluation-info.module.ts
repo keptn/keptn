0e59ac406743ab318b2a6df15b360a6d2b33e2d2
Klaus Strießnig <k.striessnig@gmail.com>
2022-07-27 13:38:43 +0200

    removed unused modules
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/client/app/_components/ktb-evaluation-info/ktb-evaluation-info.module.ts
+++ bridge/client/app/_components/ktb-evaluation-info/ktb-evaluation-info.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KtbEvaluationInfoComponent } from './ktb-evaluation-info.component';
-import { DtTagModule } from '@dynatrace/barista-components/tag';
 import { FlexModule } from '@angular/flex-layout';
-import { KtbEvaluationDetailsModule } from '../ktb-evaluation-details/ktb-evaluation-details.module';
 import { KtbEvaluationBadgeModule } from '../ktb-evaluation-badge/ktb-evaluation-badge.module';
 
 @NgModule({
   declarations: [KtbEvaluationInfoComponent],
-  imports: [CommonModule, DtTagModule, FlexModule, KtbEvaluationDetailsModule, KtbEvaluationBadgeModule],
+  imports: [CommonModule, FlexModule, KtbEvaluationBadgeModule],
   exports: [KtbEvaluationInfoComponent],
 })
 export class KtbEvaluationInfoModule {}
