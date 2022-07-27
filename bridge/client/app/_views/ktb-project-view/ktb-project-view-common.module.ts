f6e29eb38b16f8105831e5c691c85a8a866880b3
Ermin Muratovic <ermin.muratovic@dynatrace.com>
2022-07-27 14:25:44 +0200

    refactor(bridge): Refactor project settings (#8510)
    * add missing MatDialogModule
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * move resourceServiceEnabled to state$
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * make things reactive
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * split create and update tests
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * adopt unit tests
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * listen for projectCreated in queryParam
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * remove unnecessary test
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * remove unnecessary module
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * make filter more readable and map projectName null to undefined
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * move projectDeletionData to state$
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * get rid of local projectName
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * refactor project settings
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * fix unit tests
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    
    * fix unit tests
    
    Signed-off-by: ermin.muratovic <ermin.muratovic@dynatrace.com>
    Signed-off-by: Philipp Hinteregger <philipp.hinteregger@dynatrace.com>

--- bridge/client/app/_views/ktb-project-view/ktb-project-view-common.module.ts
+++ bridge/client/app/_views/ktb-project-view/ktb-project-view-common.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { KtbProjectViewComponent } from './ktb-project-view.component';
-import { DtEmptyStateModule } from '@dynatrace/barista-components/empty-state';
-import { KtbPipeModule } from '../../_pipes/ktb-pipe.module';
 import { RouterModule } from '@angular/router';
+import { FlexModule } from '@angular/flex-layout';
+import { DtEmptyStateModule } from '@dynatrace/barista-components/empty-state';
 import { DtButtonModule } from '@dynatrace/barista-components/button';
 import { DtIconModule } from '@dynatrace/barista-components/icon';
-import { FlexModule } from '@angular/flex-layout';
-import { KtbLoadingModule } from '../../_components/ktb-loading/ktb-loading.module';
 import { DtMenuModule } from '@dynatrace/barista-components/menu';
 import { DtOverlayModule } from '@dynatrace/barista-components/overlay';
+import { KtbPipeModule } from '../../_pipes/ktb-pipe.module';
+import { KtbProjectViewComponent } from './ktb-project-view.component';
+import { KtbLoadingModule } from '../../_components/ktb-loading/ktb-loading.module';
 
 @NgModule({
   declarations: [KtbProjectViewComponent],
