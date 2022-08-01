d25b410ccb88553eb8b2401872fa772612f9e545
Bernd Warmuth <72415058+warber@users.noreply.github.com>
2022-08-01 09:29:13 +0200

    chore(shipyard-controller): Cleanup package structure (#8537)
    * created sub packages
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>
    Signed-off-by: Philipp Hinteregger <philipp.hinteregger@dynatrace.com>

---
+++ shipyard-controller/internal/controller/utils.go
@@ -0,0 +1,189 @@
+package controller
+
+import (
+	"encoding/json"
+	"errors"
+	apimodels "github.com/keptn/go-utils/pkg/api/models"
+	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
+	"github.com/keptn/keptn/shipyard-controller/internal/common"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+const TestShipyardFile = `apiVersion: spec.keptn.sh/0.2.0
+kind: Shipyard
+metadata:
+  name: test-shipyard
+spec:
+  stages:
+  - name: dev
+    sequences:
+    - name: artifact-delivery
+      tasks:
+      - name: deployment
+        properties:  
+          strategy: direct
+      - name: test
+        properties:
+          kind: functional
+      - name: evaluation 
+      - name: release 
+
+  - name: hardening
+    sequences:
+    - name: artifact-delivery
+      triggers:
+      - dev.artifact-delivery.finished
+      tasks:
+      - name: deployment
+        properties: 
+          strategy: blue_green_service
+      - name: test
+        properties:  
+          kind: performance
+      - name: evaluation
+      - name: release
+        
+  - name: production
+    sequences:
+    - name: artifact-delivery 
+      triggers:
+      - hardening.artifact-delivery.finished
+      tasks:
+      - name: deployment
+        properties:
+          strategy: blue_green
+      - name: release
+      
+    - name: remediation
+      tasks:
+      - name: remediation
+      - name: evaluation`
+
+func ShouldContainEvent(t *testing.T, events []apimodels.KeptnContextExtendedCE, eventType string, stage string, eval func(t *testing.T, event apimodels.KeptnContextExtendedCE) bool) {
+	var foundEvent *apimodels.KeptnContextExtendedCE
+	for index, event := range events {
+		scope, _ := getEventScope(event)
+		if *event.Type == eventType {
+			if stage == "" {
+				foundEvent = &events[index]
+				break
+			} else if stage != "" && scope.Stage == stage {
+				foundEvent = &events[index]
+				break
+			}
+		}
+	}
+
+	require.NotNil(t, foundEvent)
+
+	if eval != nil {
+		require.False(t, eval(t, *foundEvent))
+	}
+}
+
+func ShouldNotContainEvent(t *testing.T, events []apimodels.KeptnContextExtendedCE, eventType string, stage string) {
+	var foundEvent *apimodels.KeptnContextExtendedCE
+	for _, event := range events {
+		if *event.Type == eventType {
+			scope, _ := getEventScope(event)
+			if stage == "" {
+				foundEvent = &event
+				break
+			} else if stage != "" && scope.Stage == stage {
+				foundEvent = &event
+				break
+			}
+		}
+	}
+	require.Nil(t, foundEvent)
+}
+
+func getEventScope(event apimodels.KeptnContextExtendedCE) (*keptnv2.EventData, error) {
+	marshal, err := json.Marshal(event.Data)
+	if err != nil {
+		return nil, err
+	}
+	data := &keptnv2.EventData{}
+	err = json.Unmarshal(marshal, data)
+	if err != nil {
+		return nil, err
+	}
+	if data.Project == "" {
+		return nil, errors.New("event does not contain a project")
+	}
+	if data.Stage == "" {
+		return nil, errors.New("event does not contain a stage")
+	}
+	if data.Service == "" {
+		return nil, errors.New("event does not contain a service")
+	}
+	return data, nil
+}
+
+type EventScope struct {
+	Project string `json:"project"`
+	Stage   string `json:"stage"`
+	Service string `json:"service"`
+}
+
+func GetTestTriggeredEvent() apimodels.KeptnContextExtendedCE {
+	return apimodels.KeptnContextExtendedCE{
+		Contenttype:    "application/json",
+		Data:           EventScope{Project: "test-project", Stage: "dev", Service: "carts"},
+		Extensions:     nil,
+		ID:             "test-triggered-id",
+		Shkeptncontext: "test-context",
+		Source:         common.Stringp("test-source"),
+		Specversion:    "0.2",
+		Time:           time.Now(),
+		Triggeredid:    "",
+		Type:           common.Stringp("sh.keptn.event.approval.triggered"),
+	}
+}
+
+func GetTestStartedEvent() apimodels.KeptnContextExtendedCE {
+	return apimodels.KeptnContextExtendedCE{
+		Contenttype:    "application/json",
+		Data:           EventScope{Project: "test-project", Stage: "dev", Service: "carts"},
+		Extensions:     nil,
+		ID:             "test-started-id",
+		Shkeptncontext: "test-context",
+		Source:         common.Stringp("test-source"),
+		Specversion:    "0.2",
+		Time:           time.Now(),
+		Triggeredid:    "test-triggered-id",
+		Type:           common.Stringp("sh.keptn.event.approval.started"),
+	}
+}
+
+func GetTestStartedEventWithUnmatchedTriggeredID() apimodels.KeptnContextExtendedCE {
+	return apimodels.KeptnContextExtendedCE{
+		Contenttype:    "application/json",
+		Data:           EventScope{Project: "test-project", Stage: "dev", Service: "carts"},
+		Extensions:     nil,
+		ID:             "test-started-id",
+		Shkeptncontext: "test-context",
+		Source:         common.Stringp("test-source"),
+		Specversion:    "0.2",
+		Time:           time.Now(),
+		Triggeredid:    "unmatched-test-triggered-id",
+		Type:           common.Stringp("sh.keptn.event.approval.started"),
+	}
+}
+
+func GetTestFinishedEventWithUnmatchedSource() apimodels.KeptnContextExtendedCE {
+	return apimodels.KeptnContextExtendedCE{
+		Contenttype:    "application/json",
+		Data:           EventScope{Project: "test-project", Stage: "dev", Service: "carts"},
+		Extensions:     nil,
+		ID:             "test-finished-id",
+		Shkeptncontext: "test-context",
+		Source:         common.Stringp("unmatched-test-source"),
+		Specversion:    "0.2",
+		Time:           time.Now(),
+		Triggeredid:    "test-triggered-id",
+		Type:           common.Stringp("sh.keptn.event.approval.finished"),
+	}
+}
---
+++ shipyard-controller/internal/db/repos.go
@@ -0,0 +1,99 @@
+package db
+
+import (
+	"github.com/keptn/keptn/shipyard-controller/internal/common"
+	"time"
+
+	apimodels "github.com/keptn/go-utils/pkg/api/models"
+	"github.com/keptn/keptn/shipyard-controller/models"
+)
+
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/sequencestaterepo_mock.go . SequenceStateRepo
+type SequenceStateRepo interface {
+	CreateSequenceState(state apimodels.SequenceState) error
+	FindSequenceStates(filter apimodels.StateFilter) (*apimodels.SequenceStates, error)
+	UpdateSequenceState(state apimodels.SequenceState) error
+	DeleteSequenceStates(filter apimodels.StateFilter) error
+}
+
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/uniformrepo_mock.go . UniformRepo
+type UniformRepo interface {
+	GetUniformIntegrations(filter models.GetUniformIntegrationsParams) ([]apimodels.Integration, error)
+	DeleteUniformIntegration(id string) error
+	CreateUniformIntegration(integration apimodels.Integration) error
+	CreateOrUpdateUniformIntegration(integration apimodels.Integration) error
+	CreateOrUpdateSubscription(integrationID string, subscription apimodels.EventSubscription) error
+	DeleteServiceFromSubscriptions(subscriptionName string) error
+	DeleteSubscription(integrationID, subscriptionID string) error
+	GetSubscription(integrationID, subscriptionID string) (*apimodels.EventSubscription, error)
+	GetSubscriptions(integrationID string) ([]apimodels.EventSubscription, error)
+	UpdateLastSeen(integrationID string) (*apimodels.Integration, error)
+	UpdateVersionInfo(integrationID, integrationVersion, distributorVersion string) (*apimodels.Integration, error)
+}
+
+type LogRepo interface {
+	CreateLogEntries(entries []apimodels.LogEntry) error
+	GetLogEntries(filter models.GetLogParams) (*models.GetLogsResponse, error)
+	DeleteLogEntries(params models.DeleteLogParams) error
+}
+
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/eventqueuerepo_mock.go . EventQueueRepo
+// EventQueueRepo defines the interface for storing, retrieving and deleting queued events
+type EventQueueRepo interface {
+	QueueEvent(item models.QueueItem) error
+	GetQueuedEvents(timestamp time.Time) ([]models.QueueItem, error)
+	IsEventInQueue(eventID string) (bool, error)
+	IsSequenceOfEventPaused(eventScope models.EventScope) bool
+	DeleteQueuedEvent(eventID string) error
+	DeleteQueuedEvents(scope models.EventScope) error
+	CreateOrUpdateEventQueueState(state models.EventQueueSequenceState) error
+	GetEventQueueSequenceStates(filter models.EventQueueSequenceState) ([]models.EventQueueSequenceState, error)
+	DeleteEventQueueStates(state models.EventQueueSequenceState) error
+}
+
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/eventrepo_mock.go . EventRepo
+type EventRepo interface {
+	GetEvents(project string, filter common.EventFilter, status ...common.EventStatus) ([]apimodels.KeptnContextExtendedCE, error)
+	GetRootEvents(params models.GetRootEventParams) (*models.GetEventsResult, error)
+	InsertEvent(project string, event apimodels.KeptnContextExtendedCE, status common.EventStatus) error
+	DeleteEvent(project string, eventID string, status common.EventStatus) error
+	DeleteEventCollections(project string) error
+	GetStartedEventsForTriggeredID(eventScope models.EventScope) ([]apimodels.KeptnContextExtendedCE, error)
+	GetEventsWithRetry(project string, filter common.EventFilter, status common.EventStatus, nrRetries int) ([]apimodels.KeptnContextExtendedCE, error)
+	GetTaskSequenceTriggeredEvent(eventScope models.EventScope, taskSequenceName string) (*apimodels.KeptnContextExtendedCE, error)
+	DeleteAllFinishedEvents(eventScope models.EventScope) error
+	GetFinishedEvents(eventScope models.EventScope) ([]apimodels.KeptnContextExtendedCE, error)
+}
+
+// ProjectRepo is an interface to access projects
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/projectrepo_mock.go . ProjectRepo
+type ProjectRepo interface {
+	GetProjects() ([]*apimodels.ExpandedProject, error)
+	GetProject(projectName string) (*apimodels.ExpandedProject, error)
+	CreateProject(project *apimodels.ExpandedProject) error
+	UpdateProject(project *apimodels.ExpandedProject) error
+	UpdateProjectUpstream(projectName string, uri string, user string) error
+	DeleteProject(projectName string) error
+}
+
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/sequencequeuerepo_mock.go . SequenceQueueRepo
+// SequenceQueueRepo defines the interface for storing, retrieving and deleting queued events
+type SequenceQueueRepo interface {
+	QueueSequence(item models.QueueItem) error
+	GetQueuedSequences() ([]models.QueueItem, error)
+	DeleteQueuedSequences(itemFilter models.QueueItem) error
+}
+
+//go:generate moq --skip-ensure -pkg db_mock -out ./mock/sequenceexecution_mock.go . SequenceExecutionRepo
+type SequenceExecutionRepo interface {
+	Get(filter models.SequenceExecutionFilter) ([]models.SequenceExecution, error)
+	GetPaginated(filter models.SequenceExecutionFilter, paginationParams models.PaginationParams) ([]models.SequenceExecution, *models.PaginationResult, error)
+	GetByTriggeredID(project, triggeredID string) (*models.SequenceExecution, error)
+	Upsert(item models.SequenceExecution, options *models.SequenceExecutionUpsertOptions) error
+	AppendTaskEvent(taskSequence models.SequenceExecution, event models.TaskEvent) (*models.SequenceExecution, error)
+	UpdateStatus(taskSequence models.SequenceExecution) (*models.SequenceExecution, error)
+	PauseContext(eventScope models.EventScope) error
+	ResumeContext(eventScope models.EventScope) error
+	IsContextPaused(eventScope models.EventScope) bool
+	Clear(projectName string) error
+}
