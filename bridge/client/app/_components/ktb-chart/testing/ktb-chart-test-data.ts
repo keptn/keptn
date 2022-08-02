85e04c10398afac335039da032d1df7635d0d1e8
Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
2022-08-02 15:18:37 +0200

    feat(bridge): Improve ktb-chart (#8561)
    - Clean API model
    - Fix right tick values
    - Adopt unit test
    - Adopt implementations
    
    Signed-off-by: Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
    Signed-off-by: Philipp Hinteregger <philipp.hinteregger@dynatrace.com>

--- bridge/client/app/_components/ktb-chart/testing/ktb-chart-test-data.ts
+++ bridge/client/app/_components/ktb-chart/testing/ktb-chart-test-data.ts
@@ -11,122 +11,103 @@ const tooltipLabels = {
 const data: ChartItem[] = [
   {
     type: 'score-bar',
-    identifier: 'Score',
+    label: 'Score',
     points: [
       {
         x: 0,
         y: 33,
         color: '#e30505',
-        identifier: 'e0',
       },
       {
         x: 1,
         y: 66,
         color: '#ffaa00',
-        identifier: 'e1',
       },
       {
         x: 2,
         y: 88,
         color: '#518637',
-        identifier: 'e2',
       },
       {
         x: 3,
         y: 50,
         color: '#a97223',
-        identifier: 'e3',
       },
       {
         x: 4,
         y: 2,
         color: '#f30010',
-        identifier: 'e4',
       },
     ],
   },
   {
     type: 'score-line',
-    identifier: 'Score',
+    label: 'Score',
     points: [
       {
         x: 0,
         y: 33,
-        identifier: 'e0',
       },
       {
         x: 1,
         y: 66,
-        identifier: 'e1',
       },
       {
         x: 2,
         y: 88,
-        identifier: 'e2',
       },
       {
         x: 3,
         y: 50,
-        identifier: 'e3',
       },
       {
         x: 4,
         y: 2,
-        identifier: 'e4',
       },
     ],
   },
   {
     type: 'metric-line',
-    identifier: 'Metric 1',
     label: 'My custom metric 1 label',
     points: [
       {
         x: 1,
         y: 30,
-        identifier: 'e1',
       },
       {
         x: 2,
         y: 40,
-        identifier: 'e2',
       },
       {
         x: 3,
         y: 144.5,
-        identifier: 'e3',
       },
       {
         x: 4,
         y: 10,
-        identifier: 'e4',
       },
     ],
   },
   {
     type: 'metric-line',
-    identifier: 'Metric 2',
+    label: 'Metric 2',
     invisible: true,
     points: [
       {
         x: 1,
         y: 4,
-        identifier: 'e1',
       },
       {
         x: 2,
         y: 5,
-        identifier: 'e2',
       },
       {
         x: 3,
         y: 12,
-        identifier: 'e3',
       },
       {
         x: 4,
         y: 12,
-        identifier: 'e4',
       },
     ],
   },
