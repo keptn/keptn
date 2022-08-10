5bfdf1b81222a5d673df894858459b8127c2c40c
warber <bernd.warmuth@dynatrace.com>
2022-08-10 14:55:39 +0200

    helm adjustments to cover env vars
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- api/restapi/configure_keptn.go
+++ api/restapi/configure_keptn.go
@@ -43,7 +43,7 @@ type EnvConfig struct {
 	MaxAuthRequestsPerSecond  float64 `envconfig:"MAX_AUTH_REQUESTS_PER_SECOND" default:"1"`
 	MaxAuthRequestBurst       int     `envconfig:"MAX_AUTH_REQUESTS_BURST" default:"2"`
 	MaxImportUncompressedSize uint64  `envconfig:"MAX_IMPORT_UNCOMPRESSED_SIZE" default:"52428800"` // 50MB default value
-	MaxEventSizeKB            int64   `envconfig:"MAX_EVENT_BODY_SIZE_KB" default:"64"`
+	MaxEventSizeKB            int64   `envconfig:"MAX_EVENT_SIZE_KB" default:"64"`
 	OAuthEnabled              bool    `envconfig:"OAUTH_ENABLED" default:"false"`
 	OAuthPrefix               string  `envconfig:"OAUTH_PREFIX" default:"keptn:"`
 }
@@ -134,7 +134,7 @@ func configureAPI(api *operations.KeptnAPI) http.Handler {
 		api.AddMiddlewareFor(http.MethodPost, "/auth", rateLimiter.Handle)
 	}
 
-	if env.MaxEventSizeKB > 0 {
+	if env.EventValidationEnabled && env.MaxEventSizeKB > 0 {
 		api.AddMiddlewareFor(http.MethodPost, "/event", custommiddleware.EnforceMaxEventSize(env.MaxEventSizeBytes()))
 	}
 
