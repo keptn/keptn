d3db4258004c2f402db801fabfc715d5a3fe0afa
Andreas Samonik <andreas.samonik@dynatrace.com>
2022-08-11 10:57:38 +0200

    repo test
    Signed-off-by: Andreas Samonik <andreas.samonik@dynatrace.com>

--- shipyard-controller/internal/db/mongodb_dbdump_repo_test.go
+++ shipyard-controller/internal/db/mongodb_dbdump_repo_test.go
@@ -1,32 +1,41 @@
-package db_test
+package db
 
 import (
 	"encoding/json"
 	"testing"
 
 	apimodels "github.com/keptn/go-utils/pkg/api/models"
-	"github.com/keptn/keptn/shipyard-controller/internal/db"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMongoDBDumpRepo_ListAllCollections(t *testing.T) {
-	repo := db.NewMongoDBDumpRepo(db.GetMongoDBConnectionInstance())
+	dumpRepo := NewMongoDBDumpRepo(GetMongoDBConnectionInstance())
+	projectsRepo := NewMongoDBProjectsRepo(GetMongoDBConnectionInstance())
 
-	projectNames, err := repo.ListAllCollections()
-	require.NotEmpty(t, projectNames)
+	err := projectsRepo.CreateProject(&apimodels.ExpandedProject{
+		ProjectName: "my-project",
+	})
 	require.Nil(t, err)
+
+	projectNames, err := dumpRepo.ListAllCollections()
+
+	require.Len(t, projectNames, 1)
+	require.Nil(t, err)
+	require.Contains(t, projectNames, "keptnProjectsMV")
+
+	CleanupDB(t, projectsRepo)
 }
 
 func TestMongoDBDumpRepo_GetDump(t *testing.T) {
-	dumpRepo := db.NewMongoDBDumpRepo(db.GetMongoDBConnectionInstance())
-	projectsRepo := db.NewMongoDBProjectsRepo(db.GetMongoDBConnectionInstance())
+	dumpRepo := NewMongoDBDumpRepo(GetMongoDBConnectionInstance())
+	projectsRepo := NewMongoDBProjectsRepo(GetMongoDBConnectionInstance())
 
 	// get random collection dump
 	_, err := dumpRepo.GetDump("test")
 	require.Nil(t, err)
 
-	// compare project collection to projectsrepo collection
-	projects_repo, err := projectsRepo.GetProjects()
+	// compare to project repo: 0 projects
+	projects, err := projectsRepo.GetProjects()
 	require.Nil(t, err)
 
 	dump, err := dumpRepo.GetDump("keptnProjectsMV")
@@ -37,5 +46,43 @@ func TestMongoDBDumpRepo_GetDump(t *testing.T) {
 	err = json.Unmarshal(bytes, &projects_dump)
 
 	require.Nil(t, err)
-	require.Equal(t, projects_dump, projects_repo)
+	require.Equal(t, projects_dump, projects)
+
+	// compare to project repo: 1 project
+	err = projectsRepo.CreateProject(&apimodels.ExpandedProject{
+		ProjectName: "my-project",
+	})
+	require.Nil(t, err)
+
+	projects, err = projectsRepo.GetProjects()
+	require.Nil(t, err)
+
+	dump, err = dumpRepo.GetDump("keptnProjectsMV")
+	require.Nil(t, err)
+
+	bytes, err = json.Marshal(dump)
+	err = json.Unmarshal(bytes, &projects_dump)
+
+	require.Nil(t, err)
+	require.Equal(t, projects_dump, projects)
+
+	// compare to project repo: 2 projects
+	err = projectsRepo.CreateProject(&apimodels.ExpandedProject{
+		ProjectName: "my-project2",
+	})
+	require.Nil(t, err)
+
+	projects, err = projectsRepo.GetProjects()
+	require.Nil(t, err)
+
+	dump, err = dumpRepo.GetDump("keptnProjectsMV")
+	require.Nil(t, err)
+
+	bytes, err = json.Marshal(dump)
+	err = json.Unmarshal(bytes, &projects_dump)
+
+	require.Nil(t, err)
+	require.Equal(t, projects_dump, projects)
+
+	CleanupDB(t, projectsRepo)
 }
