fb4461f8edb3ec46d04403401214b1f3cc8bb083
Andreas Samonik <andreas.samonik@dynatrace.com>
2022-08-11 08:42:55 +0200

    additional repo test case
    Signed-off-by: Andreas Samonik <andreas.samonik@dynatrace.com>

--- shipyard-controller/internal/db/mongodb_dbdump_repo_test.go
+++ shipyard-controller/internal/db/mongodb_dbdump_repo_test.go
@@ -1,8 +1,10 @@
 package db_test
 
 import (
+	"encoding/json"
 	"testing"
 
+	apimodels "github.com/keptn/go-utils/pkg/api/models"
 	"github.com/keptn/keptn/shipyard-controller/internal/db"
 	"github.com/stretchr/testify/require"
 )
@@ -16,8 +18,24 @@ func TestMongoDBDumpRepo_ListAllCollections(t *testing.T) {
 }
 
 func TestMongoDBDumpRepo_GetDump(t *testing.T) {
-	repo := db.NewMongoDBDumpRepo(db.GetMongoDBConnectionInstance())
+	dumpRepo := db.NewMongoDBDumpRepo(db.GetMongoDBConnectionInstance())
+	projectsRepo := db.NewMongoDBProjectsRepo(db.GetMongoDBConnectionInstance())
 
-	_, err := repo.GetDump("test")
+	// get random collection dump
+	_, err := dumpRepo.GetDump("test")
 	require.Nil(t, err)
+
+	// compare project collection to projectsrepo collection
+	projects_repo, err := projectsRepo.GetProjects()
+	require.Nil(t, err)
+
+	dump, err := dumpRepo.GetDump("keptnProjectsMV")
+	require.Nil(t, err)
+
+	var projects_dump []*apimodels.ExpandedProject
+	bytes, err := json.Marshal(dump)
+	err = json.Unmarshal(bytes, &projects_dump)
+
+	require.Nil(t, err)
+	require.Equal(t, projects_dump, projects_repo)
 }
