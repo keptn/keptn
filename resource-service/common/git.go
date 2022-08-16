7672e5f7d2b23a36e2912c8128b23acf88cc81d4
Florian Bacher <florian.bacher@dynatrace.com>
2022-08-16 15:01:46 +0200

    store name of default branch in init.defaultBranch config after initial clone
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- resource-service/common/git.go
+++ resource-service/common/git.go
@@ -170,11 +170,30 @@ func (g Git) CloneRepo(gitContext common_models.GitContext) (bool, error) {
 		return false, err
 	}
 
-	_, err = clone.Head()
+	head, err := clone.Head()
 	if err != nil {
 		return false, fmt.Errorf(kerrors.ErrMsgCouldNotGitAction, "clone", gitContext.Project, err)
 	}
+
+	if err := g.storeDefaultBranchConfig(gitContext, err, clone, head); err != nil {
+		return false, err
+	}
 	return true, nil
+}
+
+func (g Git) storeDefaultBranchConfig(gitContext common_models.GitContext, err error, clone *git.Repository, head *plumbing.Reference) error {
+	cfg, err := clone.Config()
+	if err != nil {
+		return fmt.Errorf(kerrors.ErrMsgCouldNotGitAction, "get config", gitContext.Project, err)
+	}
+
+	cfg.Init.DefaultBranch = head.Name().String()
+
+	err = clone.SetConfig(cfg)
+	if err != nil {
+		return fmt.Errorf(kerrors.ErrMsgCouldNotGitAction, "set config", gitContext.Project, err)
+	}
+	return nil
 }
 
 func (g Git) init(gitContext common_models.GitContext, projectPath string) (*git.Repository, error) {
