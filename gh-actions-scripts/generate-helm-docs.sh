34dd3f3a0cc88510c56ec72532ac264821e6e001
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-08-18 11:18:56 +0200

    ci: Add pipeline to check helm docs against master (#8661)

---
+++ gh-actions-scripts/generate-helm-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Readme generator for Keptn Helm Chart
+#
+# This script will install the readme generator if it's not installed already
+# and then it will generate the readme from the local helm values
+#
+# Dependencies:
+# Node >=16
+
+echo "Checking if readme generator is installed already..."
+if [[ $(npm list -g | grep -c 'readme-generator-for-helm') -eq 0 ]]; then
+  echo "Readme Generator not installed, installing now..."
+  git clone https://github.com/bitnami-labs/readme-generator-for-helm.git
+  npm install -g ./readme-generator-for-helm
+else
+  echo "Readme Generator is already installed, continuing..."
+fi
+
+echo "Generating readme now..."
+readme-generator --values=./installer/manifests/keptn/values.yaml --readme=./installer/manifests/keptn/README.md
+
+# Please be aware, the readme file needs to exist and needs to have a Parameters section, as only this section will be re-generated
