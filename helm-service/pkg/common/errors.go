19db8891155ea79056b8863a8b71277df51f5623
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2022-08-24 13:35:31 +0200

    feat: Move Helm and JMeter Service into keptn-contrib (#8700)
    BREAKING CHANGE: Helm-service and jmeter-service were moved into their own repositories under the keptn-contrib GH organization. 0.18.1 was the last version that included them in the Keptn core release. Please check [keptn-contrib/helm-service](https://github.com/keptn-contrib/helm-service) and [keptn-contrib/jmeter-service](https://github.com/keptn-contrib/jmeter-service) for the latest versions.

--- helm-service/pkg/common/errors.go
+++
@@ -1,3 +0,0 @@
-package common
-
-const ErrPackageChartMsg = "Error when packaging chart: %s"
--- jmeter-service/buildlocally.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## requires go 1.12+
-
-# shellcheck disable=SC2154
-if [ -n "$debugBuild" ]; then export BUILDFLAGS='-gcflags "all=-N -l"'; fi
-go build -ldflags '-linkmode=external' -o jmeter-extended-service
