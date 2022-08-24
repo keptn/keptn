5e88010a40ee32d6e075c9c93d8eb1f5e3fe5cc1
RealAnna <anna.reale@dynatrace.com>
2022-08-24 11:50:50 +0200

    feat: added return nothing unittest
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- secret-service/pkg/handler/secrethandler.go
+++ secret-service/pkg/handler/secrethandler.go
@@ -113,8 +113,8 @@ func (s SecretHandler) UpdateSecret(c *gin.Context) {
 // @Description  <span class="oauth-scopes">Required OAuth scopes: ${prefix}secrets:delete</span>
 // @Tags         Secrets
 // @Security     ApiKeyAuth
-// @Param        name   query  string  true  "The name of the secret"
-// @Param        scope  query  string  true  "The scope of the secret"
+// @Param        name   query  string  false  "The name of the secret"
+// @Param        scope  query  string  false  "The scope of the secret"
 // @Success      200    "OK"
 // @Failure      400    {object}  model.Error  "Invalid payload"
 // @Failure      404    {object}  model.Error  "Not Found"
