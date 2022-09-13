84f34ef83a8975e45f7208ffb021e97f1b04ba02
odubajDT <ondrej.dubaj@dynatrace.com>
2022-09-13 13:04:14 +0200

    minor fix
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- test/go-tests/test_custom_git_branch_name.go
+++ test/go-tests/test_custom_git_branch_name.go
@@ -25,17 +25,15 @@ func Test_CreateProjectWithCustomBranchName(t *testing.T) {
 
 	defer func() {
 		_, err = ExecuteCommandf("kubectl set env deployment/resource-service DEFAULT_REMOTE_GIT_BRANCH=%s -n %s", "master", keptnNamespace)
+		require.Nil(t, err)
 	}()
 
 	t.Logf("Setting up DEFAULT_REMOTE_GIT_BRANCH env variable to main-custom")
 	_, err = ExecuteCommandf("kubectl set env deployment/resource-service DEFAULT_REMOTE_GIT_BRANCH=%s -n %s", "main-custom", keptnNamespace)
 	require.Nil(t, err)
 
-	t.Logf("Sleeping for 30s to make sure resource-service pod is deleted...")
-	time.Sleep(30 * time.Second)
-
-	err = WaitForPodOfDeployment("resource-service")
-	require.Nil(t, err)
+	t.Logf("Sleeping for 45s to make sure resource-service pod is restarted...")
+	time.Sleep(45 * time.Second)
 
 	t.Logf("Creating a new project %s with a Gitea Upstream", projectMain)
 	projectName, err = CreateProject(projectMain, shipyardFilePath)
