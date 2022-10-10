0f4c16f31079fa8d8dc0581ef2d0d961cc4c2e41
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2022-10-10 15:00:40 +0200

    try using library/busybox
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- installer/airgapped/install_keptn.sh
+++ installer/airgapped/install_keptn.sh
@@ -20,6 +20,7 @@ echo "-----------------------------------------------------------------------"
 helm template keptn "${KEPTN_HELM_CHART}" -n "${KEPTN_NAMESPACE}" \
 --set="apiGatewayNginx.type=${KEPTN_SERVICE_TYPE},\
 global.keptn.registry=${TARGET_INTERNAL_DOCKER_REGISTRY}${DOCKER_ORG},\
+global.initContainers.image=${TARGET_INTERNAL_DOCKER_REGISTRY}library/busybox1.35.0,\
 mongo.image.registry=${TARGET_INTERNAL_DOCKER_REGISTRY%/},\
 nats.nats.image=${TARGET_INTERNAL_DOCKER_REGISTRY}nats:2.7.2-alpine,\
 nats.reloader.image=${TARGET_INTERNAL_DOCKER_REGISTRY}natsio/nats-server-config-reloader:0.6.2,\
@@ -30,7 +31,7 @@ apiGatewayNginx.image.repository=${TARGET_INTERNAL_DOCKER_REGISTRY}nginxinc/ngin
 helm upgrade keptn "${KEPTN_HELM_CHART}" --install -n "${KEPTN_NAMESPACE}" --create-namespace --wait --timeout 12m \
 --set="apiGatewayNginx.type=${KEPTN_SERVICE_TYPE},\
 global.keptn.registry=${TARGET_INTERNAL_DOCKER_REGISTRY}${DOCKER_ORG},\
-global.initContainers.image=${TARGET_INTERNAL_DOCKER_REGISTRY}busybox,\
+global.initContainers.image=${TARGET_INTERNAL_DOCKER_REGISTRY}library/busybox:1.35.0,\
 mongo.image.registry=${TARGET_INTERNAL_DOCKER_REGISTRY%/},\
 nats.nats.image=${TARGET_INTERNAL_DOCKER_REGISTRY}nats:2.7.2-alpine,\
 nats.reloader.image=${TARGET_INTERNAL_DOCKER_REGISTRY}natsio/nats-server-config-reloader:0.6.2,\
