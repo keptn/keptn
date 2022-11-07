dbd0ddba017001f4864a40b6a26cf867614ea1dc
odubajDT <93584209+odubajDT@users.noreply.github.com>
2022-11-07 09:33:03 +0100

    fix(resource-service): Force checkout event if unstaged changes are present (#9107)
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- resource-service/common/git.go
+++ resource-service/common/git.go
@@ -240,15 +240,21 @@ func (g Git) commitAll(gitContext common_models.GitContext, message string) (str
 }
 
 func (g Git) StageAndCommitAll(gitContext common_models.GitContext, message string) (string, error) {
-
 	id, err := g.commitAll(gitContext, message)
 	if err != nil {
+		if err = g.ResetHard(gitContext, "HEAD~0"); err != nil {
+			logger.WithError(err).Warn("could not reset after commitAll")
+		} else {
+			logger.Warn("untracked changes were removed")
+		}
 		return "", fmt.Errorf(kerrors.ErrMsgCouldNotCommit, gitContext.Project, mapError(err))
 	}
 	rollbackFunc := func() {
 		err := g.ResetHard(gitContext, "HEAD~1")
 		if err != nil {
 			logger.WithError(err).Warn("could not reset")
+		} else {
+			logger.Warn("commited changes were removed")
 		}
 	}
 	err = g.Pull(gitContext)
@@ -457,6 +463,7 @@ func (g *Git) CheckoutBranch(gitContext common_models.GitContext, branch string)
 
 	err := g.checkoutBranch(gitContext, &git.CheckoutOptions{
 		Branch: b,
+		Force:  true,
 	})
 	if err != nil {
 		if errors.Is(err, plumbing.ErrReferenceNotFound) {
