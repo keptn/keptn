b8e398225cee79bef3ae7f7f089b4c62b037f054
Klaus Strießnig <k.striessnig@gmail.com>
2022-12-05 16:08:59 +0100

    fixed typescript error
    Signed-off-by: Klaus Strießnig <k.striessnig@gmail.com>

--- bridge/server/utils/print-utils.ts
+++ bridge/server/utils/print-utils.ts
@@ -7,7 +7,7 @@ const log = new ComponentLogger('API');
 export function printError(err: AxiosError | Error): void {
   if (isAxiosError(err)) {
     const method = (err.request || err.config).method;
-    const url = err.request?.path ?? err.config.url;
+    const url = err.request?.path ?? err.config?.url;
     log.error(`Error for ${method} ${url}: ${err.message}`);
   } else {
     const msg = err instanceof Error ? `${err.name}: ${err.message}` : `${err}`;
